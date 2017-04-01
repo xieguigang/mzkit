e3dd8fb1bdfc27f53045afb27ae1635e25994d60
谢桂纲 <xie.guigang@gcmodeller.org>
2017-04-01 12:48:29 +0800

    extract extensions for the peaks data

---
+++ spectrum/spectrum/Extensions.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.proteomics.MS_Spectrum.mzXML
+
+Public Module Extensions
+
+    ''' <summary>
+    ''' 从mzXML文件之中解析出色谱峰数据
+    ''' </summary>
+    ''' <param name="peaks"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function ExtractMzI(peaks As peaks) As spectrumData
+
+    End Function
+End Module
