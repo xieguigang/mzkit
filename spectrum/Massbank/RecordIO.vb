3046644ad9618ba0bb202f7f3920ba6e51779047
谢桂纲 <xie.guigang@gcmodeller.org>
2017-04-10 16:21:09 +0800

    parsing lines into table

--- spectrum/Massbank/RecordIO.vb
+++ spectrum/Massbank/RecordIO.vb
@@ -1,7 +1,50 @@
-﻿Public Module RecordIO
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+Imports Node = System.Collections.Generic.Dictionary(Of String, Microsoft.VisualBasic.Language.List(Of String))
 
-    Public Function LoadFile(txt$) As Record
+Public Module RecordIO
 
+    Public Function LoadFile(txt$) As Record()
+        Dim out As New List(Of Record)
 
+        Static type As Type = GetType(Record)
+
+        For Each data As String() In txt.ReadAllLines.Split("//")
+            Dim nodes As New Dictionary(Of String, Node) From {
+                {"$_", New Node},
+                {"CH", New Node},
+                {"AC", New Node},
+                {"MS", New Node},
+                {"PK", New Node}
+            }
+            Dim o As Object = Activator.CreateInstance(GetType(Record))
+            Dim schema = DataFramework.Schema(
+                GetType(Record),
+                PropertyAccess.Writeable,,
+                True)
+
+            For Each line$ In data
+                Dim value As NamedValue(Of String) = line.GetTagValue(":", trim:=True)
+
+                If value.Name.Contains("$") Then
+                    Dim nodeName = value.Name.GetTagValue("$")
+                    Dim node As Node = nodes(nodeName.Name)
+
+                    If Not node.ContainsKey(nodeName.Value) Then
+                        node(nodeName.Value) = New List(Of String)
+                    End If
+
+                    node(nodeName.Value) += value.Value
+                Else
+                    If Not nodes("$_").ContainsKey(value.Name) Then
+                        nodes("$_")(value.Name) = New List(Of String)
+                    End If
+
+                    nodes("$_")(value.Name) += value.Value
+                End If
+            Next
+        Next
+
+        Return out
     End Function
 End Module
