d1323ea484c4c166419562af3edecbc7db0d26f8
谢桂纲 <xie.guigang@gcmodeller.org>
2017-04-11 10:54:51 +0800

    read massbank object model

--- spectrum/Massbank/Model.vb
+++ spectrum/Massbank/Model.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports SMRUCC.proteomics.MS_Spectrum.DATA.Massbank.DATA
 
 Public Module Model
@@ -9,11 +10,97 @@ Public Module Model
             Return Nothing
         Else
             Dim links$() = record.CH.LINK
-            Dim data = links.Select(Function(l) l.GetTagValue(" ", trim:=True)).ToArray
-            Dim table As Dictionary(Of String, String()) = data _
-                .ToDictionary(Function(l) l.Name,
-                              Function(l) l.Value.StringSplit("\s+"))
+            Dim table = links.__internalTable
             Return table
         End If
+    End Function
+
+    <Extension>
+    Public Function FOCUSED_ION(record As Record) As FOCUSED_ION
+        If record.MS Is Nothing OrElse record.MS.FOCUSED_ION.IsNullOrEmpty Then
+            Return Nothing
+        Else
+            Dim values$() = record.MS.FOCUSED_ION
+            Dim table = values.__internalTable
+            Dim getValue = table.__getValue
+            Dim ion As New FOCUSED_ION With {
+                .DERIVATIVE_FORM = getValue(NameOf(.DERIVATIVE_FORM)).FirstOrDefault,
+                .DERIVATIVE_MASS = getValue(NameOf(.DERIVATIVE_MASS)).FirstOrDefault,
+                .DERIVATIVE_TYPE = getValue(NameOf(.DERIVATIVE_TYPE)).FirstOrDefault,
+                .PRECURSOR_TYPE = getValue(NameOf(.PRECURSOR_TYPE)).FirstOrDefault,
+                .PRECURSOR_MZ = getValue("PRECURSOR_M/Z").FirstOrDefault
+            }
+
+            Return ion
+        End If
+    End Function
+
+    <Extension>
+    Public Function CHROMATOGRAPHY(record As Record) As CHROMATOGRAPHY
+        If record.AC Is Nothing OrElse record.AC.CHROMATOGRAPHY.IsNullOrEmpty Then
+            Return Nothing
+        Else
+            Dim values$() = record.AC.CHROMATOGRAPHY
+            Dim table = values.__internalTable
+            Dim getValue = table.__getValue
+            Dim chr As New CHROMATOGRAPHY With {
+                .COLUMN_NAME = getValue(NameOf(.COLUMN_NAME)).FirstOrDefault,
+                .COLUMN_TEMPERATURE = getValue(NameOf(.COLUMN_TEMPERATURE)).FirstOrDefault,
+                .FLOW_GRADIENT = getValue(NameOf(.FLOW_GRADIENT)).FirstOrDefault,
+                .FLOW_RATE = getValue(NameOf(.FLOW_RATE)).FirstOrDefault,
+                .RETENTION_TIME = getValue(NameOf(.RETENTION_TIME)).FirstOrDefault,
+                .SAMPLING_CONE = getValue(NameOf(.SAMPLING_CONE)).FirstOrDefault,
+                .SOLVENT = getValue(NameOf(.SOLVENT)).FirstOrDefault
+            }
+
+            Return chr
+        End If
+    End Function
+
+    <Extension>
+    Public Function MASS_SPECTROMETRY(record As Record) As MASS_SPECTROMETRY
+        If record.AC Is Nothing OrElse record.AC.MASS_SPECTROMETRY.IsNullOrEmpty Then
+            Return Nothing
+        Else
+            Dim values$() = record.AC.MASS_SPECTROMETRY
+            Dim table = values.__internalTable
+            Dim getValue = table.__getValue
+            Dim ms As New MASS_SPECTROMETRY With {
+                .COLLISION_ENERGY = getValue(NameOf(.COLLISION_ENERGY)).FirstOrDefault,
+                .DATAFORMAT = getValue(NameOf(.DATAFORMAT)).FirstOrDefault,
+                .DESOLVATION_GAS_FLOW = getValue(NameOf(.DESOLVATION_GAS_FLOW)).FirstOrDefault,
+                .DESOLVATION_TEMPERATURE = getValue(NameOf(.DESOLVATION_TEMPERATURE)).FirstOrDefault,
+                .FRAGMENTATION_METHOD = getValue(NameOf(.FRAGMENTATION_METHOD)).FirstOrDefault,
+                .IONIZATION = getValue(NameOf(.IONIZATION)).FirstOrDefault,
+                .ION_MODE = getValue(NameOf(.ION_MODE)).FirstOrDefault,
+                .MS_TYPE = getValue(NameOf(.MS_TYPE)).FirstOrDefault,
+                .SCANNING = getValue(NameOf(.SCANNING)).FirstOrDefault,
+                .SOURCE_TEMPERATURE = getValue(NameOf(.SOURCE_TEMPERATURE)).FirstOrDefault
+            }
+
+            Return ms
+        End If
+    End Function
+
+    <Extension>
+    Private Function __getValue(table As Dictionary(Of String, String())) As Func(Of String, String())
+        Return Function(key$)
+                   If table.ContainsKey(key) Then
+                       Return table(key)
+                   Else
+                       Return {}
+                   End If
+               End Function
+    End Function
+
+    <Extension>
+    Private Function __internalTable(values$()) As Dictionary(Of String, String())
+        Dim data As NamedValue(Of String)() = values _
+            .Select(Function(l) l.GetTagValue(" ", trim:=True)) _
+            .ToArray
+        Dim table As Dictionary(Of String, String()) = data _
+            .ToDictionary(Function(l) l.Name,
+                          Function(l) l.Value.StringSplit("\s+"))
+        Return table
     End Function
 End Module
