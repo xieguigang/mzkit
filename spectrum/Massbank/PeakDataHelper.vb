09c3cf098c2f91b9068868a237124c864c81239a
谢桂纲 <xie.guigang@gcmodeller.org>
2017-04-11 11:39:06 +0800

    how to normalize the peaks data??

--- spectrum/Massbank/PeakDataHelper.vb
+++ spectrum/Massbank/PeakDataHelper.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.TagData
+Imports SMRUCC.proteomics.MS_Spectrum.DATA.Massbank.DATA
 
 Public Module PeakDataHelper
 
@@ -13,5 +14,15 @@ Public Module PeakDataHelper
         Return peakData _
             .Select(Function(pk) $"{pk.Tag},{pk.value}") _
             .JoinBy(" ")
+    End Function
+
+    ''' <summary>
+    ''' 对峰的信号量进行归一化处理
+    ''' </summary>
+    ''' <param name="record">``MS/MS``信号峰数据</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Normalize(record As Record) As DoubleTagged(Of Double)()
+
     End Function
 End Module
