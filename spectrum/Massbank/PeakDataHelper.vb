19819eee5bde6f4af3a22aa23527502857059891
谢桂纲 <xie.guigang@gcmodeller.org>
2017-04-11 11:35:35 +0800

    generate peaks line data

--- spectrum/Massbank/PeakDataHelper.vb
+++ spectrum/Massbank/PeakDataHelper.vb
@@ -1,3 +1,17 @@
-﻿Public Module PeakDataHelper
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.TagData
 
+Public Module PeakDataHelper
+
+    ''' <summary>
+    ''' ``x,y x,y x,y``
+    ''' </summary>
+    ''' <param name="peakData"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Join(peakData As IEnumerable(Of DoubleTagged(Of Double))) As String
+        Return peakData _
+            .Select(Function(pk) $"{pk.Tag},{pk.value}") _
+            .JoinBy(" ")
+    End Function
 End Module
