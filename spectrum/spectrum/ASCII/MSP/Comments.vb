f80058b7ec13cc1ba2b90a731c0d2256bd016bdf
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-23 11:26:29 +0800

    add msp data model

---
+++ spectrum/spectrum/ASCII/MSP/Comments.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CommandLine.Parsers
+
+Namespace ASCII.MSP
+
+    Public Module Comments
+
+        ''' <summary>
+        ''' 解析放置于注释之中的代谢物注释元数据
+        ''' </summary>
+        ''' <param name="comments$"></param>
+        ''' <returns></returns>
+        <Extension> Public Function ToTable(comments$) As Dictionary(Of String, String)
+            Dim tokens$() = CLIParser.GetTokens(comments)
+            Dim data As Dictionary(Of String, String) = tokens _
+                .Select(Function(s)
+                            Return s.GetTagValue("=", trim:=True)
+                        End Function) _
+                .ToDictionary(Function(k) k.Name,
+                              Function(x) x.Value)
+            Return data
+        End Function
+
+        <Extension> Public Function FillData(comments$) As MetaData
+            Dim table As Dictionary(Of String, String) = comments.ToTable
+
+        End Function
+    End Module
+
+    Public Structure MetaData
+
+    End Structure
+End Namespace
---
+++ spectrum/spectrum/ASCII/MSP/MspData.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
+
+Namespace ASCII.MSP
+
+    Public Class MspData
+
+        Public Property Name As String
+        Public Property Synonym As String
+
+        <Column("DB#")>
+        Public Property DBId As String
+        Public Property InChIKey As String
+        Public Property MW As Double
+        Public Property Formula As String
+        Public Property PrecursorMZ As Double
+        Public Property Comments As String
+
+        Public ReadOnly Property MetaDB As MetaData
+            Get
+                Return Comments.FillData
+            End Get
+        End Property
+
+        Public Property Peaks As MSSignal()
+
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
+    End Class
+End Namespace
