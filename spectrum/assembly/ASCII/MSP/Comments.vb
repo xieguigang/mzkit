e2ca0d8913705366ae4c8cacfba740a516e7ace4
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-23 13:27:19 +0800

    fix parser errors

--- spectrum/assembly/ASCII/MSP/Comments.vb
+++ spectrum/assembly/ASCII/MSP/Comments.vb
@@ -1,6 +1,8 @@
-﻿Imports System.Data.Linq.Mapping
+﻿Imports System.Collections.Specialized
+Imports System.Data.Linq.Mapping
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Parsers
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 
 Namespace ASCII.MSP
 
@@ -11,19 +13,26 @@ Namespace ASCII.MSP
         ''' </summary>
         ''' <param name="comments$"></param>
         ''' <returns></returns>
-        <Extension> Public Function ToTable(comments$) As Dictionary(Of String, String)
+        <Extension> Public Function ToTable(comments$) As NameValueCollection
             Dim tokens$() = CLIParser.GetTokens(comments)
-            Dim data As Dictionary(Of String, String) = tokens _
+            Dim data = tokens _
                 .Select(Function(s)
                             Return s.GetTagValue("=", trim:=True)
                         End Function) _
-                .ToDictionary(Function(k) k.Name,
-                              Function(x) x.Value)
-            Return data
+                .GroupBy(Function(o) o.Name)
+            Dim table As New NameValueCollection  ' 为了兼容两个SMILES结构
+
+            For Each g In data
+                For Each s As NamedValue(Of String) In g
+                    Call table.Add(g.Key, s.Value)
+                Next
+            Next
+
+            Return table
         End Function
 
         <Extension> Public Function FillData(comments$) As MetaData
-            Dim table As Dictionary(Of String, String) = comments.ToTable
+            Dim table As NameValueCollection = comments.ToTable
 
 
 
@@ -51,7 +60,7 @@ Namespace ASCII.MSP
         Dim ionization_mode As String
         <Column(Name:="Last Auto-Curation")>
         Dim Last_AutoCuration As String
-        Dim SMILES As String
+        Dim SMILES As String()
         Dim InChI As String
         <Column(Name:="molecular formula")>
         Dim molecular_formula As String
