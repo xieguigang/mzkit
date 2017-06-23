1f9fbc22ce761e3121341404307d844241253576
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-23 13:07:48 +0800

    parse msp files

--- spectrum/assembly/ASCII/MSP/MspData.vb
+++ spectrum/assembly/ASCII/MSP/MspData.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.Linq.Mapping
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 
 Namespace ASCII.MSP
 
@@ -30,20 +31,54 @@ Namespace ASCII.MSP
         ''' <summary>
         ''' 
         ''' </summary>
-        ''' <param name="msp$">``*.msp``代谢组参考库文件路径</param>
+        ''' <param name="path">``*.msp``代谢组参考库文件路径</param>
         ''' <returns></returns>
-        Public Shared Iterator Function Load(msp$) As IEnumerable(Of MspData)
-            Dim file$() = msp.ReadAllLines
+        Public Shared Iterator Function Load(path$) As IEnumerable(Of MspData)
+            Dim file$() = path.ReadAllLines
             Dim libs = file _
                 .Split(AddressOf StringEmpty, DelimiterLocation.NotIncludes) _
-                .Where(Function(c) c.Length = 0) _
+                .Where(Function(c) c.Length > 0) _
                 .ToArray
 
-            For Each metabolite In libs
-                Dim parts = metabolite.Split(Function(s) s.MatchPattern("Num Peaks[:]\s*\d+", RegexICSng))
-                Dim metadata = parts.First
-                Dim peaksdata = parts.Last
+            For Each reference As String() In libs
+                Dim parts = reference _
+                    .Split(Function(s)
+                               Return s.MatchPattern("Num Peaks[:]\s*\d+", RegexICSng)
+                           End Function) _
+                    .ToArray
+                Dim metadata As Dictionary(Of NamedValue(Of String)) =
+                    parts _
+                    .First _
+                    .Select(Function(s) s.GetTagValue(":", trim:=True)) _
+                    .ToDictionary
+                Dim peaksdata As MSMSPeak() =
+                    parts _
+                    .Last _
+                    .Select(Function(s)
+                                With s.Split(" "c)
+                                    Return New MSMSPeak(
+                                        mz:= .First,
+                                        intensity:= .Last)
+                                End With
+                            End Function) _
+                    .ToArray
+                Dim getValue = Function(key$)
+                                   Return metadata.TryGetValue(key).Value
+                               End Function
 
+                Dim msp As New MspData With {
+                    .Peaks = peaksdata,
+                    .Comments = getValue(NameOf(MspData.Comments)),
+                    .DBId = getValue("DB#"),
+                    .Formula = getValue(NameOf(MspData.Formula)),
+                    .InChIKey = getValue(NameOf(MspData.InChIKey)),
+                    .MW = getValue(NameOf(MspData.MW)),
+                    .Name = getValue(NameOf(MspData.Name)),
+                    .PrecursorMZ = getValue(NameOf(MspData.PrecursorMZ)),
+                    .Synonym = getValue(NameOf(MspData.Synonym))
+                }
+
+                Yield msp
             Next
         End Function
     End Class
--- spectrum/assembly/ASCII/Peaks.vb
+++ spectrum/assembly/ASCII/Peaks.vb
@@ -5,6 +5,16 @@ Public Structure MSMSPeak
     <XmlAttribute> Public Property mz As Double
     <XmlAttribute> Public Property intensity As Double
 
+    Sub New(mz$, intensity$)
+        Me.mz = Val(mz)
+        Me.intensity = Val(intensity)
+    End Sub
+
+    Sub New(mz#, intensity#)
+        Me.mz = mz
+        Me.intensity = intensity
+    End Sub
+
     Public Overrides Function ToString() As String
         Return $"{mz} ({intensity})"
     End Function
