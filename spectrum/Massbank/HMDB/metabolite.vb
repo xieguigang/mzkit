13c2d76656dbb019e27cc736fd5ad552c708bf4c
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-03 20:09:47 +0800

    add xml model

---
+++ spectrum/Massbank/HMDB/metabolite.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class metabolite
+
+    Public Property version As String
+    Public Property creation_date As String
+    Public Property update_date As String
+    Public Property accession As String
+    Public Property secondary_accessions As secondary_accessions
+    Public Property name As String
+    Public Property description As String
+    Public Property synonyms As synonyms
+    Public Property chemical_formula As String
+    Public Property average_molecular_weight As Double
+    Public Property monisotopic_molecular_weight As Double
+    Public Property iupac_name As String
+    Public Property traditional_iupac As String
+    Public Property cas_registry_number As String
+    Public Property smiles As String
+    Public Property inchi As String
+    Public Property inchikey As String
+    Public Property taxonomy As taxonomy
+End Class
+
+Public Structure secondary_accessions
+    <XmlElement> Public Property accession As String()
+
+    Public Overrides Function ToString() As String
+        Return Me.accession.GetJson
+    End Function
+End Structure
+
+Public Structure synonyms
+    <XmlElement> Public Property synonym As String()
+
+    Public Overrides Function ToString() As String
+        Return Me.synonym.GetJson
+    End Function
+End Structure
---
+++ spectrum/Massbank/HMDB/taxonomy.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Xml.Serialization
+
+Public Class taxonomy
+
+    Public Property description As String
+    Public Property direct_parent As String
+    Public Property kingdom As String
+    Public Property super_class As String
+    Public Property [class] As String
+    Public Property sub_class As String
+    Public Property molecular_framework As String
+    Public Property alternative_parents As alternative_parents
+    Public Property substituents As substituents
+
+End Class
+
+Public Structure alternative_parents
+    <XmlElement> Public Property alternative_parent As String()
+End Structure
+
+Public Structure substituents
+    <XmlElement> Public Property substituent As String()
+End Structure
