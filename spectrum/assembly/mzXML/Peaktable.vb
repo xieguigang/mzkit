6af4b2a421856e1f5ce0ee668e6342402595f5c5
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-04 09:01:17 +0800

    add peaktable model for mzXML data output

---
+++ spectrum/assembly/mzXML/Peaktable.vb
@@ -0,0 +1,16 @@
+﻿Public Class Peaktable
+
+    Public Property mz As Double
+    Public Property mzmin As Double
+    Public Property mzmax As Double
+    Public Property rt As Double
+    Public Property rtmin As Double
+    Public Property rtmax As Double
+    Public Property into As Double
+    Public Property intb As Double
+    Public Property maxo As Double
+    Public Property sn As Double
+    Public Property sample As Double
+    Public Property index As Double
+
+End Class
