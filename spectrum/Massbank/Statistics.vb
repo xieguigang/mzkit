5557fb32d63176b7800c74039aff011c32ecada6
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-04 20:26:46 +0800

    analysis extensions

---
+++ spectrum/Massbank/Statistics.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module Statistics
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="cpd">KEGG compounds id list.</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function KEGGPathwayCoverages(cpd As IEnumerable(Of String)) As Dictionary(Of String, (cover%, ALL%))
+
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="list">HMDB id list</param>
+    ''' <param name="hmdb">The hmdb XML database file path.</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function HMDBCoverages(list As IEnumerable(Of String), hmdb$) As Dictionary(Of String, (cover%, ALL%))
+        Dim secondary2Main As New 
+    End Function
+End Module
