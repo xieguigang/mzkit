9be241caf354a7419ea419877eb1d667eaa05034
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-04 20:45:22 +0800

    hmdb coverage counts

--- spectrum/Massbank/Statistics.vb
+++ spectrum/Massbank/Statistics.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
 
 Public Module Statistics
 
@@ -19,7 +20,63 @@ Public Module Statistics
     ''' <param name="hmdb">The hmdb XML database file path.</param>
     ''' <returns></returns>
     <Extension>
-    Public Function HMDBCoverages(list As IEnumerable(Of String), hmdb$) As Dictionary(Of String, (cover%, ALL%))
-        Dim secondary2Main As New 
+    Public Function HMDBCoverages(list As IEnumerable(Of String), hmdb$, Optional ByRef unknwons% = -1) As Dictionary(Of String, (cover%, ALL%))
+        Dim secondary2Main As New SecondaryIDSolver
+        Dim metabolites As New Dictionary(Of String, metabolite)
+
+        For Each m As metabolite In metabolite.Load(hmdb)
+            With m
+                Call secondary2Main.Add(
+                    .accession,
+                    .secondary_accessions.accession)
+                Call metabolites.Add(.accession.ToLower, m)
+            End With
+        Next
+
+        ' All counts
+        Dim id2Class As Dictionary(Of String, Integer) = metabolites _
+            .Values _
+            .Where(Function(m) Not m.taxonomy Is Nothing) _
+            .Select(Function(id)
+                        Return (id.accession, id.taxonomy.class)
+                    End Function) _
+            .GroupBy(Function(t) t.Item2) _
+            .ToDictionary(Function(name) name.Key,
+                          Function(ALL) ALL.Count)
+
+        ' counts for inputs
+        Dim counts As New Dictionary(Of String, Integer)
+        Dim classKey$
+
+        counts!unknown = 1
+
+        For Each id As String In list
+            id = secondary2Main(id)
+            classKey = metabolites(id).taxonomy?.class
+
+            If classKey.StringEmpty Then
+                counts!unknown += 1
+            Else
+                If Not counts.ContainsKey(classKey) Then
+                    Call counts.Add(classKey, 1)
+                Else
+                    counts(classKey) += 1
+                End If
+            End If
+        Next
+
+        Dim coverages As New Dictionary(Of String, (cover%, ALL%))
+
+        For Each classKey In id2Class.Keys
+            If counts.ContainsKey(classKey) Then
+                coverages(classKey) = (counts(classKey), id2Class(classKey))
+            Else
+                coverages(classKey) = (0, id2Class(classKey))
+            End If
+        Next
+
+        unknwons = counts!unknown
+
+        Return coverages
     End Function
 End Module
