0ad1d6ccd8702cbc80a4e83a335cbc5e8fca8aa7
xieguigang <xie.guigang@gcmodeller.org>
2017-10-05 14:57:58 +0800

    fix reference conflicts

--- spectrum/ProteoWizard.d/Server.vb
+++ spectrum/ProteoWizard.d/Server.vb
@@ -1,3 +1,9 @@
-﻿Public Class Server
+﻿Imports SMRUCC.WebCloud.HTTPInternal.AppEngine
+Imports SMRUCC.WebCloud.HTTPInternal.Platform
 
+Public Class Server : Inherits WebApp
+
+    Sub New(main As PlatformEngine)
+        Call MyBase.New(main)
+    End Sub
 End Class
