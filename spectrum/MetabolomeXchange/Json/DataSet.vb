58e35fc5b9620a7dfc37c60b54523e740bf513a1
xieguigang <xie.guigang@gcmodeller.org>
2017-10-19 09:34:36 +0800

    add data model of the response

--- spectrum/MetabolomeXchange/Json/DataSet.vb
+++ spectrum/MetabolomeXchange/Json/DataSet.vb
@@ -1,3 +1,5 @@
+Imports Microsoft.VisualBasic.Serialization.JSON
+
 Namespace Json
 
     Public Structure DataSet
@@ -9,7 +11,47 @@ Namespace Json
         Public Property [date] As String
         Public Property timestamp As String
         Public Property submitter As String()
+        Public Property publications As publication()
+        Public Property meta As meta
 
+        Public Overrides Function ToString() As String
+            Return title
+        End Function
     End Structure
 
+    Public Structure meta
+
+        Public Property analysis As String
+        Public Property platform As String
+        Public Property organism As Object
+        Public Property organism_parts As Object
+        Public Property metabolites As String()
+
+        Public Overrides Function ToString() As String
+            Return metabolites.GetJson
+        End Function
+    End Structure
+
+    Public Structure publication
+
+        Public Property title As String
+        Public Property doi As String
+        Public Property pubmed As String
+
+        Public Overrides Function ToString() As String
+            Return title
+        End Function
+    End Structure
+
+    Public Structure response
+
+        Public Property name As String
+        Public Property url As String
+        Public Property description As String
+        Public Property datasets As DataSet()
+
+        Public Overrides Function ToString() As String
+            Return url
+        End Function
+    End Structure
 End Namespace
--- spectrum/MetabolomeXchange/Provider.vb
+++ spectrum/MetabolomeXchange/Provider.vb
@@ -1,13 +1,24 @@
 Imports Microsoft.VisualBasic.Serialization.JSON
+Imports MetabolomeXchange.Json
 
 Public Module Provider
 
     Const api$ = "http://api.metabolomexchange.org"
 
-    Public Function GetAllDataSet(provider As String)
+    ''' <summary>
+    ''' Get all info about a single provider, including a list of the datasets of that provider.
+    ''' 
+    ''' > ``/provider/{shortname}``
+    ''' </summary>
+    ''' <param name="provider">The provider shortname, default is using ``metabolights`` repostiory</param>
+    ''' <returns></returns>
+    Public Function GetAllDataSetJson(Optional provider$ = "mtbls") As String
         Dim url$ = $"{api}/provider/{provider}"
         Dim json$ = url.GET
-        Return json.LoadObject(Of DataSet)
+        Return json
     End Function
 
+    Public Function GetAllDataSet(Optional provider$ = "mtbls") As DataSet()
+        Return GetAllDataSetJson(provider).LoadObject(Of response).datasets
+    End Function
 End Module
