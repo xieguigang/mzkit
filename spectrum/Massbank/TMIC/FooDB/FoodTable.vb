2981f79e526e313f49c4002d50bc15dc0f5150f3
xieguigang <xie.guigang@gcmodeller.org>
2017-10-20 22:18:35 +0800

    add table names

--- spectrum/Massbank/TMIC/FooDB/FoodTable.vb
+++ spectrum/Massbank/TMIC/FooDB/FoodTable.vb
@@ -1,6 +1,24 @@
 ﻿Namespace TMIC.FooDB
 
-    Public Class FoodTable
+    Public Class compound_alternate_parents : End Class
+    Public Class compound_external_descriptors : End Class
+    Public Class compound_substituents : End Class
+    Public Class compound_synonyms : End Class
+    Public Class compounds : End Class
+    Public Class compounds_enzymes : End Class
+    Public Class compounds_flavors : End Class
+    Public Class compounds_health_effects : End Class
+    Public Class compounds_pathways : End Class
+    Public Class contents : End Class
+    Public Class enzymes : End Class
+    Public Class flavors : End Class
+    Public Class food_taxonomies : End Class
+    Public Class foodcomex_compound_providers : End Class
+    Public Class foodcomex_compounds : End Class
+    Public Class foods : End Class
+    Public Class health_effects : End Class
+    Public Class nutrients : End Class
+    Public Class pathways : End Class
+    Public Class references : End Class
 
-    End Class
 End Namespace
