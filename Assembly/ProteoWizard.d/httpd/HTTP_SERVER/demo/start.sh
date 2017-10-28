dc1a788b83205cca8fe62997b83e3bb679c3e110
xieguigang <xie.guigang@gcmodeller.org>
2017-10-29 02:20:56 +0800

    what?

--- Assembly/ProteoWizard.d/httpd/HTTP_SERVER/demo/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# start the httpd web server
-# listen on port 80
-# using /@set parameter to initialize the mysql connection in visitor statics plugin in dll file: VisitStat.dll
-./bin/httpd.exe /start /port 80 /@set "host=localhost;mysql_port=3306;user=root;password=1234;database=test"
