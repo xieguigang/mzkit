66102b674cca2b6fcf5a72f30a773f0eea915aa8
xieguigang <xie.guigang@gcmodeller.org>
2017-10-29 13:51:10 +0800

    ms2_simulator based on the GA and graph theroy

---
+++ DATA/ms2_simulator/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 組件的一般資訊是由下列的屬性集控制。
+' 變更這些屬性的值即可修改組件的相關
+' 資訊。
+
+' 檢閱組件屬性的值
+
+<Assembly: AssemblyTitle("ms2_simulator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("ms2_simulator")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+<Assembly: Guid("0d8bebdf-ddeb-47ed-815b-64e8a35064ff")>
+
+' 組件的版本資訊由下列四個值所組成: 
+'
+'      主要版本
+'      次要版本
+'      組建編號
+'      修訂編號
+'
+' 您可以指定所有的值，或將組建編號或修訂編號設為預設值
+' 指定為預設值: 
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
