bd4bda704bf4147ee14767a262e5e02318961353
xieguigang <xie.guigang@gcmodeller.org>
2017-10-30 22:28:11 +0800

    add test

--- DATA/ms2_simulator/EnergyModel.vb
+++ DATA/ms2_simulator/EnergyModel.vb
@@ -1,7 +1,23 @@
-﻿
+﻿Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.Calculus
+Imports Microsoft.VisualBasic.Data.csv
+
 ''' <summary>
 ''' 分子的能量分布模型
 ''' </summary>
 Public Class EnergyModel
 
+    Dim ODE As New ODE With {.y0 = 0, .df = Function(x, y) Bootstraping.ProbabilityDensity(x, 5, 5)}
+
+    Sub New()
+
+        ODE.RK4(3000, 0, 100)
+
+        Dim X = {"X"}.JoinIterates(ODE.x.Select(Function(n) CStr(n))).ToArray
+        Dim Y = {"Y"}.JoinIterates(ODE.y.Select(Function(n) CStr(n))).ToArray
+        Dim csv = {X, Y}.JoinColumns
+
+        Call csv.Save("./test.csv")
+    End Sub
 End Class
---
+++ DATA/ms2_simulator/test/Module1.vb
@@ -0,0 +1,9 @@
+﻿Imports ms2_simulator
+
+Module Module1
+
+    Sub Main()
+        Dim x As New EnergyModel
+    End Sub
+
+End Module
