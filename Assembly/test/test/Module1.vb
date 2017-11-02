0839a84a15b503535e904ad7178ee3291880c454
xieguigang <xie.guigang@gcmodeller.org>
2017-11-02 11:22:32 +0800

    add msp data load test

---
+++ Assembly/test/test/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports SMRUCC.MassSpectrum.Assembly.ASCII.MSP
+
+Module Module1
+
+    Sub Main()
+
+        Dim testMsp = MspData.Load("D:\MassSpectrum-toolkits\Assembly\test\HMDB00008.msp").ToArray
+
+
+    End Sub
+
+End Module
