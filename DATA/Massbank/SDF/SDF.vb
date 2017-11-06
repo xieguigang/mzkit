87b326dd0cf97968f34f0d79f59ce07405ffad5c
xieguigang <xie.guigang@gcmodeller.org>
2017-11-06 11:27:54 +0800

    fix for SDF data parser

--- DATA/Massbank/SDF/SDF.vb
+++ DATA/Massbank/SDF/SDF.vb
@@ -9,12 +9,11 @@ Imports Microsoft.VisualBasic.Language
 ''' + PubChem FTP SDF(ftp://ftp.ncbi.nlm.nih.gov/pubchem/Compound/CURRENT-Full/SDF)
 ''' </summary>
 Public Class SDF : Implements INamedValue
-    Implements Value(Of MetaData).IValueOf
 
     Public Property ID As String Implements IKeyedEntity(Of String).Key
     Public Property [Class] As String
     Public Property Molecule As String
-    Public Property MetaData As MetaData Implements Value(Of MetaData).IValueOf.Value
+    Public Property MetaData As Dictionary(Of String, String())
 
     Public Shared Iterator Function IterateParser(path$) As IEnumerable(Of SDF)
         Dim o As SDF
@@ -43,11 +42,24 @@ Public Class SDF : Implements INamedValue
             mol = .First.Join({molEnds}).JoinBy(vbLf)
         End With
 
+        Dim metaData As Dictionary(Of String, String()) =
+            metas _
+                .Split(Function(s) s.StringEmpty, includes:=False) _
+                .Where(Function(t) Not t.IsNullOrEmpty) _
+                .ToDictionary(Function(t)
+                                  Return Mid(t(0), 4, t(0).Length - 4)
+                              End Function,
+                              Function(t)
+                                  Return t _
+                                      .Skip(1) _
+                                      .ToArray
+                              End Function)
+
         Return New SDF With {
             .ID = ID.Trim,
             .Molecule = mol,
             .Class = [class].Trim,
-            .MetaData = MetaData.Data(metas)
+            .MetaData = metaData
         }
     End Function
 End Class
