17c31d6167f44e5e87cea37494943cf48ee740f6
xieguigang <xie.guigang@gcmodeller.org>
2017-11-06 15:06:56 +0800

    add matrix model

--- Assembly/assembly/LibraryMatrix.vb
+++ Assembly/assembly/LibraryMatrix.vb
@@ -1,6 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.Serialization.JSON
 
-Public Class LibraryMatrix
+Public Class Library
 
     Public Property ID As String
     Public Property PrecursorMz As String
@@ -11,4 +11,13 @@ Public Class LibraryMatrix
     Public Overrides Function ToString() As String
         Return Me.GetJson
     End Function
+End Class
+
+Public Class ms2
+    Public Property mz As Double
+    Public Property intensity As Double
+End Class
+
+Public Class LibraryMatrix
+    Public Property ms2 As ms2()
 End Class
