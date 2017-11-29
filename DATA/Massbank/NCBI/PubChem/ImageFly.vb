c55c6238f16e7d46c939c752c7d35c66a406d5ee
xieguigang <xie.guigang@gcmodeller.org>
2017-11-29 16:30:04 +0800

    add NCBI pubchem imagefly API

---
+++ DATA/Massbank/NCBI/PubChem/ImageFly.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
+
+Public Module ImageFly
+
+    Public Function GetImage(cid$, Optional width% = 500, Optional height% = 500) As Bitmap
+        Dim url$ = $"https://pubchem.ncbi.nlm.nih.gov/image/imagefly.cgi?cid={cid}&width={width}&height={height}"
+        Dim tmp$ = App.GetAppSysTempFile(".png", sessionID:=App.PID)
+
+        If Not url.DownloadFile(save:=tmp) Then
+            Return Nothing
+        End If
+
+        Dim bitmap As Bitmap = New Bitmap(tmp) _
+            .CorpBlank(margin:=5, blankColor:=Color.White) _
+            .ColorReplace(Color.White, Color.Transparent)
+        Return bitmap
+    End Function
+End Module
