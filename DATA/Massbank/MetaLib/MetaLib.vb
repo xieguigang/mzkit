4e9bac45286327b33a569e45e39febea82cf6310
xieguigang <xie.guigang@gcmodeller.org>
2017-11-30 16:48:00 +0800

    add data model

--- DATA/Massbank/MetaLib/MetaLib.vb
+++ DATA/Massbank/MetaLib/MetaLib.vb
@@ -1,26 +1,43 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 
-''' <summary>
-''' 对chebi/kegg/pubchem/HMDB/metlin的物质注释信息整合库
-''' </summary>
-Public Class MetaLib : Implements INamedValue
+Namespace MetaLib
 
     ''' <summary>
-    ''' 该物质在整合库之中的唯一标识符
+    ''' 对``chebi/kegg/pubchem/HMDB/metlin``的物质注释信息整合库，这个数据库只要为了生成编号，名称之类的注释信息而构建的
     ''' </summary>
-    ''' <returns></returns>
-    Public Property ID As String Implements IKeyedEntity(Of String).Key
+    Public Class MetaLib : Implements INamedValue
 
-    ''' <summary>
-    ''' chebi主编号
-    ''' </summary>
-    ''' <returns></returns>
-    Public Property chebi As String
+        ''' <summary>
+        ''' 该物质在整合库之中的唯一标识符
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property ID As String Implements IKeyedEntity(Of String).Key
+        Public Property xref As xref
+        Public Property name As String
+        Public Property formula As String
+        Public Property mass As Double
 
+        Public Overrides Function ToString() As String
+            Return name
+        End Function
+    End Class
 
-End Class
+    Public Class xref
 
-Public Class xref
+        ''' <summary>
+        ''' chebi主编号
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property chebi As String
+        Public Property KEGG As String
+        Public Property pubchem As String
+        Public Property HMDB As String
+        Public Property metlin As String
+        Public Property CAS As String
+        Public Property InChIkey As String
+        Public Property InChI As String
 
-End Class
+    End Class
+End Namespace
