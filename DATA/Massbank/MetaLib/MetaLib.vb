cce70d2c87ef76e7602d0700a1602d6ded772273
xieguigang <xie.guigang@gcmodeller.org>
2017-11-30 16:42:54 +0800

    move directory

---
+++ DATA/Massbank/MetaLib/MetaLib.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+
+''' <summary>
+''' 对chebi/kegg/pubchem/HMDB/metlin的物质注释信息整合库
+''' </summary>
+Public Class MetaLib : Implements INamedValue
+
+    ''' <summary>
+    ''' 该物质在整合库之中的唯一标识符
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property ID As String Implements IKeyedEntity(Of String).Key
+
+    ''' <summary>
+    ''' chebi主编号
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property chebi As String
+
+
+End Class
+
+Public Class xref
+
+End Class
