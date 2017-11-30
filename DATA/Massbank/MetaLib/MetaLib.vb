d6e6edb28137d1f578a7086e31d4c91199c59a77
xieguigang <xie.guigang@gcmodeller.org>
2017-11-30 17:22:17 +0800

    add properties

--- DATA/Massbank/MetaLib/MetaLib.vb
+++ DATA/Massbank/MetaLib/MetaLib.vb
@@ -21,6 +21,29 @@ Namespace MetaLib
         Public Property formula As String
         Public Property mass As Double
 
+        ''' <summary>
+        ''' 化合物分类
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property compound_class As String
+        ''' <summary>
+        ''' 仅限于人体内环境，这个化合物所存在的组织列表
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property tissue_locations As String()
+        ''' <summary>
+        ''' 仅限于人体内环境，这个化合物所存在的生物体液列表
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property biofluid_locations As String()
+
+        ''' <summary>
+        ''' 包含有这个物质的KEGG pathway的编号的集合，只有当<see cref="DATA.MetaLib.xref.KEGG"/>
+        ''' 存在值的时候才会存在这个属性
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property pathways As String()
+
         Public Overrides Function ToString() As String
             Return name
         End Function
