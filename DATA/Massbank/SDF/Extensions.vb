56482bc0986336446920ff3b7806f9b727b98f41
xieguigang <xie.guigang@gcmodeller.org>
2017-12-21 10:17:41 +0800

    dump pubchem *.sdf as csv file

--- DATA/Massbank/SDF/Extensions.vb
+++ DATA/Massbank/SDF/Extensions.vb
@@ -1,5 +1,9 @@
 ﻿Imports System.Reflection
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Data.csv.IO.Linq
+Imports Microsoft.VisualBasic.Language.Default
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
 Namespace File
@@ -23,6 +27,75 @@ Namespace File
             Next
 
             Return DirectCast(meta, MetaData)
+        End Function
+
+        ReadOnly defaultKeys As DefaultValue(Of String()) = (
+            <json>
+            [
+              "PUBCHEM_COMPOUND_CID",
+              "PUBCHEM_COMPOUND_CANONICALIZED",
+              "PUBCHEM_CACTVS_COMPLEXITY",
+              "PUBCHEM_CACTVS_HBOND_ACCEPTOR",
+              "PUBCHEM_CACTVS_HBOND_DONOR",
+              "PUBCHEM_CACTVS_ROTATABLE_BOND",
+              "PUBCHEM_CACTVS_SUBSKEYS",
+              "PUBCHEM_IUPAC_OPENEYE_NAME",
+              "PUBCHEM_IUPAC_CAS_NAME",
+              "PUBCHEM_IUPAC_NAME",
+              "PUBCHEM_IUPAC_SYSTEMATIC_NAME",
+              "PUBCHEM_IUPAC_TRADITIONAL_NAME",
+              "PUBCHEM_IUPAC_INCHI",
+              "PUBCHEM_IUPAC_INCHIKEY",
+              "PUBCHEM_XLOGP3_AA",
+              "PUBCHEM_EXACT_MASS",
+              "PUBCHEM_MOLECULAR_FORMULA",
+              "PUBCHEM_MOLECULAR_WEIGHT",
+              "PUBCHEM_OPENEYE_CAN_SMILES",
+              "PUBCHEM_OPENEYE_ISO_SMILES",
+              "PUBCHEM_CACTVS_TPSA",
+              "PUBCHEM_MONOISOTOPIC_WEIGHT",
+              "PUBCHEM_TOTAL_CHARGE",
+              "PUBCHEM_HEAVY_ATOM_COUNT",
+              "PUBCHEM_ATOM_DEF_STEREO_COUNT",
+              "PUBCHEM_ATOM_UDEF_STEREO_COUNT",
+              "PUBCHEM_BOND_DEF_STEREO_COUNT",
+              "PUBCHEM_BOND_UDEF_STEREO_COUNT",
+              "PUBCHEM_ISOTOPIC_ATOM_COUNT",
+              "PUBCHEM_COMPONENT_COUNT",
+              "PUBCHEM_CACTVS_TAUTO_COUNT",
+              "PUBCHEM_COORDINATE_TYPE",
+              "PUBCHEM_BONDANNOTATIONS",
+              "PUBCHEM_XLOGP3",
+              "PUBCHEM_NONSTANDARDBOND"
+            ]
+            </json>
+        ).Value _
+         .LoadObject(Of String())
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="repository$">The NCBI ftp download directory</param>
+        ''' <param name="keys">Using user custom selected keys or pubchem default keys for save the meta data.</param>
+        ''' <param name="save$"></param>
+        ''' <returns></returns>
+        Public Function DumpingPubChemAnnotations(repository$, save$, Optional keys$() = Nothing) As Boolean
+            Using writer As New WriteStream(Of EntityObject)(path:=save, metaKeys:=keys Or defaultKeys)
+                Dim write As EntityObject
+
+                For Each molecule As SDF In SDF.MoleculePopulator(directory:=repository)
+                    write = New EntityObject With {
+                        .ID = molecule.ID,
+                        .Properties = molecule _
+                            .MetaData _
+                            .ToDictionary(Function(map) map.Key,
+                                          Function(map) map.Value.GetJson)
+                    }
+                    writer.Flush(write)
+                Next
+            End Using
+
+            Return 0
         End Function
     End Module
 End Namespace
--- DATA/Massbank/test/Module1.vb
+++ DATA/Massbank/test/Module1.vb
@@ -3,9 +3,22 @@
 Module Module1
 
     Sub Main()
+
+        Call dumpPubChem()
+
+
         Dim keys = SMRUCC.MassSpectrum.DATA.File.SDF.ScanKeys("D:\smartnucl_integrative\DATA\NCBI\SDF").ToArray
 
         Call keys.GetJson.SaveTo("./keys.json")
+
+
+        Pause()
+    End Sub
+
+
+    Sub dumpPubChem()
+
+        Call SMRUCC.MassSpectrum.DATA.File.Extensions.DumpingPubChemAnnotations("D:\smartnucl_integrative\DATA\NCBI\SDF", "./ddddd.csv")
 
 
         Pause()
