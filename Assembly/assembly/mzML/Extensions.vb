459c7e6fa839331c61f0aa97aa4f13c7b5ea3933
xieguigang <xie.guigang@gcmodeller.org>
2018-01-18 11:19:32 +0800

    split source file

--- Assembly/assembly/mzML/Extensions.vb
+++ Assembly/assembly/mzML/Extensions.vb
@@ -1,11 +1,7 @@
-﻿Imports System.Drawing
-Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Text.Xml.Linq
 Imports SMRUCC.MassSpectrum.Math
-Imports sys = System.Math
 
 Namespace mzML
 
@@ -77,31 +73,4 @@ Namespace mzML
             Return data
         End Function
     End Module
-
-    Public Class IonPair : Implements INamedValue
-
-        Public Property name As String Implements IKeyedEntity(Of String).Key
-        Public Property precursor As Double
-        Public Property product As Double
-
-        Public Overrides Function ToString() As String
-            If name.StringEmpty Then
-                Return $"{precursor}/{product}"
-            Else
-                Return $"Dim {name} As [{precursor}, {product}]"
-            End If
-        End Function
-
-        Public Function Assert(chromatogram As chromatogram) As Boolean
-            Dim pre = chromatogram.precursor.MRMTargetMz
-            Dim pro = chromatogram.product.MRMTargetMz
-
-            ' less than 0.3da or 20ppm??
-            If sys.Abs(Val(pre) - precursor) <= 0.3 AndAlso sys.Abs(Val(pro) - product) <= 0.3 Then
-                Return True
-            Else
-                Return False
-            End If
-        End Function
-    End Class
 End Namespace
---
+++ Assembly/assembly/mzML/IonPair.vb
@@ -0,0 +1,44 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
+Imports sys = System.Math
+
+Namespace mzML
+
+    Public Class IonPair : Implements INamedValue
+
+        ''' <summary>
+        ''' The database accession ID
+        ''' </summary>
+        ''' <returns></returns>
+        <Column("ID")>
+        Public Property AccID As String
+        ''' <summary>
+        ''' The display title name
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property name As String Implements IKeyedEntity(Of String).Key
+        Public Property precursor As Double
+        Public Property product As Double
+
+        Public Overrides Function ToString() As String
+            If name.StringEmpty Then
+                Return $"{precursor}/{product}"
+            Else
+                Return $"Dim {name} As [{precursor}, {product}]"
+            End If
+        End Function
+
+        Public Function Assert(chromatogram As chromatogram) As Boolean
+            Dim pre = chromatogram.precursor.MRMTargetMz
+            Dim pro = chromatogram.product.MRMTargetMz
+
+            ' less than 0.3da or 20ppm??
+            If sys.Abs(Val(pre) - precursor) <= 0.3 AndAlso sys.Abs(Val(pro) - product) <= 0.3 Then
+                Return True
+            Else
+                Return False
+            End If
+        End Function
+    End Class
+End Namespace
