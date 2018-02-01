eca9f0d2a97da2bb7b1039598bb452c0643f78e3
xieguigang <xie.guigang@gcmodeller.org>
2018-02-01 17:22:19 +0800

    namespace renames

--- DATA/ms2_math-core/Chromatogram/Chromatogram.vb
+++ DATA/ms2_math-core/Chromatogram/Chromatogram.vb
@@ -7,114 +7,118 @@ Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Math.Quantile
 Imports Microsoft.VisualBasic.Math.Scripting
 
-Public Module Chromatogram
+Namespace Chromatogram
 
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    <Extension>
-    Public Function TimeArray(chromatogram As IEnumerable(Of ChromatogramTick)) As Vector
-        Return chromatogram.Select(Function(c) c.Time).AsVector
-    End Function
+    Public Module ChromatogramMath
 
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    <Extension>
-    Public Function IntensityArray(chromatogram As IEnumerable(Of ChromatogramTick)) As Vector
-        Return chromatogram.Select(Function(c) c.Intensity).AsVector
-    End Function
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function TimeArray(chromatogram As IEnumerable(Of ChromatogramTick)) As Vector
+            Return chromatogram.Select(Function(c) c.Time).AsVector
+        End Function
 
-    <Extension>
-    Public Function Base(chromatogram As IEnumerable(Of ChromatogramTick), Optional quantile# = 0.65) As Double
-        Dim q As QuantileEstimationGK = chromatogram.Shadows!Intensity.GKQuantile
-        Dim baseValue = q.Query(quantile)
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function IntensityArray(chromatogram As IEnumerable(Of ChromatogramTick)) As Vector
+            Return chromatogram.Select(Function(c) c.Intensity).AsVector
+        End Function
 
-        Return baseValue
-    End Function
+        <Extension>
+        Public Function Base(chromatogram As IEnumerable(Of ChromatogramTick), Optional quantile# = 0.65) As Double
+            Dim q As QuantileEstimationGK = chromatogram.Shadows!Intensity.GKQuantile
+            Dim baseValue = q.Query(quantile)
 
-    ''' <summary>
-    ''' Returns time range for the peak
-    ''' </summary>
-    ''' <param name="chromatogram">应该是按照时间升序排序了的</param>
-    ''' <param name="threshold">Unit in degree, values in range ``[0-90]``</param>
-    ''' <returns></returns>
-    <Extension>
-    Public Function MRMPeak(chromatogram As VectorModel(Of ChromatogramTick), Optional threshold# = 45, Optional winSize% = 5) As DoubleRange
+            Return baseValue
+        End Function
 
-        ' 先找到最高的信号，然后逐步分别往两边延伸
-        ' 直到下降的速率小于阈值
-        ' 因为MRM方法在一个色谱之中只出一个峰，所以在这里仅仅实现一个非常简单的峰的边界检测的算法
+        ''' <summary>
+        ''' Returns time range for the peak
+        ''' </summary>
+        ''' <param name="chromatogram">应该是按照时间升序排序了的</param>
+        ''' <param name="threshold">Unit in degree, values in range ``[0-90]``</param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function MRMPeak(chromatogram As VectorModel(Of ChromatogramTick), Optional threshold# = 45, Optional winSize% = 5) As DoubleRange
 
-        Dim maxIndex
-        Dim timeRange#()
+            ' 先找到最高的信号，然后逐步分别往两边延伸
+            ' 直到下降的速率小于阈值
+            ' 因为MRM方法在一个色谱之中只出一个峰，所以在这里仅仅实现一个非常简单的峰的边界检测的算法
 
-        ' 2018-1-18 如果事先将基线移除的话，或导致峰的范围扩大
-        ' 取消掉
-        ' removes all of the ticks that intensity value less than baseline.
-        ' chromatogram = chromatogram(chromatogram!Intensity >= chromatogram.Base(baselineQuantile))
-        maxIndex = Which.Max(chromatogram!Intensity)
+            Dim maxIndex
+            Dim timeRange#()
 
-        With chromatogram.ToArray
+            ' 2018-1-18 如果事先将基线移除的话，或导致峰的范围扩大
+            ' 取消掉
+            ' removes all of the ticks that intensity value less than baseline.
+            ' chromatogram = chromatogram(chromatogram!Intensity >= chromatogram.Base(baselineQuantile))
+            maxIndex = Which.Max(chromatogram!Intensity)
 
-            threshold = Cos(threshold.ToRadians)
-            ' split
-            timeRange = {
-                .Take(maxIndex).Reverse.ToArray.MakeExtension(threshold, winSize),   ' left
-                .Skip(maxIndex).ToArray.MakeExtension(threshold, winSize)            ' right
-            }
-        End With
+            With chromatogram.ToArray
 
-        Return New DoubleRange(timeRange)
-    End Function
+                threshold = Cos(threshold.ToRadians)
+                ' split
+                timeRange = {
+                    .Take(maxIndex).Reverse.ToArray.MakeExtension(threshold, winSize),   ' left
+                    .Skip(maxIndex).ToArray.MakeExtension(threshold, winSize)            ' right
+                }
+            End With
 
-    ''' <summary>
-    ''' t1 -> t2
-    ''' </summary>
-    ''' <param name="chromatogram"></param>
-    ''' <param name="threshold#">cos value</param>
-    ''' <param name="winSize%"></param>
-    ''' <returns></returns>
-    <Extension>
-    Private Function MakeExtension(chromatogram As ChromatogramTick(), threshold#, winSize%) As Double
-        Dim vector = chromatogram.Shadows
-        Dim timeRange As DoubleRange = vector!Time
-        Dim normInto As Vector = vector!Intensity _
-            .RangeTransform(timeRange) _
-            .AsVector
-        Dim windows = chromatogram _
-            .Select(Function(c, i)
-                        Return New ChromatogramTick With {
-                            .Time = c.Time,
-                            .Intensity = normInto(i)
-                        }
-                    End Function) _
-            .SlideWindows(winSize) _
-            .ToArray
+            Return New DoubleRange(timeRange)
+        End Function
 
-        For Each block As SlideWindow(Of ChromatogramTick) In windows
+        ''' <summary>
+        ''' t1 -> t2
+        ''' </summary>
+        ''' <param name="chromatogram"></param>
+        ''' <param name="threshold#">cos value</param>
+        ''' <param name="winSize%"></param>
+        ''' <returns></returns>
+        <Extension>
+        Private Function MakeExtension(chromatogram As ChromatogramTick(), threshold#, winSize%) As Double
+            Dim vector = chromatogram.Shadows
+            Dim timeRange As DoubleRange = vector!Time
+            Dim normInto As Vector = vector _
+                !Intensity _
+                .RangeTransform(timeRange) _
+                .AsVector
+            Dim windows = chromatogram _
+                .Select(Function(c, i)
+                            Return New ChromatogramTick With {
+                                .Time = c.Time,
+                                .Intensity = normInto(i)
+                            }
+                        End Function) _
+                .SlideWindows(winSize) _
+                .ToArray
 
-            '      A
-            '     /|
-            '    / |
-            '   /  |
-            '  /   |
-            ' ------
-            ' t0   t1
-            ' 
-            ' cos(threshold) = (t1 - t0) / ( distance((t0, 0), (t1, A)) )
-            '
+            For Each block As SlideWindow(Of ChromatogramTick) In windows
 
-            Dim t10 = block.Shadows!Time.Range.Length
-            Dim t1 = block.First
-            Dim t0 = block.Last
-            Dim A = Abs(t0.Intensity - t1.Intensity)
+                '      A
+                '     /|
+                '    / |
+                '   /  |
+                '  /   |
+                ' ------
+                ' t0   t1
+                ' 
+                ' cos(threshold) = (t1 - t0) / ( distance((t0, 0), (t1, A)) )
+                '
 
-            Dim C = (t1.Time, A).Distance(t0.Time, 0R)
-            Dim cos# = t10 / C
+                Dim t10 = block.Shadows!Time.Range.Length
+                Dim t1 = block.First
+                Dim t0 = block.Last
+                Dim A = Abs(t0.Intensity - t1.Intensity)
 
-            If threshold <= cos Then
-                Return t0.Time
-            End If
-        Next
+                Dim C = (t1.Time, A).Distance(t0.Time, 0R)
+                Dim cos# = t10 / C
 
-        ' using the entire area???
-        Return chromatogram.Last.Time
-    End Function
-End Module
+                If threshold <= cos Then
+                    Return t0.Time
+                End If
+            Next
+
+            ' using the entire area???
+            Return chromatogram.Last.Time
+        End Function
+    End Module
+End Namespace
--- DATA/ms2_math-core/Chromatogram/PeakArea.vb
+++ DATA/ms2_math-core/Chromatogram/PeakArea.vb
@@ -3,87 +3,90 @@ Imports Microsoft.VisualBasic.ComponentModel.Ranges
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.Scripting
 
-Public Module PeakArea
+Namespace Chromatogram
 
-    ''' <summary>
-    ''' ``B + A = S``
-    ''' </summary>
-    ''' <param name="chromatogram"></param>
-    ''' <param name="peak">The time range of the peak</param>
-    ''' <param name="baseline">The quantile threshold of the baseline</param>
-    ''' <returns></returns>
-    ''' 
-    <Extension>
-    Public Function PeakArea(chromatogram As VectorModel(Of ChromatogramTick), peak As DoubleRange, Optional baseline# = 0.65) As Double
-        ' gets all signals that its chromatogram time inside the peak time range
-        ' time >= time range min andalso time <= time range max 
-        Dim S = chromatogram((chromatogram!Time >= peak.Min) & (chromatogram!Time <= peak.Max))  ' TPA
-        Dim B = chromatogram.Base(quantile:=baseline)
+    Public Module PeakArea
 
-        ' 2018-1-18 
-        ' 下面的聚合表达式只会计算去除本底之后的信号量大于零的信号量的和
-        ' 取大于零是为了解决类似于 Homocysteine_chromatogram.png 这类基线过高的问题
-        ' 因为可能峰检测可能会将本底的部分也计算在内，在这些额外被计算在内的基线信号之中，由于有些信号量低于baseline基线的，所以会出现负值
-        ' 很明显这个是不需要的
-        Dim A = Aggregate signal As ChromatogramTick
-                In S
-                Let PA = signal.Intensity - B
-                Where PA > 0
-                Into Sum(PA)
+        ''' <summary>
+        ''' ``B + A = S``
+        ''' </summary>
+        ''' <param name="chromatogram"></param>
+        ''' <param name="peak">The time range of the peak</param>
+        ''' <param name="baseline">The quantile threshold of the baseline</param>
+        ''' <returns></returns>
+        ''' 
+        <Extension>
+        Public Function PeakArea(chromatogram As VectorModel(Of ChromatogramTick), peak As DoubleRange, Optional baseline# = 0.65) As Double
+            ' gets all signals that its chromatogram time inside the peak time range
+            ' time >= time range min andalso time <= time range max 
+            Dim S = chromatogram((chromatogram!Time >= peak.Min) & (chromatogram!Time <= peak.Max))  ' TPA
+            Dim B = chromatogram.Base(quantile:=baseline)
 
-        Return A
-    End Function
+            ' 2018-1-18 
+            ' 下面的聚合表达式只会计算去除本底之后的信号量大于零的信号量的和
+            ' 取大于零是为了解决类似于 Homocysteine_chromatogram.png 这类基线过高的问题
+            ' 因为可能峰检测可能会将本底的部分也计算在内，在这些额外被计算在内的基线信号之中，由于有些信号量低于baseline基线的，所以会出现负值
+            ' 很明显这个是不需要的
+            Dim A = Aggregate signal As ChromatogramTick
+                    In S
+                    Let PA = signal.Intensity - B
+                    Where PA > 0
+                    Into Sum(PA)
 
-    ''' <summary>
-    ''' 只是返回peak的顶点坐标和顶点值
-    ''' </summary>
-    ''' <param name="problem#"></param>
-    ''' <param name="left%"></param>
-    ''' <param name="right%"></param>
-    ''' <returns></returns>
-    ''' <remarks>
-    ''' https://github.com/fekberg/Algorithms/blob/master/Peak%20Finding/Peak%20Finding/Program.cs
-    ''' </remarks>
-    <Extension>
-    Public Function FindPeak(problem#()(), Optional left% = 0, Optional right% = -1) As Double
-        If problem.Length <= 0 Then
-            Return 0
-        End If
-        If right = -1 Then
-            right = problem(0).Length
-        End If
+            Return A
+        End Function
 
-        Dim j% = (left + right) / 2
-        Dim globalMax = problem.FindGlobalMax(j)
+        ''' <summary>
+        ''' 只是返回peak的顶点坐标和顶点值
+        ''' </summary>
+        ''' <param name="problem#"></param>
+        ''' <param name="left%"></param>
+        ''' <param name="right%"></param>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' https://github.com/fekberg/Algorithms/blob/master/Peak%20Finding/Peak%20Finding/Program.cs
+        ''' </remarks>
+        <Extension>
+        Public Function FindPeak(problem#()(), Optional left% = 0, Optional right% = -1) As Double
+            If problem.Length <= 0 Then
+                Return 0
+            End If
+            If right = -1 Then
+                right = problem(0).Length
+            End If
 
-        If (globalMax - 1 > 0 AndAlso problem(globalMax)(j) >= problem(globalMax - 1)(j)) AndAlso
-           (globalMax + 1 < problem.Length AndAlso problem(globalMax)(j) >= problem(globalMax + 1)(j)) AndAlso
-           (j - 1 > 0 AndAlso problem(globalMax)(j) >= problem(globalMax)(j - 1)) AndAlso
-           (j + 1 < problem(globalMax).Length AndAlso problem(globalMax)(j) >= problem(globalMax)(j + 1)) Then
+            Dim j% = (left + right) / 2
+            Dim globalMax = problem.FindGlobalMax(j)
+
+            If (globalMax - 1 > 0 AndAlso problem(globalMax)(j) >= problem(globalMax - 1)(j)) AndAlso
+               (globalMax + 1 < problem.Length AndAlso problem(globalMax)(j) >= problem(globalMax + 1)(j)) AndAlso
+               (j - 1 > 0 AndAlso problem(globalMax)(j) >= problem(globalMax)(j - 1)) AndAlso
+               (j + 1 < problem(globalMax).Length AndAlso problem(globalMax)(j) >= problem(globalMax)(j + 1)) Then
+
+                Return problem(globalMax)(j)
+            ElseIf j > 0 AndAlso problem(globalMax)(j - 1) > problem(globalMax)(j) Then
+                right = j
+                Return FindPeak(problem, left, right)
+            ElseIf j + 1 < problem(globalMax).Length AndAlso problem(globalMax)(j + 1) > problem(globalMax)(j) Then
+                left = j
+                Return FindPeak(problem, left, right)
+            End If
 
             Return problem(globalMax)(j)
-        ElseIf j > 0 AndAlso problem(globalMax)(j - 1) > problem(globalMax)(j) Then
-            right = j
-            Return FindPeak(problem, left, right)
-        ElseIf j + 1 < problem(globalMax).Length AndAlso problem(globalMax)(j + 1) > problem(globalMax)(j) Then
-            left = j
-            Return FindPeak(problem, left, right)
-        End If
+        End Function
 
-        Return problem(globalMax)(j)
-    End Function
+        <Extension>
+        Public Function FindGlobalMax(problem#()(), column%) As Integer
+            Dim max#, index%
 
-    <Extension>
-    Public Function FindGlobalMax(problem#()(), column%) As Integer
-        Dim max#, index%
+            For i As Integer = 0 To problem.Length - 1
+                If max < problem(i)(column) Then
+                    max = problem(i)(column)
+                    index = i
+                End If
+            Next
 
-        For i As Integer = 0 To problem.Length - 1
-            If max < problem(i)(column) Then
-                max = problem(i)(column)
-                index = i
-            End If
-        Next
-
-        Return index
-    End Function
-End Module
+            Return index
+        End Function
+    End Module
+End Namespace
