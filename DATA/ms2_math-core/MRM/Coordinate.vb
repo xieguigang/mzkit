760df89b2d336784bde42e43085bf01f826616f7
xieguigang <xie.guigang@gcmodeller.org>
2018-02-05 09:50:36 +0800

    add MRM data models

---
+++ DATA/ms2_math-core/MRM/Coordinate.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace MRM
+
+    Public Class Coordinate
+
+        Public Property HMDB As String
+        Public Property Name As String
+        Public Property C As Dictionary(Of String, Double)
+        Public Property ISTD As String
+        ''' <summary>
+        ''' 内标的编号，需要使用这个编号来分别找到离子对和浓度信息
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property [IS] As String
+
+        Public Overrides Function ToString() As String
+            Return $"[{[IS]}] {HMDB}: {Name}, {C.GetJson}"
+        End Function
+    End Class
+End Namespace
---
+++ DATA/ms2_math-core/MRM/IS.vb
@@ -0,0 +1,13 @@
+﻿Namespace MRM
+
+    Public Class [IS]
+
+        Public Property ID As String
+        Public Property name As String
+        Public Property CIS As Double
+
+        Public Overrides Function ToString() As String
+            Return $"Dim {name} As {ID} = {CIS}"
+        End Function
+    End Class
+End Namespace
