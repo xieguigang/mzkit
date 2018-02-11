9c37264cfa76b3458eff6d3d01dfcc89f7cfa244
xieguigang <xie.guigang@gcmodeller.org>
2018-02-11 17:24:45 +0800

    fix xml code comments

--- visual/plot/Visualize/ChromatogramPlot.vb
+++ visual/plot/Visualize/ChromatogramPlot.vb
@@ -58,7 +58,6 @@ Public Module ChromatogramPlot
     ''' <summary>
     ''' 从mzML文件之中解析出色谱数据之后，将所有的色谱峰都绘制在一张图之中进行可视化
     ''' </summary>
-    ''' <param name="mzML$"></param>
     ''' <param name="size$"></param>
     ''' <param name="margin$"></param>
     ''' <param name="bg$"></param>
