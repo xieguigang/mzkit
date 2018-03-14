25990d4061e217511645cab57f0f31056ba51dbd
xieguigang <xie.guigang@gcmodeller.org>
2018-03-14 10:48:23 +0800

    units

---
+++ DATA/Massbank/Concentration.vb
@@ -0,0 +1,97 @@
+﻿Public Module Concentration
+
+End Module
+
+Public Enum Units As Integer
+
+    ''' <summary>
+    ''' 百分之一``[%]``
+    ''' </summary>
+    percent = 1
+    ''' <summary>
+    ''' 千分之一``[‰]``
+    ''' </summary>
+    permil
+    ''' <summary>
+    ''' ``液量盎司/加仑``(UK)
+    ''' </summary>
+    floz_gallon_UK
+    ''' <summary>
+    ''' ``液量盎司/加仑``(US)
+    ''' </summary>
+    floz_gallon_US
+    ''' <summary>
+    ''' ``立方英寸/立方英尺``
+    ''' </summary>
+    cuin_cuft
+    ''' <summary>
+    ''' ``立方英寸/立方码``
+    ''' </summary>
+    cuin_cuyard
+    ''' <summary>
+    ''' ``滴/加仑``(UK)
+    ''' </summary>
+    drops_gallon_UK
+    ''' <summary>
+    ''' ``滴/加仑``(US)
+    ''' </summary>
+    drops_gallon_US
+    ''' <summary>
+    ''' ``滴/立方英尺``
+    ''' </summary>
+    drops_cuft
+    ''' <summary>
+    ''' ``盎司/镑``
+    ''' </summary>
+    oz_pound
+    ''' <summary>
+    ''' ``盎司/吨``(UK)
+    ''' </summary>
+    oz_ton_UK
+    ''' <summary>
+    ''' ``盎司/吨``(US)
+    ''' </summary>
+    oz_ton_US
+    ppm
+    ''' <summary>
+    ''' 十亿分之一
+    ''' </summary>
+    parts_billion
+    ''' <summary>
+    ''' ``毫升/升``
+    ''' </summary>
+    mL_litre
+    ''' <summary>
+    ''' ``毫升/兆升``
+    ''' </summary>
+    mL_megalitre
+    ''' <summary>
+    ''' ``毫升/立方米``
+    ''' </summary>
+    mL_cumetre
+    ''' <summary>
+    ''' ``滴/毫升``
+    ''' </summary>
+    drops_mL
+    ''' <summary>
+    ''' ``滴/升``
+    ''' </summary>
+    drops_litre
+    ''' <summary>
+    ''' ``滴/立方米``
+    ''' </summary>
+    drops_cumetre
+    ''' <summary>
+    ''' ``毫克/公斤`` 
+    ''' </summary>
+    milligrams_kg
+    ''' <summary>
+    ''' ``克/公斤``
+    ''' </summary>
+    grams_kg
+    ''' <summary>
+    ''' ``克/吨`` 
+    ''' </summary>
+    grams_tonne
+
+End Enum
