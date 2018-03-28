5dd973eedc71ca8d181ca5451bc82b12f7748002
xieguigang <xie.guigang@gcmodeller.org>
2018-03-28 16:43:59 +0800

    Atomic.group.Weight

--- MwtWinDll/Formula.R/R/Formula.R
+++ MwtWinDll/Formula.R/R/Formula.R
@@ -13,6 +13,29 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+# 在这个函数之中尽可能多的枚举出目前已知的所有的原子基团
+Atomic.group <- function() {
+    group <- list(
+        CH3 = list(atoms = list(C = 1, H = 3), charge = -1)
+    );
+
+    group;
+}
+
+# 元素周期表
+Atomic.Weights <- function() {
+    list(
+        C = 12, H = 1
+    );
+}
+
+# 计算出指定的原子基团的分子质量大小
+Atomic.group.Weight <- function(Atomic.group, name) {
+    Atomic.group <- Atomic.group[[name]];
+    atoms        <- Atomic.group[["atoms"]];
+    weight       <- Atomic.Weights();
+    weight       <- weight[names(atoms)];
+    weight       <- weight * atoms;
+
+    weight;
 }
