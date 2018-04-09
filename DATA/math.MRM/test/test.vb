3c68428a0d524b72d025866692253986b26008e1
xieguigang <xie.guigang@gcmodeller.org>
2018-04-09 10:49:39 +0800

    config test

--- DATA/math.MRM/test/test.vb
+++ DATA/math.MRM/test/test.vb
@@ -11,13 +11,13 @@ Module test
 
     Sub Main()
 
-        Dim std = "E:\MRM_Test\MetaCardio_STD-X.csv".LoadCsv(Of Standards)
-        Dim [IS] As [IS]() = "E:\MRM_Test\IS.csv".LoadCsv(Of [IS])
-        Dim ion_pairs = "E:\MRM_Test\ion_pairs.csv".LoadCsv(Of IonPair)
+        Dim std = "D:\ProteoWizard.d\MRM_Test\MetaCardio_STD-X.csv".LoadCsv(Of Standards)
+        Dim [IS] As [IS]() = "D:\ProteoWizard.d\MRM_Test\IS.csv".LoadCsv(Of [IS])
+        Dim ion_pairs = "D:\ProteoWizard.d\MRM_Test\ion_pairs.csv".LoadCsv(Of IonPair)
 
         Dim fits As NamedValue(Of FitResult)() = Nothing
         Dim X As List(Of DataSet) = Nothing
-        Dim result = MRMSamples.QuantitativeAnalysis("E:\MRM_Test\Data20180313.wiff", ion_pairs, std, [IS], fits, X, calibrationNamedPattern:=".+M1[-]L\d+").ToArray
+        Dim result = MRMSamples.QuantitativeAnalysis("D:\ProteoWizard.d\Data20180313\Data20180313.wiff", ion_pairs, std, [IS], fits, X, calibrationNamedPattern:=".+M1[-]L\d+").ToArray
 
 
         Pause()
