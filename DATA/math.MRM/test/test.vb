8d7bcb6c3de0f0a14e4816034c7e73249833d5a5
xieguigang <xie.guigang@gcmodeller.org>
2018-04-09 11:18:13 +0800

    add plot test

--- DATA/math.MRM/test/test.vb
+++ DATA/math.MRM/test/test.vb
@@ -2,11 +2,13 @@
 Imports Microsoft.VisualBasic.Data.Bootstrapping
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.MassSpectrum.Assembly.MarkupData.mzML
 Imports SMRUCC.MassSpectrum.Math.Chromatogram
 Imports SMRUCC.MassSpectrum.Math.MRM
 Imports SMRUCC.MassSpectrum.Math.MRM.Models
+Imports SMRUCC.MassSpectrum.Visualization
 
 Module test
 
@@ -16,10 +18,53 @@ Module test
         Dim [IS] As [IS]() = "D:\ProteoWizard.d\MRM_Test\IS.csv".LoadCsv(Of [IS])
         Dim ion_pairs = "D:\ProteoWizard.d\MRM_Test\ion_pairs.csv".LoadCsv(Of IonPair)
 
+        Call ROIvizTest(ion_pairs)
+
+
         Dim fits As NamedValue(Of FitResult)() = Nothing
         Dim X As List(Of DataSet) = Nothing
         Dim result = MRMSamples.QuantitativeAnalysis("D:\ProteoWizard.d\Data20180313\Data20180313.wiff", ion_pairs, std, [IS], fits, X, calibrationNamedPattern:=".+M1[-]L\d+", peakAreaMethod:=PeakArea.Methods.NetPeakSum).ToArray
 
+
+        Pause()
+    End Sub
+
+    Sub ROIvizTest(ionpairs As IonPair())
+        For Each file As String In {"D:\ProteoWizard.d\Data20180313\Data20180313-M1-L1.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M1-L2.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M1-L3.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M1-L4.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M1-L5.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M1-L6.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M1-L7.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M2-L1.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M2-L2.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M2-L3.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M2-L4.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M2-L5.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M2-L6.mzML",
+"D:\ProteoWizard.d\Data20180313\Data20180313-M2-L7.mzML"} '  "D:\ProteoWizard.d\Data20180313".EnumerateFiles("*.mzML")
+            Dim ionData = LoadChromatogramList(path:=file) _
+                .MRMSelector(ionpairs) _
+                .Where(Function(ion) Not ion.chromatogram Is Nothing) _
+                .Select(Function(ion)
+                            Return New NamedValue(Of ChromatogramTick()) With {
+                                .Name = ion.ion.AccID,
+                                .Description = ion.ion.ToString,
+                                .Value = ion.chromatogram.Ticks
+                            }
+                        End Function) _
+                .ToArray
+
+            Dim dir = file.TrimSuffix
+
+            For Each ion In ionData
+                Dim path = $"{dir}/{ion.Name}.png"
+
+                Call ion.Value.Plot.AsGDIImage.SaveAs(path)
+                Call path.__INFO_ECHO
+            Next
+        Next
 
         Pause()
     End Sub
