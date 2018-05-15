8f842c887f0f7a82efd2781bbd9f5c9a715e667f
中二病でも恋がしたい！ <xie.guigang@gcmodeller.org>
2018-05-15 18:01:38 +0800

    Update PrecursorType.R

--- DATA/MetaDNA/MetaDNA/R/PrecursorType.R
+++ DATA/MetaDNA/MetaDNA/R/PrecursorType.R
@@ -1,3 +1,4 @@
+# https://github.com/xieguigang/MassSpectrum-toolkits/blob/6f4284a0d537d86c112877243d9e3b8d9d35563f/DATA/ms2_math-core/Ms1/PrecursorType.vb
 
 ## @param mass 分子的质量
 ## @param adduct 离子的质量
