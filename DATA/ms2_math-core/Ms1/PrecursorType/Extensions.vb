e1fb919be6705ef74fc3c3518c8929f756d11aa6
xieguigang <xie.guigang@gcmodeller.org>
2018-06-12 17:20:28 +0800

    PrecursorTypeParser for mz calculator

--- DATA/ms2_math-core/Ms1/PrecursorType/Extensions.vb
+++ DATA/ms2_math-core/Ms1/PrecursorType/Extensions.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Emit.Marshal
+Imports Microsoft.VisualBasic.Language
 
 Namespace Ms1.PrecursorType
 
@@ -26,19 +27,34 @@ Namespace Ms1.PrecursorType
                 M = 1
             End If
 
-            Dim formulas As New List(Of String)
-            Dim parser As New Pointer(Of Char)(type)
+            Dim formulas As New List(Of (sign%, expression As String))
+            Dim parser As New Pointer(Of Char)(type.StringReplace("\d*M", ""))
             Dim buffer As New List(Of Char)
             Dim c As Char
+            Dim sign%
 
             Do While Not parser.EndRead
+                c = ++parser
 
+                If c = "+"c OrElse c = "-"c Then
+                    If buffer > 0 Then
+                        formulas += (sign, buffer.CharString)
+                        buffer *= 0
+                    Else
+                        sign = If(c = "+"c, 1, -1)
+                    End If
+                Else
+                    buffer += c
+                End If
             Loop
 
-            Dim adducts# = Aggregate formula As String
+            ' 补上最后一个元素
+            formulas += (sign, buffer.CharString)
+
+            Dim adducts# = Aggregate formula
                            In formulas
-                           Let mass As Double = formulaMass(formula)
-                           Into Sum(mass)
+                           Let mass As Double = formulaMass(formula.expression)
+                           Into Sum(formula.sign * mass)
 
             Return New MzCalculator With {
                 .M = M,
