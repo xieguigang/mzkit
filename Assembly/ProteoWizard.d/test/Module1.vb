6668c738bcea45ddd2330a7fd35cd5ca20a4f9af
xieguigang <xie.guigang@gcmodeller.org>
2018-06-12 10:02:19 +0800

    add test

---
+++ Assembly/ProteoWizard.d/test/Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+
+Module Module1
+
+    Sub Main()
+
+        Call testExtractZip()
+
+
+    End Sub
+
+    Sub testExtractZip()
+        Dim zip1 = "C:\Users\administrator\Desktop\95-1.D.zip"
+        Dim test1 = GZip.IsSourceFolderZip(zip1)
+
+        Dim zip2 = "C:\Users\administrator\Desktop\95-1.D-flat.zip"
+        Dim test2 = GZip.IsSourceFolderZip(zip2)
+
+        Call GZip.ImprovedExtractToDirectory(zip1, "./", extractToFlat:=True)
+
+
+    End Sub
+
+End Module
