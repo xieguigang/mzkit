b56d9277aab7eb5d68af5d3c9462540fcc39d714
xieguigang <xie.guigang@gcmodeller.org>
2018-06-19 11:33:33 +0800

    build network

--- DATA/MetaDNA/KEGG_MetaDNA/Program.vb
+++ DATA/MetaDNA/KEGG_MetaDNA/Program.vb
@@ -1,5 +1,8 @@
 ﻿Imports Microsoft.VisualBasic.Language.UnixBash
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports R_server = RDotNet.Extensions.VisualBasic.RSystem
+Imports Rbase = RDotNet.Extensions.VisualBasic.API.base
+Imports VisualBasic = Microsoft.VisualBasic.Language.Runtime
 
 Module Program
 
@@ -19,6 +22,30 @@ Module Program
                               Return g.First
                           End Function)
 
+        ' network <- list(rxnID, reactants, products);
+        Dim network = Rbase.list()
 
+        SyncLock R_server.R
+            With R_server.R
+
+                With New VisualBasic
+
+                    network = Rbase.lapply(
+                        x:=reactions,
+                        FUN:=Function(reaction)
+                                 Dim model = reaction.ReactionModel
+
+                                 Return Rbase.list(
+                                    !rxnID = reaction.ID,
+                                    !define = reaction.Definition,
+                                    !reactants = Rbase.c(model.Reactants.Keys, stringVector:=True),
+                                    !products = Rbase.c(model.Products.Keys, stringVector:=True)
+                                 )
+                             End Function)
+
+                End With
+
+            End With
+        End SyncLock
     End Sub
 End Module
