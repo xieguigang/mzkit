e1a38683f58c6327a0947371fc424e883ab25d7c
xieguigang <xie.guigang@gcmodeller.org>
2018-06-26 14:43:39 +0800

    add reference kegg meta data

---
+++ DATA/MetaDNA/KEGG_MetaDNA/KEGGMetaDB.vb
@@ -0,0 +1,21 @@
+﻿''' <summary>
+''' MetaDNA算法所需要的KEGG注释信息的结构
+''' </summary>
+Public Class KEGGMetaDB
+
+    Public Property keggID As String
+    Public Property name As String
+    Public Property formula As String
+    Public Property mass As Double
+
+    ''' <summary>
+    ''' 指向来源的数据集的唯一编号
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property libname As String
+
+    Public Overrides Function ToString() As String
+        Return $"[{libname}] {keggID}={name}"
+    End Function
+
+End Class
