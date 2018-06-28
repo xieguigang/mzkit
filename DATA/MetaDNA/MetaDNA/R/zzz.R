9f13f0a18236145fc0428f9cb473035220e122c8
xieguigang <xie.guigang@gcmodeller.org>
2018-06-28 17:41:26 +0800

    compile R document

--- DATA/MetaDNA/MetaDNA/R/zzz.R
+++ DATA/MetaDNA/MetaDNA/R/zzz.R
@@ -1,3 +1,7 @@
 .onLoad <- function(libname, pkgname) {
 	require(VisualBasic.R);
+	
+	Imports("Microsoft.VisualBasic.Data.Linq", frame = globalenv());
 }
+
+.flashLoad <- function() .onLoad(NULL, NULL);
