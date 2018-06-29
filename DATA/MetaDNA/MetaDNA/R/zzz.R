6f30574dc92d6525b7abf0c2ee1404a27b37b569
xieguigang <xie.guigang@gcmodeller.org>
2018-06-29 13:29:19 +0800

    removes source file

--- DATA/MetaDNA/MetaDNA/R/zzz.R
+++ DATA/MetaDNA/MetaDNA/R/zzz.R
@@ -2,7 +2,7 @@
 	require(VisualBasic.R);
 
   Imports("Microsoft.VisualBasic.Data", frame = globalenv());
-	Imports("Microsoft.VisualBasic.Data.Linq", frame = globalenv());
+  Imports("Microsoft.VisualBasic.Data.Linq", frame = globalenv());
 }
 
 .flashLoad <- function() .onLoad(NULL, NULL);
