50aafcb2774920fcbeb6803c196705df1ba8da02
xieguigang <xie.guigang@gcmodeller.org>
2018-07-03 11:22:50 +0800

    tweaks on the description

--- DATA/MetaDNA/MetaDNA/R/zzz.R
+++ DATA/MetaDNA/MetaDNA/R/zzz.R
@@ -1,7 +1,9 @@
 .onLoad <- function(libname, pkgname) {
+
 	require(VisualBasic.R);
 
-  Imports("Microsoft.VisualBasic.Data", frame = globalenv());
+  # enable additional language feature
+  Imports("Microsoft.VisualBasic.Data",      frame = globalenv());
   Imports("Microsoft.VisualBasic.Data.Linq", frame = globalenv());
 
   list(Calculator = init_calc()) %=>% Set;
@@ -9,12 +11,22 @@
 
   print("Pre-defined m/z calculator:");
   cat("\n");
-  cat("m/z+\n\n");
+  cat(" [m/z]+\n\n");
   print(sapply(Calculator$`+`, function(type) type$Name) %=>% as.character);
   cat("\n");
-  cat("m/z-\n\n");
+  cat(" [m/z]-\n\n");
   print(sapply(Calculator$`-`, function(type) type$Name) %=>% as.character);
   cat("\n");
+
+  cat("You can acquire the toolkit's source code from github:");
+  cat("\n\n");
+  cat("     https://github.com/xieguigang/MassSpectrum-toolkits");
+  cat("\n\n");
+  cat("If any problem for this package, contact the author:");
+  cat("\n\n");
+  cat("     xieguigang <xie.guigang@gcmodeller.org>")
+  cat("\n\n");
+
 }
 
 .flashLoad <- function() .onLoad(NULL, NULL);
