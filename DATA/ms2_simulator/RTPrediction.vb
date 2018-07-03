f3a08b86fb331699f1ea7f295996d0b78eed8994
xieguigang <xie.guigang@gcmodeller.org>
2018-07-03 14:55:56 +0800

    get compositions

--- DATA/ms2_simulator/RTPrediction.vb
+++ DATA/ms2_simulator/RTPrediction.vb
@@ -5,7 +5,7 @@ Imports SMRUCC.Chemistry.Model
 ''' <summary>
 ''' Prediciton of the relative LC-MS retention time for measuring ``rt.error``
 ''' in small compound metabolite identification, based on the KEGG KCF 
-''' moleculate strucutre composition linear regression model.
+''' moleculate strucutre composition non-linear regression model.
 ''' </summary>
 Public Module RTPrediction
 
@@ -15,8 +15,11 @@ Public Module RTPrediction
     ''' <param name="KCF"></param>
     ''' <returns>Regression model factors</returns>
     <Extension>
-    Public Function KCFComposition(KCF As KCF) As Dictionary(Of String, Integer)
-
+    Public Function KCFComposition(KCF As KCF) As Dictionary(Of String, Double)
+        Return KCF.Atoms _
+            .GroupBy(Function(a) a.KEGGAtom) _
+            .ToDictionary(Function(a) a.Key,
+                          Function(a) CDbl(a.Count))
     End Function
 
     ''' <summary>
@@ -25,9 +28,19 @@ Public Module RTPrediction
     ''' </summary>
     ''' <param name="experimental">Experimental rt value of a given set of metabolite</param>
     ''' <returns>
-    ''' Using this linear regression fitted model for predicted of the unknowns
+    ''' Using this non-linear regression fitted model for predicted of the unknowns
     ''' </returns>
+    ''' 
+    <Extension>
     Public Function RtRegression(experimental As IEnumerable(Of (metabolite As KCF, rt#))) As FitResult
+        Dim matrix = experimental _
+            .Select(Function(m)
+                        Return New LMA.FitInput With {
+                            .factors = m.metabolite.KCFComposition,
+                            .y = m.rt
+                        }
+                    End Function)
+        Dim fit = matrix.NonLinearFit
 
     End Function
 End Module
--- DATA/ms2_simulator/test/rt_test.vb
+++ DATA/ms2_simulator/test/rt_test.vb
@@ -4,5 +4,8 @@ Module rt_test
 
     Sub Main()
         Dim C00047 As KCF = IO.LoadKCF("https://www.kegg.jp/dbget-bin/www_bget?-f+k+compound+C00047")
+
+
+        Pause()
     End Sub
 End Module
