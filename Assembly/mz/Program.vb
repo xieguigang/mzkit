6d1f3b7e7922a4f45e8cb04766387cb473885e6b
xieguigang <xie.guigang@gcmodeller.org>
2018-09-20 11:08:29 +0800

    export a given scan

---
+++ Assembly/mz/Program.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.MassSpectrum.Assembly.MarkupData
+Imports SMRUCC.MassSpectrum.Assembly.MarkupData.mzXML
+
+Module Program
+
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("/export")>
+    <Usage("/export /in <data.mzXML> /scan <ms2_scan> [/out <out.txt>]")>
+    Public Function MGF(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim scan& = args <= "/scan"
+        Dim out$ = args("/out") Or $"{[in]}#{scan}.txt"
+        Dim allScans = mzXML.XML.LoadScans([in]).ToArray
+        Dim allMs2FullScan = allScans _
+            .Where(Function(s) s.msLevel = "2") _
+            .ToArray
+        Dim ms2Scan = allMs2FullScan(scan)
+        Dim ms2Peaks = ms2Scan.ExtractMzI
+
+        Using file As StreamWriter = out.OpenWriter
+            Call file.WriteLine(ms2Peaks.name)
+            Call file.WriteLine($"mz range: [{ms2Scan.lowMz}, {ms2Scan.highMz}]")
+            Call file.WriteLine($"peaks: {ms2Scan.peaksCount}")
+            Call file.WriteLine($"activation: {ms2Scan.precursorMz.activationMethod} @ {ms2Scan.collisionEnergy}V")
+            Call file.WriteLine(ms2Peaks.peaks.Print(addBorder:=False))
+            Call file.WriteLine()
+        End Using
+
+        Return 0
+    End Function
+End Module
