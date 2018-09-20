88f7cd42731ed118eb7ddb748f7c4c02b3aa71c2
xieguigang <xie.guigang@gcmodeller.org>
2018-09-20 11:35:19 +0800

    config projects

--- DATA/ms2_math-core/Ms2/GlobalAlignment.vb
+++ DATA/ms2_math-core/Ms2/GlobalAlignment.vb
@@ -17,37 +17,37 @@ Public Module GlobalAlignment
 
     ReadOnly da3 As DefaultValue(Of Tolerance) = New DAmethod(0.3).Interface
 
-    ''' <summary>
-    ''' ### shared peak count
-    ''' 
-    ''' In the matrix ``M``, ``i`` and ``j`` are positions, where ``i`` is the
-    ''' horizontal coordinate And ``j`` Is the vertical coordinate. For Each
-    ''' cell in the matrix, a score Is calculated (Si,j). If the two compared
-    ''' masses are a match, Then ``C`` the cost Is equal To 0. However, If
-    ''' the two masses are outside the designated ppm Error window,
-    ''' then the cost Is equal to 1.
-    ''' </summary>
-    ''' <param name="query"></param>
-    ''' <param name="subject"></param>
-    ''' <returns></returns>
-    Public Function NWGlobalAlign(query As LibraryMatrix, subject As LibraryMatrix, Optional tolerance As Tolerance = Nothing) As GlobalAlign(Of ms2)()
-        Dim massEquals As IEquals(Of ms2)
-        Dim empty As New ms2 With {
-            .mz = -1,
-            .intensity = -1,
-            .quantity = -1
-        }
+    '''' <summary>
+    '''' ### shared peak count
+    '''' 
+    '''' In the matrix ``M``, ``i`` and ``j`` are positions, where ``i`` is the
+    '''' horizontal coordinate And ``j`` Is the vertical coordinate. For Each
+    '''' cell in the matrix, a score Is calculated (Si,j). If the two compared
+    '''' masses are a match, Then ``C`` the cost Is equal To 0. However, If
+    '''' the two masses are outside the designated ppm Error window,
+    '''' then the cost Is equal to 1.
+    '''' </summary>
+    '''' <param name="query"></param>
+    '''' <param name="subject"></param>
+    '''' <returns></returns>
+    'Public Function NWGlobalAlign(query As LibraryMatrix, subject As LibraryMatrix, Optional tolerance As Tolerance = Nothing) As GlobalAlign(Of ms2)()
+    '    Dim massEquals As IEquals(Of ms2)
+    '    Dim empty As New ms2 With {
+    '        .mz = -1,
+    '        .intensity = -1,
+    '        .quantity = -1
+    '    }
 
-        With tolerance Or da3
-            massEquals = Function(q, s)
-                             Return .Assert(q.mz, s.mz)
-                         End Function
-        End With
+    '    With tolerance Or da3
+    '        massEquals = Function(q, s)
+    '                         Return .Assert(q.mz, s.mz)
+    '                     End Function
+    '    End With
 
-        With New NeedlemanWunsch(Of ms2)(query, subject, massEquals, empty, Function(ms2) ms2.ToString.First).Compute()
-            Return .PopulateAlignments.ToArray
-        End With
-    End Function
+    '    With New NeedlemanWunsch(Of ms2)(query, subject, massEquals, empty, Function(ms2) ms2.ToString.First).Compute()
+    '        Return .PopulateAlignments.ToArray
+    '    End With
+    'End Function
 
     Public Function SharedPeakCount(query As LibraryMatrix, subject As LibraryMatrix, Optional tolerance As Tolerance = Nothing, Optional top% = 10) As Integer
         Dim q = query.OrderByDescending(Function(mz) mz.quantity).Take(top).ToArray
