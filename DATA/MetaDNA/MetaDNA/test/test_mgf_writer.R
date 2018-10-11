3e3018d0daec02df01fffb20a74c0f22fd8afc54
xieguigang <xie.guigang@gcmodeller.org>
2018-10-11 16:34:06 +0800

    fix for write test demo

--- DATA/MetaDNA/MetaDNA/test/test_mgf_writer.R
+++ DATA/MetaDNA/MetaDNA/test/test_mgf_writer.R
@@ -1,16 +1,23 @@
 xLoad("./GABA.rda");
 
+meta.db         <- as.data.frame(meta.db);
 meta.db[, "mz"] <- meta.db[, "precursor_m/z"] %=>% as.numeric;
 
 # populate ms2 data union libpos and libneg
 ms2 <- function(libname) {
 	peaks <- lib.pos[[libname]];
+		
+	if (peaks %=>% IsNothing) {
+		peaks <- lib.neg[[libname]];	
+	}
 	
 	if (peaks %=>% IsNothing) {
-		lib.neg[[libname]];
-	} else {
-		peaks;
+		return(NULL);
 	}
+	
+	peaks[, "mz"]   <- peaks[, "ProductMz"];
+	peaks[, "into"] <- peaks[, "LibraryIntensity"];
+	peaks;
 }
 
 write.mgf(meta.db, ms2, "./GABA.mgf");
