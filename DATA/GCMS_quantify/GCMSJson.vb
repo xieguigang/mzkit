d1242c563900b5daedef1f00ea11d6f0e435dd33
xieguigang <xie.guigang@gcmodeller.org>
2018-11-08 16:58:27 +0800

    add data model for GC/MS

---
+++ DATA/GCMS_quantify/GCMSJson.vb
@@ -0,0 +1,14 @@
+﻿Public Class GCMSJson
+
+    Public Property time
+    Public Property series As SerialPoint()
+
+End Class
+
+Public Class SerialPoint
+
+    Public Property name As String
+    Public Property dimension As Integer
+    Public Property data
+
+End Class
