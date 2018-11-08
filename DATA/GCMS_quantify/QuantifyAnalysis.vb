53c4be17f7894921350d7fe095d64d38524cd3a3
xieguigang <xie.guigang@gcmodeller.org>
2018-11-08 16:37:06 +0800

    config for test

--- DATA/GCMS_quantify/QuantifyAnalysis.vb
+++ DATA/GCMS_quantify/QuantifyAnalysis.vb
@@ -1,3 +1,18 @@
-﻿Public Module QuantifyAnalysis
+﻿Imports Microsoft.VisualBasic.MIME.application.netCDF
 
+Public Module QuantifyAnalysis
+
+    ''' <summary>
+    ''' 读取CDF文件然后读取原始数据
+    ''' </summary>
+    ''' <param name="cdfPath"></param>
+    ''' <returns></returns>
+    Public Function ReadData(cdfPath$, Optional showSummary As Boolean = True)
+        Dim cdf As New netCDFReader(cdfPath)
+
+        If showSummary Then
+            Call Console.WriteLine(cdf.ToString)
+        End If
+
+    End Function
 End Module
--- DATA/GCMS_quantify/test/Module1.vb
+++ DATA/GCMS_quantify/test/Module1.vb
@@ -1,7 +1,11 @@
-﻿Module Module1
+﻿Imports GCMS_quantify
+
+Module Module1
 
     Sub Main()
+        Dim data = QuantifyAnalysis.ReadData("D:\smartnucl_integrative\biodeepDB\smartnucl_integrative\16s_contents\SCFA\scfa200ppmAIAEXPRT.AIA\20ppm-未处理.CDF")
 
+        Pause()
     End Sub
 
 End Module
