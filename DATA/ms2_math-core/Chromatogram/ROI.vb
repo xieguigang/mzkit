a1633db9731663150efe997181321a2babaffac7
xieguigang <xie.guigang@gcmodeller.org>
2018-11-12 11:35:20 +0800

    TIC ROI test success for GC/MS data

--- DATA/ms2_math-core/Chromatogram/ROI.vb
+++ DATA/ms2_math-core/Chromatogram/ROI.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 
 Namespace Chromatogram
 
@@ -18,12 +19,23 @@ Namespace Chromatogram
         ''' <returns></returns>
         Public Property MaxInto As Double
         Public Property Ticks As ChromatogramTick()
+        ''' <summary>
+        ''' 所计算出来的基线的响应强度
+        ''' </summary>
+        ''' <returns></returns>
         Public Property Baseline As Double
         ''' <summary>
         ''' 当前的这个ROI的峰面积积分值
         ''' </summary>
         ''' <returns></returns>
         Public Property Integration As Double
+
+        Public ReadOnly Property PeakWidth As Single
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return Time.Length
+            End Get
+        End Property
 
         Public Overrides Function ToString() As String
             Return Time.ToString
--- visual/test/GCMS.vb
+++ visual/test/GCMS.vb
@@ -1,5 +1,7 @@
 ﻿Imports GCMS_quantify
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Math
+Imports SMRUCC.MassSpectrum.Math.Chromatogram
 Imports SMRUCC.MassSpectrum.Visualization
 
 Module GCMS
@@ -7,6 +9,7 @@ Module GCMS
     Sub Main()
         Dim gcData = QuantifyAnalysis.ReadData("D:\smartnucl_integrative\biodeepDB\smartnucl_integrative\16s_contents\SCFA\scfa200ppmAIAEXPRT.AIA\200ppm-未处理.CDF")
         Dim tic = {gcData.GetTIC}
+        Dim ROIlist = gcData.GetTIC.Shadows.PopulateROI.ToArray
 
         Call tic.TICplot().AsGDIImage.SaveAs("./test_gcms_ticplot.png")
 
