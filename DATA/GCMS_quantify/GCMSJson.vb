72f6f98558a925dc112e029e8d23c7ef0bf4c75e
xieguigang <xie.guigang@gcmodeller.org>
2018-11-12 09:32:41 +0800

    generate ticks data

--- DATA/GCMS_quantify/GCMSJson.vb
+++ DATA/GCMS_quantify/GCMSJson.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports SMRUCC.MassSpectrum.Math
 Imports SMRUCC.MassSpectrum.Math.Chromatogram
 
@@ -9,7 +10,15 @@ Public Class GCMSJson
     Public Property tic As Double()
     Public Property ms As ms1_scan()()
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetTIC() As NamedCollection(Of ChromatogramTick)
-
+        Return New NamedCollection(Of ChromatogramTick) With {
+            .Name = name,
+            .Value = times _
+                .Select(Function(time, i)
+                            Return New ChromatogramTick(time, tic(i))
+                        End Function) _
+                .ToArray
+        }
     End Function
 End Class
