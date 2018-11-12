f546e14faa406bb9db571c93ef10c10c08af9a38
xieguigang <xie.guigang@gcmodeller.org>
2018-11-12 09:29:54 +0800

    config for plot test

--- DATA/GCMS_quantify/GCMSJson.vb
+++ DATA/GCMS_quantify/GCMSJson.vb
@@ -1,14 +1,15 @@
-﻿Imports Microsoft.VisualBasic.MIME.application.netCDF.Components
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports SMRUCC.MassSpectrum.Math
 Imports SMRUCC.MassSpectrum.Math.Chromatogram
 
 Public Class GCMSJson
 
+    Public Property name As String
     Public Property times As Double()
     Public Property tic As Double()
     Public Property ms As ms1_scan()()
 
-    Public Function GetTIC() As IEnumerable(Of ChromatogramTick)
+    Public Function GetTIC() As NamedCollection(Of ChromatogramTick)
 
     End Function
 End Class
---
+++ visual/test/GCMS.vb
@@ -0,0 +1,14 @@
+﻿Imports GCMS_quantify
+Imports Microsoft.VisualBasic.Imaging
+Imports SMRUCC.MassSpectrum.Visualization
+
+Module GCMS
+
+    Sub Main()
+        Dim gcData = QuantifyAnalysis.ReadData("D:\smartnucl_integrative\biodeepDB\smartnucl_integrative\16s_contents\SCFA\scfa200ppmAIAEXPRT.AIA\200ppm-未处理.CDF")
+        Dim tic = {gcData.GetTIC}
+
+        Call tic.TICplot().AsGDIImage.SaveAs("./test_gcms_ticplot.png")
+
+    End Sub
+End Module
