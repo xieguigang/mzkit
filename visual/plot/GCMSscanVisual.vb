a4a475b930b4c464e7193138df0f525d347d589f
xieguigang <xie.guigang@gcmodeller.org>
2018-11-13 15:59:11 +0800

    gcms scan mode viz

---
+++ visual/plot/GCMSscanVisual.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Runtime.CompilerServices
+Imports GCMS_quantify
+Imports Microsoft.VisualBasic.Imaging.Driver
+
+Public Module GCMSscanVisual
+
+    <Extension>
+    Public Function PlotScans(data As GCMSJson) As GraphicsData
+
+    End Function
+End Module
