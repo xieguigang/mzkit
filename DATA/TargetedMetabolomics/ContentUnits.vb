20b8bcc7b4e5aa98d7d73031603372664e9655c6
xieguigang <xie.guigang@gcmodeller.org>
2018-11-15 10:15:21 +0800

    content value parser

--- DATA/TargetedMetabolomics/ContentUnits.vb
+++ DATA/TargetedMetabolomics/ContentUnits.vb
@@ -1,4 +1,6 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.Ranges
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
+Imports r = System.Text.RegularExpressions.Regex
 
 Public Enum ContentUnits As Integer
     ppm = ppb * 1000
@@ -10,5 +12,17 @@ Public Module UnitExtensions
 
     Public Function ppm2ppb(ppm As Double) As Double
         Return ppm.Unit(ContentUnits.ppm).ScaleTo(ContentUnits.ppb).Value
+    End Function
+
+    Const contentPattern$ = NumericPattern & "\s*pp(m|b|t)"
+
+    <Extension>
+    Public Function ParseContent(text As String) As UnitValue(Of ContentUnits)
+        text = LCase(r.Match(text, contentPattern, RegexICSng).Value)
+
+        Return New UnitValue(Of ContentUnits) With {
+            .Unit = [Enum].Parse(GetType(ContentUnits), r.Match(text, "pp(m|b|t)", RegexICSng).Value),
+            .Value = Val(text)
+        }
     End Function
 End Module
