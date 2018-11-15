39f0d53fbc6965fc2d4a1a8ab6bde3a4768ced62
xieguigang <xie.guigang@gcmodeller.org>
2018-11-15 15:52:14 +0800

    create standard curve linear regression model for GCMS target

--- DATA/TargetedMetabolomics/GCMS/StandardCurve.vb
+++ DATA/TargetedMetabolomics/GCMS/StandardCurve.vb
@@ -1,12 +1,24 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.Bootstrapping
 
 Namespace GCMS
 
     Public Module StandardCurve
 
         <Extension>
-        Public Function LinearRegression(raw As (content#, data As ROITable)())
-
+        Public Function LinearRegression(raw As (content#, data As ROITable)()) As WeightedFit
+            Dim line As PointF() = raw _
+                .OrderBy(Function(p)
+                             ' 从小到大进行排序
+                             Return p.content
+                         End Function) _
+                .Select(Function(p)
+                            Return New PointF(p.data.integration, p.content)
+                        End Function) _
+                .ToArray
+            Dim model As WeightedFit = FitModel.CreateLinearRegression(line)
+            Return model
         End Function
     End Module
 End Namespace
