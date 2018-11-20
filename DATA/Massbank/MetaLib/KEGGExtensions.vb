889d23ad4b8fa0ed9dc9e007d8354aa4734e5ba2
xieguigang <xie.guigang@gcmodeller.org>
2018-11-20 16:42:28 +0800

    kegg id conversion

---
+++ DATA/Massbank/MetaLib/KEGGExtensions.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic.Language.UnixBash
+Imports SMRUCC.genomics.Assembly.KEGG
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.Assembly.KEGG.Medical
+
+Namespace MetaLib
+
+    Public Module KEGGExtensions
+
+        ''' <summary>
+        ''' 将KEGG数据库之中的药物编号以及Glyan物质的编号转换为Compound编号
+        ''' </summary>
+        ''' <param name="keggDrug">
+        ''' KEGG的药物数据库的文件路径
+        ''' </param>
+        ''' <param name="KEGGcpd">
+        ''' KEGG的代谢物数据库的文件夹路径，在这个文件夹里面应该包含有Glyan物质的数据
+        ''' </param>
+        ''' <returns></returns>
+        Public Function KEGGDrugGlyan2Compound(keggDrug$, KEGGcpd$) As Dictionary(Of String, String)
+            Dim drugs = DrugParser.ParseStream(keggDrug).ToArray
+            Dim idMaps As New Dictionary(Of String, String)
+
+            Call "Convert drug id to compound id...".__DEBUG_ECHO
+
+            For Each d As Medical.Drug In drugs
+                Dim CId As String() = d.CompoundID
+
+                If Not CId.IsNullOrEmpty Then
+                    idMaps(d.Entry) = CId.First
+                End If
+            Next
+
+            Dim gl = (ls - l - r - "*.Xml" <= KEGGcpd) _
+                .Where(Function(f) f.BaseName.IsPattern("G\d+")) _
+                .Select(AddressOf LoadXml(Of Glycan))
+
+            Call "Scan glycan and convert glucan id to compound id...".__DEBUG_ECHO
+
+            For Each glycan As Glycan In gl
+                Dim CId = glycan.CompoundId
+
+                If Not CId.IsNullOrEmpty Then
+                    idMaps(glycan.Entry) = CId.First
+                End If
+            Next
+
+            Return idMaps
+        End Function
+    End Module
+End Namespace
