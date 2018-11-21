59786b73b6a2e87161e46445c5d1cb067c6939c7
xieguigang <xie.guigang@gcmodeller.org>
2018-11-21 11:25:10 +0800

    using as tuple extensions

--- DATA/Massbank/MetaLib/KEGGExtensions.vb
+++ DATA/Massbank/MetaLib/KEGGExtensions.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.Language.UnixBash
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language.UnixBash
 Imports SMRUCC.genomics.Assembly.KEGG
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports SMRUCC.genomics.Assembly.KEGG.Medical
@@ -10,15 +11,16 @@ Namespace MetaLib
         ''' <summary>
         ''' 将KEGG数据库之中的药物编号以及Glyan物质的编号转换为Compound编号
         ''' </summary>
-        ''' <param name="keggDrug">
-        ''' KEGG的药物数据库的文件路径
-        ''' </param>
-        ''' <param name="KEGGcpd">
-        ''' KEGG的代谢物数据库的文件夹路径，在这个文件夹里面应该包含有Glyan物质的数据
+        ''' <param name="input">
+        ''' 包含有两个字段的tuple数据：
+        ''' 
+        ''' + keggDrug KEGG的药物数据库的文件路径
+        ''' + KEGGcpd KEGG的代谢物数据库的文件夹路径，在这个文件夹里面应该包含有Glyan物质的数据
         ''' </param>
         ''' <returns></returns>
-        Public Function KEGGDrugGlyan2Compound(keggDrug$, KEGGcpd$) As Dictionary(Of String, String)
-            Dim drugs = DrugParser.ParseStream(keggDrug).ToArray
+        <Extension>
+        Public Function KEGGDrugGlyan2Compound(input As (keggDrug$, KEGGcpd$)) As Dictionary(Of String, String)
+            Dim drugs = DrugParser.ParseStream(input.keggDrug).ToArray
             Dim idMaps As New Dictionary(Of String, String)
 
             Call "Convert drug id to compound id...".__DEBUG_ECHO
@@ -31,7 +33,7 @@ Namespace MetaLib
                 End If
             Next
 
-            Dim gl = (ls - l - r - "*.Xml" <= KEGGcpd) _
+            Dim gl = (ls - l - r - "*.Xml" <= input.KEGGcpd) _
                 .Where(Function(f) f.BaseName.IsPattern("G\d+")) _
                 .Select(AddressOf LoadXml(Of Glycan))
 
