28fbbbf63476f3fcaab39b113c0c67d4f255b779
xieguigang <xie.guigang@gcmodeller.org>
2019-02-21 10:48:33 +0800

    query pubchem by CAS id

---
+++ DATA/Massbank/Public/NCBI/PubChem/Web/Query.vb
@@ -0,0 +1,39 @@
+﻿Imports Microsoft.VisualBasic.Language.C
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace NCBI.PubChem
+
+    Public Module Query
+
+        ''' <summary>
+        ''' Search pubchem by CAS
+        ''' </summary>
+        Const queryCAS_URL As String = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/%s/cids/JSON"
+
+        Public Function QueryCAS(CAS As String) As IdentifierList
+            Dim url As String = sprintf(queryCAS_URL, CAS)
+            Dim list As IdentifierList = url.GET.LoadJSON(Of QueryResponse)?.IdentifierList
+
+            Return list
+        End Function
+
+    End Module
+
+    Public Class QueryResponse
+
+        Public Property IdentifierList As IdentifierList
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+
+    Public Class IdentifierList
+
+        Public Property CID As String()
+
+        Public Overrides Function ToString() As String
+            Return CID.GetJson
+        End Function
+    End Class
+End Namespace
