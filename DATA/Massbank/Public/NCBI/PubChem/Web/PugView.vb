e268c49520e603350d5e195779ce3fe35b667471
xieguigang <xie.guigang@gcmodeller.org>
2019-02-26 14:16:34 +0800

    create general meta info??

--- DATA/Massbank/Public/NCBI/PubChem/Web/PugView.vb
+++ DATA/Massbank/Public/NCBI/PubChem/Web/PugView.vb
@@ -11,9 +11,20 @@ Namespace NCBI.PubChem
 
         <XmlElement("Section")>
         Public Property Sections As Section()
+            Get
+                Return sectionTable.Values.ToArray
+            End Get
+            Set(value As Section())
+                sectionTable = value.ToDictionary(Function(sec) sec.TOCHeading)
+            End Set
+        End Property
+
+        Dim sectionTable As Dictionary(Of String, Section)
 
         Public Function GetMetaInfo() As MetaInfo
+            Dim identifier = sectionTable("Names and Identifiers")
 
+            Return New MetaInfo ' With {}
         End Function
 
         Public Overrides Function ToString() As String
