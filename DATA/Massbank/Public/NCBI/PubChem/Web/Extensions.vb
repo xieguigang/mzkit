209cd12fff18b91309ef197de74470dda799b850
xieguigang <xie.guigang@gcmodeller.org>
2019-02-28 09:06:12 +0800

    add member query extensions

---
+++ DATA/Massbank/Public/NCBI/PubChem/Web/Extensions.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
+
+Namespace NCBI.PubChem
+
+    Public Module Extensions
+
+        <Extension>
+        Public Function GetInformationString(section As Section, key$) As String
+            If section Is Nothing Then
+                Return ""
+            Else
+                Return section _
+                    .Information _
+                    .SafeQuery _
+                    .FirstOrDefault(Function(i) i.Name = key) _
+                   ?.StringValue
+            End If
+        End Function
+    End Module
+End Namespace
--- DATA/Massbank/Public/NCBI/PubChem/Web/PugView.vb
+++ DATA/Massbank/Public/NCBI/PubChem/Web/PugView.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Linq
 Imports MetaInfo = SMRUCC.MassSpectrum.DATA.MetaLib.MetaLib
 
 Namespace NCBI.PubChem
@@ -25,9 +26,23 @@ Namespace NCBI.PubChem
         Dim sectionTable As Dictionary(Of String, Section)
 
         Public Function GetMetaInfo() As MetaInfo
-            Dim identifier = sectionTable("Names and Identifiers")
+            Dim identifier = sectionTable.TryGetValue("Names and Identifiers")
+            Dim formula = sectionTable.TryGetValue("Molecular Formula")
+            Dim SMILES = sectionTable.TryGetValue("Canonical SMILES")
+            Dim InChIKey = sectionTable.TryGetValue("InChI Key")
+            Dim InChI = sectionTable.TryGetValue("InChI")
+            Dim xref As New MetaLib.xref With {
+                .InChI = InChI.GetInformationString("InChI")
+            }
 
-            Return New MetaInfo ' With {}
+            Return New MetaInfo With {
+                .formula = formula.GetInformationString("Molecular Formula"),
+                .xref = xref,
+                .name = identifier _
+                    .Sections _
+                    .FirstOrDefault(Function(s) s.TOCHeading = "Record Title") _
+                    .GetInformationString("Record Title")
+            }
         End Function
 
         Public Overrides Function ToString() As String
