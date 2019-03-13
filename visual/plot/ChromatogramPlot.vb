cc2efa67b3f6c7ee20ce815d778d554ac9ac9f7c
xieguigang <xie.guigang@gcmodeller.org>
2019-03-13 14:12:47 +0800

    add tic plot interface for single chromgram

--- visual/plot/ChromatogramPlot.vb
+++ visual/plot/ChromatogramPlot.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::9444440c33f6f354a7255e943cabc051, plot\ChromatogramPlot.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module ChromatogramPlot
-    ' 
-    '     Function: MRMChromatogramPlot, TICplot
-    ' 
-    ' /********************************************************************************/
+' Module ChromatogramPlot
+' 
+'     Function: MRMChromatogramPlot, TICplot
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -100,6 +100,49 @@ Public Module ChromatogramPlot
             margin:=margin,
             penStyle:=penStyle,
             fillCurve:=False
+        )
+    End Function
+
+    ''' <summary>
+    ''' 从mzML文件之中解析出色谱数据之后，将所有的色谱峰都绘制在一张图之中进行可视化
+    ''' </summary>
+    ''' <param name="size$"></param>
+    ''' <param name="margin$"></param>
+    ''' <param name="bg$"></param>
+    ''' <returns></returns>
+    ''' 
+    <Extension>
+    Public Function TICplot(ionData As NamedCollection(Of ChromatogramTick),
+                            Optional size$ = "1600,1000",
+                            Optional margin$ = g.DefaultLargerPadding,
+                            Optional bg$ = "white",
+                            Optional colorsSchema$ = "scibasic.category31()",
+                            Optional penStyle$ = Stroke.ScatterLineStroke,
+                            Optional labelFontStyle$ = CSSFont.Win7Normal,
+                            Optional labelConnectorStroke$ = Stroke.StrongHighlightStroke,
+                            Optional labelTicks% = 500,
+                            Optional showLabels As Boolean = True,
+                            Optional fillCurve As Boolean = True,
+                            Optional axisLabelFont$ = CSSFont.Win7Large,
+                            Optional axisTickFont$ = CSSFont.Win10NormalLarger,
+                            Optional showLegends As Boolean = True,
+                            Optional legendFontCSS$ = CSSFont.Win10Normal) As GraphicsData
+
+        Return {ionData}.TICplot(
+            size:=size,
+            axisLabelFont:=axisLabelFont,
+            axisTickFont:=axisTickFont,
+            bg:=bg,
+            penStyle:=penStyle,
+            labelFontStyle:=labelFontStyle,
+            labelConnectorStroke:=labelConnectorStroke,
+            colorsSchema:=colorsSchema,
+            margin:=margin,
+            labelTicks:=labelTicks,
+            showLabels:=showLabels,
+            showLegends:=showLegends,
+            fillCurve:=fillCurve,
+            legendFontCSS:=legendFontCSS
         )
     End Function
 
