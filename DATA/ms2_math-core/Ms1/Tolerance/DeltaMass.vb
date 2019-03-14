1e213f9feef76e894ab0de5a0833c856ff7f29f4
xieguigang <xie.guigang@gcmodeller.org>
2019-03-14 11:13:57 +0800

    split source file

---
+++ DATA/ms2_math-core/Ms1/Tolerance/DeltaMass.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Runtime.CompilerServices
+Imports sys = System.Math
+
+Namespace Ms1
+
+    ''' <summary>
+    ''' Mass tolerance in delta mass error
+    ''' </summary>
+    Public Class DAmethod : Inherits Tolerance
+
+        Public Property da As Double
+
+        Sub New(Optional da# = 0.3)
+            Me.da = da
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function Assert(mz1 As Double, mz2 As Double) As Boolean
+            Return sys.Abs(mz1 - mz2) <= da
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function AsScore(mz1 As Double, mz2 As Double) As Double
+            Return 1 - (sys.Abs(mz1 - mz2) / da)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function MassError(mz1 As Double, mz2 As Double) As Double
+            Return sys.Abs(mz1 - mz2)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return $"|mz1 - mz2| <= {da}"
+        End Function
+    End Class
+End Namespace
---
+++ DATA/ms2_math-core/Ms1/Tolerance/PPM.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Runtime.CompilerServices
+Imports sys = System.Math
+
+Namespace Ms1
+
+    ''' <summary>
+    ''' PPM tolerance calculator
+    ''' </summary>
+    Public Class PPMmethod : Inherits Tolerance
+
+        Public Property ppmValue As Double
+
+        Sub New()
+        End Sub
+
+        Sub New(ppm#)
+            ppmValue = ppm
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"ppm(mz1, mz2) <= {ppmValue}"
+        End Function
+
+        ''' <summary>
+        ''' 分子量差值
+        ''' </summary>
+        ''' <param name="measured#"></param>
+        ''' <param name="actualValue#"></param>
+        ''' <returns></returns>
+        Public Overloads Shared Function ppm(measured#, actualValue#) As Double
+            ' （测量值-实际分子量）/ 实际分子量
+            ' |(实验数据 - 数据库结果)| / 实验数据 * 1000000
+            Dim ppmd# = sys.Abs(measured - actualValue) / actualValue
+            ppmd = ppmd * 1000000
+            Return ppmd
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function Assert(mz1 As Double, mz2 As Double) As Boolean
+            Return ppm(mz1, mz2) <= ppmValue
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function AsScore(mz1 As Double, mz2 As Double) As Double
+            Return 1 - (ppm(mz1, mz2) / ppmValue)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function MassError(mz1 As Double, mz2 As Double) As Double
+            Return ppm(mz1, mz2)
+        End Function
+    End Class
+End Namespace
