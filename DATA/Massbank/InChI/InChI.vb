e7871a661b1559f0173f93b6256d038afa5e29d9
xieguigang <xie.guigang@gcmodeller.org>
2019-03-22 08:37:46 +0800

    add inchi model objects

---
+++ DATA/Massbank/InChI/InChI.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
+
+''' <summary>
+''' The InChI identifier parser
+''' 
+''' 国际化合物标识（英语：InChI，英语：International Chemical Identifier）是由国际纯粹与
+''' 应用化学联合会和美国国家标准技术研究所（National Institute of Standards and Technology，NIST）
+''' 联合制定的，用以唯一标识化合物IUPAC名称的字符串。
+''' </summary>
+<ContentType("chemical/x-inchi")> Public Class InChI
+
+    Sub New(inchi As String)
+
+    End Sub
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Shared Function Parse(inchi As String) As InChI
+        Return New InChI(inchi)
+    End Function
+End Class
+
+
---
+++ DATA/Massbank/InChI/Layers.vb
@@ -0,0 +1,49 @@
+﻿Namespace Layers
+
+    Public MustInherit Class Layer
+
+    End Class
+
+    ''' <summary>
+    ''' 主层（main layer）：以``1``表示
+    ''' </summary>
+    Public Class MainLayer : Inherits Layer
+
+    End Class
+
+    ''' <summary>
+    ''' 电荷层（charge layer）：以``q``表示
+    ''' </summary>
+    Public Class ChargeLayer : Inherits Layer
+
+    End Class
+
+    ''' <summary>
+    ''' 立体化学层（Stereochemical layer）：以``t``，``m``，``s``表示
+    ''' </summary>
+    Public Class StereochemicalLayer : Inherits Layer
+
+    End Class
+
+    ''' <summary>
+    ''' 异构体层（Isotopic layer）：以``i``表示
+    ''' </summary>
+    Public Class IsotopicLayer : Inherits Layer
+
+    End Class
+
+    ''' <summary>
+    ''' 固定氢原子（Fixed-H layer）：以``f``表示
+    ''' </summary>
+    Public Class FixedHLayer : Inherits Layer
+
+    End Class
+
+    ''' <summary>
+    ''' 再连接层（Reconnected Layer）：以``r``表示
+    ''' </summary>
+    Public Class ReconnectedLayer : Inherits Layer
+
+    End Class
+
+End Namespace
