1d7a43c046663c7c424023d9b7356266d341688e
xieguigang <xie.guigang@gcmodeller.org>
2019-03-25 10:06:58 +0800

    add spectra tree creator

---
+++ Assembly/assembly/Class1.vb
@@ -0,0 +1,27 @@
+﻿Imports SMRUCC.MassSpectrum.Assembly.MarkupData
+Imports SMRUCC.MassSpectrum.Math
+
+Public Class SpectrumTree : Inherits Spectra.SpectrumTree
+
+    Sub New(showReport As Boolean)
+        Call MyBase.New(showReport)
+    End Sub
+
+    Public Overloads Function doCluster(ms2list As mzXML.scan(), fileName$) As SpectrumTree
+        Return doCluster(ms2list _
+            .Select(Function(scan) scan.ScanData(fileName)) _
+            .OrderBy(Function(ms) ms.rt) _
+            .ToArray
+        )
+    End Function
+
+    Public Shared Function CreateTreeFromFile(file As String) As SpectrumTree
+        Dim allMs2Scans = mzXML.XML _
+            .LoadScans(file) _
+            .Where(Function(s) s.msLevel = "2") _
+            .OrderBy(Function(mz) mz.precursorMz) _
+            .ToArray
+
+        Return New SpectrumTree(True).doCluster(allMs2Scans, file.FileName)
+    End Function
+End Class
--- DATA/ms2_math-core/Spectra/SpectrumTree.vb
+++ DATA/ms2_math-core/Spectra/SpectrumTree.vb
@@ -89,24 +89,5 @@ Namespace Spectra
 
             Return Me
         End Function
-
-        Public Function doCluster(ms2list As mzXML.scan(), fileName$) As SpectrumTree
-            Return doCluster(ms2list _
-                .Select(Function(scan) scan.ScanData(fileName)) _
-                .OrderBy(Function(ms) ms.rt) _
-                .ToArray
-            )
-        End Function
-
-        Public Shared Function CreateTreeFromFile(file As String) As SpectrumTree
-            Dim allMs2Scans = mzXML.XML _
-                .LoadScans(file) _
-                .Where(Function(s) s.msLevel = "2") _
-                .OrderBy(Function(mz) mz.precursorMz) _
-                .ToArray
-
-            Return New SpectrumTree(True).doCluster(allMs2Scans, file.FileName)
-        End Function
-
     End Class
 End Namespace
