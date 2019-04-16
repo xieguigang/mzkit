5339a4c79b712a1a93475d82a8df7a1ef805279a
xieguigang <xie.guigang@gcmodeller.org>
2019-04-16 15:16:03 +0800

    add properties name

--- DATA/Massbank/SDF/ChemicalProperties.vb
+++ DATA/Massbank/SDF/ChemicalProperties.vb
@@ -2,5 +2,30 @@
 
     Public Class ChemicalProperties
 
+        ''' <summary>
+        ''' Computed Octanol/Water Partition Coefficient
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property XLogP3 As Double
+        ''' <summary>
+        ''' Hydrogen Bond Donor Count
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property HydrogenDonors As Integer
+        ''' <summary>
+        ''' Hydrogen Bond Acceptor count
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property HydrogenAcceptor As Integer
+        ''' <summary>
+        ''' Rotatable Bond Count
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property RotatableBonds As Integer
+        Public Property ExactMass As Double
+        Public Property TopologicalPolarSurfaceArea As Double
+        Public Property HeavyAtoms As Integer
+        Public Property FormalCharge As Integer
+        Public Property Complexity As Integer
     End Class
 End Namespace
