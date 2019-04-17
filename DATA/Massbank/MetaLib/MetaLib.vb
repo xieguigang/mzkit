e8cbf0c09a4b6372d814c06575ce0e9475d7afc3
xieguigang <xie.guigang@gcmodeller.org>
2019-04-17 09:35:02 +0800

    improvements of the db xref model

--- DATA/Massbank/MetaLib/MetaLib.vb
+++ DATA/Massbank/MetaLib/MetaLib.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::7777eee1dadb44a5628987be258c9bb9, Massbank\MetaLib\MetaLib.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class MetaInfo
-    ' 
-    '         Properties: formula, ID, mass, name
-    ' 
-    '     Class MetaLib
-    ' 
-    '         Properties: biofluid_locations, compound_class, pathways, tissue_locations, xref
-    ' 
-    '         Function: Equals, ParseInteger, ToString
-    ' 
-    '     Class xref
-    ' 
-    '         Properties: CAS, chebi, HMDB, InChI, InChIkey
-    '                     KEGG, metlin, pubchem
-    ' 
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class MetaInfo
+' 
+'         Properties: formula, ID, mass, name
+' 
+'     Class MetaLib
+' 
+'         Properties: biofluid_locations, compound_class, pathways, tissue_locations, xref
+' 
+'         Function: Equals, ParseInteger, ToString
+' 
+'     Class xref
+' 
+'         Properties: CAS, chebi, HMDB, InChI, InChIkey
+'                     KEGG, metlin, pubchem
+' 
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -178,7 +178,7 @@ Namespace MetaLib
             Call compareInteger(xref.pubchem, other.xref.pubchem)
             Call compareInteger(xref.HMDB, other.xref.HMDB)
 
-            If xref.CAS = other.xref.CAS Then
+            If xref.CAS.Any(Function(id) other.xref.CAS.IndexOf(id) > -1) Then
                 yes()
             Else
                 no()
@@ -211,6 +211,9 @@ Namespace MetaLib
         End Function
     End Class
 
+    ''' <summary>
+    ''' 对某一个物质在数据库之间的相互引用编号
+    ''' </summary>
     Public Class xref
 
         ''' <summary>
@@ -222,9 +225,29 @@ Namespace MetaLib
         Public Property pubchem As String
         Public Property HMDB As String
         Public Property metlin As String
-        Public Property CAS As String
+        ''' <summary>
+        ''' Multiple CAS id may exists
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property CAS As String()
         Public Property InChIkey As String
         Public Property InChI As String
+
+        Public Shared Function IsChEBI(synonym As String) As Boolean
+            Return synonym.IsPattern("CHEBI[:]\d+", RegexICSng)
+        End Function
+
+        Public Shared Function IsCAS(synonym As String) As Boolean
+            Return synonym.IsPattern("\d+([-]\d+)+", RegexICSng)
+        End Function
+
+        Public Shared Function IsHMDB(synonym As String) As Boolean
+            Return synonym.IsPattern("HMDB\d+", RegexICSng)
+        End Function
+
+        Public Shared Function IsKEGG(synonym As String) As Boolean
+            Return synonym.IsPattern("C((\d){5})", RegexICSng)
+        End Function
 
         Public Overrides Function ToString() As String
             Return Me.GetJson
