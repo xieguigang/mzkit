b197d5ec8d6745101667f5fbb95901f4fe9aa687
xieguigang <xie.guigang@gcmodeller.org>
2019-04-17 14:24:20 +0800

    add progress display

--- DATA/pubchem/Program.vb
+++ DATA/pubchem/Program.vb
@@ -1,5 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text.Xml.Linq
 Imports SMRUCC.MassSpectrum.DATA.MetaLib
 Imports SMRUCC.MassSpectrum.DATA.NCBI.PubChem
@@ -17,8 +18,15 @@ Module Program
         Dim out$ = args("/out") Or $"{[in].TrimSuffix}.metlib.Xml"
 
         Using dataset As New DataSetWriter(Of MetaLib)(out)
+            Dim i As VBInteger = 0
+
             For Each meta As MetaLib In CIDSynonym.LoadMetaInfo([in])
                 Call dataset.Write(meta)
+
+                If ++i Mod 100 = 0 Then
+                    Call Console.Write(i)
+                    Call Console.Write(vbTab)
+                End If
             Next
         End Using
 
