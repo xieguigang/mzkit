10dca4b23fc261f42235d816b6c4b5d2236c02c1
xieguigang <xie.guigang@gcmodeller.org>
2019-04-17 09:39:15 +0800

    yield database id

--- DATA/Massbank/Public/NCBI/PubChem/CIDSynonym.vb
+++ DATA/Massbank/Public/NCBI/PubChem/CIDSynonym.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.Text
+Imports SMRUCC.MassSpectrum.DATA.MetaLib
 
 Namespace NCBI.PubChem
 
@@ -19,25 +20,25 @@ Namespace NCBI.PubChem
 
         Public ReadOnly Property IsChEBI As Boolean
             Get
-                Return Synonym.IsPattern("CHEBI[:]\d+", RegexICSng)
+                Return xref.IsChEBI(Synonym)
             End Get
         End Property
 
         Public ReadOnly Property IsCAS As Boolean
             Get
-                Return Synonym.IsPattern("\d+([-]\d+)+", RegexICSng)
+                Return xref.IsCAS(Synonym)
             End Get
         End Property
 
         Public ReadOnly Property IsHMDB As Boolean
             Get
-                Return Synonym.IsPattern("HMDB\d+", RegexICSng)
+                Return xref.IsHMDB(Synonym)
             End Get
         End Property
 
         Public ReadOnly Property IsKEGG As Boolean
             Get
-                Return Synonym.IsPattern("C((\d){5})", RegexICSng)
+                Return xref.IsKEGG(Synonym)
             End Get
         End Property
 
@@ -47,15 +48,35 @@ Namespace NCBI.PubChem
 
         Public Shared Iterator Function LoadNames(file As String) As IEnumerable(Of CIDSynonym)
             Dim cid$ = 0
+            Dim name As String
             Dim t As String()
 
             ' 第一个名称是权重最好的名称
             For Each line As String In file.IterateAllLines
                 t = line.Split(ASCII.TAB)
+                name = t(1)
 
                 If t(0) <> cid Then
                     ' 这个是新的物质行
                     ' 第一个名称是权重最高的名称, 直接返回
+                    Yield New CIDSynonym With {
+                        .CID = t(0),
+                        .Synonym = name
+                    }
+
+                    cid = t(0)
+                Else
+                    ' 只返回kegg/hmdb/chebi/cas编号名称
+                    If xref.IsCAS(name) OrElse
+                        xref.IsChEBI(name) OrElse
+                        xref.IsHMDB(name) OrElse
+                        xref.IsKEGG(name) Then
+
+                        Yield New CIDSynonym With {
+                            .CID = cid,
+                            .Synonym = name
+                        }
+                    End If
                 End If
             Next
         End Function
