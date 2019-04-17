59875314e68aecf9f71ebf17e968e6d16674e1b9
xieguigang <xie.guigang@gcmodeller.org>
2019-04-17 16:27:00 +0800

    chemical descriptor database in binary

--- DATA/Massbank/SDF/ChemicalDescriptor.vb
+++ DATA/Massbank/SDF/ChemicalDescriptor.vb
@@ -1,4 +1,8 @@
-﻿Namespace File
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+
+Namespace File
 
     ''' <summary>
     ''' Chemical descriptor
@@ -10,6 +14,7 @@
         ''' </summary>
         ''' <returns></returns>
         Public Property XLogP3 As Double
+        Public Property XLogP3_AA As Double
 
         ''' <summary>
         ''' Hydrogen Bond Donor Count
@@ -32,6 +37,12 @@
         Public Property FormalCharge As Integer
         Public Property Complexity As Integer
 
+        Shared ReadOnly schema As PropertyInfo() = DataFramework _
+            .Schema(Of ChemicalDescriptor)(PropertyAccess.Readable, True, True) _
+            .Values _
+            .OrderBy(Function(p) p.Name) _
+            .ToArray
+
         Sub New(data As Dictionary(Of String, String()))
             Dim read = getOne(data)
 
@@ -39,6 +50,7 @@
 
             ExactMass = Double.Parse(read("PUBCHEM_EXACT_MASS"))
             XLogP3 = Double.Parse(read("PUBCHEM_XLOGP3"))
+            XLogP3_AA = Double.Parse(read("PUBCHEM_XLOGP3_AA"))
             FormalCharge = Integer.Parse(read("PUBCHEM_TOTAL_CHARGE"))
             TopologicalPolarSurfaceArea = Double.Parse(read("PUBCHEM_CACTVS_TPSA"))
             HydrogenAcceptor = Integer.Parse(read("PUBCHEM_CACTVS_HBOND_ACCEPTOR"))
@@ -51,9 +63,48 @@
         Sub New()
         End Sub
 
+        Public Shared Function GetBytesBuffer(descript As ChemicalDescriptor) As Byte()
+            Dim bytes As New List(Of Byte)
+
+            For Each data As PropertyInfo In schema
+                Dim value = data.GetValue(descript)
+
+                If TypeOf value Is Double Then
+                    bytes += BitConverter.GetBytes(DirectCast(value, Double))
+                ElseIf TypeOf value Is Integer Then
+                    bytes += BitConverter.GetBytes(DirectCast(value, Integer))
+                Else
+                    Throw New NotImplementedException
+                End If
+            Next
+
+            Return bytes
+        End Function
+
+        Public Shared Function FromBytes(stream As Byte()) As ChemicalDescriptor
+            Dim descript As New ChemicalDescriptor
+            Dim i As Integer = 0
+            Dim value As Object
+
+            For Each data As PropertyInfo In schema
+                Select Case data.PropertyType
+                    Case GetType(Double)
+                        value = BitConverter.ToDouble(stream, i)
+                        i += 8
+                    Case GetType(Integer)
+                        value = BitConverter.ToInt32(stream, i)
+                        i += 4
+                    Case Else
+                        Throw New NotImplementedException
+                End Select
+            Next
+
+            Return descript
+        End Function
+
         Private Shared Function getOne(data As Dictionary(Of String, String())) As Func(Of String, String)
             Return Function(key)
-                       Return data.TryGetValue(key, [default]:={CStr(1 / 0)}).FirstOrDefault
+                       Return data.TryGetValue(key, [default]:={CStr(-10000000)}).FirstOrDefault
                    End Function
         End Function
     End Class
