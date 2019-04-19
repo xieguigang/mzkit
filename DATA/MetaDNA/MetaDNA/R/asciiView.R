e003700715caa4fcc9ee7a74a2280dca49d6c440
xieguigang <xie.guigang@gcmodeller.org>
2019-04-19 16:14:18 +0800

    try add labels for mz

--- DATA/MetaDNA/MetaDNA/R/asciiView.R
+++ DATA/MetaDNA/MetaDNA/R/asciiView.R
@@ -1,6 +1,16 @@
-asciiView <- function(spectra, into.cutoff = 0.05, maxWidth = 80) {
+
+#' ASCII View of Spectra Matrix
+#'
+asciiView <- function(spectra, 
+	into.cutoff = 0.05, 
+	maxWidth = 80, 
+	show.mz.labels = TRUE) {
+	
+	# Extract spectra matrix data
 	mz <- spectra[, 1] %=>% as.numeric;
+	# column steps in viewer
 	delta.mz <- (max(mz) - min(mz)) / (maxWidth - 10);
+	# cleanup matrix
 	into <- spectra[, 2] %=>% as.numeric;
 	into <- into / max(into);
 	mz <- mz[into >= into.cutoff];
@@ -10,7 +20,9 @@ asciiView <- function(spectra, into.cutoff = 0.05, maxWidth = 80) {
 	fill.1 <- rep(" ", times = offset);
 	fill.2 <- rep(" ", times = offset);
 	fill.3 <- rep(" ", times = offset);
+	fill.4 <- rep(" ", times = offset);
 	fill.axis <- rep("-", times = offset);
+	labels <- list();
 	
 	mzx <- min(mz);
 	mzl <- 0;
@@ -26,27 +38,38 @@ asciiView <- function(spectra, into.cutoff = 0.05, maxWidth = 80) {
 			fill.1 <- append(fill.1, " ");
 			fill.2 <- append(fill.2, " ");
 			fill.3 <- append(fill.3, " ");	
+			fill.4 <- append(fill.4, " ");	
 			fill.axis <- append(fill.axis, "-");			
 		} else {
 			int <- max(into[mzi]);
 			mz.i <- mz[int == into];
 			fill.axis <- append(fill.axis, "+");
+			labels[[as.character(i)]] <- mz.i;
 			
-			if (int >= 0.7) {
+			if (int >= 0.85) {
 				# fill 123				
 				fill.1 <- append(fill.1, "|");
 				fill.2 <- append(fill.2, "|");
-				fill.3 <- append(fill.3, "|");			
-			} else if (int >= 0.3) {
+				fill.3 <- append(fill.3, "|");		
+				fill.4 <- append(fill.4, "|");						
+			} else if (int >= 0.6) {
 				# fill 23							
 				fill.1 <- append(fill.1, " ");
 				fill.2 <- append(fill.2, "|");
-				fill.3 <- append(fill.3, "|");			
+				fill.3 <- append(fill.3, "|");		
+				fill.4 <- append(fill.4, "|");								
+			} else if (int >= 0.4) {
+				# fill 23							
+				fill.1 <- append(fill.1, " ");
+				fill.2 <- append(fill.2, " ");
+				fill.3 <- append(fill.3, "|");		
+				fill.4 <- append(fill.4, "|");								
 			} else {
 				# fill 3				
 				fill.1 <- append(fill.1, " ");
 				fill.2 <- append(fill.2, " ");
-				fill.3 <- append(fill.3, "|");		
+				fill.3 <- append(fill.3, " ");		
+				fill.4 <- append(fill.4, "|");				
 			}
 		}
 	}	
@@ -54,7 +77,12 @@ asciiView <- function(spectra, into.cutoff = 0.05, maxWidth = 80) {
 	a <- paste(fill.1, collapse = "");
 	b <- paste(fill.2, collapse = "");
 	c <- paste(fill.3, collapse = "");
-	d <- paste(fill.axis, collapse = "");
+	d <- paste(fill.4, collapse = "");
+	e <- paste(fill.axis, collapse = "");
 	
-	paste(c(a,b,c,d, ""), collapse = "\n");
+	views <- c("", a,b,c,d,e);
+	
+	
+	
+	paste(append(view, ""), collapse = "\n");
 }
