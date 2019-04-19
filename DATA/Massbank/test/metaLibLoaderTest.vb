bce744999bed39b9ea2fb03cb34a826893eb79cc
xieguigang <xie.guigang@gcmodeller.org>
2019-04-19 10:20:03 +0800

    loader test success

---
+++ DATA/Massbank/test/metaLibLoaderTest.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.Text.Xml.Linq
+Imports SMRUCC.MassSpectrum.DATA.MetaLib
+
+Module metaLibLoaderTest
+
+    Sub Main()
+
+        Dim libs = Data.LoadUltraLargeXMLDataSet(Of MetaLib)(path:="D:\Database\CID-Synonym-filtered\CID-Synonym-filtered.metlib_hmdb.Xml").ToArray
+
+
+        Pause()
+    End Sub
+End Module
