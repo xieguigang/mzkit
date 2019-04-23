d74f74e88020a53d7e176532865823389af76b48
xieguigang <xie.guigang@gcmodeller.org>
2019-04-23 14:02:22 +0800

    build index file for metabolite Synonym query pubchem CID

--- DATA/XrefEngine/index/CIDSynonymIndex.vb
+++ DATA/XrefEngine/index/CIDSynonymIndex.vb
@@ -1,3 +1,20 @@
-﻿Public Class CIDSynonymIndex
+﻿Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Data.IO.SearchEngine.Index
+Imports SMRUCC.MassSpectrum.DATA.NCBI.PubChem
 
+Public Class CIDSynonymIndex
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="mapFile">CID-Synonym-filtered.txt</param>
+    Public Shared Sub BuildIndex(mapFile As String, indexFile As String)
+        Using index As New TrieIndexWriter(New BinaryDataWriter(indexFile.Open(doClear:=False)))
+            For Each name As CIDSynonym In CIDSynonym.LoadNames(mapFile, filter:=False)
+                Call index.AddTerm(name.Synonym)
+            Next
+
+            Stop
+        End Using
+    End Sub
 End Class
