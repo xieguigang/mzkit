c2efeb369048ee90385ae70151f7228019d778b2
xieguigang <xie.guigang@gcmodeller.org>
2019-05-08 17:16:30 +0800

    add file loader test

---
+++ DATA/Massbank/test/oboTest.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.genomics.foundation.OBO_Foundry
+
+Module oboTest
+
+    Sub Main()
+        Dim tree = New OBOFile("D:\MassSpectrum-toolkits\DATA\DATA\ChemOnt_2_1.obo.TXT").GetRawTerms.BuildTree
+
+
+        Pause()
+    End Sub
+End Module
--- DATA/XrefEngine/ChemOntClassify.vb
+++ DATA/XrefEngine/ChemOntClassify.vb
@@ -1,6 +1,16 @@
-﻿Public Module ChemOntClassify
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.foundation.OBO_Foundry
 
-    Public Function GetTermByLevel(obo As )
+Public Module ChemOntClassify
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="obo">Tree was created by <see cref="Builder.BuildTree(IEnumerable(Of RawTerm))"/> function.</param>
+    ''' <returns></returns>
+    ''' 
+    <Extension>
+    Public Function GetTermByLevel(obo As GenericTree, level%) As GenericTree
 
     End Function
 End Module
