6a161690bc27865c7dc00eacdfebe3cb87b9ad7c
xieguigang <xie.guigang@gcmodeller.org>
2019-05-13 17:56:28 +0800

    CH3 molecule weight

--- DATA/ms2_math-core/Ms1/PrecursorType/MolWeight.vb
+++ DATA/ms2_math-core/Ms1/PrecursorType/MolWeight.vb
@@ -54,6 +54,7 @@ Namespace Ms1.PrecursorType
         ReadOnly weights As New Dictionary(Of String, Double) From {
             {"H", 1.007276},
             {"CH3COO", 59.013},
+            {"CH3", 15.032528},
             {"C", 12.0107},
             {"Na", 22.98976928},
             {"NH4", 18.035534},
