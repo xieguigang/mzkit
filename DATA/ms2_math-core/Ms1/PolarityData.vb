1711b0fa5b14edd0f8e77e7a9df747d0f24b1565
xieguigang <xie.guigang@gcmodeller.org>
2019-05-14 14:23:41 +0800

    data model convert from tuple

--- DATA/ms2_math-core/Ms1/PolarityData.vb
+++ DATA/ms2_math-core/Ms1/PolarityData.vb
@@ -7,4 +7,11 @@ Public Class PolarityData(Of T)
     Public Overrides Function ToString() As String
         Return $"(+) {positive} / (-) {negative}"
     End Function
+
+    Public Shared Widening Operator CType(tuple As (pos As T, neg As T)) As PolarityData(Of T)
+        Return New PolarityData(Of T) With {
+            .positive = tuple.pos,
+            .negative = tuple.neg
+        }
+    End Operator
 End Class
