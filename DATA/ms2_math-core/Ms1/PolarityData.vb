d0921f4f5656ec6555d9d3f3f63ba4f2b4cf720e
xieguigang <xie.guigang@gcmodeller.org>
2019-05-14 17:48:21 +0800

    access data by ion mode

--- DATA/ms2_math-core/Ms1/PolarityData.vb
+++ DATA/ms2_math-core/Ms1/PolarityData.vb
@@ -1,8 +1,27 @@
 ﻿
+Imports SMRUCC.MassSpectrum.Math.Ms1.PrecursorType
+
 Public Class PolarityData(Of T)
 
     Public Property positive As T
     Public Property negative As T
+
+    Default Public Property Item(ionMode As String) As T
+        Get
+            If Provider.ParseIonMode(ionMode) = 1 Then
+                Return positive
+            Else
+                Return negative
+            End If
+        End Get
+        Set
+            If Provider.ParseIonMode(ionMode) = 1 Then
+                positive = Value
+            Else
+                negative = Value
+            End If
+        End Set
+    End Property
 
     Public Overrides Function ToString() As String
         Return $"(+) {positive} / (-) {negative}"
