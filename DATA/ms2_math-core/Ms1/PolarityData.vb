0265664efc970aec04e40272c41c9d3c8c9af321
xieguigang <xie.guigang@gcmodeller.org>
2019-05-14 14:21:54 +0800

    add polarity tuple data model

---
+++ DATA/ms2_math-core/Ms1/PolarityData.vb
@@ -0,0 +1,10 @@
+﻿
+Public Class PolarityData(Of T)
+
+    Public Property positive As T
+    Public Property negative As T
+
+    Public Overrides Function ToString() As String
+        Return $"(+) {positive} / (-) {negative}"
+    End Function
+End Class
