ccf55fca973cf87fe887b6781e7453b72fec11c8
xieguigang <xie.guigang@gcmodeller.org>
2019-06-11 11:27:30 +0800

    get by key

--- DATA/XrefEngine/FillClass.vb
+++ DATA/XrefEngine/FillClass.vb
@@ -3,6 +3,9 @@ Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports SMRUCC.genomics.foundation.OBO_Foundry
 
+''' <summary>
+''' 因为在这里填充数据是直接进行的,所以structure而言是值类型,只能够修改函数之中的副本,所以在这个模块之中,类型参数都必须要限制为引用类型
+''' </summary>
 Public Module FillClass
 
     <Extension>
@@ -18,23 +21,35 @@ Public Module FillClass
         Next
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
     Public Function ClassyfireFillerLambda(Of cpd As {Class, INamedValue, ICompoundClass})(anno As IEnumerable(Of ClassyfireAnnotation), classifyObo As ChemOntClassify) As Func(Of cpd, cpd)
+        Return anno.ClassyfireFillerLambda(Of cpd)(classifyObo, Function(c) c.Key)
+    End Function
+
+    <Extension>
+    Public Function ClassyfireFillerLambda(Of cpd As {Class, ICompoundClass})(
+             anno As IEnumerable(Of ClassyfireAnnotation),
+             classifyObo As ChemOntClassify,
+             getKey As Func(Of cpd, String)) As Func(Of cpd, cpd)
+
         Dim annotations = anno _
-            .GroupBy(Function(a) a.CompoundID) _
-            .ToDictionary(Function(a) a.Key,
-                          Function(a)
-                              Return a.ToArray
-                          End Function)
+           .GroupBy(Function(a) a.CompoundID) _
+           .ToDictionary(Function(a) a.Key,
+                         Function(a)
+                             Return a.ToArray
+                         End Function)
         Dim kingdom As Index(Of String) = classifyObo.kingdom.TermIndex
         Dim super_class As Index(Of String) = classifyObo.superClass.TermIndex
         Dim [class] As Index(Of String) = classifyObo.class.TermIndex
         Dim sub_class As Index(Of String) = classifyObo.subClass.TermIndex
         Dim molecular_framework = classifyObo.molecularFramework.TermIndex
 
-        Return Function(compound)
-                   If annotations.ContainsKey(compound.Key) Then
-                       Dim classyfire As ClassyfireAnnotation() = annotations(compound.Key)
+        Return Function(compound) As cpd
+                   Dim compoundKey As String = getKey(compound)
+
+                   If annotations.ContainsKey(compoundKey) Then
+                       Dim classyfire As ClassyfireAnnotation() = annotations(compoundKey)
                        Dim getByLevel = Function(level As Index(Of String)) As String
                                             Return classyfire _
                                                 .FirstOrDefault(Function(a) a.ChemOntID Like level) _
