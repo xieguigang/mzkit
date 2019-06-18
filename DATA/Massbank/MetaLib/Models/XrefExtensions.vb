4b7a01d844e3d5a05aa7faf9c1728066bc14e282
xieguigang <xie.guigang@gcmodeller.org>
2019-06-18 14:28:25 +0800

    xref id format normalize extension method helper

--- DATA/Massbank/MetaLib/Models/XrefExtensions.vb
+++ DATA/Massbank/MetaLib/Models/XrefExtensions.vb
@@ -1,7 +1,34 @@
-﻿Namespace MetaLib.Models
+﻿Imports System.Runtime.CompilerServices
+
+Namespace MetaLib.Models
 
     <HideModuleName>
     Public Module XrefExtensions
 
+        <Extension>
+        Public Function FormatChEbiID(id As String) As String
+            id = id.Match("\d+")
+
+            If Val(id) <= 0 Then
+                Return ""
+            Else
+                Return $"CHEBI:{id}"
+            End If
+        End Function
+
+        <Extension>
+        Public Function FormatHMDBId(id As String) As String
+            If Not xref.IsHMDB(id) Then
+                Return ""
+            Else
+                id = id.Match("\d+").ParseInteger.ToString
+
+                If Val(id) <= 0 Then
+                    Return ""
+                Else
+                    Return $"HMDB{id.FormatZero("0000000")}"
+                End If
+            End If
+        End Function
     End Module
 End Namespace
