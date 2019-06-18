f7365e4f10cda6444c817eaab06d31873d62d668
xieguigang <xie.guigang@gcmodeller.org>
2019-06-18 15:00:33 +0800

    query test success

---
+++ DATA/Massbank/test/HMDBqueryTest.vb
@@ -0,0 +1,8 @@
+﻿Module HMDBqueryTest
+
+    Sub Main()
+        Dim metabolite = SMRUCC.MassSpectrum.DATA.TMIC.HMDB.Repository.GetMetabolite("HMDB0014118")
+
+        Pause()
+    End Sub
+End Module
