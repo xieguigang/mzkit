64231ea42b2da84d6f6da6d9729ae1fb30ea034d
xieguigang <xie.guigang@gcmodeller.org>
2019-06-18 13:32:03 +0800

    split xref model from source file

--- DATA/Massbank/MetaLib/Models/MetaLib.vb
+++ DATA/Massbank/MetaLib/Models/MetaLib.vb
@@ -58,11 +58,9 @@
 
 #End Region
 
-Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
-Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace MetaLib
 
@@ -137,65 +135,6 @@ Namespace MetaLib
             Else
                 Return MetaEquals.Equals(Me, other)
             End If
-        End Function
-    End Class
-
-    ''' <summary>
-    ''' 对某一个物质在数据库之间的相互引用编号
-    ''' </summary>
-    Public Class xref
-
-        ''' <summary>
-        ''' chebi主编号
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property chebi As String
-        Public Property KEGG As String
-        ''' <summary>
-        ''' The pubchem cid
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property pubchem As String
-        Public Property HMDB As String
-        Public Property metlin As String
-        Public Property Wikipedia As String
-        ''' <summary>
-        ''' Multiple CAS id may exists
-        ''' </summary>
-        ''' <returns></returns>
-        <XmlElement>
-        Public Property CAS As String()
-        Public Property InChIkey As String
-        Public Property InChI As String
-        Public Property SMILES As String
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Shared Function IsChEBI(synonym As String) As Boolean
-            Return synonym.IsPattern("CHEBI[:]\d+", RegexICSng)
-        End Function
-
-        ''' <summary>
-        ''' ``XXX-XXX-XXX``
-        ''' </summary>
-        ''' <param name="synonym"></param>
-        ''' <returns></returns>
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Shared Function IsCASNumber(synonym As String) As Boolean
-            Return synonym.IsPattern("\d+([-]\d+)+", RegexICSng)
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Shared Function IsHMDB(synonym As String) As Boolean
-            Return synonym.IsPattern("HMDB\d+", RegexICSng)
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Shared Function IsKEGG(synonym As String) As Boolean
-            Return synonym.IsPattern("C((\d){5})", RegexICSng)
-        End Function
-
-        Public Overrides Function ToString() As String
-            Return Me.GetJson
         End Function
     End Class
 End Namespace
---
+++ DATA/Massbank/MetaLib/Models/Xref.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace MetaLib
+
+    ''' <summary>
+    ''' 对某一个物质在数据库之间的相互引用编号
+    ''' </summary>
+    Public Class xref
+
+        ''' <summary>
+        ''' chebi主编号
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property chebi As String
+        Public Property KEGG As String
+        ''' <summary>
+        ''' The pubchem cid
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property pubchem As String
+        Public Property HMDB As String
+        Public Property metlin As String
+        Public Property Wikipedia As String
+        ''' <summary>
+        ''' Multiple CAS id may exists
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlElement>
+        Public Property CAS As String()
+        Public Property InChIkey As String
+        Public Property InChI As String
+        Public Property SMILES As String
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function IsChEBI(synonym As String) As Boolean
+            Return synonym.IsPattern("CHEBI[:]\d+", RegexICSng)
+        End Function
+
+        ''' <summary>
+        ''' ``XXX-XXX-XXX``
+        ''' </summary>
+        ''' <param name="synonym"></param>
+        ''' <returns></returns>
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function IsCASNumber(synonym As String) As Boolean
+            Return synonym.IsPattern("\d+([-]\d+)+", RegexICSng)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function IsHMDB(synonym As String) As Boolean
+            Return synonym.IsPattern("HMDB\d+", RegexICSng)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function IsKEGG(synonym As String) As Boolean
+            Return synonym.IsPattern("C((\d){5})", RegexICSng)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+End Namespace
