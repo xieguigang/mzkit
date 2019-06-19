628dcd61f5be2c5092073d8c93210e003c3ce9ea
xieguigang <xie.guigang@gcmodeller.org>
2019-06-19 10:05:04 +0800

    calculate ion charge value

--- DATA/ms2_simulator/Emulator.vb
+++ DATA/ms2_simulator/Emulator.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::a11ef99f5bdf4e2f11d3248bbb3aadaa, ms2_simulator\Emulator.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Emulator
-    ' 
-    '     Function: BreakBonds, CalculateMZ, (+2 Overloads) MolecularFragment
-    ' 
-    ' /********************************************************************************/
+' Module Emulator
+' 
+'     Function: BreakBonds, CalculateMZ, (+2 Overloads) MolecularFragment
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -130,14 +130,17 @@ Public Module Emulator
         Dim mass# = Aggregate atom As Node
                     In fragment.nodes
                     Into Sum(atom.Data.mass)
-        Dim charge#
+
+        ' 如果计算原子基团的charge电荷量？
+        Dim charge# = fragment.AtomGroupCharge
 
         Return mass / charge
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
     Public Function MolecularFragment(molecule As Model.KCF, energy As EnergyModel, Optional step% = 100) As LibraryMatrix
-        Return molecule.Graph.MolecularFragment(energy, [step])
+        Return molecule.CreateGraph.MolecularFragment(energy, [step])
     End Function
 
     ''' <summary>
@@ -148,7 +151,8 @@ Public Module Emulator
     ''' <returns></returns>
     <Extension>
     Public Function BreakBonds(molecule As NetworkGraph, energy#) As NetworkGraph
-        Dim copy As NetworkGraph = molecule.Copy  ' 需要做一次复制，因为class是按内存地址引用的，否则后面的计算都会乱的
+        ' 需要做一次复制，因为class是按内存地址引用的，否则后面的计算都会乱的
+        Dim copy As NetworkGraph = molecule.Copy
 
         ' 将键能低于能量值的边链接都删除掉
         ' 因为他们都被打断了
