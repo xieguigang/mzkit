3b46429e2a74414d309f3ba128013d69943a380c
xieguigang <xie.guigang@gcmodeller.org>
2019-06-20 17:59:44 +0800

    Update BoundEnergyFinder.vb
    get bound atom information

--- DATA/ms2_simulator/Emulator/BoundEnergyFinder.vb
+++ DATA/ms2_simulator/Emulator/BoundEnergyFinder.vb
@@ -1,11 +1,15 @@
-﻿Public Class BoundEnergyFinder
+﻿Imports BioNovoGene.BioDeep.Chemistry.Model
+
+Public Class BoundEnergyFinder
 
     ReadOnly boundTable As Dictionary(Of String, BoundEnergy)
 
     Public ReadOnly Property min As Double
 
     Sub New()
-        Dim bounds = BoundEnergy.GetEnergyTable.GroupBy(Function(b) b.bound).ToArray
+        Dim bounds = BoundEnergy.GetEnergyTable _
+            .GroupBy(Function(b) b.bound) _
+            .ToArray
         Dim table As Dictionary(Of String, BoundEnergy) = bounds _
             .ToDictionary(Function(b) b.Key,
                           Function(g)
@@ -37,6 +41,63 @@
     ''' <param name="atom2"></param>
     ''' <returns></returns>
     Public Function FindByKCFAtoms(atom1 As String, atom2 As String) As Double
+        Dim link As (f$, r$)
+        Dim n As Integer = GetNumberOfBounds(atom1, atom2)
 
+        atom1 = TranslateKCFAtom(atom1)
+        atom2 = TranslateKCFAtom(atom2)
+
+        Select Case n
+            Case 1 : link = ($"{atom1}-{atom2}", $"{atom2}-{atom1}")
+            Case 2 : link = ($"{atom1}={atom2}", $"{atom2}={atom1}")
+            Case 3 : link = ($"{atom1}≡{atom2}", $"{atom2}≡{atom1}")
+            Case Else
+                Throw New NotImplementedException
+        End Select
+
+        If boundTable.ContainsKey(link.f) Then
+            Return boundTable(link.f).H0
+        ElseIf boundTable.ContainsKey(link.r) Then
+            Return boundTable(link.r).H0
+        Else
+            Throw New KeyNotFoundException()
+        End If
+    End Function
+
+    ''' <summary>
+    ''' Get bounds number from KCF atoms
+    ''' </summary>
+    ''' <param name="atom1"></param>
+    ''' <param name="atom2"></param>
+    ''' <returns></returns>
+    Public Shared Function GetNumberOfBounds(atom1 As String, atom2 As String) As Integer
+        Throw New NotImplementedException
+    End Function
+
+    ''' <summary>
+    ''' KCF atom label => atom character
+    ''' </summary>
+    ''' <param name="atom"></param>
+    ''' <returns></returns>
+    Public Shared Function TranslateKCFAtom(atom As String) As String
+        Dim type As KegAtomType = KegAtomType.GetAtom(atom)
+
+        Select Case type.type
+            Case KegAtomType.Types.Carbon
+                Return "C"
+            Case KegAtomType.Types.Nitrogen
+                Return "N"
+            Case KegAtomType.Types.Oxygen
+                Return "O"
+            Case KegAtomType.Types.Sulfur
+                Return "S"
+            Case KegAtomType.Types.Phosphorus
+                Return "P"
+            Case KegAtomType.Types.Halogen
+                ' F/Cl/Br/I
+                Return type.formula
+            Case Else
+                Return "NA"
+        End Select
     End Function
 End Class
