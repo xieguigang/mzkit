859d3b55cf585aa18dd193e1410cd3d51b5eabda
xieguigang <xie.guigang@gcmodeller.org>
2019-06-21 10:00:50 +0800

    config projects

--- DATA/ms2_simulator/test/simulatorTest.vb
+++ DATA/ms2_simulator/test/simulatorTest.vb
@@ -1,6 +1,7 @@
 ﻿Imports BioNovoGene.BioDeep.Chemistry.Model
 Imports BioNovoGene.BioDeep.Chemistry.Model.Graph
 Imports ms2_simulator
+Imports SMRUCC.MassSpectrum.Math.Spectra
 
 Module simulatorTest
 
@@ -12,7 +13,9 @@ Module simulatorTest
         Dim energy As New EnergyModel(Function(x, y)
                                           Return x
                                       End Function, 0, 1000)
-        Dim result = molecule.MolecularFragment(energy)
+        Dim result As LibraryMatrix = molecule.MolecularFragment(energy)
+
+
 
         Pause()
     End Sub
