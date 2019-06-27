52085bce75534ba750e807ffeb9ee02459912ad4
xieguigang <xie.guigang@gcmodeller.org>
2019-06-27 11:12:11 +0800

    add conversion extensions

--- MetaDNA/MetaDNA_visual/test/Module1.vb
+++ MetaDNA/MetaDNA_visual/test/Module1.vb
@@ -1,17 +1,20 @@
 ﻿Imports MetaDNA.visual
-Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 
 Module Module1
 
     Sub Main()
 
-        Dim model = Global.MetaDNA.visual.XML.LoadDocument("D:\MassSpectrum-toolkits\MetaDNA\test\MetaDNA.Xml")
-        Dim graph = model.CreateGraph
-        Dim data = graph.Tabular
+        Call dump("D:\MassSpectrum-toolkits\MetaDNA\test\human_blood.Xml")
 
-        Call data.Save("D:\MassSpectrum-toolkits\MetaDNA\test\MetaDNA\")
+
 
         Pause()
+    End Sub
+
+    Private Sub dump(file As String)
+        Dim model = MetaDNA.visual.XML.LoadDocument(file)
+
+        Call model.TranslateAsTable.Save(file.TrimSuffix)
     End Sub
 
 End Module
