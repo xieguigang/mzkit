7aa7c1f0e0deceeedba46f7c2157efd56e29c731
xieguigang <xie.guigang@gcmodeller.org>
2019-06-27 08:58:41 +0800

    network data model xml loader test success

--- DATA/MetaDNA/MetaDNA_visual/test/Module1.vb
+++ DATA/MetaDNA/MetaDNA_visual/test/Module1.vb
@@ -2,6 +2,10 @@
 
     Sub Main()
 
+        Dim model = Global.MetaDNA.visual.XML.LoadDocument("D:\MassSpectrum-toolkits\DATA\MetaDNA\test\MetaDNA.Xml")
+
+
+        Pause()
     End Sub
 
 End Module
