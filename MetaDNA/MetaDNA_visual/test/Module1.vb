2978cb6204aee0aa927a8ae4e4acc701daa5e2bc
xieguigang <xie.guigang@gcmodeller.org>
2019-06-27 10:47:41 +0800

    save as csv table

--- MetaDNA/MetaDNA_visual/test/Module1.vb
+++ MetaDNA/MetaDNA_visual/test/Module1.vb
@@ -1,4 +1,5 @@
 ﻿Imports MetaDNA.visual
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 
 Module Module1
 
@@ -6,6 +7,9 @@ Module Module1
 
         Dim model = Global.MetaDNA.visual.XML.LoadDocument("D:\MassSpectrum-toolkits\MetaDNA\test\MetaDNA.Xml")
         Dim graph = model.CreateGraph
+        Dim data = graph.Tabular
+
+        Call data.Save("D:\MassSpectrum-toolkits\MetaDNA\test\MetaDNA\")
 
         Pause()
     End Sub
