24a0ad629a2cd40a8a64b9a0d0a61cc78d8ac055
xieguigang <xie.guigang@gcmodeller.org>
2019-07-02 15:41:36 +0800

    add network viz modules

---
+++ MetaDNA/MetaDNA_visual/viz.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Imaging.Driver
+
+Public Module viz
+
+    <Extension>
+    Public Function Draw(metaDNA As NetworkGraph) As GraphicsData
+
+    End Function
+End Module
