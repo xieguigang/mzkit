70004a87d69b6dc9894a73eaa2619f7d5ab438b6
xieguigang <xie.guigang@gcmodeller.org>
2019-07-04 14:39:35 +0800

    Update batchConvertor.vb

--- Assembly/ProteoWizard.d/test/batchConvertor.vb
+++ Assembly/ProteoWizard.d/test/batchConvertor.vb
@@ -6,6 +6,8 @@ Module batchConvertor
         Dim cli As New ProteoWizardCLI(App.GetVariable("bin"))
 
         For Each dir As String In App.CommandLine.Name.ListDirectory
+            Call dir.__INFO_ECHO
+
             For Each file As String In dir.EnumerateFiles("*.raw")
                 If Not file.ChangeSuffix("mzXML").FileLength > 1024 Then
                     Call cli.Convert2mzML(file, dir, ProteoWizardCLI.OutFileTypes.mzXML)
