3f649035fcb13a79e922539dba6fc9fa2f281856
xieguigang <xie.guigang@gcmodeller.org>
2019-07-04 12:56:46 +0800

    convertor script

---
+++ Assembly/ProteoWizard.d/test/batchConvertor.vb
@@ -0,0 +1,12 @@
+﻿Imports ProteoWizard.d
+
+Module batchConvertor
+
+    Sub Main()
+        Dim cli As New ProteoWizardCLI(App.GetVariable("bin"))
+
+        For Each file As String In App.Command.EnumerateFiles("*.raw")
+            Call cli.Convert2mzML(file, App.Command, ProteoWizardCLI.OutFileTypes.mzXML)
+        Next
+    End Sub
+End Module
