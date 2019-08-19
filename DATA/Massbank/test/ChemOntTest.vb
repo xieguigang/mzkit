084d8426fe030a3d48980052ee6ef91b637f4d16
xieguigang <xie.guigang@gcmodeller.org>
2019-08-19 16:29:09 +0800

    create classy table

--- DATA/Massbank/test/ChemOntTest.vb
+++ DATA/Massbank/test/ChemOntTest.vb
@@ -1,10 +1,15 @@
-﻿Imports SMRUCC.MassSpectrum.DATA.MetaLib
+﻿Imports Microsoft.VisualBasic.Data.csv
+Imports SMRUCC.MassSpectrum.DATA.MetaLib
 
 Public Module ChemOntTest
 
     Sub Main()
         Dim ChemOnt As New ChemOntClassify("D:\MassSpectrum-toolkits\DATA\DATA\ChemOnt_2_1.obo.TXT")
+        Dim anno = "D:\Database\CID-Synonym-filtered\class\HMDB_36_classyfire_21_annotations.csv".LoadCsv(Of ClassyfireAnnotation)
+        Dim table = ClassyfireInfoTable.PopulateMolecules(anno, ChemOnt).ToArray
 
+
+        Call table.SaveTo("D:\Database\CID-Synonym-filtered\class\HMDB_36_classyfire_21_annotations_infoTable.csv")
 
         Pause()
     End Sub
--- DATA/XrefEngine/ClassyfireInfoTable.vb
+++ DATA/XrefEngine/ClassyfireInfoTable.vb
@@ -1,4 +1,7 @@
-﻿Imports SMRUCC.MassSpectrum.DATA.MetaLib.Models
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+Imports SMRUCC.genomics.foundation.OBO_Foundry.Tree
+Imports SMRUCC.MassSpectrum.DATA.MetaLib.Models
 
 Public Class ClassyfireInfoTable : Implements ICompoundClass
 
@@ -13,9 +16,30 @@ Public Class ClassyfireInfoTable : Implements ICompoundClass
     Public Property sub_class As String Implements ICompoundClass.sub_class
     Public Property molecular_framework As String Implements ICompoundClass.molecular_framework
 
-    Public Shared Iterator Function PopulateMolecules(anno As IEnumerable(Of ClassyfireAnnotation)) As IEnumerable(Of ClassyfireInfoTable)
-        For Each compound In anno.GroupBy(Function(a) a.CompoundID)
+    Public Shared Iterator Function PopulateMolecules(anno As IEnumerable(Of ClassyfireAnnotation), chemOntClassify As ChemOntClassify) As IEnumerable(Of ClassyfireInfoTable)
+        Dim lineages As New Dictionary(Of String, NamedCollection(Of GenericTree))
 
+        For Each compound In anno.GroupBy(Function(a) a.CompoundID)
+            lineages.Clear()
+
+            For Each term In compound
+                With chemOntClassify.GetLineages(term.ChemOntID)
+                    For Each line In .Where(Function(node) node.value.Length >= 6)
+                        lineages(line.description) = line
+                    Next
+                End With
+            Next
+
+            For Each classy In lineages.Values
+                Yield New ClassyfireInfoTable With {
+                    .CompoundID = compound.Key,
+                    .kingdom = classy(1).name,
+                    .super_class = classy(2).name,
+                    .[class] = classy(3).name,
+                    .sub_class = classy(4).name,
+                    .molecular_framework = classy(5).name
+                }
+            Next
         Next
     End Function
 End Class
