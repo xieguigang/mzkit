f29e9b6fc6e4b2dbb530240037fee8040b072e30
xieguigang <xie.guigang@gcmodeller.org>
2019-08-19 17:20:26 +0800

    not sure how to unique best

--- DATA/Massbank/test/ChemOntTest.vb
+++ DATA/Massbank/test/ChemOntTest.vb
@@ -6,10 +6,10 @@ Public Module ChemOntTest
     Sub Main()
         Dim ChemOnt As New ChemOntClassify("D:\MassSpectrum-toolkits\DATA\DATA\ChemOnt_2_1.obo.TXT")
         Dim anno = "D:\Database\CID-Synonym-filtered\class\HMDB_36_classyfire_21_annotations.csv".LoadCsv(Of ClassyfireAnnotation)
-        Dim table = ClassyfireInfoTable.PopulateMolecules(anno, ChemOnt).ToArray
+        Dim table = ClassyfireInfoTable.PopulateMolecules(anno, ChemOnt).GroupBy(Function(c) c.CompoundID).Select(Function(g) g.First).ToArray
 
 
-        Call ClassyfireInfoTable.Unique(table).ToArray.SaveTo("D:\Database\CID-Synonym-filtered\class\HMDB_36_classyfire_21_annotations_infoTable.csv")
+        Call table.SaveTo("D:\Database\CID-Synonym-filtered\class\HMDB_36_classyfire_21_annotations_infoTable.csv")
 
         Pause()
     End Sub
