7e71966fd0e0727303740b933e7e96ede25c0d36
xieguigang <xie.guigang@gcmodeller.org>
2019-08-19 16:04:16 +0800

    config test

---
+++ DATA/Massbank/test/ChemOntTest.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.MassSpectrum.DATA.MetaLib
+
+Public Module ChemOntTest
+
+    Sub Main()
+        Dim ChemOnt As New ChemOntClassify("D:\MassSpectrum-toolkits\DATA\DATA\ChemOnt_2_1.obo.TXT")
+
+
+        Pause()
+    End Sub
+End Module
