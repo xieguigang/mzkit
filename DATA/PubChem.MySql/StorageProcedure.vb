6e2d59a43ef79c9fd5e4d6cbb582553eb89db6bb
xieguigang <xie.guigang@gcmodeller.org>
2019-09-04 13:32:42 +0800

    populate table data

--- DATA/PubChem.MySql/StorageProcedure.vb
+++ DATA/PubChem.MySql/StorageProcedure.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Net.Http
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Oracle.LinuxCompatibility.MySQL
 Imports SMRUCC.MassSpectrum.DATA.File
 Imports SMRUCC.MassSpectrum.DATA.NCBI.PubChem
@@ -18,6 +20,9 @@ Public Module StorageProcedure
     Private Iterator Function PopulateData(source As IEnumerable(Of SDF)) As IEnumerable(Of MySQLTable)
         For Each molecule As SDF In source
             Dim descriptor As ChemicalDescriptor = molecule.ChemicalProperties
+            Dim readStr = molecule.getOne
+            Dim readStrings = molecule.getAll
+            Dim molJSON$ = molecule.Structure.GetJson
 
             Yield New mysql.descriptor With {
                 .atom_def_stereo_count = 0,
@@ -28,6 +33,57 @@ Public Module StorageProcedure
                 .xlogp3_aa = descriptor.XLogP3_AA,
                 .tpsa = descriptor.TopologicalPolarSurfaceArea
             }
+
+            Yield New mysql.compound With {
+                .canonicalized = readStr("PUBCHEM_COMPOUND_CANONICALIZED"),
+                .cid = molecule.ID,
+                .inchi_key = readStr("RDHQFKQIGNGIED-UHFFFAOYSA-N")
+            }
+
+            Yield New mysql.IUPAC With {
+                .cas_name = readStr("PUBCHEM_IUPAC_CAS_NAME"),
+                .cid = molecule.ID,
+                .inchi = readStr("PUBCHEM_IUPAC_INCHI"),
+                .name = readStr("PUBCHEM_IUPAC_NAME"),
+                .name_markup = readStr("PUBCHEM_IUPAC_NAME_MARKUP"),
+                .openeye_name = readStr("PUBCHEM_IUPAC_OPENEYE_NAME"),
+                .systematic_name = readStr("PUBCHEM_IUPAC_SYSTEMATIC_NAME"),
+                .traditional_name = readStr("PUBCHEM_IUPAC_TRADITIONAL_NAME")
+            }
+
+            Yield New mysql.structure With {
+                .cid = molecule.ID,
+                .bond_annotations = readStrings("PUBCHEM_BONDANNOTATIONS").JoinBy(","),
+                .coordinate_type = readStrings("PUBCHEM_COORDINATE_TYPE").JoinBy(","),
+                .model_base64 = molJSON.Base64String,
+                .checksum = molJSON.MD5
+            }
         Next
+    End Function
+
+    <Extension>
+    Private Function getAll(sdf As SDF) As Func(Of String, String())
+        Dim meta As Dictionary(Of String, String()) = sdf.MetaData
+
+        Return Function(key)
+                   If meta.ContainsKey(key) Then
+                       Return meta(key)
+                   Else
+                       Return {}
+                   End If
+               End Function
+    End Function
+
+    <Extension>
+    Private Function getOne(sdf As SDF) As Func(Of String, String)
+        Dim meta As Dictionary(Of String, String()) = sdf.MetaData
+
+        Return Function(key)
+                   If meta.ContainsKey(key) Then
+                       Return meta(key)(Scan0)
+                   Else
+                       Return Nothing
+                   End If
+               End Function
     End Function
 End Module
