26f2ceffb00a63f7125fe4d2a3a447768b9ca464
xieguigang <xie.guigang@gcmodeller.org>
2019-09-04 09:03:29 +0800

    pubchem xml model

---
+++ DATA/Massbank/Public/NCBI/PubChem/PCXml/Elements.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Xml.Serialization
+
+Namespace NCBI.PubChem.PCCompound.Elements
+
+    Public Class Count
+        <XmlElement("heavy-atom")> Public Property heavy_atom As Integer
+        <XmlElement("atom-chiral")> Public Property atom_chiral As Integer
+        <XmlElement("atom-chiral-def")> Public Property atom_chiral_def As Integer
+        <XmlElement("atom-chiral-undef")> Public Property atom_chiral_undef As Integer
+        <XmlElement("bond-chiral")> Public Property bond_chiral As Integer
+        <XmlElement("bond-chiral-def")> Public Property bond_chiral_def As Integer
+        <XmlElement("bond-chiral-undef")> Public Property bond_chiral_undef As Integer
+        <XmlElement("isotope-atom")> Public Property isotope_atom As Integer
+        <XmlElement("covalent-unit")> Public Property covalent_unit As Integer
+        <XmlElement("tautomers")> Public Property tautomers As Integer
+    End Class
+
+    Public Class CompoundType
+        Public Property id As id
+    End Class
+
+    Public Class id
+        Public Property cid As String
+    End Class
+End Namespace
---
+++ DATA/Massbank/Public/NCBI/PubChem/PCXml/PC-Compound.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Xml.Serialization
+Imports SMRUCC.MassSpectrum.DATA.NCBI.PubChem.PCCompound.Elements
+
+Namespace NCBI.PubChem.PCCompound
+
+    <XmlType("PC-Compound")> Public Class Compound
+
+        Public Property id As PC.id
+        Public Property count As PC.count
+
+        Public Shared Function LoadFromXml(xml As String) As Compound
+            xml = xml.Replace("PC-Compound_", "")
+            xml = xml.Replace("PC-Count_", "")
+            xml = xml.Replace("PC-CompoundType_", "")
+
+            Return xml.LoadFromXml(Of Compound)
+        End Function
+    End Class
+
+    Namespace PC
+
+        Public Class count
+            <XmlElement("PC-Count")>
+            Public Property Count As Elements.Count
+        End Class
+
+        Public Class id
+            <XmlElement("PC-CompoundType")>
+            Public Property Type As Elements.CompoundType
+        End Class
+    End Namespace
+
+End Namespace
