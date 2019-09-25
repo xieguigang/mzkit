e692f213a8a2a1c8636b6499e1dbd0fa665818d8
xieguigang <xie.guigang@gcmodeller.org>
2019-09-25 16:04:12 +0800

    create ML samples

--- DATA/ms2_simulator/FragmentSamples.vb
+++ DATA/ms2_simulator/FragmentSamples.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.BinaryTree
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.MachineLearning.StoreProcedure
+Imports Microsoft.VisualBasic.Math.Quantile
 Imports SMRUCC.MassSpectrum.Math.Ms1
 Imports SMRUCC.MassSpectrum.Math.Spectra
 
@@ -13,17 +16,7 @@ Public Module FragmentSamples
     ''' <returns></returns>
     <Extension>
     Public Function GetFragmentsMz(ms2 As IEnumerable(Of LibraryMatrix), Optional tolerance As Tolerance = Nothing) As Double()
-        Dim offset As Func(Of Double, Double, Boolean) = AddressOf (tolerance Or ppm20).Assert
-        Dim productMz As New NaiveBinaryTree(Of Double, Double)(
-            Function(x, y)
-                If offset(x, y) Then
-                    Return 0
-                ElseIf x > y Then
-                    Return 1
-                Else
-                    Return -1
-                End If
-            End Function)
+        Dim productMz As NaiveBinaryTree(Of Double, Double) = (tolerance Or ppm20).productTree
 
         For Each ion As LibraryMatrix In ms2
             For Each mz As ms2 In ion
@@ -37,4 +30,66 @@ Public Module FragmentSamples
             .ToArray
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
+    Private Function productTree(tolerance As Tolerance) As NaiveBinaryTree(Of Double, Double)
+        Return New NaiveBinaryTree(Of Double, Double)(
+            Function(x, y)
+                If tolerance.Assert(x, y) Then
+                    Return 0
+                ElseIf x > y Then
+                    Return 1
+                Else
+                    Return -1
+                End If
+            End Function)
+    End Function
+
+    ''' <summary>
+    ''' Create samples data for ML training or rt evaluation
+    ''' </summary>
+    ''' <param name="ms2"></param>
+    ''' <param name="fragments#"></param>
+    ''' <param name="tolerance"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Iterator Function RtSamples(ms2 As IEnumerable(Of PeakMs2), fragments#(), Optional tolerance As Tolerance = Nothing) As IEnumerable(Of Sample)
+        Dim rt#
+        ' parent/mz, product/mz intensity
+        Dim mzSample As New List(Of Double)
+        Dim equals As Func(Of Double, Double, Boolean) = AddressOf (tolerance Or ppm20).Assert
+        Dim into As ms2()
+
+        fragments = fragments _
+            .OrderBy(Function(x) x) _
+            .ToArray
+
+        For Each matrix As PeakMs2 In ms2
+            rt = matrix.rt
+            mzSample *= 0
+
+            For Each indexMz As Double In fragments
+                into = matrix.mzInto _
+                    .Where(Function(m) equals(m.mz, indexMz)) _
+                    .ToArray
+
+                If into.Length = 0 Then
+                    mzSample += 0
+                Else
+                    mzSample += into _
+                        .Select(Function(m) m.intensity) _
+                        .Quartile _
+                        .ModelSamples _
+                        .normal _
+                        .Average
+                End If
+            Next
+
+            Yield New Sample With {
+                .ID = matrix.ToString,
+                .target = {rt},
+                .status = matrix.mz + mzSample
+            }
+        Next
+    End Function
 End Module
