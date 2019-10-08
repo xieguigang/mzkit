1b51245e84e8f7f1e043e05a4473d6f6188f975c
xieguigang <xie.guigang@gcmodeller.org>
2019-10-08 16:11:22 +0800

    split cli module

---
+++ DATA/pubchem/CLI/WebApi.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Imaging
+Imports SMRUCC.MassSpectrum.DATA.NCBI.PubChem
+
+Partial Module CLI
+
+    <ExportAPI("/image.fly")>
+    <Usage("/image.fly /cid <cid> [/out <save.png>]")>
+    Public Function ImageFlyCLI(args As CommandLine) As Integer
+        Dim cid$ = args("/cid")
+        Dim out$ = args("/out") Or $"./{cid}.png"
+
+        Return ImageFly.GetImage(cid) _
+            .SaveAs(out) _
+            .CLICode
+    End Function
+End Module
