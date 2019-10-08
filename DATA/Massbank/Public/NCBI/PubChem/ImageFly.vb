5e2784a37ea112d3c24dfa8e20c92f3e1aac93a3
xieguigang <xie.guigang@gcmodeller.org>
2019-10-08 16:28:43 +0800

    use new wget api for web download

--- DATA/Massbank/Public/NCBI/PubChem/ImageFly.vb
+++ DATA/Massbank/Public/NCBI/PubChem/ImageFly.vb
@@ -1,60 +1,71 @@
 ﻿#Region "Microsoft.VisualBasic::2909fa3989c7d763f4dab9f11367bf17, Massbank\Public\NCBI\PubChem\ImageFly.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ImageFly
-    ' 
-    '         Function: GetImage
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ImageFly
+' 
+'         Function: GetImage
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports Microsoft.VisualBasic.Imaging.BitmapImage
+Imports Microsoft.VisualBasic.Net.Http
 
 Namespace NCBI.PubChem
 
     Public Module ImageFly
 
-        Public Function GetImage(cid$, Optional width% = 300, Optional height% = 300) As Bitmap
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="cid"></param>
+        ''' <param name="width"></param>
+        ''' <param name="height"></param>
+        ''' <param name="doBgTransparent">
+        ''' 是否将得到的图片的背景设置为透明
+        ''' </param>
+        ''' <returns></returns>
+        Public Function GetImage(cid$, Optional width% = 300, Optional height% = 300, Optional doBgTransparent As Boolean = True) As Bitmap
             Dim url$ = $"https://pubchem.ncbi.nlm.nih.gov/image/imagefly.cgi?cid={cid}&width={width}&height={height}"
             Dim tmp$ = App.GetAppSysTempFile(".png", sessionID:=App.PID)
 
-            If Not url.DownloadFile(save:=tmp) Then
+            If Not wget.Download(url, save:=tmp) Then
                 Return Nothing
             End If
 
