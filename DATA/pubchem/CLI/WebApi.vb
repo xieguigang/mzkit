e6e72e95399691229be55f8a3ee69d5c6a5e3845
xieguigang <xie.guigang@gcmodeller.org>
2019-10-08 16:17:46 +0800

    web query cli

--- DATA/pubchem/CLI/WebApi.vb
+++ DATA/pubchem/CLI/WebApi.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.CommandLine
+﻿Imports System.ComponentModel
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Imaging
 Imports SMRUCC.MassSpectrum.DATA.NCBI.PubChem
@@ -14,5 +15,22 @@ Partial Module CLI
         Return ImageFly.GetImage(cid) _
             .SaveAs(out) _
             .CLICode
+    End Function
+
+    <ExportAPI("/query")>
+    <Description("Do pubchem database query.")>
+    <Usage("/query /terms <list.txt/json/xml> [/include.image /out <directory>]")>
+    <Argument("/terms", False, CLITypes.File, PipelineTypes.std_in,
+              AcceptTypes:={GetType(String())},
+              Extensions:="*.txt, *.json, *.xml",
+              Description:="This parameter should be a string list of names")>
+    <Argument("/include.image", True, CLITypes.Boolean,
+              AcceptTypes:={GetType(Boolean)},
+              Description:="Also includes 2D structure images.")>
+    Public Function QueryPubchem(args As CommandLine) As Integer
+        Dim in$ = args <= "/terms"
+        Dim out$ = args("/out") Or $"{[in].TrimSuffix}.query_result/"
+        Dim terms$() = [in].SolveListStream.ToArray
+        Dim includeImage As Boolean = args("/include.image")
     End Function
 End Module
