7e316d0b852bc00f2061a531291a7264fe171bf6
xieguigang <xie.guigang@gcmodeller.org>
2019-10-08 16:26:11 +0800

    query database

--- DATA/pubchem/CLI/WebApi.vb
+++ DATA/pubchem/CLI/WebApi.vb
@@ -2,6 +2,7 @@
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Imaging
+Imports SMRUCC.MassSpectrum.DATA.NCBI
 Imports SMRUCC.MassSpectrum.DATA.NCBI.PubChem
 
 Partial Module CLI
@@ -34,5 +35,23 @@ Partial Module CLI
         Dim out$ = args("/out") Or $"{[in].TrimSuffix}.query_result/"
         Dim terms$() = [in].SolveListStream.ToArray
         Dim includeImage As Boolean = args("/include.image")
+        Dim cid$()
+
+        For Each term As String In terms
+            cid = PubChem _
+                .QueryPugViews(term, cacheFolder:=$"{out}/.cache/") _
+                .Keys _
+                .ToArray
+
+            If includeImage Then
+                For Each id As String In cid
+                    Call ImageFly _
+                        .GetImage(id, 500, 500) _
+                        .SaveAs($"{out}/{id}.png")
+                Next
+            End If
+        Next
+
+        Return 0
     End Function
 End Module
