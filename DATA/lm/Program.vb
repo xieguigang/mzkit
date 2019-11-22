c7fca493c9661c929d8736a0ce01365348d6f9d6
xieguigang <xie.guigang@gcmodeller.org>
2019-11-22 13:33:53 +0800

    improvements of the output data report

--- DATA/lm/Program.vb
+++ DATA/lm/Program.vb
@@ -13,11 +13,12 @@ Module Program
     End Function
 
     <ExportAPI("/fitbest")>
-    <Usage("/fitbest /data <measure.csv> /ref <reference.csv> [/out <result.csv>]")>
+    <Usage("/fitbest /data <measure.csv> /ref <reference.csv> [/format <numericFormat, default=G5> /out <result.csv>]")>
     Public Function lm(args As CommandLine) As Integer
         Dim in$ = args <= "/data"
         Dim ref$ = args <= "/ref"
         Dim out$ = args("/out") Or $"{[in].TrimSuffix}.reference_to={ref.BaseName}.csv"
+        Dim numFormat$ = args("/format") Or "G5"
         Dim data = DataSet.LoadDataSet([in]).ToDictionary
         Dim reference As DataSet() = DataSet.LoadDataSet(ref).ToArray
         Dim result As New List(Of EntityObject)
@@ -26,18 +27,24 @@ Module Program
         For Each metabolite As DataSet In reference
             Dim refData As DataSet = data(metabolite.ID)
             Dim fit As IFitted = LmMath.lm.CreateLinearModel(metabolite, refData)
+            Dim modelFit As New EntityObject With {
+                .ID = metabolite.ID,
+                .Properties = New Dictionary(Of String, String) From {
+                    {"R2", fit.CorrelationCoefficient},
+                    {"formula", "f(x)=" & fit.Polynomial.ToString(numFormat)},
+                    {"|||||||", ""}
+                }
+            }
+
+            For Each sample In refData.Properties
+                modelFit(sample.Key) = fit.GetY(sample.Value)
+            Next
 
             models += New NamedValue(Of IFitted) With {
                 .Name = metabolite.ID,
                 .Value = fit
             }
-            result += New EntityObject With {
-                .ID = metabolite.ID,
-                .Properties = New Dictionary(Of String, String) From {
-                    {"R2", fit.CorrelationCoefficient},
-                    {"formula", "f(x)=" & fit.Polynomial.ToString("G4")}
-                }
-            }
+            result += modelFit
         Next
 
         Call result.SaveDataSet(out)
