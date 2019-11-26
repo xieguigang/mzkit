4f44a1906d4b1b4548770cbda21cea5bb37d567a
xieguigang <xie.guigang@gcmodeller.org>
2019-11-26 10:49:48 +0800

    add test

---
+++ Assembly/py/test.py
@@ -0,0 +1,6 @@
+import mzxml
+
+for scan in mzxml.read_mzXML("D:/biodeep/biodeepDB/protocols/biodeepMSMS1/biodeepMSMS/test/lxy-CID30.mzXML", 1):
+
+    rt = mzxml.parse_scan_rt(scan)
+    print("rt: " + str(rt))
