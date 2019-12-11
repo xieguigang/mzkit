864c4d7d0dc71d133cfa8578637b1bb266ea1128
xieguigang <xie.guigang@gcmodeller.org>
2019-12-11 18:41:36 +0800

    parse mgf ions

--- Assembly/py/mgf.py
+++ Assembly/py/mgf.py
@@ -1,14 +1,13 @@
-
+import strings
 
 def readMgf (path) :
     "Read a mgf text file from a given file path."
 
     file = open(path, "r")
-    lines = file.readlines()
-    buf = []
+    lines = []   
     ions = []
 
-    for line in lines:
+    for line in file.readlines():
         if (line == "END ION") :
             ions.append(parseIon(lines))
             lines = []
@@ -43,6 +42,10 @@ def parseIon(linesBlock) :
     # END IONS
 
     meta = {}
+    mz   = []
+    into = []
+    pms2 = 0
+    l = len(linesBlock)
 
     # loop for each text line until 
     # read a fragment data line
@@ -51,6 +54,39 @@ def parseIon(linesBlock) :
     # the "BEGIN IONS" tag
     # skip the first line
     #
-    for i in range(1, len(linesBlock)):
+    for i in range(1, l):
         if ("=" in linesBlock[i]) :
-            
+            name,value = strings.getTagValue(linesBlock[i])
+            meta[name] = value
+        else :
+            # this is the end of the meta data region
+            pms2 = i
+            break
+
+    # parse ms2 data
+    for i in range(pms2, l - 1):
+        mzinto = linesBlock[i].split(' ')
+        mz.append(float(mzinto[0]))
+        into.append(float(mzinto[1]))
+
+    # post processing
+    title_meta = parseTitleMeta(meta["TITLE"])
+    meta["TITLE"] = title_meta["title"]
+    ms2 = {"mz": mz, "into": into}
+    ion = {"meta" : meta, "ms2": ms2, "annotation": title_meta["meta"]}
+
+    return ion
+
+def parseTitleMeta(title):
+    "Parse meta data that contains in title value string"
+
+    title_str = title.split(" ", 1)
+    meta_str = title[len(title_str) + 1: len(title)]
+    meta_str = meta_str.split('", ')
+    meta = {}
+
+    for attr in meta_str:
+        name,value = strings.getTagValue(attr, ':"')
+        meta[name] = value
+
+    return {"title": title_str, "meta" : meta}
--- Assembly/py/strings.py
+++ Assembly/py/strings.py
@@ -3,4 +3,14 @@
 def getTagValue(str, deli):
     "Get tagged string value as key-value pair"
 
-    
+    if (deli in str): 
+        # contains a name
+        pos = str.index(deli)
+        name = str[0:pos]
+        value = str[pos+len(deli):len(str)]
+    else :
+        # name is null
+        name = ""
+        value = str
+
+    return name,value
