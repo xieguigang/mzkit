e98932e3f6f11fc9ea4a65f2169e50a031270773
xieguigang <xie.guigang@gcmodeller.org>
2019-12-12 14:04:52 +0800

    formula test success

--- Rscript/formula.R
+++ Rscript/formula.R
@@ -3,3 +3,4 @@ imports 'mzkit.formula' from "mzkit.dll";
 print("(CH3)3CH" :> scan.formula);
 print("H2O" :> scan.formula);
 print("(CH3)4C" :> scan.formula);
+print("C18H23N4O14P(C9H11N2O8P)5" :> scan.formula);
