b080b6d06c2d8676087fd77dfc9794be6cac835f
xieguigang <xie.guigang@gcmodeller.org>
2019-12-12 13:50:56 +0800

    config for test

--- Rscript/formula.R
+++ Rscript/formula.R
@@ -1,5 +1,5 @@
 imports 'mzkit.formula' from "mzkit.dll";
 
-print("H2O" :> scan.formula);
 print("(CH3)3CH" :> scan.formula);
+print("H2O" :> scan.formula);
 print("(CH3)4C" :> scan.formula);
--- mzkit/Formula.vb
+++ mzkit/Formula.vb
@@ -1,12 +1,22 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.MassSpectrum.DATA.MetaLib
+Imports REnv = SMRUCC.Rsharp.Runtime.Internal.ConsolePrinter
 
 ''' <summary>
 ''' 
 ''' </summary>
 <Package("mzkit.formula", Category:=APICategories.UtilityTools)>
 Module Formula
+
+    Sub New()
+        Call REnv.AttachConsoleFormatter(Of FormulaComposition)(AddressOf FormulaCompositionString)
+    End Sub
+
+    Private Function FormulaCompositionString(formula As FormulaComposition) As String
+        Return formula.EmpiricalFormula & $" ({formula.CountsByElement.GetJson})"
+    End Function
 
     <ExportAPI("scan.formula")>
     Public Function ScanFormula(formula As String) As FormulaComposition
