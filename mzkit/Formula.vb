78e20e8d93b4aeffd76e5afa5649b9cd3aca5e75
xieguigang <xie.guigang@gcmodeller.org>
2019-12-12 13:43:19 +0800

    scan formula

---
+++ mzkit/Formula.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.MassSpectrum.DATA.MetaLib
+
+''' <summary>
+''' 
+''' </summary>
+<Package("mzkit.formula", Category:=APICategories.UtilityTools)>
+Module Formula
+
+    <ExportAPI("scan.formula")>
+    Public Function ScanFormula(formula As String) As FormulaComposition
+        Return FormulaScanner.ScanFormula(formula)
+    End Function
+End Module
