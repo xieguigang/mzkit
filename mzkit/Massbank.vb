c9201c7cbfd0fc44dfa6698f4ab7a81ed412a62c
xieguigang <xie.guigang@gcmodeller.org>
2019-12-17 09:25:11 +0800

    secondary2main.mapping

--- mzkit/Massbank.vb
+++ mzkit/Massbank.vb
@@ -1,19 +1,49 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
+Imports SMRUCC.Rsharp
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Interop
 Imports ChEBIRepo = SMRUCC.genomics.Assembly.ELIXIR.EBI.ChEBI.DATA
+Imports REnv = SMRUCC.Rsharp.Runtime.Internal.Invokes.base
 
 <Package("mzkit.massbank")>
 Module Massbank
 
     <ExportAPI("chebi.secondary2main.mapping")>
-    Public Function chebiSecondary2Main(repository As String) As Dictionary(Of String, String)
-        Return ChEBIRepo.ScanEntities(repository).GroupBy(Function(c) c.chebiId).Select(Function(c) c.First).Select(Function(c)
-                                                                                                                        If c.SecondaryChEBIIds.IsNullOrEmpty Then
-                                                                                                                            Return {(c.chebiId, c.chebiId)}
-                                                                                                                        Else
-                                                                                                                            Return c.SecondaryChEBIIds.Select(Function(sid) (sid, c.chebiId))
-                                                                                                                        End If
-                                                                                                                    End Function).IteratesALL.GroupBy(Function(c) c.Item1).ToDictionary(Function(c) c.Key, Function(g) g.Select(Function(t) t.Item2).First)
+    Public Function chebiSecondary2Main(repository As String) As Dictionary(Of String, String())
+        Return ChEBIRepo.ScanEntities(repository) _
+            .GroupBy(Function(c) c.chebiId) _
+            .Select(Function(c) c.First) _
+            .ToDictionary(Function(c) c.chebiId,
+                          Function(c)
+                              Return c.SecondaryChEBIIds _
+                                  .SafeQuery _
+                                  .ToArray
+                          End Function)
+    End Function
+
+    <ExportAPI("hmdb.secondary2main.mapping")>
+    Public Function hmdbSecondary2Main(repository As String) As Dictionary(Of String, String())
+
+    End Function
+
+    ''' <summary>
+    ''' Create SecondaryIDSolver object from mapping file or mapping dictionary object data.
+    ''' </summary>
+    ''' <param name="mapping"></param>
+    ''' <returns></returns>
+    ''' 
+    <ExportAPI("secondary2main.mapping")>
+    Public Function createIdMapping(mapping As Object, Optional envir As Environment = Nothing) As RReturn
+        If mapping Is Nothing Then
+            Return REnv.stop("No mapping data provided!", envir)
+        ElseIf mapping.GetType Is GetType(String) Then
+            mapping = DirectCast(mapping, String).LoadJSON(Of Dictionary(Of String, String()))
+        End If
+
+        Return SecondaryIDSolver.FromMaps(mapping).AsRReturn
     End Function
 End Module
