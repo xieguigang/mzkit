00d837452426ba1d32097e0c025426a46ecac6df
xieguigang <xie.guigang@gcmodeller.org>
2019-12-17 09:33:12 +0800

    id mapping

--- mzkit/Massbank.vb
+++ mzkit/Massbank.vb
@@ -3,6 +3,7 @@ Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
+Imports SMRUCC.MassSpectrum.DATA.TMIC
 Imports SMRUCC.Rsharp
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Interop
@@ -27,7 +28,16 @@ Module Massbank
 
     <ExportAPI("hmdb.secondary2main.mapping")>
     Public Function hmdbSecondary2Main(repository As String) As Dictionary(Of String, String())
-
+        Return HMDB.metabolite _
+            .Load(repository) _
+            .ToDictionary(Function(a) a.accession,
+                          Function(a)
+                              If a.secondary_accessions.accession.IsNullOrEmpty Then
+                                  Return {a.accession}
+                              Else
+                                  Return a.secondary_accessions.accession
+                              End If
+                          End Function)
     End Function
 
     ''' <summary>
@@ -45,5 +55,16 @@ Module Massbank
         End If
 
         Return SecondaryIDSolver.FromMaps(mapping).AsRReturn
+    End Function
+
+    <ExportAPI("save.mapping")>
+    Public Function saveIDMapping(mapping As Dictionary(Of String, String()), file$, Optional envir As Environment = Nothing) As Object
+        If mapping Is Nothing Then
+            Return REnv.stop("No mapping data provided!", envir)
+        ElseIf file.StringEmpty Then
+            Return REnv.stop("Missing file parameter!", envir)
+        End If
+
+        Return mapping.GetJson.SaveTo(file)
     End Function
 End Module
