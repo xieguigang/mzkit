e107f1fcdaad3f11ce45271030602c82a5545047
xieguigang <xie.guigang@gcmodeller.org>
2019-12-27 13:31:11 +0800

    MRM peaktable test success

--- Rscript/demo/mrm_quantify.R
+++ Rscript/demo/mrm_quantify.R
@@ -5,11 +5,18 @@ let ions = read.ion_pairs(mrmInfo, "ion pairs");
 let wiff as string = "T:\test\Data20190522liaoning-Cal";
 
 list.files(wiff, pattern = "*.mzML")
-:> wiff.scans(ions,peakAreaMethod= 0, TPAFactors = NULL, ) 
+:> wiff.scans(ions,peakAreaMethod= 0, TPAFactors = NULL) 
 :> write.csv(file = "T:\test\L.csv");
+
+for(mzML in list.files(wiff, pattern = "*.mzML")) {
+	let fileName = basename(mzML);
+	let peaks = MRM.peaks(mzML, ions, peakAreaMethod= 0, TPAFactors = NULL);
+	
+	write.csv( peaks, file = `T:\test\peaktables/${fileName}.csv`);
+}
 
 wiff = "T:\test\Data20190222";
 
 list.files(wiff, pattern = "*.mzML")
-:> wiff.scans(ions,peakAreaMethod= 0, TPAFactors = NULL, ) 
+:> wiff.scans(ions,peakAreaMethod= 0, TPAFactors = NULL) 
 :> write.csv(file = "T:\test\samples.csv");
