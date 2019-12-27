ca37d43036acc2faa03426108ad61b7170fdd3a3
xieguigang <xie.guigang@gcmodeller.org>
2019-12-27 17:10:10 +0800

    plot standard curve

--- Rscript/Library/mzkit.quantitative/Visual.vb
+++ Rscript/Library/mzkit.quantitative/Visual.vb
@@ -1,7 +1,16 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.MassSpectrum.Math
+Imports SMRUCC.MassSpectrum.Visualization
 
 <Package("mzkit.quantify.visual")>
 Module Visual
 
+    <ExportAPI("standard_curve")>
+    Public Function StandardCurve(model As FitModel, Optional name$ = "", Optional samples As NamedValue(Of Double)() = Nothing) As GraphicsData
+        Return StandardCurvesPlot.StandardCurves(model, samples, name)
+    End Function
 End Module
