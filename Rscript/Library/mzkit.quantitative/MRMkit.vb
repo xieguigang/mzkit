b8c9083ecfdad4da40f5895c4964726de46cbcae
xieguigang <xie.guigang@gcmodeller.org>
2019-12-27 09:46:56 +0800

    mrm kit

--- Rscript/Library/mzkit.quantitative/MRMkit.vb
+++ Rscript/Library/mzkit.quantitative/MRMkit.vb
@@ -1,6 +1,20 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.MassSpectrum.Assembly.MarkupData.mzML
+Imports SMRUCC.MassSpectrum.Math.Chromatogram
 
 <Package("mzkit.mrm")>
 Public Module MRMkit
 
+    ''' <summary>
+    ''' Extract ion peaks
+    ''' </summary>
+    ''' <param name="mzML$"></param>
+    ''' <param name="ionpairs"></param>
+    ''' <returns></returns>
+    <ExportAPI("extract.ions")>
+    Public Function ExtractIonData(mzML$, ionpairs As IonPair()) As NamedCollection(Of ChromatogramTick)()
+
+    End Function
 End Module
