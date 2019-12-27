0f7bda0e580a46a98b6d7728a3cdd9d85c76a978
xieguigang <xie.guigang@gcmodeller.org>
2019-12-27 17:48:13 +0800

    Update mrm_quantify.R
    code style improvements

--- Rscript/demo/mrm_quantify.R
+++ Rscript/demo/mrm_quantify.R
@@ -1,18 +1,29 @@
+# imports mzkit library modules
 imports ["mzkit.mrm", "mzkit.quantify.visual"] from "mzkit.quantitative.dll";
 
-let wiff as string = "T:\test\Data20190522liaoning-Cal";
-let mrmInfo as string = "T:\_ref\MetaCardio_STD_v5.xlsx";
-let [ions, ref, is] = mrmInfo :> [
+# config of the standard curve data files
+let wiff as string     = ?"--Cal"    || stop("No standard curve data provides!");
+let sample as string   = ?"--data"   || stop("No sample data files provided!");
+let MRM.info as string = ?"--MRM"    || stop("Missing MRM information table file!");
+let dir as string      = ?"--export" || `${wiff :> trim(" ")}-result/`;
+
+# read MRM, standard curve and IS information from the given file
+let [ions, ref, is]    = MRM.info :> [
 	read.ion_pairs("ion pairs"), 
 	read.reference("coordinates"), 
 	read.IS("IS")
 ];
 
+# Get raw scan data for given ions
 let CAL = list.files(wiff, pattern = "*.mzML")
-:> wiff.scans(ions,peakAreaMethod= 0, TPAFactors = NULL);
+:> wiff.scans(
+	ions           = ions, 
+	peakAreaMethod = 0, 
+	TPAFactors     = NULL
+);
 
 CAL 
-:> write.csv(file = "T:\test\L.csv");
+:> write.csv(file = `${dir}/CAL.csv`);
 
 ref <- CAL 
 :> linears(ref, is) 
@@ -28,21 +39,21 @@ for(line in ref) {
 	
 	line
 	:> standard_curve
-	:> save.graphics(file = `T:\test/${id}.png`);
+	:> save.graphics(file = `${dir}/standard_curves/${id}.png`);
 }
 
 for(mzML in list.files(wiff, pattern = "*.mzML")) {
 	let fileName = basename(mzML);
-	let peaks = MRM.peaks(mzML, ions, peakAreaMethod= 0, TPAFactors = NULL);
+	let peaks = MRM.peaks(mzML, ions, peakAreaMethod = 0, TPAFactors = NULL);
 	
-	write.csv( peaks, file = `T:\test\peaktables/${fileName}.csv`);
+	write.csv( peaks, file = `${dir}/peaktables/${fileName}.csv`);
 }
 
-wiff = "T:\test\Data20190222";
+wiff <- sample;
 
 list.files(wiff, pattern = "*.mzML")
 :> wiff.scans(ions,peakAreaMethod= 0, TPAFactors = NULL) 
-:> write.csv(file = "T:\test\samples.csv");
+:> write.csv(file = `${dir}\samples.csv`);
 
 let scans = [];
 
