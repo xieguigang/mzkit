4629591a496acf69c507157a9a4f5618f7e2d76a
xieguigang <xie.guigang@gcmodeller.org>
2019-12-27 11:21:34 +0800

    fix pattern bugs

--- Rscript/demo/mrm_quantify.R
+++ Rscript/demo/mrm_quantify.R
@@ -4,5 +4,5 @@ let mrmInfo as string = "T:\_ref\MetaCardio_STD_v5.xlsx";
 let ions = read.ion_pairs(mrmInfo, "ion pairs");
 let wiff as string = "T:\test\Data20190522liaoning-Cal";
 
-wiff.standard_curve(wiff, ions,peakAreaMethod= 0, TPAFactors = NULL, calibrationNamedPattern = ".+[-]CAL[-]\d+", levelPattern = ".+[-]CAL[-]\d+") 
+wiff.standard_curve(wiff, ions,peakAreaMethod= 0, TPAFactors = NULL, calibrationNamedPattern = ".+[-]CAL[-]\d+", levelPattern = "[-]CAL[-]\d+") 
 :> write.csv(file = "T:\test\L.csv");
