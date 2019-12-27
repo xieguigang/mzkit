093daa803cf90be7e32d056a4131726cc17ef9cd
xieguigang <xie.guigang@gcmodeller.org>
2019-12-27 10:12:58 +0800

    config projects

--- Rscript/demo/mrm.R
+++ Rscript/demo/mrm.R
@@ -3,3 +3,5 @@ imports "mzkit.mrm" from "mzkit.quantitative.dll";
 let mzML as string = "D:\biodeep\biodeepDB\smartnucl_integrative\build_tools\CVD_kb\smartnucl.CVD_kb\data\day3_test\files\day3-L44.mzML";
 let mrmInfo as string = "T:\_ref\MetaCardio_STD_v5.xlsx";
 let ions = read.ion_pairs(mrmInfo, "ion pairs");
+
+let mrm.ions = extract.ions(mzML, ions);
