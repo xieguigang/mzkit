984051d9ed3fc9e02ef6b691e40d4de1ac956c56
xieguigang <xie.guigang@gcmodeller.org>
2019-12-30 16:32:36 +0800

    Update wiff2mzML.R
    fix of the string interpolation

--- dist/Rscript/wiff2mzML.R
+++ dist/Rscript/wiff2mzML.R
@@ -1,14 +1,14 @@
 require(ProteoWizard);
 
 let sample as string = ?"--samples" || stop("No wiff raw samples data directory provided!");
-let output as string = ?"--output"  || `${dirname(sample)}/{basename(sample)}.mzML/` ;
+let output as string = ?"--output"  || `${dirname(sample)}/${basename(sample)}.mzML/` ;
 let wiff as string   = list.files(sample, "*.wiff");
 
 print(`we have ${length(wiff)} raw files data for convert to mzML files`);
 print(basename(wiff, TRUE));
 
 for(raw in wiff) {
-	let output.dir = `${output}/{basename(raw)}`;
+	let output.dir = `${output}/${basename(raw)}`;
 	let mzML = MRM.mzML(wiff = raw, output = output.dir);
 	
 	print(`there are ${length(mzML)} samples in raw data '${basename(raw, TRUE)}'`);
