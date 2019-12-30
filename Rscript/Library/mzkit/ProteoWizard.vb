e9c9b436b0ae71c4afe263bbf40afdb750f9e393
xieguigang <xie.guigang@gcmodeller.org>
2019-12-30 14:24:17 +0800

    wiff to mzML raw files

--- Rscript/Library/mzkit/ProteoWizard.vb
+++ Rscript/Library/mzkit/ProteoWizard.vb
@@ -1,10 +1,25 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
 ''' <summary>
 ''' ProteoWizard helper
+''' 
+''' You should config the bin path of ProteoWizard at first 
+''' by using ``options`` function
 ''' </summary>
 <Package("ProteoWizard", Category:=APICategories.UtilityTools)>
 Module ProteoWizard
 
+    ''' <summary>
+    ''' Convert MRM wiff file to mzMl files
+    ''' </summary>
+    ''' <param name="wiff">The file path of the wiff file</param>
+    ''' <returns>
+    ''' File path collection of the converted mzML files.
+    ''' </returns>
+    <ExportAPI("MRM.mzML")>
+    Public Function wiffMRM(wiff As String) As Object
+
+    End Function
 End Module
