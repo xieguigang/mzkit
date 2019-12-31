0ed3fd98228f5d7b6dcdb2b29eb467d3cdf0aa30
xieguigang <xie.guigang@gcmodeller.org>
2019-12-31 17:17:10 +0800

    config project profiles

--- dist/Rscript/MRM-quantify.cmd
+++ dist/Rscript/MRM-quantify.cmd
@@ -7,4 +7,7 @@ SET data="D:\biodeep\biodeepDB\smartnucl_integrative\mzML\Data20190222"
 SET MRM="D:\biodeep\biodeepDB\smartnucl_integrative\mzML\MetaCardio_STD_v5.xlsx"
 SET output="D:\biodeep\biodeepDB\smartnucl_integrative\mzML\output"
 
-%R% --Cal %CAL% --data %data% --MRM %MRM% --export %output%
+%R% ./mrm_quantify.R --Cal %CAL% --data %data% --MRM %MRM% --export %output% --debug
+
+
+pause
