62fb1e1ac045d574e1354a205da918640dc3c44a
xieguigang <xie.guigang@gcmodeller.org>
2019-12-31 15:24:44 +0800

    raw files

---
+++ Rscript/demo/rawfiles.R
@@ -0,0 +1,6 @@
+imports "mzkit.mrm" from "mzkit.quantify.dll";
+
+let mzML = "D:\20191230_WYL_MRM\MeiZhou\raw\20191224cal";
+let files = mzML :> wiff.rawfiles("[-]?LM\d+") :> as.object :> do.call("GetRawFileList");
+
+print(files);
