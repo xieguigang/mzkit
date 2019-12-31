774f44e6964a25709f8ff379d7da7eb8bb8f85f5
xieguigang <xie.guigang@gcmodeller.org>
2019-12-31 14:38:55 +0800

    add docs

--- Assembly/assembly/ASCII/MSN/FileReader.vb
+++ Assembly/assembly/ASCII/MSN/FileReader.vb
@@ -10,7 +10,11 @@
         Public Iterator Function GetIons(data As String) As IEnumerable(Of MGF.Ions)
             Dim metaPeaksFile As String = $"{data.ParentPath}/{data.BaseName.BaseName}.peak-table.txt"
             Dim peaks As PeakTable() = PeakTable.ParseTable(metaPeaksFile)
-            Dim ions As Dictionary(Of String, MsnList) = MsnList.GetMsnList(data).ToDictionary(Function(p) p.id)
+            Dim ions As Dictionary(Of String, MsnList) = MsnList _
+                .GetMsnList(data) _
+                .ToDictionary(Function(p) p.id)
+
+
         End Function
     End Module
 End Namespace
--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,14 @@
 # ``mzkit`` R# scripting guildline
 
+The mzkit is a ``R#`` package modules for the data analysis of the mass spectrum data and chemoinformatics.
+
+Currently the mzkit contains two library module:
+
++ ``mzkit.dll``
++ ``mzkit.quantify.dll``
+
+## Quantitative targeting
+
+The ``mzkit.quantify`` library module apply for the quantitative targeting analysis for the Targeted Metabolomics analysis. The analysis protocols in this library module is apply to the MRM data and the GCMS SIM data.
+
+For learning the MRM analysis using mzkit, please read this document: 
