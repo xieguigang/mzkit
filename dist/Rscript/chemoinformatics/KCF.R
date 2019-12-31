4be0c675fe0e5565db4e83929598ce31a2b10d20
xieguigang <xie.guigang@gcmodeller.org>
2019-12-31 14:21:52 +0800

    view summary information of the KCF model

--- dist/Rscript/chemoinformatics/KCF.R
+++ dist/Rscript/chemoinformatics/KCF.R
@@ -2,3 +2,19 @@ imports "mzkit.formula" from "mzkit.dll";
 
 require(igraph);
 require(igraph.comparison);
+
+setwd(!script$dir);
+
+# test of the butyrate
+let butyrate    <- read.KCF("butyrate.txt")    :> KCF.graph;
+let butyryl.CoA <- read.KCF("butyryl-CoA.txt") :> KCF.graph;
+
+print(butyrate);
+print(butyryl.CoA);
+
+# test of the ATP etc
+let ATP <- read.KCF("ATP.txt") :> KCF.graph;
+let AMP <- read.KCF("AMP.txt") :> KCF.graph;
+
+print(ATP);
+print(AMP);
