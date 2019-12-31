5a07f26d2dc5f78403b0e380afb5793eb19922f2
xieguigang <xie.guigang@gcmodeller.org>
2019-12-31 18:08:51 +0800

    Update KCF.R

--- dist/Rscript/chemoinformatics/KCF.R
+++ dist/Rscript/chemoinformatics/KCF.R
@@ -9,12 +9,18 @@ setwd(!script$dir);
 let butyrate    <- read.KCF("butyrate.txt")    :> KCF.graph;
 let butyryl.CoA <- read.KCF("butyryl-CoA.txt") :> KCF.graph;
 
-print(butyrate);
-print(butyryl.CoA);
+# print(butyrate);
+# print(butyryl.CoA);
+
+print("Similarity between [Butyrate] and [Butyryl-CoA]:");
+print(graph.jaccard(butyrate, butyryl.CoA, 0.9));
 
 # test of the ATP etc
 let ATP <- read.KCF("ATP.txt") :> KCF.graph;
 let AMP <- read.KCF("AMP.txt") :> KCF.graph;
 
-print(ATP);
-print(AMP);
+# print(ATP);
+# print(AMP);
+
+print("Similarity between [ATP] and [AMP]:");
+print(graph.jaccard(ATP, AMP, 0.9));
