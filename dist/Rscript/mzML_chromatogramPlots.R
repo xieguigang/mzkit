f3ed8e7251a0095e8c4820e0bfe3e0baeb7c8983
xieguigang <xie.guigang@gcmodeller.org>
2020-01-03 14:03:14 +0800

    do plot of the multiple mzML raw data files

--- dist/Rscript/mzML_chromatogramPlots.R
+++ dist/Rscript/mzML_chromatogramPlots.R
@@ -1,36 +1,60 @@
 imports ["mzkit.mrm", "mzkit.quantify.visual"] from "mzkit.quantify.dll";
 
-let file.mzML as string = ?"--mzML" || stop("Missing the MRM mzML raw data file!");
-let MRM.xlsx as string  = ?"--MRM"  || stop("No mrm information provided!");
-
+let file.mzML as string  = ?"--mzML"   || stop("Missing the MRM mzML raw data file!");
+let MRM.xlsx as string   = ?"--MRM"    || stop("No mrm information provided!");
+let output.dir as string = ?"--output" || `${dirname(file.mzML)}/${basename(file.mzML)}.chromatogramPlots`;
+	
 # Get ion pair information from 
 # a given excel table
 let ions <- read.ion_pairs(MRM.xlsx, "ion pairs");
-let raw  <- [?"--mzML"]
-# Extract ion peaks from given MRM data file
-# the ion data is specific by the MRM ion pairs
-:> extract.ions(ionpairs = ions);
 
-# do file data plots
-let output as string = ?"--output" || `${dirname(file.mzML)}/${basename(file.mzML)}.chromatogramPlots`;
+let plot.mzML as function(file, output) {
 
-for(ion in raw :> projectAs(as.object)) {
-	let save as string = `${output}/chromatogramROI/${ion$name}.png`;
-	
-	# draw MRM peak for each metabolite ion
-	# 
-	# 1. [blue] Area Integration, is the peak area integration plot
-	# 2. [red] chromatography ROI, the region of interseted, MRM chromatogram peak region, from rtmin to rtmax
-	# 3. [green] baseline, The chromatogram signal baseline, the lower of the baseline it is, the better of the ion signal it is
-	# 4. [blank] chromatogram, The MRM ion chromatogram signal data
-	#
-	ion$value 
-	:> MRM.chromatogramPeaks.plot(title = ion$description)
-	:> save.graphics(file = save)
-	;
+	let raw  <- file
+	  # Extract ion peaks from given MRM data file
+	  # the ion data is specific by the MRM ion pairs
+	  :> extract.ions(ionpairs = ions);
+	  
+	for(ion in raw :> projectAs(as.object)) {
+		let save as string = `${output}/chromatogramROI/${ion$name}.png`;
+		
+		# draw MRM peak for each metabolite ion
+		# 
+		# 1. [blue] Area Integration, is the peak area integration plot
+		# 2. [red] chromatography ROI, the region of interseted, MRM chromatogram peak region, from rtmin to rtmax
+		# 3. [green] baseline, The chromatogram signal baseline, the lower of the baseline it is, the better of the ion signal it is
+		# 4. [blank] chromatogram, The MRM ion chromatogram signal data
+		#
+		ion$value 
+		:> MRM.chromatogramPeaks.plot(title = ion$description)
+		:> save.graphics(file = save)
+		;
+	}
+
+	# Draw all of the MRM ions onto one chromatogram plot image
+	file
+	:> chromatogram.plot(ions) 
+	:> save.graphics(file = `${output}/chromatogram.png`);
 }
 
-# Draw all of the MRM ions onto one chromatogram plot image
-[?"--mzML"]
-:> chromatogram.plot(ions) 
-:> save.graphics(file = `${output}/chromatogram.png`);
+if (dir.exists(file.mzML)) {	
+	# do multiple file data plots
+	file.mzML <- list.files(file.mzML, pattern = "*.mzML");
+	
+	print("Do plot of the raw data files:");
+	print(basename(file.mzML));
+	
+	for(filepath in file.mzML) {
+		plot.mzML(filepath, `${output.dir}/${basename(filepath)}`);
+		
+		print(filepath);
+		print("[JOB DONE!]");
+	}
+	
+} else {
+	# do single file data plots
+	print("Do plot of the single raw data file:");
+	print(file.mzML);
+	
+	plot.mzML(file.mzML, output.dir);
+}
