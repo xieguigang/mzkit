e71c90a590c2aa0a505bb4bd57ff2f3c5587b4bd
xieguigang <xie.guigang@gcmodeller.org>
2020-01-03 11:26:51 +0800

    Update mzML_chromatogramPlots.R
    add dev notes

--- dist/Rscript/mzML_chromatogramPlots.R
+++ dist/Rscript/mzML_chromatogramPlots.R
@@ -1,10 +1,11 @@
 imports ["mzkit.mrm", "mzkit.quantify.visual"] from "mzkit.quantify.dll";
 
 let file.mzML as string = ?"--mzML" || stop("Missing the MRM mzML raw data file!");
+let MRM.xlsx as string  = ?"--MRM"  || stop("No mrm information provided!");
 
 # Get ion pair information from 
 # a given excel table
-let ions <- read.ion_pairs(?"--mrm", "ion pairs");
+let ions <- read.ion_pairs(MRM.xlsx, "ion pairs");
 let raw  <- [?"--mzML"]
 # Extract ion peaks from given MRM data file
 # the ion data is specific by the MRM ion pairs
@@ -16,12 +17,20 @@ let output as string = ?"--output" || `${dirname(file.mzML)}/${basename(file.mzM
 for(ion in raw :> projectAs(as.object)) {
 	let save as string = `${output}/chromatogramROI/${ion$name}.png`;
 	
+	# draw MRM peak for each metabolite ion
+	# 
+	# 1. [blue] Area Integration, is the peak area integration plot
+	# 2. [red] chromatography ROI, the region of interseted, MRM chromatogram peak region, from rtmin to rtmax
+	# 3. [green] baseline, The chromatogram signal baseline, the lower of the baseline it is, the better of the ion signal it is
+	# 4. [blank] chromatogram, The MRM ion chromatogram signal data
+	#
 	ion$value 
 	:> MRM.chromatogramPeaks.plot(title = ion$description)
 	:> save.graphics(file = save)
 	;
 }
 
+# Draw all of the MRM ions onto one chromatogram plot image
 [?"--mzML"]
 :> chromatogram.plot(ions) 
 :> save.graphics(file = `${output}/chromatogram.png`);
