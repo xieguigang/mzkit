e255f853bdda27b37209d5abfa088d1b423a7468
xieguigang <xie.guigang@gcmodeller.org>
2020-01-03 15:26:39 +0800

    fix for a possible key index bugs

--- src/mzmath/TargetedMetabolomics/MRM/QuantitativeAnalysis/StandardCurveWorker.vb
+++ src/mzmath/TargetedMetabolomics/MRM/QuantitativeAnalysis/StandardCurveWorker.vb
@@ -1,46 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::40fa5872f12651ab24fc49b35d60786b, src\mzmath\TargetedMetabolomics\MRM\QuantitativeAnalysis\StandardCurveWorker.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module StandardCurveWorker
-    ' 
-    '         Function: CreateModelPoints, getBlankControls, getByLevel, getIS, Regression
-    '                   (+2 Overloads) Scan, ScanContent
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module StandardCurveWorker
+' 
+'         Function: CreateModelPoints, getBlankControls, getByLevel, getIS, Regression
+'                   (+2 Overloads) Scan, ScanContent
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -309,7 +309,7 @@ Namespace MRM
                                  End Function
             Dim refPoints = ref _
                 .ToDictionary(Function(level)
-                                  Return getLevelNumber(level.Key)
+                                  Return "L" & getLevelNumber(level.Key)
                               End Function,
                               Function(level)
                                   Return level.Value
@@ -319,7 +319,7 @@ Namespace MRM
                        If ref.Count = 0 Then
                            Return 0
                        Else
-                           Dim key As String = getLevelNumber(L.Key)
+                           Dim key As String = "L" & getLevelNumber(L.Key)
                            Dim At_i = refPoints(key)
 
                            Return At_i
