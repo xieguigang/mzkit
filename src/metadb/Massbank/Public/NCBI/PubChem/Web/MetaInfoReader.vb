0b47619255b7906bb4e2b0b107efda97f371f0ee
xieguigang <xie.guigang@gcmodeller.org>
2020-01-04 14:15:37 +0800

    fix namespace reference errors

--- src/metadb/Massbank/Public/NCBI/PubChem/Web/MetaInfoReader.vb
+++ src/metadb/Massbank/Public/NCBI/PubChem/Web/MetaInfoReader.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::6f540129bf0e7487f4f37a9889d45a28, src\metadb\Massbank\Public\NCBI\PubChem\Web\MetaInfoReader.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module MetaInfoReader
-    ' 
-    '         Function: GetInform, GetInformList, GetMetaInfo, getSynonyms, navigateView
-    '                   removesDbEntry
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module MetaInfoReader
+' 
+'         Function: GetInform, GetInformList, GetMetaInfo, getSynonyms, navigateView
+'                   removesDbEntry
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
+Imports BioNovoGene.BioDeep.Chemistry.MetaLib.Models
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Linq
-Imports SMRUCC.MassSpectrum.DATA.MetaLib.Models
-Imports MetaInfo = SMRUCC.MassSpectrum.DATA.MetaLib.Models.MetaLib
+Imports MetaInfo = BioNovoGene.BioDeep.Chemistry.MetaLib.Models.MetaLib
 
 Namespace NCBI.PubChem
 
@@ -209,7 +209,7 @@ Namespace NCBI.PubChem
             Return New MetaInfo With {
                 .formula = formulaStr,
                 .xref = xref,
-                .name = commonName,
+                .Name = commonName,
                 .exact_mass = exact_mass,
                 .ID = view.RecordNumber,
                 .synonym = synonyms.removesDbEntry.ToArray
