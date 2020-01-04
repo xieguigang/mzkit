02ee7e97a6c2afadb3a0ea69ab95bd951832ae03
xieguigang <xie.guigang@gcmodeller.org>
2020-01-04 18:42:57 +0800

    add test

---
+++ src/metadb/Chemoinformatics/test/inchiTest.vb
@@ -0,0 +1,72 @@
+﻿#Region "Microsoft.VisualBasic::6af0ae85b7b44d9708f24c4cac4cd858, src\metadb\Massbank\test\inchiTest.vb"
+
+    ' Author:
+    ' 
+    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+    ' 
+    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+    ' 
+    ' 
+    ' MIT License
+    ' 
+    ' 
+    ' Permission is hereby granted, free of charge, to any person obtaining a copy
+    ' of this software and associated documentation files (the "Software"), to deal
+    ' in the Software without restriction, including without limitation the rights
+    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    ' copies of the Software, and to permit persons to whom the Software is
+    ' furnished to do so, subject to the following conditions:
+    ' 
+    ' The above copyright notice and this permission notice shall be included in all
+    ' copies or substantial portions of the Software.
+    ' 
+    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    ' SOFTWARE.
+
+
+
+    ' /********************************************************************************/
+
+    ' Summaries:
+
+    ' Module inchiTest
+    ' 
+    '     Sub: Main
+    ' 
+    ' /********************************************************************************/
+
+#End Region
+
+Imports BioNovoGene.BioDeep.Chemoinformatics.IUPAC
+Imports BioNovoGene.BioDeep.Chemoinformatics.IUPAC.InChILayers
+
+Module inchiTest
+
+    Sub Main()
+
+        '    8 9      10
+        '    |  \    /
+        ' 7  2   3--4
+        '  \/ \ /   |
+        '  1   5    6
+        '     / \  / \
+        '    H   12   11
+        '
+
+        Dim ascorbicAcid As String = "InChI=1S/C6H8O6/c7-1-2(8)5-3(9)4(10)6(11)12-5/h2,5,7-10H,1H2/t2-,5+/m0/s1"
+
+        Dim inchi As New InChI(ascorbicAcid)
+
+        Dim key As String = inchi.Key
+
+        Dim bounds = MainLayer.ParseBounds("7-1-2(8)5-3(9)4(10)6(11)12-5").ToArray
+
+
+        Pause()
+    End Sub
+End Module
