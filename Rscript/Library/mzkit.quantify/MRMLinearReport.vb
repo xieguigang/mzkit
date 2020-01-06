9f55ba24c929cb439142d5715e644a855debba41
xieguigang <xie.guigang@gcmodeller.org>
2020-01-06 15:19:33 +0800

    do html reports

--- Rscript/Library/mzkit.quantify/MRMLinearReport.vb
+++ Rscript/Library/mzkit.quantify/MRMLinearReport.vb
@@ -1,6 +1,12 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Math
+Imports Microsoft.VisualBasic.Data.csv.DATA
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Scripting.SymbolBuilder
+Imports Microsoft.VisualBasic.Text.Xml
 
 Module MRMLinearReport
 
@@ -19,17 +25,59 @@ Module MRMLinearReport
                     <title>Linear Models</title>
                 </head>
                 <body>
+                    <h1>MRM Quantification Linear Models</h1>
 
+                    {$linears}
                 </body>
             </html>)
     End Function
 
     <Extension>
     Private Function doReport(report As ScriptBuilder, standardCurves As StandardCurve()) As String
-        For Each line As StandardCurve In standardCurves
+        Dim linears As New List(Of XElement)
+        Dim image As Image
+        Dim title$
 
+        For Each line As StandardCurve In standardCurves
+            title = line.points(Scan0).Name
+            image = Visual.DrawStandardCurve(line, title).AsGDIImage
+            linears +=
+                <div>
+                    <h2><%= title %></h2>
+
+                    <ul>
+                        <li>ID: <%= line.name %></li>
+                        <li>Linear: %s</li>
+                        <li>R<sup>2</sup>: <%= line.linear.CorrelationCoefficient %></li>
+                    </ul>
+
+                    <p>
+                        <img src=<%= New DataURI(image) %> style="width: 50%;"/>
+                    </p>
+
+                    <h3>Reference Points</h3>
+
+                    <p>
+                        %s
+                    </p>
+                </div>
         Next
 
+        report("linears") = linears _
+            .Select(Function(e, i)
+                        Return e.asset(standardCurves(i))
+                    End Function) _
+            .JoinBy(vbCrLf)
+
         Return report.ToString
+    End Function
+
+    <Extension>
+    Private Function asset(e As XElement, line As StandardCurve) As String
+        Dim equation$ = line.linear.Polynomial.ToString("G4", html:=True)
+        Dim title = line.points(Scan0).Name
+        Dim pointTable$ = line.points.ToHTML(title, $"Linear Model Reference Points of '{title}'")
+
+        Return sprintf(e, equation, pointTable)
     End Function
 End Module
