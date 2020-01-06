b67888836cd6418cf73a5007fc8923e233ba6cbd
xieguigang <xie.guigang@gcmodeller.org>
2020-01-06 17:43:55 +0800

    split source file

--- src/mzmath/TargetedMetabolomics/MRM/Data/MRMPeakTable.vb
+++ src/mzmath/TargetedMetabolomics/MRM/Data/MRMPeakTable.vb
@@ -54,10 +54,8 @@
 
 #End Region
 
-Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
 Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
-Imports stdNum = System.Math
 
 Namespace MRM.Data
 
@@ -130,58 +128,5 @@ Namespace MRM.Data
             Return Name
         End Function
 
-    End Class
-
-    ''' <summary>
-    ''' 表示标准曲线上面的一个实验数据点
-    ''' </summary>
-    Public Class MRMStandards
-
-        Public Property ID As String
-        Public Property Name As String
-
-        ''' <summary>
-        ''' 内标峰面积
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property AIS As Double
-        ''' <summary>
-        ''' 当前试验点的标准品峰面积
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property Ati As Double
-        ''' <summary>
-        ''' 内标浓度
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property cIS As Double
-        ''' <summary>
-        ''' 当前试验点的标准品浓度
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property Cti As Double
-        Public Property yfit As Double
-        Public ReadOnly Property [error] As Double
-            Get
-                Return stdNum.Abs(stdNum.Round(yfit - Cti, 4))
-            End Get
-        End Property
-
-        Public ReadOnly Property [variant] As Double
-            Get
-                Return stdNum.Round([error] / Cti, 4)
-            End Get
-        End Property
-
-        ''' <summary>
-        ''' 浓度梯度水平的名称，例如：``L1, L2, L3, ...``
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property level As String
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overrides Function ToString() As String
-            Return $"Dim {Name} As {ID} = {Cti}"
-        End Function
     End Class
 End Namespace
