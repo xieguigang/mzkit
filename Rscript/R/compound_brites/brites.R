5eb29e050e31f9cff1b06e66529fb481f7c353c7
xieguigang <xie.guigang@gcmodeller.org>
2020-01-10 17:51:52 +0800

    move tables to external folder

--- Rscript/R/compound_brites/brites.R
+++ Rscript/R/compound_brites/brites.R
@@ -1,5 +1,9 @@
 imports "kegg.brite" from "kegg_kit.dll";
 
+setwd(!script$dir);
+
+let brites <- list();
+
 for(id in ["br08001","br08002","br08003","br08005","br08006","br08007","br08008","br08009","br08010","br08021"]) {
 	id :> brite.parse 
 	   :> brite.as.table 
@@ -7,3 +11,4 @@ for(id in ["br08001","br08002","br08003","br08005","br08006","br08007","br08008"
 	   
 	print(id);
 }
+
