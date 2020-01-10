6e56c3c80a55dd1629c228cdef980e57f08f7635
xieguigang <xie.guigang@gcmodeller.org>
2020-01-10 18:16:55 +0800

    save r dataset

--- Rscript/R/compound_brites/brites.R
+++ Rscript/R/compound_brites/brites.R
@@ -2,7 +2,12 @@ imports "kegg.brite" from "kegg_kit.dll";
 
 setwd(!script$dir);
 
-let kegg_id as string <- ["br08001","br08002","br08003","br08005","br08006","br08007","br08008","br08009","br08010","br08021"];
+let kegg_id as string <- [
+	"br08001","br08002","br08003",
+	"br08005","br08006","br08007",
+	"br08008","br08009","br08010",
+	"br08021"
+];
 let brites <- lapply(kegg_id, id -> id :> brite.parse :> brite.as.table);
 
 for(id in names(brites)) {
@@ -12,3 +17,10 @@ for(id in names(brites)) {
 	brites[[id]] :> write.csv(file = `./${id}.csv`);
 }
 
+# save tables as rda files
+for(id in kegg_id) {
+	R.base::assign(id, value = R.utils.read::read.csv(`./${id}.csv`));
+}
+
+# write rda files that contains these brite id tables
+R.base::save(kegg_id, file = "../MetaDNA/data/KEGG_brites.rda");
