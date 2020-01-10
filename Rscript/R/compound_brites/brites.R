6fbe41a2de5b342057b22305ffb3ecdb1e3503dc
xieguigang <xie.guigang@gcmodeller.org>
2020-01-10 17:54:44 +0800

    code style improvements of the brite script

--- Rscript/R/compound_brites/brites.R
+++ Rscript/R/compound_brites/brites.R
@@ -2,12 +2,13 @@ imports "kegg.brite" from "kegg_kit.dll";
 
 setwd(!script$dir);
 
-let brites <- list();
+let kegg_id as string <- ["br08001","br08002","br08003","br08005","br08006","br08007","br08008","br08009","br08010","br08021"];
+let brites <- lapply(kegg_id, id -> id :> brite.parse :> brite.as.table);
 
-for(id in ["br08001","br08002","br08003","br08005","br08006","br08007","br08008","br08009","br08010","br08021"]) {
-	brites[[id]] <- id :> brite.parse :> brite.as.table;
-	brites[[id]] :> write.csv(file = `./${id}.csv`);
-	   
+for(id in names(brites)) {
 	print(id);
+	
+	# write id file
+	brites[[id]] :> write.csv(file = `./${id}.csv`);
 }
 
