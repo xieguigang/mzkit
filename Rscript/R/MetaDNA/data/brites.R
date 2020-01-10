9b2ad5a43867f0a59ab9abf01ffce20083193190
xieguigang <xie.guigang@gcmodeller.org>
2020-01-10 17:38:33 +0800

    Update brites.R

--- Rscript/R/MetaDNA/data/brites.R
+++ Rscript/R/MetaDNA/data/brites.R
@@ -4,4 +4,6 @@ for(id in ["br08001","br08002","br08003","br08005","br08006","br08007","br08008"
 	id :> brite.parse 
 	   :> brite.as.table 
 	   :> write.csv(file = `./${id}.csv`);
+	   
+	print(id);
 }
