0ed94e6623db7f90a5131fe7e6fe034be8c6c929
xieguigang <xie.guigang@gcmodeller.org>
2020-01-10 17:37:24 +0800

    Create brites.R
    write brite tables

---
+++ Rscript/R/MetaDNA/data/brites.R
@@ -0,0 +1,7 @@
+imports "kegg.brite" from "kegg_kit.dll";
+
+for(id in ["br08001","br08002","br08003","br08005","br08006","br08007","br08008","br08009","br08010","br08021"]) {
+	id :> brite.parse 
+	   :> brite.as.table 
+	   :> write.csv(file = `./${id}.csv`);
+}
