5c0497286aea206a982425d02af55b93367c346f
xieguigang <xie.guigang@gcmodeller.org>
2020-01-10 17:51:59 +0800

    Update brites.R

--- Rscript/R/compound_brites/brites.R
+++ Rscript/R/compound_brites/brites.R
@@ -5,9 +5,8 @@ setwd(!script$dir);
 let brites <- list();
 
 for(id in ["br08001","br08002","br08003","br08005","br08006","br08007","br08008","br08009","br08010","br08021"]) {
-	id :> brite.parse 
-	   :> brite.as.table 
-	   :> write.csv(file = `./${id}.csv`);
+	brites[[id]] <- id :> brite.parse :> brite.as.table;
+	brites[[id]] :> write.csv(file = `./${id}.csv`);
 	   
 	print(id);
 }
