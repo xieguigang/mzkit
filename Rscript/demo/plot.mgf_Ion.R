afb7760bef8ff88c3002f4bd156a17674183b9fc
xieguigang <xie.guigang@gcmodeller.org>
2020-01-13 18:10:37 +0800

    plot mgf ion test success

--- Rscript/demo/plot.mgf_Ion.R
+++ Rscript/demo/plot.mgf_Ion.R
@@ -1,8 +1,13 @@
 imports "mzkit.assembly" from "mzkit.dll";
 imports "mzkit.quantify.visual" from "mzkit.quantify.dll";
 
-["../../DATA\test\HCD_profiles.txt"]
-:> read.mgf()[1]
+setwd(!script$dir);
+
+let ions <- ["../../DATA\test\HCD_profiles.txt"]
+:> read.mgf()
+;
+
+ions[1]
 :> mass_spectrum.plot
 :> save.graphics(file = "./HCD_profiles.png")
 ;
