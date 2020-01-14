29845e4ab1cea92250383b86726f6d70e5705b37
xieguigang <xie.guigang@gcmodeller.org>
2020-01-14 09:56:56 +0800

    Update asciiView.R

--- Rscript/R/mzkit/R/asciiView.R
+++ Rscript/R/mzkit/R/asciiView.R
@@ -1,9 +1,9 @@
 #Region "Microsoft.ROpen::3f3edc3157844755e929b0e8ed80d940, asciiView.R"
 
-    # Summaries:
+# Summaries:
 
-    # asciiView <- function(spectra,into.cutoff = 0.05,maxWidth = 80,show.mz.labels = TRUE) {...
-    # lenOf <- function(x) {if (x < 10) {...
+# asciiView <- function(spectra,into.cutoff = 0.05,maxWidth = 80,show.mz.labels = TRUE) {...
+# lenOf <- function(x) {if (x < 10) {...
 
 #End Region
 
@@ -19,152 +19,152 @@
 #'   function for display this \code{spectra} on the console
 #'
 asciiView <- function(spectra,
-	into.cutoff = 0.05,
-	maxWidth = 80,
-	show.mz.labels = TRUE) {
+                      into.cutoff = 0.05,
+                      maxWidth = 80,
+                      show.mz.labels = TRUE) {
 
-	# Extract spectra matrix data
-	mz <- spectra[, 1] %=>% as.numeric;
-	# column steps in viewer
-	delta.mz <- (max(mz) - min(mz)) / (maxWidth - 10);
-	# cleanup matrix
-	into <- spectra[, 2] %=>% as.numeric;
-	into <- into / max(into);
-	mz <- mz[into >= into.cutoff];
-	into <- into[into >= into.cutoff];
+  # Extract spectra matrix data
+  mz <- spectra[, 1] %=>% as.numeric;
+  # column steps in viewer
+  delta.mz <- (max(mz) - min(mz)) / (maxWidth - 10);
+  # cleanup matrix
+  into <- spectra[, 2] %=>% as.numeric;
+  into <- into / max(into);
+  mz <- mz[into >= into.cutoff];
+  into <- into[into >= into.cutoff];
 
-	offset <- 6;
-	fill.1 <- rep(" ", times = offset);
-	fill.2 <- rep(" ", times = offset);
-	fill.3 <- rep(" ", times = offset);
-	fill.4 <- rep(" ", times = offset);
-	fill.5 <- rep(" ", times = offset);
-	fill.axis <- rep("-", times = offset);
-	labels <- list();
+  offset <- 6;
+  fill.1 <- rep(" ", times = offset);
+  fill.2 <- rep(" ", times = offset);
+  fill.3 <- rep(" ", times = offset);
+  fill.4 <- rep(" ", times = offset);
+  fill.5 <- rep(" ", times = offset);
+  fill.axis <- rep("-", times = offset);
+  labels <- list();
 
-	mzx <- min(mz);
-	mzl <- 0;
+  mzx <- min(mz);
+  mzl <- 0;
 
-	for(i in offset:maxWidth) {
-		mzi <- (mz > mzl) & (mz <= mzx);
-		mzl <- mzx;
-		mzx <- mzx + delta.mz;
+  for(i in offset:maxWidth) {
+    mzi <- (mz > mzl) & (mz <= mzx);
+    mzl <- mzx;
+    mzx <- mzx + delta.mz;
 
-		if (sum(mzi) == 0) {
-			# no fragment;
-			# fill empty
-			fill.1 <- append(fill.1, " ");
-			fill.2 <- append(fill.2, " ");
-			fill.3 <- append(fill.3, " ");
-			fill.4 <- append(fill.4, " ");
-			fill.5 <- append(fill.5, " ");
-			fill.axis <- append(fill.axis, "-");
-		} else {
-			int <- max(into[mzi]);
-			mz.i <- mz[int == into];
-			fill.axis <- append(fill.axis, "+");
-			labels[[as.character(i)]] <- mz.i;
+    if (sum(mzi) == 0) {
+      # no fragment;
+      # fill empty
+      fill.1 <- append(fill.1, " ");
+      fill.2 <- append(fill.2, " ");
+      fill.3 <- append(fill.3, " ");
+      fill.4 <- append(fill.4, " ");
+      fill.5 <- append(fill.5, " ");
+      fill.axis <- append(fill.axis, "-");
+    } else {
+      int <- max(into[mzi]);
+      mz.i <- mz[int == into];
+      fill.axis <- append(fill.axis, "+");
+      labels[[as.character(i)]] <- mz.i;
 
-			if (int >= 0.85) {
-				# fill 123
-				fill.1 <- append(fill.1, "|");
-				fill.2 <- append(fill.2, "|");
-				fill.3 <- append(fill.3, "|");
-				fill.4 <- append(fill.4, "|");
-				fill.5 <- append(fill.5, "|");
-			} else if (int >= 0.6) {
-				# fill 23
-				fill.1 <- append(fill.1, " ");
-				fill.2 <- append(fill.2, "|");
-				fill.3 <- append(fill.3, "|");
-				fill.4 <- append(fill.4, "|");
-				fill.5 <- append(fill.5, "|");
-			} else if (int >= 0.4) {
-				# fill 23
-				fill.1 <- append(fill.1, " ");
-				fill.2 <- append(fill.2, " ");
-				fill.3 <- append(fill.3, "|");
-				fill.4 <- append(fill.4, "|");
-				fill.5 <- append(fill.5, "|");
-			} else if (int >= 0.2) {
-				# fill 23
-				fill.1 <- append(fill.1, " ");
-				fill.2 <- append(fill.2, " ");
-				fill.3 <- append(fill.3, " ");
-				fill.4 <- append(fill.4, "|");
-				fill.5 <- append(fill.5, "|");
-			} else {
-				# fill 3
-				fill.1 <- append(fill.1, " ");
-				fill.2 <- append(fill.2, " ");
-				fill.3 <- append(fill.3, " ");
-				fill.4 <- append(fill.4, " ");
-				fill.5 <- append(fill.5, "|");
-			}
-		}
-	}
+      if (int >= 0.85) {
+        # fill 123
+        fill.1 <- append(fill.1, "|");
+        fill.2 <- append(fill.2, "|");
+        fill.3 <- append(fill.3, "|");
+        fill.4 <- append(fill.4, "|");
+        fill.5 <- append(fill.5, "|");
+      } else if (int >= 0.6) {
+        # fill 23
+        fill.1 <- append(fill.1, " ");
+        fill.2 <- append(fill.2, "|");
+        fill.3 <- append(fill.3, "|");
+        fill.4 <- append(fill.4, "|");
+        fill.5 <- append(fill.5, "|");
+      } else if (int >= 0.4) {
+        # fill 23
+        fill.1 <- append(fill.1, " ");
+        fill.2 <- append(fill.2, " ");
+        fill.3 <- append(fill.3, "|");
+        fill.4 <- append(fill.4, "|");
+        fill.5 <- append(fill.5, "|");
+      } else if (int >= 0.2) {
+        # fill 23
+        fill.1 <- append(fill.1, " ");
+        fill.2 <- append(fill.2, " ");
+        fill.3 <- append(fill.3, " ");
+        fill.4 <- append(fill.4, "|");
+        fill.5 <- append(fill.5, "|");
+      } else {
+        # fill 3
+        fill.1 <- append(fill.1, " ");
+        fill.2 <- append(fill.2, " ");
+        fill.3 <- append(fill.3, " ");
+        fill.4 <- append(fill.4, " ");
+        fill.5 <- append(fill.5, "|");
+      }
+    }
+  }
 
-	a <- paste(fill.1, collapse = "");
-	b <- paste(fill.2, collapse = "");
-	c <- paste(fill.3, collapse = "");
-	d <- paste(fill.4, collapse = "");
-	e <- paste(fill.5, collapse = "");
-	f <- paste(fill.axis, collapse = "");
+  a <- paste(fill.1, collapse = "");
+  b <- paste(fill.2, collapse = "");
+  c <- paste(fill.3, collapse = "");
+  d <- paste(fill.4, collapse = "");
+  e <- paste(fill.5, collapse = "");
+  f <- paste(fill.axis, collapse = "");
 
-	views <- c("", a, b, c, d, e, f);
-	lenOf <- function(x) {
-		if (x < 10) {
-			1;
-		} else if (x < 100) {
-			2;
-		} else if (x < 1000) {
-			3;
-		} else if (x < 10000) {
-			4;
-		}
-	}
+  views <- c("", a, b, c, d, e, f);
+  lenOf <- function(x) {
+    if (x < 10) {
+      1;
+    } else if (x < 100) {
+      2;
+    } else if (x < 1000) {
+      3;
+    } else if (x < 10000) {
+      4;
+    }
+  }
 
-	if (show.mz.labels) {
-		first <- "m/z:  ";
-		label_matrix <- list();
+  if (show.mz.labels) {
+    first <- "m/z:  ";
+    label_matrix <- list();
 
-		for(i in offset:maxWidth) {
-		    if (is.null(labels[[as.character(i)]])) {
-				first <- append(first, " ");
-			} else {
-				first <- append(first, "|");
-			}
-		}
+    for(i in offset:maxWidth) {
+      if (is.null(labels[[as.character(i)]])) {
+        first <- append(first, " ");
+      } else {
+        first <- append(first, "|");
+      }
+    }
 
-		views <- append(views, paste(first, collapse = ""));
-		d <- 2;
-		index <- names(labels)
+    views <- append(views, paste(first, collapse = ""));
+    d <- 2;
+    index <- names(labels)
 
-		for(j in index) {
-			mz <- labels[[j]];
-			row <- round(mz) %=>% as.character;
-			n <- lenOf(mz) + d;
-			row <- append(rep(" ", times = offset - n), row);
-			row <- append(row, rep(" ", times = d));
-			labels[[j]] <- NULL;
+    for(j in index) {
+      mz <- labels[[j]];
+      row <- round(mz) %=>% as.character;
+      n <- lenOf(mz) + d;
+      row <- append(rep(" ", times = offset - n), row);
+      row <- append(row, rep(" ", times = d));
+      labels[[j]] <- NULL;
 
-			j <- as.numeric(j);
+      j <- as.numeric(j);
 
-			for (i in offset:maxWidth) {
-				if (i == j) {
-					row <- append(row, "+");
-				} else if (i < j) {
-					row <- append(row, "-");
-				} else if (is.null(labels[[as.character(i)]])) {
-					row <- append(row, " ");
-				} else {
-					row <- append(row, "|");
-				}
-			}
+      for (i in offset:maxWidth) {
+        if (i == j) {
+          row <- append(row, "+");
+        } else if (i < j) {
+          row <- append(row, "-");
+        } else if (is.null(labels[[as.character(i)]])) {
+          row <- append(row, " ");
+        } else {
+          row <- append(row, "|");
+        }
+      }
 
-			views <- append(views, paste(row, collapse = ""));
-		}
-	}
+      views <- append(views, paste(row, collapse = ""));
+    }
+  }
 
-	paste(append(views, ""), collapse = "\n");
+  paste(append(views, ""), collapse = "\n");
 }
