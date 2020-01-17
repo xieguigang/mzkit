6ba7c6b342af3342807d266f34100358a4bdef2d
xieguigang <xie.guigang@gcmodeller.org>
2020-01-17 09:47:24 +0800

    adds test script

---
+++ dist/Rscript/raw2mzXML.R
@@ -0,0 +1,19 @@
+require(ProteoWizard);
+
+let sample as string = ?"--samples" || stop("No raw samples data directory provided!");
+let output as string = ?"--output"  || `${dirname(sample)}/mzXML/` ;
+let raw as string   = list.files(sample, "*.raw");
+
+if (!msconvert.ready()) {
+	# missing ProteoWizard configuration data or its configuration value
+	# invalids
+	# do ProteoWizard configuration via commandline input
+	let msconvert = ?"--msconvert" || stop("Missing 'ProteoWizard' configuration or its configuration value is invalids!");
+	
+	# msconvert is the file path of the 
+	# program ``msconvert.exe`` in ProteoWizard.
+	options(ProteoWizard = msconvert);
+}
+
+print(`we have ${length(raw)} raw files data for convert to mzXML files`);
+print(basename(raw, TRUE));
