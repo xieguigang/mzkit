18fcb89484644d280faa813035daaebd6f57ac02
xieguigang <xie.guigang@gcmodeller.org>
2020-02-10 15:25:46 +0800

    add kcf api

--- Rscript/Library/mzkit.insilicons/ms2_simulator.vb
+++ Rscript/Library/mzkit.insilicons/ms2_simulator.vb
@@ -1,6 +1,22 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Insilicon
+Imports BioNovoGene.BioDeep.Chemistry.Model
+Imports BioNovoGene.BioDeep.Chemistry.Model.Graph
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Scripting.MetaData
 
 <Package("mzkit.simulator")>
 Module ms2_simulator
 
+    <ExportAPI("read.kcf")>
+    Public Function loadKCF(file As String) As KCF
+        Return file.LoadKCF(False)
+    End Function
+
+    <ExportAPI("molecular.graph")>
+    Public Function MolecularGraph(mol As KCF) As NetworkGraph
+        Return mol _
+           .CreateGraph _
+           .FillBoundEnergy(New BoundEnergyFinder)
+    End Function
 End Module
