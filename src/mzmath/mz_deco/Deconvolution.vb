e1360c6bd30da78ab86ab7fe9ba200ef5cd1840e
xieguigang <xie.guigang@gcmodeller.org>
2020-02-15 18:02:05 +0800

    add dev note

--- src/mzmath/mz_deco/Deconvolution.vb
+++ src/mzmath/mz_deco/Deconvolution.vb
@@ -1,6 +1,16 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports scan = BioNovoGene.Analytical.MassSpectrometry.Math.IMs1Scan
 
+''' <summary>
+''' #### 解卷积计算步骤
+''' 
+''' 1. 首先对每一个原始数据点按照mz进行数据分组
+''' 2. 对每一个mz数据分组按照rt进行升序排序
+''' 3. 对每一个mz数据分组进行解卷积，得到峰列表
+''' 4. 输出peaktable结果，完成解卷积操作
+''' </summary>
 Public Module Deconvolution
 
     ''' <summary>
@@ -16,5 +26,13 @@ Public Module Deconvolution
         Throw New NotImplementedException
     End Function
 
+    ''' <summary>
+    ''' 进行原始数据的mz分组操作，然后进行rt的升序排序
+    ''' </summary>
+    ''' <param name="scans"></param>
+    ''' <returns></returns>
+    Public Function GetMzGroup(scans As IEnumerable(Of scan), Optional tolerance As Tolerance = Nothing) As IEnumerable(Of scan())
+
+    End Function
 
 End Module
