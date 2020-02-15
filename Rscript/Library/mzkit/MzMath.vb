53d612ababd3368f4cf9cd60b208b7c020421b04
xieguigang <xie.guigang@gcmodeller.org>
2020-02-15 18:44:12 +0800

    get ms1 scan data

--- Rscript/Library/mzkit/MzMath.vb
+++ Rscript/Library/mzkit/MzMath.vb
@@ -1,7 +1,9 @@
-﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
 Imports REnv = SMRUCC.Rsharp.Runtime.Internal.ConsolePrinter
 
 <Package("mzkit.math")>
@@ -18,5 +20,16 @@ Module MzMath
     <ExportAPI("mz")>
     Public Function mz(mass As Double, Optional mode As Object = "+") As MzReport()
         Return MzCalculator.Calculate(mass, Scripting.ToString(mode, "+")).ToArray
+    End Function
+
+    ''' <summary>
+    ''' Chromatogram data deconvolution
+    ''' </summary>
+    ''' <param name="ms1"></param>
+    ''' <param name="tolerance"></param>
+    ''' <returns></returns>
+    <ExportAPI("mz.deco")>
+    Public Function mz_deco(<RRawVectorArgument> ms1 As Object, Optional tolerance As Tolerance = Nothing) As Object
+
     End Function
 End Module
