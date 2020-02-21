3268ad6d3985db80ca7d865a4eb335609f50764f
xieguigang <xie.guigang@gcmodeller.org>
2020-02-21 10:39:01 +0800

    add kegg reaction class data model

---
+++ src/metadna/KEGG_MetaDNA/ReactionLink.vb
@@ -0,0 +1,7 @@
+﻿Public Class ReactionLink
+    Public Property rxnID As String
+    Public Property name As String
+    Public Property define As String
+    Public Property reactants As String()
+    Public Property products As String()
+End Class
