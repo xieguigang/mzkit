3a8415e18aa099529d2340904e8aa66a58510ad4
xieguigang <xie.guigang@gcmodeller.org>
2020-03-09 11:48:09 +0800

    read kegg reaction class as table

--- Rscript/Library/mzkit.insilicons/metaDNA.vb
+++ Rscript/Library/mzkit.insilicons/metaDNA.vb
@@ -1,10 +1,15 @@
 ﻿
 Imports MetaDNA.visual
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Language.UnixBash
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports kegReactionClass = SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject.ReactionClass
+Imports ReactionClassTbl = MetaDNA.visual.ReactionClass
 Imports REnv = SMRUCC.Rsharp.Runtime.Internal
 
 <Package("mzkit.metadna")>
@@ -24,5 +29,36 @@ Module metaDNA
         End If
 
         Return DirectCast(debugOutput, Global.MetaDNA.visual.XML).CreateGraph
+    End Function
+
+    <ExportAPI("reaction_class.table")>
+    <RApiReturn(GetType(ReactionClassTbl()))>
+    Public Function readReactionClassTable(file As String, Optional env As Environment = Nothing) As Object
+        If file.ExtensionSuffix("csv") Then
+            Return file.LoadCsv(Of ReactionClassTbl)
+        ElseIf file.DirectoryExists Then
+            Dim loadFolder = Iterator Function() As IEnumerable(Of kegReactionClass)
+                                 For Each xml As String In ls - l - r - "*.xml" <= file
+                                     Yield xml.LoadXml(Of kegReactionClass)
+                                 Next
+                             End Function
+
+            Return loadFolder() _
+                .Select(Function(cls)
+                            Return cls.reactantPairs _
+                                .Select(Function(r)
+                                            Return New ReactionClassTbl With {
+                                                .define = cls.definition,
+                                                .from = r.from,
+                                                .[to] = r.to,
+                                                .rId = cls.entryId
+                                            }
+                                        End Function)
+                        End Function) _
+                .IteratesALL _
+                .ToArray
+        Else
+            Return Internal.debug.stop($"unable to determin the data source type of the given file '{file}'", env)
+        End If
     End Function
 End Module
---
+++ src/metadna/MetaDNA_visual/ReactionClass.vb
@@ -0,0 +1,8 @@
+﻿Public Class ReactionClass
+
+    Public Property rId As String
+    Public Property from As String
+    Public Property [to] As String
+    Public Property define As String
+
+End Class
