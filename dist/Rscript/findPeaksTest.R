ce1e4934e98d40977e5be5b36519643d67453d9d
xieguigang <xie.guigang@gcmodeller.org>
2020-03-10 10:06:01 +0800

    Create findPeaksTest.R

---
+++ dist/Rscript/findPeaksTest.R
@@ -0,0 +1,9 @@
+imports ["mzkit.mrm", "mzkit.quantify.visual"] from "mzkit.quantify.dll";
+
+let name as string = "NorDCA";
+let data = [`S:\胆汁酸保留时间漂移测试\20200309三条标曲\test\cal.chromatogramPlots\20200305-cal10\chromatogramROI/${name}.csv`]
+:> read.csv
+:> as.chromatogram("Time", "Intensity")
+:> peakROI
+:> print
+;
