9118847645b0964cfa03da536240a540375a9ae8
xieguigang <xie.guigang@gcmodeller.org>
2020-03-10 09:43:57 +0800

    fix code merge conflicts

--- src/mzmath/TargetedMetabolomics/QuantitativeAnalysis/TPAExtensions.vb
+++ src/mzmath/TargetedMetabolomics/QuantitativeAnalysis/TPAExtensions.vb
@@ -152,15 +152,9 @@ Public Module TPAExtensions
             .OrderBy(Function(r) r.Time.Min) _
             .ToArray
 
-		' 20200309 因为噪声的积分面积可能会大于目标物质峰
-		' 的面积，所以在这里应该是使用峰高进行ROI的排序操
-		' 作
-			
-        Dim region As ROI = ROIData _
-            .Where(Function(r)
-                       Return r.Time.IsOverlapping(find)
-                   End Function) _
-            .FirstOrDefault
+        ' 20200309 因为噪声的积分面积可能会大于目标物质峰
+        ' 的面积，所以在这里应该是使用峰高进行ROI的排序操
+        ' 作
         If Not target.rt Is Nothing Then
             ' 20200304
             ' System.InvalidOperationException: Nullable object must have a value.
