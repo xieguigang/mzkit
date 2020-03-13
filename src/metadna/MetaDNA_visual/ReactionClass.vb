49551118792d225560915dcc413049b1ec84caad
xieguigang <xie.guigang@gcmodeller.org>
2020-03-13 11:08:23 +0800

    Update ReactionClass.vb

--- src/metadna/MetaDNA_visual/ReactionClass.vb
+++ src/metadna/MetaDNA_visual/ReactionClass.vb
@@ -3,6 +3,7 @@
 Public Class ReactionClass : Implements INamedValue
 
     Public Property rId As String Implements INamedValue.Key
+    Public Property category As Integer
     Public Property from As String
     Public Property [to] As String
     Public Property define As String
