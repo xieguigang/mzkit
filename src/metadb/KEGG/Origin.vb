88c2fff0e80e7ce06c777cf0f3eac8600d627553
xieguigang <xie.guigang@gcmodeller.org>
2020-06-28 15:43:32 +0800

    Update Origin.vb

--- src/metadb/KEGG/Origin.vb
+++ src/metadb/KEGG/Origin.vb
@@ -4,4 +4,21 @@
 ''' </summary>
 Public Class Origin
 
+    ''' <summary>
+    ''' kegg id of the given compounds
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property kegg_id As String
+    ''' <summary>
+    ''' ncbi taxonomy id
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property ncbi_taxonomy As String()
+
+
+    Public Shared Function CreateOrigin() As Origin
+
+    End Function
+
 End Class
+
