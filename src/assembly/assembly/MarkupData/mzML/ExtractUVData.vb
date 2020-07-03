a1c47ef89d2622d16cb660c59f02d221e7356a72
xieguigang <xie.guigang@gcmodeller.org>
2020-07-03 16:13:36 +0800

    add meta data for the generated UV spectrum signals

--- src/assembly/assembly/MarkupData/mzML/ExtractUVData.vb
+++ src/assembly/assembly/MarkupData/mzML/ExtractUVData.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Math.SignalProcessing
+Imports Microsoft.VisualBasic.Text.Xml
 Imports Microsoft.VisualBasic.Text.Xml.Linq
 
 Namespace MarkupData.mzML
@@ -21,20 +22,29 @@ Namespace MarkupData.mzML
         Public Iterator Function PopulatesElectromagneticRadiationSpectrum(spectrums As IEnumerable(Of spectrum), instrumentConfigurationId As String) As IEnumerable(Of GeneralSignal)
             For Each rawScan As spectrum In spectrums
                 If rawScan.scanList.scans.Any(Function(scan) scan.instrumentConfigurationRef = instrumentConfigurationId) Then
-                    Yield rawScan.CreateGeneralSignal
+                    Yield rawScan.CreateGeneralSignal(instrumentConfigurationId)
                 End If
             Next
         End Function
 
         <Extension>
-        Private Function CreateGeneralSignal(rawScan As spectrum) As GeneralSignal
+        Private Function CreateGeneralSignal(rawScan As spectrum, instrumentConfigurationId As String) As GeneralSignal
             Const type As String = "electromagnetic radiation spectrum"
 
             Dim descriptor As Double() = rawScan.binaryDataArrayList.list(Scan0).Base64Decode
             Dim intensity As Double() = rawScan.binaryDataArrayList.list(1).Base64Decode
+            Dim info As New Dictionary(Of String, String)
+            Dim UVscan As scan = rawScan.scanList.scans.First(Function(a) a.instrumentConfigurationRef = instrumentConfigurationId)
+
+            info.Add("total_ion_current", rawScan.cvParams.KeyItem("total ion current")?.value)
+            info.Add("lowest_wavelength", rawScan.cvParams.KeyItem("lowest observed wavelength")?.value)
+            info.Add("highest_wavelength", rawScan.cvParams.KeyItem("highest observed wavelength")?.value)
+            info.Add("title", XmlEntity.UnescapingXmlEntity(rawScan.cvParams.KeyItem("spectrum title")?.value))
+            info.Add("scan_time", Val(UVscan.cvParams.KeyItem("scan start time")?.value) * 60)
 
             Return New GeneralSignal With {
                 .description = type,
+                .meta = info,
                 .measureUnit = "wavelength(nanometer)",
                 .Measures = descriptor,
                 .Strength = intensity
