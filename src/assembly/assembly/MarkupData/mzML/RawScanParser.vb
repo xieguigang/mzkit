81d459e8f8d550d475ce230f0219497948dc1a6f
xieguigang <xie.guigang@gcmodeller.org>
2020-07-06 10:35:46 +0800

    unsrure about the raw scan data parser model

---
+++ src/assembly/assembly/MarkupData/mzML/RawScanParser.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic.Linq
+
+Namespace MarkupData.mzML
+
+    Public Class RawScanParser
+
+
+        Public Shared Function CreateScanReader(rawScan As spectrum, basename$) As RawScanParser
+            Dim msLevel As Integer = rawScan.ms_level.DoCall(AddressOf ParseInteger)
+
+            Throw New NotImplementedException
+        End Function
+
+    End Class
+
+    Public Class Ms1RawScan : Inherits RawScanParser
+
+
+    End Class
+
+    Public Class MsnRawScan : Inherits RawScanParser
+
+    End Class
+End Namespace
