6e09b85407b2b23b3a8f9435b25ed261561b5b62
xieguigang <xie.guigang@gcmodeller.org>
2020-07-16 11:33:25 +0800

    UV signal plot

---
+++ src/visualize/plot/UVsignalPlot.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
+Imports Microsoft.VisualBasic.Data.ChartPlots
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Legend
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Math.SignalProcessing
+
+Public Module UVsignalPlot
+
+    Public Function Plot(signals As IEnumerable(Of GeneralSignal),
+                         Optional size As String = "1600,1200",
+                         Optional padding As String = g.DefaultPadding,
+                         Optional colorSet As String = "Set1:c8",
+                         Optional pt_size As Single = 8,
+                         Optional line_width As Single = 5) As GraphicsData
+
+        Dim colors As LoopArray(Of Color) = Designer.GetColors(colorSet)
+        Dim data As SerialData() = signals _
+            .Select(Function(line)
+                        Return New SerialData With {
+                            .color = colors.Next,
+                            .lineType = DashStyle.Solid,
+                            .pointSize = pt_size,
+                            .shape = LegendStyles.Triangle,
+                            .width = line_width,
+                            .title = line.reference,
+                            .pts = line _
+                                .PopulatePoints _
+                                .Select(Function(p)
+                                            Return New PointData(p)
+                                        End Function) _
+                                .ToArray
+                        }
+                    End Function) _
+            .ToArray
+
+        Return Scatter.Plot(
+            c:=data,
+            size:=size,
+            padding:=padding,
+            Xlabel:="time(sec)",
+            Ylabel:="intensity"
+        )
+    End Function
+End Module
