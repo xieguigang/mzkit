7270f688435b3a6e3a785c3489567e5728285f57
xieguigang <xie.guigang@gcmodeller.org>
2020-08-10 20:58:53 +0800

    add forms

--- src/mzkit/forms/frmMain.Designer.vb
+++ src/mzkit/forms/frmMain.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,16 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "frmMain"
         Me.Text = "Form1"
-    End Sub
+        Me.ResumeLayout(False)
 
+    End Sub
 End Class
---
+++ src/mzkit/forms/frmMain.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmMain
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
