96a86d851cc6a065d8542bfb05e6f9db37cc1c3b
xieguigang <xie.guigang@gcmodeller.org>
2020-08-10 21:57:58 +0800

    export data

--- src/mzkit/mzkit/forms/tools/frmCalculator.Designer.vb
+++ src/mzkit/mzkit/forms/tools/frmCalculator.Designer.vb
@@ -28,13 +28,33 @@ Partial Class frmCalculator
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.PrecursorType = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Adducts = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.M = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Charge = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PrecursorMz = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 29)
+        Me.Label1.Location = New System.Drawing.Point(12, 39)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(129, 13)
         Me.Label1.TabIndex = 0
@@ -42,14 +62,14 @@ Partial Class frmCalculator
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(147, 29)
+        Me.TextBox1.Location = New System.Drawing.Point(164, 36)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(304, 20)
         Me.TextBox1.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(474, 29)
+        Me.Button1.Location = New System.Drawing.Point(491, 36)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(111, 23)
         Me.Button1.TabIndex = 2
@@ -68,6 +88,7 @@ Partial Class frmCalculator
         '
         'TabPage1
         '
+        Me.TabPage1.Controls.Add(Me.DataGridView1)
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
@@ -78,6 +99,7 @@ Partial Class frmCalculator
         '
         'TabPage2
         '
+        Me.TabPage2.Controls.Add(Me.DataGridView2)
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
@@ -85,6 +107,98 @@ Partial Class frmCalculator
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "Negative"
         Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.PrecursorType, Me.Adducts, Me.M, Me.Charge, Me.PrecursorMz})
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(740, 728)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'PrecursorType
+        '
+        Me.PrecursorType.HeaderText = "Precursor Type"
+        Me.PrecursorType.Name = "PrecursorType"
+        '
+        'Adducts
+        '
+        Me.Adducts.HeaderText = "Adducts"
+        Me.Adducts.Name = "Adducts"
+        '
+        'M
+        '
+        Me.M.HeaderText = "M"
+        Me.M.Name = "M"
+        '
+        'Charge
+        '
+        Me.Charge.HeaderText = "Charge"
+        Me.Charge.Name = "Charge"
+        '
+        'PrecursorMz
+        '
+        Me.PrecursorMz.HeaderText = "Precursor m/z"
+        Me.PrecursorMz.Name = "PrecursorMz"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn5})
+        Me.DataGridView2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView2.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(740, 728)
+        Me.DataGridView2.TabIndex = 1
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Precursor Type"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Adducts"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.HeaderText = "M"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Charge"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.HeaderText = "Precursor m/z"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(781, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExportToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExportToolStripMenuItem
+        '
+        Me.ExportToolStripMenuItem.Name = "ExportToolStripMenuItem"
+        Me.ExportToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExportToolStripMenuItem.Text = "Export"
         '
         'frmCalculator
         '
@@ -95,10 +209,18 @@ Partial Class frmCalculator
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "frmCalculator"
         Me.Text = "m/z Calculator"
         Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -110,4 +232,19 @@ Partial Class frmCalculator
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents PrecursorType As DataGridViewTextBoxColumn
+    Friend WithEvents Adducts As DataGridViewTextBoxColumn
+    Friend WithEvents M As DataGridViewTextBoxColumn
+    Friend WithEvents Charge As DataGridViewTextBoxColumn
+    Friend WithEvents PrecursorMz As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As DataGridViewTextBoxColumn
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExportToolStripMenuItem As ToolStripMenuItem
 End Class
--- src/mzkit/mzkit/forms/tools/frmCalculator.vb
+++ src/mzkit/mzkit/forms/tools/frmCalculator.vb
@@ -1,3 +1,52 @@
-﻿Public Class frmCalculator
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Linq
 
+Public Class frmCalculator
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim exact_mass As Double = Val(TextBox1.Text)
+
+        Call Update(exact_mass, Provider.GetCalculator("+").Values, DataGridView1)
+        Call Update(exact_mass, Provider.GetCalculator("-").Values, DataGridView2)
+    End Sub
+
+    Overloads Sub Update(exact_mass As Double, mode As IEnumerable(Of MzCalculator), show As DataGridView)
+        Dim mzText As String
+
+        Call show.Rows.Clear()
+
+        For Each type As MzCalculator In mode
+            mzText = type.CalcMZ(exact_mass)
+            mzText = If(Val(mzText) < 0, "n/a", mzText)
+
+            Call show.Rows.Add(type.ToString, type.adducts, type.M, type.charge, mzText)
+        Next
+    End Sub
+
+    Private Sub ExportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportToolStripMenuItem.Click
+        Using file As New SaveFileDialog With {.Filter = "Excel Table|*.csv"}
+            If file.ShowDialog = DialogResult.OK Then
+                Dim currentTab As DataGridView = (From ctrl As Control In TabControl1.SelectedTab.Controls Where TypeOf ctrl Is DataGridView).First
+                Dim table As New List(Of PrecursorInfo)
+
+                For Each row As DataGridViewRow In currentTab.Rows
+                    If row.Cells(0).Value Is Nothing Then
+                        Exit For
+                    End If
+
+                    Call New PrecursorInfo With {
+                        .precursor_type = row.Cells(0).Value.ToString,
+                        .adduct = CDbl(row.Cells(1).Value.ToString),
+                        .M = CInt(row.Cells(2).Value.ToString),
+                        .charge = CDbl(row.Cells(3).Value),
+                        .mz = row.Cells(4).Value.ToString
+                    }.DoCall(AddressOf table.Add)
+                Next
+
+                Call table.SaveTo(file.FileName)
+                Call MessageBox.Show($"Export table to [{file.FileName}] success!", "Table Export", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End Using
+    End Sub
 End Class
