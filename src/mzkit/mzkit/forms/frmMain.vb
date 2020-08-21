524d18c66ff19b451e1d9a666596bb30111f449d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-21 19:44:52 +0800

    recent items

--- src/mzkit/mzkit/forms/frmMain.vb
+++ src/mzkit/mzkit/forms/frmMain.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Threading
+Imports RibbonLib
 Imports RibbonLib.Controls.Events
+Imports RibbonLib.Interop
 
 Public Class frmMain
 
@@ -100,6 +102,7 @@ Public Class frmMain
 
     Private m_ChildFormNumber As Integer
     Dim ribbonItems As RibbonItems
+    Private _recentItems As List(Of RecentItemsPropertySet)
 
     Public Sub New()
 
@@ -119,6 +122,61 @@ Public Class frmMain
     End Sub
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitRecentItems()
         ToolStripStatusLabel.Text = "Ready!"
+    End Sub
+
+    Private Sub InitRecentItems()
+        ' prepare list of recent items
+        _recentItems = New List(Of RecentItemsPropertySet)()
+        _recentItems.Add(New RecentItemsPropertySet() With {.Label = "Recent item 1", .LabelDescription = "Recent item 1 description", .Pinned = True})
+        _recentItems.Add(New RecentItemsPropertySet() With {.Label = "Recent item 2", .LabelDescription = "Recent item 2 description", .Pinned = False})
+
+        ribbonItems.RecentItems.RecentItems = _recentItems
+    End Sub
+
+    Private Sub _recentItems_ExecuteEvent(ByVal sender As Object, ByVal e As ExecuteEventArgs)
+        If e.Key.PropertyKey = RibbonProperties.RecentItems Then
+            ' go over recent items
+            Dim objectArray() As Object = CType(e.CurrentValue.PropVariant.Value, Object())
+            For i As Integer = 0 To objectArray.Length - 1
+                Dim propertySet As IUISimplePropertySet = TryCast(objectArray(i), IUISimplePropertySet)
+
+                If propertySet IsNot Nothing Then
+                    Dim propLabel As PropVariant
+                    propertySet.GetValue(RibbonProperties.Label, propLabel)
+                    Dim label As String = CStr(propLabel.Value)
+
+                    Dim propLabelDescription As PropVariant
+                    propertySet.GetValue(RibbonProperties.LabelDescription, propLabelDescription)
+                    Dim labelDescription As String = CStr(propLabelDescription.Value)
+
+                    Dim propPinned As PropVariant
+                    propertySet.GetValue(RibbonProperties.Pinned, propPinned)
+                    Dim pinned As Boolean = CBool(propPinned.Value)
+
+                    ' update pinned value
+                    _recentItems(i).Pinned = pinned
+                End If
+            Next i
+        ElseIf e.Key.PropertyKey = RibbonProperties.SelectedItem Then
+            ' get selected item index
+            Dim selectedItem As UInteger = CUInt(e.CurrentValue.PropVariant.Value)
+
+            ' get selected item label
+            Dim propLabel As PropVariant
+            e.CommandExecutionProperties.GetValue(RibbonProperties.Label, propLabel)
+            Dim label As String = CStr(propLabel.Value)
+
+            ' get selected item label description
+            Dim propLabelDescription As PropVariant
+            e.CommandExecutionProperties.GetValue(RibbonProperties.LabelDescription, propLabelDescription)
+            Dim labelDescription As String = CStr(propLabelDescription.Value)
+
+            ' get selected item pinned value
+            Dim propPinned As PropVariant
+            e.CommandExecutionProperties.GetValue(RibbonProperties.Pinned, propPinned)
+            Dim pinned As Boolean = CBool(propPinned.Value)
+        End If
     End Sub
 End Class
