5ecff5c17e52e35682905d56e970f48604f1fffe
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-21 21:30:27 +0800

    imprvements of the splash screen

--- src/mzkit/mzkit/My Project/Application.Designer.vb
+++ src/mzkit/mzkit/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.mzkit.frmMain
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.mzkit.frmSplashScreen
+        End Sub
     End Class
 End Namespace
--- src/mzkit/mzkit/forms/frmSplashScreen.vb
+++ src/mzkit/mzkit/forms/frmSplashScreen.vb
@@ -3,12 +3,24 @@
     Public Property isAboutScreen As Boolean = False
 
     Private Sub frmSplashScreen_Deactivate(sender As Object, e As EventArgs) Handles Me.Deactivate
-
-    End Sub
-
-    Private Sub frmSplashScreen_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
         If isAboutScreen Then
             Call Me.Close()
         End If
+    End Sub
+
+    Private Sub frmSplashScreen_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://mzkit.org/")
+    End Sub
+
+    Private Sub frmSplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub frmSplashScreen_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        e.Graphics.DrawRectangle(New Pen(Color.Black, 1), New Rectangle(0, 0, Width, Height))
     End Sub
 End Class
