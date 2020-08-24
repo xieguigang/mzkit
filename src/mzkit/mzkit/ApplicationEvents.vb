b6737f2c5a291753c5b523242c9a5da2eed2c392
xieguigang <xie.guigang@gcmodeller.org>
2020-08-24 10:07:09 +0800

    Update ApplicationEvents.vb

--- src/mzkit/mzkit/ApplicationEvents.vb
+++ src/mzkit/mzkit/ApplicationEvents.vb
@@ -1,4 +1,6 @@
-﻿Namespace My
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+
+Namespace My
     ' The following events are available for MyApplication:
     ' Startup: Raised when the application starts, before the startup form is created.
     ' Shutdown: Raised after all application forms are closed.  This event is not raised if the application terminates abnormally.
@@ -6,5 +8,9 @@
     ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
     ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
     Partial Friend Class MyApplication
+
+        Private Sub MyApplication_UnhandledException(sender As Object, e As UnhandledExceptionEventArgs) Handles Me.UnhandledException
+            Call App.LogException(e.Exception)
+        End Sub
     End Class
 End Namespace
