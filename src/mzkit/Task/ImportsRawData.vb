19c9db86bb56570a5f3222f1f073f56d615f9415
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-24 09:58:17 +0800

    imports mzML

--- src/mzkit/Task/ImportsRawData.vb
+++ src/mzkit/Task/ImportsRawData.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Threading
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.mzML
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.mzXML
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports Microsoft.VisualBasic.Data.IO.netCDF
@@ -7,6 +8,7 @@ Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text
+Imports mzML = BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.mzML
 
 Public Class ImportsRawData
 
@@ -74,6 +76,47 @@ Public Class ImportsRawData
     End Sub
 
     Private Sub importsMzML()
+        Using cache As New CDFWriter(temp, Encodings.UTF8WithoutBOM)
+            Dim attrs As attribute()
+            Dim data As New List(Of Double)
+            Dim name As String
+            Dim nscans As New List(Of ScanEntry)
 
+            For Each scan As spectrum In mzML.Xml.LoadScans(source)
+                ' 在这里的attribute name需要与mzXML的名称保持一致
+                attrs = {
+                    New attribute With {.name = NameOf(mzXML.scan.msLevel), .type = CDFDataTypes.INT, .value = scan.ms_level},
+                    New attribute With {.name = NameOf(mzXML.scan.collisionEnergy), .type = CDFDataTypes.CHAR, .value = scan.precursorList.precursor(Scan0).GetCollisionEnergy},
+                    New attribute With {.name = NameOf(mzXML.scan.centroided), .type = CDFDataTypes.CHAR, .value = Not scan.profile},
+                    New attribute With {.name = NameOf(mzXML.scan.precursorMz), .type = CDFDataTypes.DOUBLE, .value = scan.selectedIon.mz},
+                    New attribute With {.name = NameOf(mzXML.scan.retentionTime), .type = CDFDataTypes.DOUBLE, .value = scan.scan_time}
+                }
+
+                Dim mz = scan.ByteArray("m/z array").Base64Decode
+                Dim intensity = scan.ByteArray("intensity array").Base64Decode
+
+                For i As Integer = 0 To mz.Length - 1
+                    data.Add(intensity(i))
+                    data.Add(mz(i))
+                Next
+
+                name = scan.ToString
+                cache.AddVariable(name, New CDFData With {.numerics = data}, New Dimension With {.name = "m/z-int,scan_" & scan.scan, .size = data.Count}, attrs)
+
+                Call New ScanEntry With {
+                    .id = name,
+                    .mz = scan.selectedIon.mz,
+                    .rt = scan.scan_time,
+                    .intensity = scan.selectedIon.intensity
+                }.DoCall(AddressOf nscans.Add)
+
+                Call showProgress(name)
+            Next
+
+            cache.GlobalAttributes(New attribute With {.name = NameOf(nscans), .type = CDFDataTypes.INT, .value = nscans.Count})
+            raw.scans = nscans.ToArray
+
+            Call showProgress("Write cache data...")
+        End Using
     End Sub
 End Class
