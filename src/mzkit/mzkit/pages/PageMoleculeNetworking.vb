3444733df1f6a60b85fc10f0a8c91db9c346cf9e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-25 09:11:25 +0800

    show network graph

--- src/mzkit/mzkit/pages/PageMoleculeNetworking.vb
+++ src/mzkit/mzkit/pages/PageMoleculeNetworking.vb
@@ -1,15 +1,23 @@
 ﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports Microsoft.VisualBasic.Data.visualize.Network
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+Imports Microsoft.VisualBasic.Imaging
 
 Public Class PageMoleculeNetworking
+
+    Dim g As NetworkGraph
 
     Public Sub loadNetwork(MN As SpectrumTreeCluster)
         DataGridView1.Rows.Clear()
         DataGridView2.Rows.Clear()
         PictureBox1.BackgroundImage = Nothing
 
-        Dim g = TreeGraph(Of PeakMs2, PeakMs2).CreateGraph(MN.getRoot, Function(a) a.lib_guid)
+        g = TreeGraph(Of PeakMs2, PeakMs2) _
+            .CreateGraph(MN.getRoot, Function(a) a.lib_guid) _
+            .doRandomLayout _
+            .doForceLayout
 
         For Each node In g.vertex
             DataGridView2.Rows.Add(node.label, node.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE))
@@ -26,7 +34,9 @@ Public Class PageMoleculeNetworking
     Private Sub TabControl1_TabIndexChanged(sender As Object, e As EventArgs) Handles TabControl1.TabIndexChanged
         If TabControl1.SelectedTab Is TabPage3 AndAlso PictureBox1.BackgroundImage Is Nothing Then
             ' render network image
-
+            If Not g Is Nothing Then
+                PictureBox1.BackgroundImage = g.DrawImage.AsGDIImage
+            End If
         End If
     End Sub
 End Class
