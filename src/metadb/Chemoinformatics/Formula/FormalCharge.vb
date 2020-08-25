e9773e0040d0e498bda4f244b6cdce77b01aff83
xieguigang <xie.guigang@gcmodeller.org>
2020-08-26 00:13:43 +0800

    add struct verification

--- src/metadb/Chemoinformatics/Formula/FormalCharge.vb
+++ src/metadb/Chemoinformatics/Formula/FormalCharge.vb
@@ -15,6 +15,7 @@ Namespace Formula
         Public I As Integer
         Public F As Integer
         Public Br As Integer
+        Public Other As Integer
 
         Sub New(formula As Formula)
             H = formula("H")
@@ -28,6 +29,22 @@ Namespace Formula
             I = formula("I")
             F = formula("F")
             Br = formula("Br")
+
+            Dim counts As New Dictionary(Of String, Integer)(formula.CountsByElement)
+
+            Call counts.Remove("H")
+            Call counts.Remove("C")
+            Call counts.Remove("Si")
+            Call counts.Remove("N")
+            Call counts.Remove("P")
+            Call counts.Remove("O")
+            Call counts.Remove("S")
+            Call counts.Remove("Cl")
+            Call counts.Remove("I")
+            Call counts.Remove("F")
+            Call counts.Remove("Br")
+
+            Other = counts.Values.Sum
         End Sub
     End Structure
 
--- src/metadb/Chemoinformatics/Formula/FormulaSearch.vb
+++ src/metadb/Chemoinformatics/Formula/FormulaSearch.vb
@@ -20,14 +20,69 @@ Namespace Formula
             Dim seed As New FormulaComposition(New Dictionary(Of String, Integer), "")
 
             For Each formula As FormulaComposition In SearchByExactMass(exact_mass, seed, elements)
-                formula.charge = FormalCharge.CorrectChargeEmpirical(formula.charge, New ElementNumType(formula))
+                Dim counts As New ElementNumType(formula)
 
-                If formula.charge >= opts.chargeRange.Min AndAlso formula.charge <= opts.chargeRange.Max Then
-                    progressReport($"find {formula} with tolerance error {formula.ppm} ppm!")
+                If ConstructAndVerifyCompoundWork(counts) Then
+                    formula.charge = FormalCharge.CorrectChargeEmpirical(formula.charge, counts)
 
-                    Yield formula
+                    If formula.charge >= opts.chargeRange.Min AndAlso formula.charge <= opts.chargeRange.Max Then
+                        progressReport($"find {formula} with tolerance error {formula.ppm} ppm!")
+                        Yield formula
+                    End If
                 End If
             Next
+        End Function
+
+        Private Shared Function ConstructAndVerifyCompoundWork(elementNum As ElementNumType) As Boolean
+            Dim maxH As Integer = 0
+
+            ' Compute maximum number of hydrogens
+            If elementNum.Si = 0 AndAlso elementNum.C = 0 AndAlso elementNum.N = 0 AndAlso
+           elementNum.P = 0 AndAlso elementNum.Other = 0 AndAlso
+           (elementNum.O > 0 OrElse elementNum.S > 0) Then
+                ' Only O and S
+                maxH = 3
+            Else
+                ' Formula is: [#C*2 + 3 - (2 if N or P present)] + [#N + 3 - (1 if C or Si present)] + [#other elements * 4 + 3], where we assume other elements can have a coordination Number of up to 7
+                If elementNum.C > 0 Or elementNum.Si > 0 Then
+                    maxH += (elementNum.C + elementNum.Si) * 2 + 3
+                    ' If udtElementNum.N > 0 Or udtElementNum.P > 0 Then maxh = maxh - 2
+                End If
+
+                If elementNum.N > 0 Or elementNum.P > 0 Then
+                    maxH += (elementNum.N + elementNum.P) + 3
+                    ' If udtElementNum.C > 0 Or udtElementNum.Si > 0 Then maxh = maxh - 1
+                End If
+
+                ' Correction for carbon contribution
+                'If (udtElementNum.C > 0 Or udtElementNum.Si > 0) And (udtElementNum.N > 0 Or udtElementNum.P > 0) Then udtElementNum.H = udtElementNum.H - 2
+
+                ' Correction for nitrogen contribution
+                'If (udtElementNum.N > 0 Or udtElementNum.P > 0) And (udtElementNum.C > 0 Or udtElementNum.Si > 0) Then udtElementNum.H = udtElementNum.H - 1
+
+                ' Combine the above two commented out if's to obtain:
+                If (elementNum.N > 0 Or elementNum.P > 0) And (elementNum.C > 0 Or elementNum.Si > 0) Then
+                    maxH = maxH - 3
+                End If
+
+                If elementNum.Other > 0 Then maxH += elementNum.Other * 4 + 3
+
+            End If
+
+            ' correct for if H only
+            If maxH < 3 Then maxH = 3
+
+            ' correct for halogens
+            maxH = maxH - elementNum.F - elementNum.Cl - elementNum.Br - elementNum.I
+
+            ' correct for negative udtElementNum.H
+            If maxH < 0 Then maxH = 0
+
+            ' Verify H's
+            Dim blnHOK = (elementNum.H <= maxH)
+
+            ' Only proceed if hydrogens check out
+            Return blnHOK
         End Function
 
         Private Iterator Function SearchByExactMass(exact_mass As Double, parent As FormulaComposition, candidates As Stack(Of ElementSearchCandiate)) As IEnumerable(Of FormulaComposition)
