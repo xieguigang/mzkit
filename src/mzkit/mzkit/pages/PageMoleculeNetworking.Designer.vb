841d68d25353b8e06c6fe6c5b6d111565d00b571
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-25 08:56:22 +0800

    how to load network

--- src/mzkit/mzkit/pages/PageMoleculeNetworking.Designer.vb
+++ src/mzkit/mzkit/pages/PageMoleculeNetworking.Designer.vb
@@ -26,7 +26,16 @@ Partial Class PageMoleculeNetworking
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TabControl1
@@ -43,6 +52,7 @@ Partial Class PageMoleculeNetworking
         '
         'TabPage1
         '
+        Me.TabPage1.Controls.Add(Me.DataGridView1)
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
@@ -53,6 +63,7 @@ Partial Class PageMoleculeNetworking
         '
         'TabPage2
         '
+        Me.TabPage2.Controls.Add(Me.DataGridView2)
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
@@ -63,12 +74,40 @@ Partial Class PageMoleculeNetworking
         '
         'TabPage3
         '
+        Me.TabPage3.Controls.Add(Me.PictureBox1)
         Me.TabPage3.Location = New System.Drawing.Point(4, 22)
         Me.TabPage3.Name = "TabPage3"
         Me.TabPage3.Size = New System.Drawing.Size(948, 572)
         Me.TabPage3.TabIndex = 2
         Me.TabPage3.Text = "Visual"
         Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(942, 566)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView2.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(942, 566)
+        Me.DataGridView2.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(948, 572)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
         '
         'PageMoleculeNetworking
         '
@@ -78,6 +117,12 @@ Partial Class PageMoleculeNetworking
         Me.Name = "PageMoleculeNetworking"
         Me.Size = New System.Drawing.Size(956, 598)
         Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -86,4 +131,7 @@ Partial Class PageMoleculeNetworking
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TabPage2 As TabPage
     Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- src/mzkit/mzkit/pages/PageMoleculeNetworking.vb
+++ src/mzkit/mzkit/pages/PageMoleculeNetworking.vb
@@ -3,6 +3,15 @@
 Public Class PageMoleculeNetworking
 
     Public Sub loadNetwork(MN As SpectrumTreeCluster)
+        DataGridView1.Rows.Clear()
+        DataGridView2.Rows.Clear()
+        PictureBox1.BackgroundImage = Nothing
+
+
+
+    End Sub
+
+    Private Sub PageMoleculeNetworking_Load(sender As Object, e As EventArgs) Handles Me.Load
 
     End Sub
 End Class
