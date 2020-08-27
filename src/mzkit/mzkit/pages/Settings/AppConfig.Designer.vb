01f1079c9bc585ed939d8b1f48ddbeeb05b1169d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-27 18:58:56 +0800

    set ribbon colors

--- src/mzkit/mzkit/pages/Settings/AppConfig.Designer.vb
+++ src/mzkit/mzkit/pages/Settings/AppConfig.Designer.vb
@@ -24,6 +24,15 @@ Partial Class AppConfig
     Private Sub InitializeComponent()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'CheckBox1
@@ -45,15 +54,78 @@ Partial Class AppConfig
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Ribbon Colors"
         '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(268, 77)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(65, 12)
+        Me.LinkLabel1.TabIndex = 2
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Background"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(270, 125)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(59, 12)
+        Me.LinkLabel2.TabIndex = 3
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Highlight"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(270, 176)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(29, 12)
+        Me.LinkLabel3.TabIndex = 4
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "Text"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(380, 64)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(84, 25)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(380, 109)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(84, 28)
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(380, 163)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(84, 25)
+        Me.PictureBox3.TabIndex = 7
+        Me.PictureBox3.TabStop = False
+        '
         'AppConfig
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.LinkLabel3)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.CheckBox1)
         Me.Name = "AppConfig"
         Me.Size = New System.Drawing.Size(732, 495)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -61,4 +133,10 @@ Partial Class AppConfig
 
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
 End Class
--- src/mzkit/mzkit/pages/Settings/AppConfig.vb
+++ src/mzkit/mzkit/pages/Settings/AppConfig.vb
@@ -1,4 +1,6 @@
-﻿Public Class AppConfig : Implements ISaveSettings, IPageSettings
+﻿Imports RibbonLib
+
+Public Class AppConfig : Implements ISaveSettings, IPageSettings
 
     Dim WithEvents colorPicker As New ThemeColorPicker
 
@@ -8,6 +10,18 @@
         End If
 
         CheckBox1.Checked = Globals.Settings.ui.rememberWindowsLocation
+
+        If Globals.Settings.ui.background.IsNullOrEmpty Then
+            Dim colors As RibbonColors = DirectCast(ParentForm, frmMain).Ribbon1.GetColors
+
+            PictureBox1.BackColor = colors.BackgroundColor
+            PictureBox2.BackColor = colors.HighlightColor
+            PictureBox3.BackColor = colors.TextColor
+
+            Globals.Settings.ui.background = {colors.BackgroundColor.R, colors.BackgroundColor.G, colors.BackgroundColor.B}
+            Globals.Settings.ui.highlight = {colors.HighlightColor.R, colors.HighlightColor.G, colors.HighlightColor.B}
+            Globals.Settings.ui.text = {colors.TextColor.R, colors.TextColor.G, colors.TextColor.B}
+        End If
     End Sub
 
     Public Sub SaveSettings() Implements ISaveSettings.SaveSettings
@@ -23,11 +37,47 @@
     End Sub
 
     Private Sub AppConfig_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Controls.Add(colorPicker)
+
+        colorPicker.Location = New Point(50, 50)
+        PictureBox1.BorderStyle = BorderStyle.FixedSingle
+
         AddHandler colorPicker.ColorSelected, AddressOf selectColor
     End Sub
 
-    Private Sub selectColor(sender As Object, e As ColorSelectedArg)
+    Private Sub selectColor(sender As Object, e As ColorSelectedArg) Handles colorPicker.ColorSelected
+        Dim color As Integer() = {e.R, e.G, e.B}
 
+        If PictureBox1.BorderStyle = BorderStyle.FixedSingle Then
+            Globals.Settings.ui.background = color
+            PictureBox1.BackColor = e.Color
+        ElseIf PictureBox2.BorderStyle = BorderStyle.FixedSingle Then
+            Globals.Settings.ui.highlight = color
+            PictureBox2.BackColor = e.Color
+        ElseIf PictureBox3.BorderStyle = BorderStyle.FixedSingle Then
+            Globals.Settings.ui.text = color
+            PictureBox3.BackColor = e.Color
+        End If
+
+        Globals.Settings.ui.setColors(DirectCast(ParentForm, frmMain).Ribbon1)
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        PictureBox1.BorderStyle = BorderStyle.FixedSingle
+        PictureBox2.BorderStyle = BorderStyle.None
+        PictureBox3.BorderStyle = BorderStyle.None
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        PictureBox1.BorderStyle = BorderStyle.None
+        PictureBox2.BorderStyle = BorderStyle.FixedSingle
+        PictureBox3.BorderStyle = BorderStyle.None
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        PictureBox1.BorderStyle = BorderStyle.None
+        PictureBox2.BorderStyle = BorderStyle.None
+        PictureBox3.BorderStyle = BorderStyle.FixedSingle
     End Sub
 End Class
 
