dee520bae0c3a7c83957852d26ae7a17e2dd06a8
xieguigang <xie.guigang@gcmodeller.org>
2020-08-29 18:44:53 +0800

    tweaks of the ribbon layout

--- src/mzkit/mzkit/RibbonItems.Designer.cs
+++ src/mzkit/mzkit/RibbonItems.Designer.cs
@@ -13,7 +13,8 @@ using RibbonLib;
 using RibbonLib.Controls;
 using RibbonLib.Interop;
 
-
+namespace RibbonLib.Controls
+{
     partial class RibbonItems
     {
         private static class Cmd
@@ -223,3 +224,4 @@ using RibbonLib.Interop;
         }
 
     }
+}
