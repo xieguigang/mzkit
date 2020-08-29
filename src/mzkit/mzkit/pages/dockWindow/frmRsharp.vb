0b84f97bebeb65b22bc9b024bdc234801267c48a
xieguigang <xie.guigang@gcmodeller.org>
2020-08-30 02:15:47 +0800

    add console control

--- src/mzkit/mzkit/pages/dockWindow/frmRsharp.vb
+++ src/mzkit/mzkit/pages/dockWindow/frmRsharp.vb
@@ -2,8 +2,15 @@
 
 Public Class frmRsharp
 
+    Dim console As New ConsoleControl.ConsoleControl
+
     Private Sub frmRsharp_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
         e.Cancel = True
         Me.Hide()
+    End Sub
+
+    Private Sub frmRsharp_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Controls.Add(console)
+        console.Dock = DockStyle.Fill
     End Sub
 End Class
