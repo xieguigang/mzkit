a52ae50ee3b1224e7a4073ea5004c5f1adefed9e
xieguigang <xie.guigang@gcmodeller.org>
2020-08-29 14:17:18 +0800

    fix bugs of parse news data

--- src/mzkit/Task/NewsFeed.vb
+++ src/mzkit/Task/NewsFeed.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.Text
+﻿Imports System.Text
+Imports Microsoft.VisualBasic.Text
 Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 
 Public Class NewsFeed
@@ -12,17 +13,24 @@ Public Class NewsFeed
 
     Public Shared Iterator Function ParseLatest() As IEnumerable(Of NewsFeed)
         Dim html As String = latestNewsPageUrl.GET
-        Dim items As String() = Splitter.Split(html, "<div class[=]""product-set-item-info-inner-container"">", False)
+        Dim items As String() = Strings.Split(html, "<div class=""product-set-item-info-inner-container"">")
 
-        For Each item As String In items
-            Dim url As String = item.Match("[<]a[>].+?[<]/a[>]")
+        For Each item As String In items.Skip(1)
+            Dim url As String = item.Match("[<]a.+?[<]/a[>]")
             Dim title As String = url.GetValue
             url = $"http://www.bionovogene.com/{url.href}"
             Dim dateTime = item.Match("发布日期：.+[<]/div[>]").Replace("</div>", "").Replace("发布日期：", "").Trim
-            Dim abstract = item.Match("<div class[=]""product-medium-description.+?</div>").GetValue
+            Dim abstract As New StringBuilder(item.Match("<div class[=]""product-medium-description.+?</div>").GetValue)
+            Dim unicodes = abstract.ToString.Matches("[&][#]\d+[;]").Distinct.ToArray
+
+            For Each code In unicodes
+                Dim charCode As Integer = Integer.Parse(code.Match("\d+"))
+                Dim [char] = Strings.ChrW(charCode)
+                abstract.Replace(code, [char])
+            Next
 
             Yield New NewsFeed With {
-                .abstract = abstract,
+                .abstract = abstract.ToString,
                 .[date] = dateTime,
                 .title = title,
                 .url = url
--- src/mzkit/mzkit/pages/PageStart.vb
+++ src/mzkit/mzkit/pages/PageStart.vb
@@ -1,8 +1,23 @@
-﻿Imports WeifenLuo.WinFormsUI.Docking
+﻿Imports System.ComponentModel
+Imports Task
+Imports WeifenLuo.WinFormsUI.Docking
 
 Public Class PageStart
 
+    Dim WithEvents BackgroundWorker As New BackgroundWorker
+
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         DirectCast(ParentForm, frmMain).fileExplorer.DockState = DockState.DockLeft
+    End Sub
+
+    Private Sub PageStart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackgroundWorker.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker.DoWork
+        Dim news As NewsFeed() = NewsFeed.ParseLatest().ToArray
+
+
+
     End Sub
 End Class
