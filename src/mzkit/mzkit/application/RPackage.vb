5f82dc6b6f21431d217ea46e4f7b89ec3a1228f5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-01 08:46:01 +0800

    R# api

---
+++ src/mzkit/mzkit/application/RPackage.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Task
+
+Namespace My
+
+    Partial Class MyApplication
+
+        Shared Sub New()
+
+        End Sub
+
+        ''' <summary>
+        ''' Get a list of raw files that opened in current workspace
+        ''' </summary>
+        ''' <returns></returns>
+        ''' 
+        <ExportAPI("list.raw")>
+        Public Shared Function ListFiles() As Raw()
+            Dim list As New List(Of Raw)
+
+            For i As Integer = 0 To host.fileExplorer.treeView1.Nodes.Count - 1
+                list.Add(host.fileExplorer.treeView1.Nodes(i).Tag)
+            Next
+
+            Return list.ToArray
+        End Function
+    End Class
+End Namespace
