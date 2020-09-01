4f381cc2b325c4ac61a812dfba375e086750540e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-01 08:53:20 +0800

    show raw files as table

--- src/mzkit/mzkit/application/RPackage.vb
+++ src/mzkit/mzkit/application/RPackage.vb
@@ -1,13 +1,30 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports Task
+Imports REnv = SMRUCC.Rsharp.Runtime.Internal
 
 Namespace My
 
     Partial Class MyApplication
 
         Shared Sub New()
-
+            REnv.Object.Converts.makeDataframe.addHandler(GetType(Raw()), AddressOf rawDataFrame)
         End Sub
+
+        Private Shared Function rawDataFrame(raws As Raw(), args As list, env As Environment) As dataframe
+            Dim table As New dataframe With {.columns = New Dictionary(Of String, Array)}
+
+            table.columns(NameOf(Raw.source)) = raws.Select(Function(a) a.source).ToArray
+            table.columns(NameOf(Raw.rtmin)) = raws.Select(Function(a) a.rtmin).ToArray
+            table.columns(NameOf(Raw.rtmax)) = raws.Select(Function(a) a.rtmax).ToArray
+            table.columns(NameOf(Raw.numOfScans)) = raws.Select(Function(a) a.numOfScans).ToArray
+            table.columns(NameOf(Raw.cache)) = raws.Select(Function(a) a.cache).ToArray
+
+            table.rownames = raws.Select(Function(a) a.source.FileName).ToArray
+
+            Return table
+        End Function
 
         ''' <summary>
         ''' Get a list of raw files that opened in current workspace
