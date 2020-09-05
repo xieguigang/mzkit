cb55a6064be5acbd10f11ff520dbaa9f52c8c979
xieguigang <xie.guigang@gcmodeller.org>
2020-09-06 00:38:15 +0800

    update to new layout engine

--- src/mzkit/mzkit/pages/PageMoleculeNetworking.vb
+++ src/mzkit/mzkit/pages/PageMoleculeNetworking.vb
@@ -58,6 +58,7 @@ Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts.ForceDirected
 Imports Microsoft.VisualBasic.DataMining.KMeans
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
@@ -122,14 +123,12 @@ Public Class PageMoleculeNetworking
                 Thread.Sleep(500)
                 progress.Invoke(Sub() progress.Label1.Text = "Run network layouts...")
 
-                graph = graph.doRandomLayout.doForceLayout(
-                    iterations:=Globals.Settings.network.layout.Iterations,
-                    Stiffness:=Globals.Settings.network.layout.Stiffness,
-                    Damping:=Globals.Settings.network.layout.Damping,
-                    Repulsion:=Globals.Settings.network.layout.Repulsion,
-                    progressCallback:=Sub(msg)
-                                          progress.Invoke(Sub() progress.Label1.Text = msg)
-                                      End Sub)
+                Dim layouts = Planner.Plan(graph, Globals.Settings.network.layout.Iterations, Sub(msg) progress.Invoke(Sub() progress.Label1.Text = msg))
+
+                For Each node In graph.vertex
+                    node.data.initialPostion = New FDGVector2(layouts(node))
+                Next
+
                 progress.Invoke(Sub() progress.Label1.Text = "do network render plot...")
 
                 Dim plot As Image = graph.DrawImage(
