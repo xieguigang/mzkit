eb5559da23c37f2736843ec0eff3e7433c2bb990
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-09 08:52:52 +0800

    update help docs

--- dist/buildPdf.cmd
+++ dist/buildPdf.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-.\wkhtmltopdf.exe --margin-bottom 5 --margin-top 5 --margin-left 5 --margin-right 5 https://mzkit.org/ "D:\mzkit\dist\README.pdf"
+wkhtmltopdf --margin-bottom 5 --margin-top 5 --margin-left 5 --margin-right 5 https://mzkit.org/ "D:\mzkit\dist\README.pdf"
