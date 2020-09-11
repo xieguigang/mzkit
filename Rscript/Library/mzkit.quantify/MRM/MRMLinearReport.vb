98f1f2cbe3c48e4ea0bdce78442879bac697175c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-11 09:15:46 +0800

    fix of reference url

--- Rscript/Library/mzkit.quantify/MRM/MRMLinearReport.vb
+++ Rscript/Library/mzkit.quantify/MRM/MRMLinearReport.vb
@@ -97,7 +97,7 @@ Module MRMLinearReport
                     <title><%= title %> | BioNovoGene</title>
 
                     <!-- Bootstrap CSS -->
-                    <link rel="stylesheet" href="https://cdn.biodeep.cn/styles/bootstrap-4.3.1-dist/css/bootstrap.min.css" crossorigin="anonymous"/>
+                    <link rel="stylesheet" href="http://cdn.biodeep.cn/styles/bootstrap-4.3.1-dist/css/bootstrap.min.css" crossorigin="anonymous"/>
 
                     <style type="text/css">
                         .even td{/*必须加td，代表的是一行进行*/
@@ -132,9 +132,9 @@ Module MRMLinearReport
 
                 <!-- Optional JavaScript -->
                 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-                <script src="https://cdn.biodeep.cn/vendor/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
-                <script src="https://cdn.biodeep.cn/vendor/popper.min.js" crossorigin="anonymous"></script>
-                <script src="https://cdn.biodeep.cn/styles/bootstrap-4.3.1-dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+                <script src="http://cdn.biodeep.cn/vendor/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+                <script src="http://cdn.biodeep.cn/vendor/popper.min.js" crossorigin="anonymous"></script>
+                <script src="http://cdn.biodeep.cn/styles/bootstrap-4.3.1-dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
             </html>)
     End Function
 
