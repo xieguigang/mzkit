2f62319f768d11501101de8e8c5c2e1859d3b371
xieguigang <xie.guigang@gcmodeller.org>
2020-09-15 20:01:44 +0800

    how to do peak annotations?

---
+++ src/metadb/FormulaSearch.Extensions/PeakAnnotation.vb
@@ -0,0 +1,8 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+
+Public Class PeakAnnotation
+
+    Public Function RunAnnotations(parent As Double, products As IEnumerable(Of ms2), charge As Double)
+
+    End Function
+End Class
