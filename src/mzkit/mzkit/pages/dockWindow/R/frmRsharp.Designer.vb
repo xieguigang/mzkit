496310944c16e6753fc0976fb639c8f8abcf5589
xieguigang <xie.guigang@gcmodeller.org>
2020-09-26 02:44:21 +0800

    removes conemu reference

--- src/mzkit/mzkit/pages/dockWindow/R/frmRsharp.Designer.vb
+++ src/mzkit/mzkit/pages/dockWindow/R/frmRsharp.Designer.vb
@@ -5,7 +5,7 @@ Partial Class frmRsharp
     Inherits DockContent
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -22,26 +22,15 @@ Partial Class frmRsharp
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.termPanel = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
-        '
-        'termPanel
-        '
-        Me.termPanel.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(54, Byte), Integer))
-        Me.termPanel.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.termPanel.Location = New System.Drawing.Point(0, 0)
-        Me.termPanel.Name = "termPanel"
-        Me.termPanel.Size = New System.Drawing.Size(1067, 637)
-        Me.termPanel.TabIndex = 1
         '
         'frmRsharp
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1067, 637)
-        Me.Controls.Add(Me.termPanel)
         Me.Font = New System.Drawing.Font("Consolas", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frmRsharp"
@@ -49,6 +38,4 @@ Partial Class frmRsharp
         Me.ResumeLayout(False)
 
     End Sub
-
-    Private WithEvents termPanel As Panel
 End Class
