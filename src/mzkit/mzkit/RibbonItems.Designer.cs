96678bd6c5929f2538679a5c976e6041bf2dc8a6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-25 17:34:07 +0800

    spectrum similarity search tool page

--- src/mzkit/mzkit/RibbonItems.Designer.cs
+++ src/mzkit/mzkit/RibbonItems.Designer.cs
@@ -13,7 +13,8 @@ using RibbonLib;
 using RibbonLib.Controls;
 using RibbonLib.Interop;
 
-
+namespace RibbonLib.Controls
+{
     partial class RibbonItems
     {
         private static class Cmd
@@ -283,3 +284,4 @@ using RibbonLib.Interop;
         }
 
     }
+}
