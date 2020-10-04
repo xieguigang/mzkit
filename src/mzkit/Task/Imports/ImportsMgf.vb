6477f81b813fb22acd2eac96416da01cd50720bf
xieguigang <xie.guigang@gcmodeller.org>
2020-10-05 03:35:48 +0800

    split source files

---
+++ src/mzkit/Task/Imports/ImportsMgf.vb
@@ -0,0 +1,9 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ASCII
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ASCII.MGF
+
+Public Class ImportsMgf : Inherits ImportsTask(Of MGF.Ions)
+
+    Protected Overrides Function getScans(source As String) As IEnumerable(Of Ions)
+        Return MGF.ReadIons(source)
+    End Function
+End Class
