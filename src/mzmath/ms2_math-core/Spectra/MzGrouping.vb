35dbb1d19625c68ee70bbe2c4ce6de668b9ce96e
xieguigang <xie.guigang@gcmodeller.org>
2020-10-05 16:10:53 +0800

    run parallel for data pre-processing

--- src/mzmath/ms2_math-core/Spectra/MzGrouping.vb
+++ src/mzmath/ms2_math-core/Spectra/MzGrouping.vb
@@ -38,48 +38,76 @@ Namespace Spectra
         End Function
 
         <Extension>
-        Public Iterator Function PopulateUniquePeakMatrix(clusters As IEnumerable(Of SpectrumCluster),
-                                                          Optional precursor$ = "ppm:20",
-                                                          Optional rtwidth# = 5,
-                                                          Optional mzwidth$ = "da:0.1",
-                                                          Optional trim$ = "0.05") As IEnumerable(Of PeakMs2)
+        Public Function PopulateUniquePeakMatrix(clusters As IEnumerable(Of SpectrumCluster),
+                                                 Optional precursor$ = "ppm:20",
+                                                 Optional rtwidth# = 5,
+                                                 Optional mzwidth$ = "da:0.1",
+                                                 Optional trim$ = "0.05") As IEnumerable(Of PeakMs2)
 
             Dim parentErr As Tolerance = Ms1.Tolerance.ParseScript(precursor)
             Dim centroidErr As Tolerance = Ms1.Tolerance.ParseScript(mzwidth)
             Dim intocutoff As LowAbundanceTrimming = LowAbundanceTrimming.ParseScript(trim)
+            Dim unique As IEnumerable(Of PeakMs2) = clusters _
+                .AsParallel _
+                .Select(Function(cluster)
+                            Return cluster.PopulateUniquePeakMatrix(parentErr, rtwidth, centroidErr, intocutoff)
+                        End Function) _
+                .IteratesALL
 
-            For Each cluster As SpectrumCluster In clusters
-                ' group by mz/rt
-                ' by mz
-                Dim mzgroups = cluster.cluster _
-                    .GroupBy(Function(a) a.mz, parentErr) _
+            Return unique
+        End Function
+
+        ''' <summary>
+        ''' group by mz/rt
+        ''' </summary>
+        ''' <param name="cluster"></param>
+        ''' <param name="parentErr"></param>
+        ''' <param name="rtwidth#"></param>
+        ''' <param name="centroidErr"></param>
+        ''' <param name="intocutoff"></param>
+        ''' <returns></returns>
+        <Extension>
+        Private Iterator Function PopulateUniquePeakMatrix(cluster As SpectrumCluster,
+                                                           parentErr As Tolerance,
+                                                           rtwidth#,
+                                                           centroidErr As Tolerance,
+                                                           intocutoff As LowAbundanceTrimming) As IEnumerable(Of PeakMs2)
+            ' group by mz/rt
+            ' by mz
+            Dim mzgroups As NamedCollection(Of PeakMs2)() = cluster.cluster _
+                .GroupBy(Function(a) a.mz, parentErr) _
+                .ToArray
+
+            ' by rt
+            For Each mz As NamedCollection(Of PeakMs2) In mzgroups
+                Dim rtgroups As NamedCollection(Of PeakMs2)() = mz _
+                    .GroupBy(Function(a) a.rt, offsets:=rtwidth) _
                     .ToArray
+                Dim mzval As Double = Aggregate ion As PeakMs2
+                                      In mz
+                                      Into Average(ion.mz)
 
-                ' by rt
-                For Each mz As NamedCollection(Of PeakMs2) In mzgroups
-                    Dim rtgroups = mz.GroupBy(Function(a) a.rt, offsets:=rtwidth).ToArray
-                    Dim mzval As Double = Aggregate ion In mz Into Average(ion.mz)
+                For Each rt As NamedCollection(Of PeakMs2) In rtgroups
+                    Dim members As String() = rt _
+                        .Select(Function(a) a.lib_guid) _
+                        .Distinct _
+                        .ToArray
+                    Dim rtval As Double = Aggregate ion As PeakMs2
+                                          In rt
+                                          Into Average(ion.rt)
+                    Dim peaks As ms2() = rt _
+                        .Select(Function(a) a.mzInto) _
+                        .IteratesALL _
+                        .ToArray _
+                        .Centroid(centroidErr, intocutoff) _
+                        .ToArray
 
-                    For Each rt As NamedCollection(Of PeakMs2) In rtgroups
-                        Dim members As String() = rt _
-                            .Select(Function(a) a.lib_guid) _
-                            .Distinct _
-                            .ToArray
-                        Dim rtval As Double = Aggregate ion In rt Into Average(ion.rt)
-                        Dim peaks As ms2() = rt _
-                            .Select(Function(a) a.mzInto) _
-                            .IteratesALL _
-                            .ToArray _
-                            .Centroid(centroidErr, intocutoff) _
-                            .ToArray
-
-                        Yield New PeakMs2 With {
-                            .lib_guid = members.JoinBy(", "),
-                            .rt = rtval,
-                            .mz = mzval,
-                            .mzInto = peaks
-                        }
-                    Next
+                    Yield New PeakMs2 With {
+                        .lib_guid = members.JoinBy(", "),
+                        .rt = rtval,
+                        .mz = mzval,
+                        .mzInto = peaks
+                    }
                 Next
             Next
         End Function
