da922fed982f59f0922642516958b8518470fdaf
xieguigang <xie.guigang@gcmodeller.org>
2020-10-05 13:00:13 +0800

    spectra trimming

---
+++ src/mzmath/ms2_math-core/Spectra/Models/Trimming/QuantileIntensityCutoff.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.Math.Quantile
+
+Namespace Spectra
+
+    Public Class QuantileIntensityCutoff : Inherits LowAbundanceTrimming
+
+        Public Sub New(cutoff As Double)
+            MyBase.New(cutoff)
+        End Sub
+
+        Protected Overrides Function lowAbundanceTrimming(spectrum() As ms2) As ms2()
+            Dim quantile = spectrum.Select(Function(a) a.intensity).GKQuantile
+            Dim threshold As Double = quantile.Query(Me.m_threshold)
+
+            Return spectrum.Where(Function(a) a.intensity >= threshold).ToArray
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return $"intensity_quantile >= {m_threshold}"
+        End Function
+
+    End Class
+End Namespace
---
+++ src/mzmath/ms2_math-core/Spectra/Models/Trimming/RelativeIntensityCutoff.vb
@@ -0,0 +1,31 @@
+﻿
+Namespace Spectra
+
+    Public Class RelativeIntensityCutoff : Inherits LowAbundanceTrimming
+
+        Public Sub New(cutoff As Double)
+            MyBase.New(cutoff)
+        End Sub
+
+        Protected Overrides Function lowAbundanceTrimming(spectrum() As ms2) As ms2()
+            Dim maxInto As Double = -999
+
+            For Each fragment As ms2 In spectrum
+                If fragment.intensity > maxInto Then
+                    maxInto = fragment.intensity
+                End If
+            Next
+
+            For Each fragment As ms2 In spectrum
+                fragment.quantity = fragment.intensity / maxInto
+            Next
+
+            Return spectrum.Where(Function(a) a.quantity >= m_threshold).ToArray
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return $"relative_intensity >= {m_threshold * 100}%"
+        End Function
+
+    End Class
+End Namespace
