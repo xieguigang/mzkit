5997eb3a6b2f83f2983278b014106d826e3bce4d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-13 17:20:52 +0800

    add custom action for installer

---
+++ src/mzkit/mzkit/application/InstallerCustomActions.Designer.vb
@@ -0,0 +1,27 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class InstallerCustomActions
+    Inherits System.Configuration.Install.Installer
+
+    'Installer 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '组件设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是组件设计器所必需的
+    '可使用组件设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New System.ComponentModel.Container()
+    End Sub
+
+End Class
---
+++ src/mzkit/mzkit/application/InstallerCustomActions.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+Imports System.Reflection
+
+Public Class InstallerCustomActions
+
+    Public Sub New()
+        MyBase.New()
+
+        '组件设计器需要此调用。
+        InitializeComponent()
+
+        '调用 InitializeComponent 后添加初始化代码
+
+    End Sub
+
+    Public Overrides Sub Commit(savedState As IDictionary)
+        MyBase.Commit(savedState)
+
+        Call Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+        Call Process.Start(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + $"\{App.AssemblyName}.exe")
+        Call Process.Start("http://www.biodeep.cn/")
+    End Sub
+End Class
