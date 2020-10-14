bf964919771068b8ee50ee43e29d7346b1bf1974
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-14 18:54:56 +0800

    improvements of the data property display

--- src/mzkit/Task/Properties/AlignmentProperty.vb
+++ src/mzkit/Task/Properties/AlignmentProperty.vb
@@ -1,16 +1,31 @@
-﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.Xml
+﻿Imports System.ComponentModel
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.Xml
 
 Public Class AlignmentProperty
 
+    <Category("Ion Information")>
     Public Property query As String
+    <Category("Ion Information")>
     Public Property reference As String
+
+    <Category("Alignment")>
     Public Property forward As Double
+    <Category("Alignment")>
     Public Property reverse As Double
+    <Category("Alignment")>
+    Public Property jaccard As Double
+    <Category("Alignment")>
+    Public Property shares As Integer
 
     Sub New(alignment As AlignmentOutput)
         query = alignment.query.id
         reference = alignment.reference.id
         forward = alignment.forward
         reverse = alignment.reverse
+
+        Dim all = alignment.alignments.Length
+
+        shares = alignment.alignments.Where(Function(a) a.da <> "NaN").Count
+        jaccard = shares / all
     End Sub
 End Class
