3165771e688329e5abf5ca12ac9f0b74a34df6fe
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-14 19:11:56 +0800

    task list window

---
+++ src/mzkit/mzkit/pages/dockWindow/tools/TaskListWindow.Designer.vb
@@ -0,0 +1,89 @@
+﻿Imports mzkit.DockSample
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class TaskListWindow
+    Inherits ToolWindow
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim TreeListViewItemCollectionComparer1 As System.Windows.Forms.TreeListViewItemCollection.TreeListViewItemCollectionComparer = New System.Windows.Forms.TreeListViewItemCollection.TreeListViewItemCollectionComparer()
+        Me.TreeListView1 = New System.Windows.Forms.TreeListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'TreeListView1
+        '
+        Me.TreeListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        TreeListViewItemCollectionComparer1.Column = 0
+        TreeListViewItemCollectionComparer1.SortOrder = System.Windows.Forms.SortOrder.Ascending
+        Me.TreeListView1.Comparer = TreeListViewItemCollectionComparer1
+        Me.TreeListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TreeListView1.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TreeListView1.GridLines = True
+        Me.TreeListView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable
+        Me.TreeListView1.HideSelection = False
+        Me.TreeListView1.Location = New System.Drawing.Point(0, 0)
+        Me.TreeListView1.Name = "TreeListView1"
+        Me.TreeListView1.Size = New System.Drawing.Size(983, 494)
+        Me.TreeListView1.TabIndex = 0
+        Me.TreeListView1.UseCompatibleStateImageBehavior = False
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Task"
+        Me.ColumnHeader1.Width = 152
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Content"
+        Me.ColumnHeader2.Width = 488
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Start Time"
+        Me.ColumnHeader3.Width = 139
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Status"
+        Me.ColumnHeader4.Width = 149
+        '
+        'TaskListWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(983, 494)
+        Me.Controls.Add(Me.TreeListView1)
+        Me.Name = "TaskListWindow"
+        Me.Text = "Task List"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TreeListView1 As TreeListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+End Class
---
+++ src/mzkit/mzkit/pages/dockWindow/tools/TaskListWindow.vb
@@ -0,0 +1,18 @@
+﻿Imports System.ComponentModel
+
+Public Class TaskListWindow
+
+    Sub New()
+
+        ' 此调用是设计器所必需的。
+        InitializeComponent()
+
+        ' 在 InitializeComponent() 调用之后添加任何初始化。
+        DoubleBuffered = True
+    End Sub
+
+    Private Sub TaskListWindow_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        e.Cancel = True
+        Me.Hide()
+    End Sub
+End Class
