8dcadf6c67c7e08044af1ff9161134bc0dc681fd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-30 08:51:05 +0800

    ms scan data reader

---
+++ src/assembly/assembly/UnifyReader/MsDataReader.vb
@@ -0,0 +1,17 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+
+Namespace DataReader
+
+    Public MustInherit Class MsDataReader(Of Scan)
+
+        Public MustOverride Function GetScanTime(scan As Scan) As Double
+        Public MustOverride Function GetScanId(scan As Scan) As Double
+        Public MustOverride Function IsEmpty(scan As Scan) As Boolean
+        Public MustOverride Function GetMsMs(scan As Scan) As ms2()
+        Public MustOverride Function GetMsLevel(scan As Scan) As Integer
+        Public MustOverride Function GetBPC(scan As Scan) As Double
+        Public MustOverride Function GetTIC(scan As Scan) As Double
+        Public MustOverride Function GetParentMz(scan As Scan) As Double
+
+    End Class
+End Namespace
