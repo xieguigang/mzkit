0c2351c5202807df6a6e6844ed080c2a2768ff40
xieguigang <xie.guigang@gcmodeller.org>
2020-12-05 01:01:17 +0800

    add xml dev notes

--- Rscript/Library/mzkit.plot/MsImaging.vb
+++ Rscript/Library/mzkit.plot/MsImaging.vb
@@ -9,11 +9,27 @@ Imports SMRUCC.Rsharp.Runtime.Interop
 <Package("MsImaging")>
 Module MsImaging
 
+    ''' <summary>
+    ''' load imzML data into the ms-imaging render
+    ''' </summary>
+    ''' <param name="imzML"></param>
+    ''' <returns></returns>
     <ExportAPI("viewer")>
     Public Function viewer(imzML As String) As Drawer
         Return New Drawer(imzML)
     End Function
 
+    ''' <summary>
+    ''' render a ms-imaging layer by a specific ``m/z`` scan.
+    ''' </summary>
+    ''' <param name="viewer"></param>
+    ''' <param name="mz"></param>
+    ''' <param name="threshold"></param>
+    ''' <param name="pixelSize"></param>
+    ''' <param name="ppm"></param>
+    ''' <param name="color$"></param>
+    ''' <param name="levels%"></param>
+    ''' <returns></returns>
     <ExportAPI("layer")>
     Public Function layer(viewer As Drawer, mz As Double,
                           Optional threshold As Double = 0.05,
