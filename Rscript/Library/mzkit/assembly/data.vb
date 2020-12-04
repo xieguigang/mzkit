f11962d3f3f01113729c9544b6b5cf5102912375
xieguigang <xie.guigang@gcmodeller.org>
2020-12-04 17:33:01 +0800

    get scans data by rt rtange

--- Rscript/Library/mzkit/assembly/data.vb
+++ Rscript/Library/mzkit/assembly/data.vb
@@ -9,6 +9,14 @@ Imports SMRUCC.Rsharp.Runtime.Interop
 <Package("data")>
 Module data
 
+    Friend Sub Main()
+
+    End Sub
+
+    Private Function XICTable(XIC As ms1_scan(), args As list, env As Environment) As dataframe
+
+    End Function
+
     <ExportAPI("XIC")>
     <RApiReturn(GetType(ms1_scan))>
     Public Function XIC(<RRawVectorArgument> ms1 As Object, mz#, Optional ppm# = 20, Optional env As Environment = Nothing) As Object
@@ -21,6 +29,24 @@ Module data
         Dim xicFilter As ms1_scan() = ms1_scans _
             .populates(Of ms1_scan)(env) _
             .Where(Function(pt) PPMmethod.PPM(pt.mz, mz) <= ppm) _
+            .OrderBy(Function(a) a.scan_time) _
+            .ToArray
+
+        Return xicFilter
+    End Function
+
+    <ExportAPI("rt_slice")>
+    <RApiReturn(GetType(ms1_scan))>
+    Public Function RtSlice(<RRawVectorArgument> ms1 As Object, rtmin#, rtmax#, Optional env As Environment = Nothing) As Object
+        Dim ms1_scans As pipeline = pipeline.TryCreatePipeline(Of ms1_scan)(ms1, env)
+
+        If ms1_scans.isError Then
+            Return ms1_scans.getError
+        End If
+
+        Dim xicFilter As ms1_scan() = ms1_scans _
+            .populates(Of ms1_scan)(env) _
+            .Where(Function(pt) pt.scan_time >= rtmin AndAlso pt.scan_time <= rtmax) _
             .OrderBy(Function(a) a.scan_time) _
             .ToArray
 
