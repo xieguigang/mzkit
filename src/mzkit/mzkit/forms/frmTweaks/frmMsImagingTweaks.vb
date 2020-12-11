1553df9a18e7710cdbf297f1c3930f07a24d54ad
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-11 14:11:47 +0800

    tweaks of the plot UI

--- src/mzkit/mzkit/forms/frmTweaks/frmMsImagingTweaks.vb
+++ src/mzkit/mzkit/forms/frmTweaks/frmMsImagingTweaks.vb
@@ -3,4 +3,9 @@
     Private Sub frmMsImagingTweaks_Load(sender As Object, e As EventArgs) Handles Me.Load
         Me.TabText = "MsImage Parameters"
     End Sub
+
+    Private Sub ClearSelectionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearSelectionToolStripMenuItem.Click
+        Call CheckedListBox1.SelectedItems.Clear()
+        Call CheckedListBox1.SelectedIndices.Clear()
+    End Sub
 End Class
