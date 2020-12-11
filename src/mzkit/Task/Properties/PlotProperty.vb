bb2cd47cfb41be04e74f73fc21f7f397b0fdc7a1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-11 10:28:12 +0800

    Update PlotProperty.vb

--- src/mzkit/Task/Properties/PlotProperty.vb
+++ src/mzkit/Task/Properties/PlotProperty.vb
@@ -1,30 +1,24 @@
-﻿Imports System.Drawing
+﻿Imports System.ComponentModel
+Imports System.Drawing
 Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
 
 Public Class PlotProperty
 
-    Public Property width As Integer
-    Public Property height As Integer
-    Public Property background As Color
-    Public Property padding As New PaddingHelper
-    Public Property showLegend As Boolean = True
-    Public Property lineWidth As Single = 5
+    <Category("Plot")> Public Property width As Integer
+    <Category("Plot")> Public Property height As Integer
+    <Category("Plot")> Public Property background As Color
 
-End Class
+    <Category("Padding")> <DisplayName("top")> Public Property padding_top As Integer = 100
+    <Category("Padding")> <DisplayName("left")> Public Property padding_left As Integer = 100
+    <Category("Padding")> <DisplayName("right")> Public Property padding_right As Integer = 100
+    <Category("Padding")> <DisplayName("bottom")> Public Property padding_bottom As Integer = 100
 
-Public Structure PaddingHelper
-
-    Public Property top As Integer
-    Public Property right As Integer
-    Public Property bottom As Integer
-    Public Property left As Integer
+    <Category("Styles")> Public Property showLegend As Boolean = True
+    <Category("Styles")> Public Property lineWidth As Single = 5
+    <Category("Styles")> Public Property PointSize As Single = 10
 
     Public Function GetPadding() As Padding
-        Return New Padding(left, top, right, bottom)
+        Return New Padding(padding_left, padding_top, padding_right, padding_bottom)
     End Function
 
-    Public Overrides Function ToString() As String
-        Return GetPadding.ToString
-    End Function
-
-End Structure
+End Class
