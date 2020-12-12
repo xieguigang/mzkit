b3791d93f9e3b6318e43f1fb83e23cce6ead74dd
xieguigang <xie.guigang@gcmodeller.org>
2020-12-12 21:09:57 +0800

    add data explorer

---
+++ src/mzkit/mzkit/pages/dockWindow/explorer/frmTreeExplorer.vb
@@ -0,0 +1,18 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
+
+Public Class frmTreeExplorer
+
+    Dim tree As SpectrumTree
+
+    Private Sub frmTreeExplorer_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.TabText = "Spectrum Tree Explorer"
+    End Sub
+
+    Public Sub LoadTree(tree As SpectrumTree)
+        Me.tree = tree
+    End Sub
+
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
+
+    End Sub
+End Class
---
+++ src/mzkit/mzkit/pages/dockWindow/explorer/frmUVScans.Designer.vb
@@ -0,0 +1,54 @@
+﻿Imports mzkit.DockSample
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmUVScans
+    ' Inherits System.Windows.Forms.Form
+    Inherits ToolWindow
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.SuspendLayout()
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CheckedListBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(0, 0)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(376, 596)
+        Me.CheckedListBox1.TabIndex = 0
+        '
+        'frmUVScans
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 596)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Name = "frmUVScans"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+End Class
