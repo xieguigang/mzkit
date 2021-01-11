1a6be6091d7f4b0053ecf8f17cec91fb22035a1f
xieguigang <xie.guigang@gcmodeller.org>
2021-01-11 21:36:26 +0800

    view demo data

--- src/mzkit/mzkit/pages/dockWindow/frmDemo.Designer.vb
+++ src/mzkit/mzkit/pages/dockWindow/frmDemo.Designer.vb
@@ -24,9 +24,66 @@ Partial Class frmDemo
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.components = New System.ComponentModel.Container()
+        Dim ListViewItem1 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"Ms-Imaging Demo", "Ms-Imaging Demo"}, 2)
+        Dim ListViewItem2 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("Mass Spectrometry Demo Data", 1)
+        Dim ListViewItem3 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("LC-MS/MS SRM Demo", 0)
+        Dim ListViewItem4 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("GC-MS SIM Demo", 0)
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmDemo))
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PropertyGrid1 = New System.Windows.Forms.PropertyGrid()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView1.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.HideSelection = False
+        ListViewItem1.StateImageIndex = 0
+        ListViewItem1.ToolTipText = "Urinary Bladder (S042_Processed_imzML1.1.1)"
+        ListViewItem2.ToolTipText = "003_Ex2_Orbitrap_CID.mzXML"
+        Me.ListView1.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem1, ListViewItem2, ListViewItem3, ListViewItem4})
+        Me.ListView1.LargeImageList = Me.ImageList1
+        Me.ListView1.Location = New System.Drawing.Point(0, 0)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(673, 711)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.TileSize = New System.Drawing.Size(600, 128)
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "office_chart_area_256px_540041_easyicon.net.png")
+        Me.ImageList1.Images.SetKeyName(1, "BPC [003_Ex2_Orbitrap_CID.mzXML].png")
+        Me.ImageList1.Images.SetKeyName(2, "s042_229_continuous_large.png")
+        '
+        'PropertyGrid1
+        '
+        Me.PropertyGrid1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.PropertyGrid1.Location = New System.Drawing.Point(673, 0)
+        Me.PropertyGrid1.Name = "PropertyGrid1"
+        Me.PropertyGrid1.Size = New System.Drawing.Size(372, 711)
+        Me.PropertyGrid1.TabIndex = 1
+        '
+        'frmDemo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1045, 711)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.PropertyGrid1)
+        Me.Name = "frmDemo"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents PropertyGrid1 As PropertyGrid
+    Friend WithEvents ImageList1 As ImageList
 End Class
--- src/mzkit/mzkit/pages/dockWindow/frmDemo.vb
+++ src/mzkit/mzkit/pages/dockWindow/frmDemo.vb
@@ -1,13 +1,10 @@
 ﻿Imports mzkit.My
+Imports Task
 Imports WeifenLuo.WinFormsUI.Docking
 
 Public Class frmDemo
 
-    Dim demoPage As New DemoDataPage
-
     Private Sub frmDemo_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Controls.Add(demoPage)
-        demoPage.Dock = DockStyle.Fill
         Me.Icon = My.Resources.chemistry
 
         Me.ShowIcon = True
@@ -17,5 +14,42 @@ Public Class frmDemo
     Public Sub ShowPage()
         Me.Show(MyApplication.host.dockPanel)
         DockState = DockState.Document
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ListView1_DoubleClick(sender As Object, e As EventArgs) Handles ListView1.DoubleClick
+        If ListView1.SelectedItems.Count = 0 Then
+            Return
+        End If
+
+        Dim i As Integer = ListView1.SelectedIndices.Item(0)
+
+        Select Case i
+            Case 0 : Call Process.Start("https://ms-imaging.org/wp/imzml/example-files-test/")
+            Case 1
+
+                Dim findRaw = MyApplication.fileExplorer.findRawFileNode("003_Ex2_Orbitrap_CID.mzXML")
+                Dim demoPath As String = $"{App.HOME}/demo/003_Ex2_Orbitrap_CID.mzXML"
+
+                If findRaw Is Nothing Then
+                    If Not demoPath.FileExists Then
+                        MyApplication.host.showStatusMessage("the demo data file is missing!", My.Resources.StatusAnnotations_Warning_32xLG_color)
+                        Return
+                    End If
+                    MyApplication.fileExplorer.addFileNode(MyApplication.fileExplorer.getRawCache(demoPath))
+                    findRaw = MyApplication.fileExplorer.findRawFileNode("003_Ex2_Orbitrap_CID.mzXML")
+                End If
+
+                MyApplication.fileExplorer.treeView1.SelectedNode = findRaw
+                MyApplication.fileExplorer.showRawFile(DirectCast(findRaw.Tag, Raw))
+                MyApplication.host.ShowMzkitToolkit()
+
+            Case 2
+            Case 3
+
+        End Select
     End Sub
 End Class
