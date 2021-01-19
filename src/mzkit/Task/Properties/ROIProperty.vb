04042ba90ca54c917ea58a9f81e80aab5bc9d1a8
xieguigang <xie.guigang@gcmodeller.org>
2021-01-19 18:27:05 +0800

    Create ROIProperty.vb

---
+++ src/mzkit/Task/Properties/ROIProperty.vb
@@ -0,0 +1,8 @@
+﻿Public Class ROIProperty
+
+    Public Property rtmin As Double
+    Public Property rtmax As Double
+    Public Property max As Double
+    Public Property peakArea As Double
+
+End Class
