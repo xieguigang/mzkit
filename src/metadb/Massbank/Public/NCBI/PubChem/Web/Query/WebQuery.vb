f1e87f5451d9da95035c9787bd1b5de0b19a8262
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-21 16:48:09 +0800

    Update WebQuery.vb

--- src/metadb/Massbank/Public/NCBI/PubChem/Web/Query/WebQuery.vb
+++ src/metadb/Massbank/Public/NCBI/PubChem/Web/Query/WebQuery.vb
@@ -140,7 +140,7 @@ Namespace NCBI.PubChem
 
     Public Class WebQuery : Inherits WebQuery(Of String)
 
-        Const fetchPugView As String = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/%s/XML/"
+        Const fetchPugView As String = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/%s/XML/?response_type=display"
 
         Public Sub New(<CallerMemberName>
                        Optional cache As String = Nothing,
