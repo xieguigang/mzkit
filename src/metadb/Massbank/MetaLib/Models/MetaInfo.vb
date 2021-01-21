c2c531415cc66ce4c482eef38065db58d49b9f2c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-21 14:50:38 +0800

    split source file module

---
+++ src/metadb/Massbank/MetaLib/Models/MetaInfo.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+
+Namespace MetaLib.Models
+
+    Public Class MetaInfo : Implements INamedValue
+        Implements IEquatable(Of MetaInfo)
+
+        ''' <summary>
+        ''' 该物质在整合库之中的唯一标识符
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property ID As String Implements IKeyedEntity(Of String).Key
+        <XmlAttribute> Public Property formula As String
+        <XmlAttribute> Public Property exact_mass As Double
+
+        Public Property name As String
+
+        <XmlElement>
+        Public Property synonym As String()
+
+        Public Property xref As xref
+
+        Public Overrides Function ToString() As String
+            Return name
+        End Function
+
+        Public Overloads Function Equals(other As MetaInfo) As Boolean Implements IEquatable(Of MetaInfo).Equals
+            Static metaEquals As MetaEquals
+
+            If metaEquals Is Nothing Then
+                metaEquals = New MetaEquals
+            End If
+
+            If other Is Nothing Then
+                Return False
+            Else
+                Return metaEquals.Equals(Me, other)
+            End If
+        End Function
+    End Class
+End Namespace
--- src/metadb/Massbank/MetaLib/Models/MetaLib.vb
+++ src/metadb/Massbank/MetaLib/Models/MetaLib.vb
@@ -1,100 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::2ba7702692fabb7f629d393bc362743b, src\metadb\Massbank\MetaLib\Models\MetaLib.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class MetaInfo
-    ' 
-    '         Properties: exact_mass, formula, ID, name, synonym
-    '                     xref
-    ' 
-    '         Function: Equals, ToString
-    ' 
-    '     Class MetaLib
-    ' 
-    '         Properties: [class], biofluid_locations, kingdom, molecular_framework, pathways
-    '                     sub_class, super_class, tissue_locations
-    ' 
-    '         Function: Equals, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class MetaInfo
+' 
+'         Properties: exact_mass, formula, ID, name, synonym
+'                     xref
+' 
+'         Function: Equals, ToString
+' 
+'     Class MetaLib
+' 
+'         Properties: [class], biofluid_locations, kingdom, molecular_framework, pathways
+'                     sub_class, super_class, tissue_locations
+' 
+'         Function: Equals, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Xml.Serialization
-Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
-
 Namespace MetaLib.Models
-
-    Public Class MetaInfo : Implements INamedValue
-        Implements IEquatable(Of MetaInfo)
-
-        ''' <summary>
-        ''' 该物质在整合库之中的唯一标识符
-        ''' </summary>
-        ''' <returns></returns>
-        <XmlAttribute> Public Property ID As String Implements IKeyedEntity(Of String).Key
-        <XmlAttribute> Public Property formula As String
-        <XmlAttribute> Public Property exact_mass As Double
-
-        Public Property name As String
-
-        <XmlElement>
-        Public Property synonym As String()
-
-        Public Property xref As xref
-
-        Public Overrides Function ToString() As String
-            Return name
-        End Function
-
-        Public Overloads Function Equals(other As MetaInfo) As Boolean Implements IEquatable(Of MetaInfo).Equals
-            Static metaEquals As MetaEquals
-
-            If metaEquals Is Nothing Then
-                metaEquals = New MetaEquals
-            End If
-
-            If other Is Nothing Then
-                Return False
-            Else
-                Return metaEquals.Equals(Me, other)
-            End If
-        End Function
-    End Class
 
     ''' <summary>
     ''' 对``chebi/kegg/pubchem/HMDB/metlin``的物质注释信息整合库，这个数据库只要为了生成编号，名称之类的注释信息而构建的
