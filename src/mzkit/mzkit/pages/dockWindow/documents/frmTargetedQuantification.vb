4583da053b3cb86b0592eac239a239d4eb140253
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-02-01 18:05:06 +0800

    select linear files

--- src/mzkit/mzkit/pages/dockWindow/documents/frmTargetedQuantification.vb
+++ src/mzkit/mzkit/pages/dockWindow/documents/frmTargetedQuantification.vb
@@ -1,6 +1,28 @@
-﻿Public Class frmTargetedQuantification
+﻿Imports mzkit.My
+Imports RibbonLib.Controls.Events
+Imports RibbonLib.Interop
+
+Public Class frmTargetedQuantification
 
     Private Sub frmTargetedQuantification_Load(sender As Object, e As EventArgs) Handles Me.Load
+        MyApplication.ribbon.TargetedContex.ContextAvailable = ContextAvailability.Active
+        AddHandler MyApplication.ribbon.ImportsLinear.ExecuteEvent, AddressOf loadLinearRaw
+    End Sub
 
+    Private Sub frmTargetedQuantification_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        RemoveHandler MyApplication.ribbon.ImportsLinear.ExecuteEvent, AddressOf loadLinearRaw
+    End Sub
+
+    Sub loadLinearRaw(sender As Object, e As ExecuteEventArgs)
+        Using importsFile As New OpenFileDialog With {
+            .Filter = "LC-MSMS / GC-MS Targeted(*.mzML)|*.mzML|GC-MS Targeted(*.cdf)|*.cdf",
+            .Multiselect = True,
+            .Title = "Select linears"
+        }
+
+            If importsFile.ShowDialog = DialogResult.OK Then
+
+            End If
+        End Using
     End Sub
 End Class
