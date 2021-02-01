b195c1a8fde7a10282cdc3b0b56ab40fbc657e05
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-02-01 14:07:29 +0800

    load mzML raw file for GCMS

--- src/assembly/assembly/MarkupData/mzML/XML/Components.vb
+++ src/assembly/assembly/MarkupData/mzML/XML/Components.vb
@@ -1,100 +1,101 @@
 ﻿#Region "Microsoft.VisualBasic::b24c4e6039dc876d93a58bc108b76465, src\assembly\assembly\MarkupData\mzML\XML\Components.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class indexList
-    ' 
-    '         Properties: index
-    ' 
-    '     Class index
-    ' 
-    '         Properties: name, offsets
-    ' 
-    '     Class offset
-    ' 
-    '         Properties: idRef, value
-    ' 
-    '         Function: ToString
-    ' 
-    '     Class mzML
-    ' 
-    '         Properties: cvList, run
-    ' 
-    '     Class cvList
-    ' 
-    '         Properties: list
-    ' 
-    '     Class List
-    ' 
-    '         Properties: count
-    ' 
-    '     Class DataList
-    ' 
-    '         Properties: defaultDataProcessingRef
-    ' 
-    '     Structure cv
-    ' 
-    '         Properties: fullName, id, URI, version
-    ' 
-    '     Class Params
-    ' 
-    '         Properties: cvParams, userParams
-    ' 
-    '     Class userParam
-    ' 
-    '         Properties: name, type, value
-    ' 
-    '         Function: ToString
-    ' 
-    '     Class cvParam
-    ' 
-    '         Properties: accession, cvRef, name, unitAccession, unitCvRef
-    '                     unitName, value
-    ' 
-    '         Function: ToString
-    ' 
-    '     Class run
-    ' 
-    '         Properties: chromatogramList, defaultInstrumentConfigurationRef, defaultSourceFileRef, id, spectrumList
-    '                     startTimeStamp
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class indexList
+' 
+'         Properties: index
+' 
+'     Class index
+' 
+'         Properties: name, offsets
+' 
+'     Class offset
+' 
+'         Properties: idRef, value
+' 
+'         Function: ToString
+' 
+'     Class mzML
+' 
+'         Properties: cvList, run
+' 
+'     Class cvList
+' 
+'         Properties: list
+' 
+'     Class List
+' 
+'         Properties: count
+' 
+'     Class DataList
+' 
+'         Properties: defaultDataProcessingRef
+' 
+'     Structure cv
+' 
+'         Properties: fullName, id, URI, version
+' 
+'     Class Params
+' 
+'         Properties: cvParams, userParams
+' 
+'     Class userParam
+' 
+'         Properties: name, type, value
+' 
+'         Function: ToString
+' 
+'     Class cvParam
+' 
+'         Properties: accession, cvRef, name, unitAccession, unitCvRef
+'                     unitName, value
+' 
+'         Function: ToString
+' 
+'     Class run
+' 
+'         Properties: chromatogramList, defaultInstrumentConfigurationRef, defaultSourceFileRef, id, spectrumList
+'                     startTimeStamp
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
@@ -133,6 +134,11 @@ Namespace MarkupData.mzML
         Public Property cvList As cvList
         Public Property run As run
         Public Property fileDescription As fileDescription
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function LoadChromatogramList(path As String) As IEnumerable(Of chromatogram)
+            Return MarkupData.mzML.LoadChromatogramList(path)
+        End Function
 
     End Class
 
--- src/mzmath/TargetedMetabolomics/GCMS/mzMLReader/mzMLReader.vb
+++ src/mzmath/TargetedMetabolomics/GCMS/mzMLReader/mzMLReader.vb
@@ -1,6 +1,38 @@
-﻿Namespace GCMS
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.mzML
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
+
+Namespace GCMS
 
     Public Module mzMLReader
 
+        Public Function LoadFile(path As String) As Raw
+            Dim chromatogramList = mzML _
+                .LoadChromatogramList(path) _
+                .ToArray
+            Dim TIC As ChromatogramTick() = chromatogramList _
+                .Where(Function(c) c.id = "TIC") _
+                .First _
+                .Ticks _
+                .OrderBy(Function(tick) tick.Time) _
+                .ToArray
+
+            Return New Raw With {
+                .fileName = path,
+                .title = path.BaseName,
+                .attributes = New Dictionary(Of String, String),
+                .tic = TIC.Select(Function(tick) tick.Intensity).ToArray,
+                .times = TIC.Select(Function(tick) tick.Time).ToArray,
+                .ms = chromatogramList _
+                    .Where(Function(c) c.id <> "TIC") _
+                    .Select(AddressOf readMs) _
+                    .ToArray
+            }
+        End Function
+
+        Private Function readMs(chromatogram As MarkupData.mzML.chromatogram) As ms1_scan()
+
+        End Function
     End Module
 End Namespace
