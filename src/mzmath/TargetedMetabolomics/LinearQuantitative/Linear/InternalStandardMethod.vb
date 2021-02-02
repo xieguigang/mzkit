91dff5ae9ce9582d61180efa12b17cc9241f9fd2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-02-02 10:40:10 +0800

    fix of namespace

---
+++ src/mzmath/TargetedMetabolomics/LinearQuantitative/Linear/InternalStandardMethod.vb
@@ -0,0 +1,10 @@
+﻿
+Namespace LinearQuantitative
+
+    ''' <summary>
+    ''' 普通标准曲线计算算法模块
+    ''' </summary>
+    Module InternalStandardMethod
+
+    End Module
+End Namespace
---
+++ src/mzmath/TargetedMetabolomics/LinearQuantitative/Linear/WorkCurve.vb
@@ -0,0 +1,9 @@
+﻿Namespace LinearQuantitative
+
+    ''' <summary>
+    ''' 工作标曲计算算法模块
+    ''' </summary>
+    Module WorkCurve
+
+    End Module
+End Namespace
