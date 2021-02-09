416264e9bea6b81437d596775a515258fd1f9da1
xieguigang <xie.guigang@gcmodeller.org>
2021-02-09 11:06:26 +0800

    check biodeep session

--- src/mzkit/Task/BioDeep/BioDeepSession.vb
+++ src/mzkit/Task/BioDeep/BioDeepSession.vb
@@ -1,6 +1,22 @@
-﻿Public Class BioDeepSession
+﻿Imports Microsoft.VisualBasic.Linq
+
+Public Class BioDeepSession
 
     Public Property cookieName As String
     Public Property ssid As String
+
+    Private Function headerProvider() As Dictionary(Of String, String)
+        Return New Dictionary(Of String, String) From {{"Cookie", $"BIODEEP_USER_SESSION={ssid};"}}
+    End Function
+
+    Public Function CheckSession() As Boolean
+        Dim url$ = "http://my.biodeep.cn/services/ping.vbs"
+        Dim result As Boolean = url _
+            .GET(headers:=headerProvider) _
+            .DoCall(AddressOf MessageParser.ParseMessage) _
+            .success
+
+        Return result
+    End Function
 
 End Class
---
+++ src/mzkit/Task/BioDeep/MessageParser.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+
+Public Module MessageParser
+
+    Public Function ParseMessage(returns As String) As JsonObject
+        Return New JsonParser().OpenJSON(returns)
+    End Function
+
+    <Extension>
+    Public Function success(msg As JsonObject) As Boolean
+        Return msg.ContainsKey("code") AndAlso msg!code.AsString = "0"
+    End Function
+
+    <Extension>
+    Public Function getMsgString(msg As JsonObject) As String
+        Return msg.ContainsKey("info") AndAlso msg!info.AsString
+    End Function
+End Module
