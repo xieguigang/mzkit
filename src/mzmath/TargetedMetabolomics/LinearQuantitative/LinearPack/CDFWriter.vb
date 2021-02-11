322fe549de7ba9746b3a08a679927ecd4ab15750
xieguigang <xie.guigang@gcmodeller.org>
2021-02-11 14:54:51 +0800

    save information

--- src/mzmath/TargetedMetabolomics/LinearQuantitative/LinearPack/CDFWriter.vb
+++ src/mzmath/TargetedMetabolomics/LinearQuantitative/LinearPack/CDFWriter.vb
@@ -1,5 +1,10 @@
 ﻿Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Data.IO.netCDF
+Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace LinearQuantitative.Data
 
@@ -7,7 +12,64 @@ Namespace LinearQuantitative.Data
 
         <Extension>
         Public Function Write(pack As LinearPack, file As Stream) As Boolean
+            Using cdffile As New netCDF.CDFWriter(file)
+                Call pack.Write(cdffile)
+            End Using
 
+            Return True
         End Function
+
+        <Extension>
+        Private Sub Write(pack As LinearPack, file As netCDF.CDFWriter)
+            Call pack.writeGlobals(file)
+            Call pack.peakLinearNames(file)
+            Call pack.writeLinears(file)
+        End Sub
+
+        <Extension>
+        Private Sub peakLinearNames(pack As LinearPack, file As netCDF.CDFWriter)
+            Dim data As New CDFData With {.chars = pack.linears.Select(Function(l) l.name).GetJson}
+            Dim size As New Dimension With {.name = "sizeofLinears", .size = data.Length}
+            Dim attrs As attribute() = {
+                New attribute With {.name = "linears", .type = CDFDataTypes.INT, .value = pack.linears.Length}
+            }
+
+            file.AddVariable("linears", data, size, attrs)
+        End Sub
+
+        <Extension>
+        Private Sub writeLinears(pack As LinearPack, file As netCDF.CDFWriter)
+            For Each line As StandardCurve In pack.linears
+                Call line.writeLinear(file)
+            Next
+        End Sub
+
+        <Extension>
+        Private Sub writeLinear(linear As StandardCurve, file As netCDF.CDFWriter)
+            Dim formula As Formula = linear.linear.Polynomial
+            Dim attrs As attribute() = {
+                New attribute With {.name = "IS", .type = CDFDataTypes.CHAR, .value = linear.IS.ID},
+                New attribute With {.name = "IS_name", .type = CDFDataTypes.CHAR, .value = linear.IS.name},
+                New attribute With {.name = "CIS", .type = CDFDataTypes.DOUBLE, .value = linear.IS.CIS},
+                New attribute With {.name = "name", .type = CDFDataTypes.CHAR, .value = linear.name},
+                New attribute With {.name = "points", .type = CDFDataTypes.INT, .value = linear.points.Length},
+                New attribute With {.name = "R2", .type = CDFDataTypes.CHAR, .value = linear.linear.R2},
+                New attribute With {.name = "a", .type = CDFDataTypes.CHAR, .value = linear.IS.ID},
+                New attribute With {.name = "b", .type = CDFDataTypes.CHAR, .value = linear.IS.ID}
+            }
+
+
+        End Sub
+
+        <Extension>
+        Private Sub writeGlobals(pack As LinearPack, file As netCDF.CDFWriter)
+            Dim title As New attribute With {.name = "title", .type = CDFDataTypes.CHAR, .value = pack.title}
+            Dim time As New attribute With {.name = "time", .type = CDFDataTypes.CHAR, .value = pack.time.ToString}
+            Dim github As New attribute With {.name = "github", .type = CDFDataTypes.CHAR, .value = "https://github.com/xieguigang/mzkit"}
+            Dim linears As New attribute With {.name = "linears", .type = CDFDataTypes.INT, .value = pack.linears.Length}
+            Dim peaks As New attribute With {.name = "peaks", .type = CDFDataTypes.INT, .value = pack.peakSamples.Length}
+
+            Call file.GlobalAttributes(title, time, github, linears, peaks)
+        End Sub
     End Module
 End Namespace
