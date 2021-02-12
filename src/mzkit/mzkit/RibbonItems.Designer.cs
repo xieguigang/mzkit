a982c80147a82776253061e1c82e09a01bc2d0f2
xieguigang <xie.guigang@gcmodeller.org>
2021-02-13 00:51:37 +0800

    update source files

--- src/mzkit/mzkit/RibbonItems.Designer.cs
+++ src/mzkit/mzkit/RibbonItems.Designer.cs
@@ -13,8 +13,7 @@ using RibbonLib;
 using RibbonLib.Controls;
 using RibbonLib.Interop;
 
-namespace RibbonLib.Controls
-{
+
     partial class RibbonItems
     {
         private static class Cmd
@@ -339,4 +338,3 @@ namespace RibbonLib.Controls
         }
 
     }
-}
