c7340f66ed07090bdca6294e0d633c4b8cb178fe
xieguigang <xie.guigang@gcmodeller.org>
2021-02-16 19:43:03 +0800

    add pdf components

--- dist/buildPdf.cmd
+++ dist/buildPdf.cmd
@@ -1,3 +1,5 @@
 @echo off
 
-wkhtmltopdf --javascript-delay 1000 --margin-bottom 5 --margin-top 5 --margin-left 5 --margin-right 5 https://mzkit.org/ "D:\mzkit\dist\README.pdf"
+SET wkhtmltopdf="../src/mzkit/dist/wkhtmltopdf.exe"
+
+%wkhtmltopdf% --javascript-delay 1000 --margin-bottom 5 --margin-top 5 --margin-left 5 --margin-right 5 https://mzkit.org/ "./README.pdf"
