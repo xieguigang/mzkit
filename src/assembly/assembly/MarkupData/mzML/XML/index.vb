5e32459b471b7abc72939231473b77169f669393
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-02-23 11:32:39 +0800

    how to parse index object values

--- src/assembly/assembly/MarkupData/mzML/XML/index.vb
+++ src/assembly/assembly/MarkupData/mzML/XML/index.vb
@@ -1,64 +1,66 @@
 ﻿#Region "Microsoft.VisualBasic::31ba491986449a7807d63483f52c7ff1, assembly\MarkupData\mzML\XML\index.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class indexList
-    ' 
-    '         Properties: index
-    ' 
-    '     Class index
-    ' 
-    '         Properties: name, offsets
-    ' 
-    '         Function: ToString
-    ' 
-    '     Class offset
-    ' 
-    '         Properties: idRef, value
-    ' 
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class indexList
+' 
+'         Properties: index
+' 
+'     Class index
+' 
+'         Properties: name, offsets
+' 
+'         Function: ToString
+' 
+'     Class offset
+' 
+'         Properties: idRef, value
+' 
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Language.Default
 
 Namespace MarkupData.mzML
@@ -67,6 +69,22 @@ Namespace MarkupData.mzML
 
         <XmlElement(NameOf(index))>
         Public Property index As index()
+
+        Friend Function ParseIndexList(bin As BinaryDataReader, offset As Long) As indexList
+
+        End Function
+
+        Public Iterator Function GetOffsets() As IEnumerable(Of NamedValue(Of Long))
+            For Each group As index In index
+                For Each offset As offset In group.offsets
+                    Yield New NamedValue(Of Long) With {
+                        .Description = group.name,
+                        .Name = offset.idRef,
+                        .Value = offset.value
+                    }
+                Next
+            Next
+        End Function
 
     End Class
 
