f234286f86aa48cead1780f47987f8796c6eefc4
xieguigang <xie.guigang@gcmodeller.org>
2021-02-26 22:36:41 +0800

    split source file modules

--- src/metadna/metaDNA/KEGGHandler.vb
+++ src/metadna/metaDNA/KEGGHandler.vb
@@ -56,16 +56,3 @@ Public Class KEGGHandler
 
 End Class
 
-''' <summary>
-''' Indexed of target compound by m/z
-''' </summary>
-Public Structure MassIndexKey
-
-    Dim mz As Double
-    Dim precursorType As String
-
-    Public Overrides Function ToString() As String
-        Return $"{precursorType} {mz}"
-    End Function
-
-End Structure
---
+++ src/metadna/metaDNA/Models/MassIndexKey.vb
@@ -0,0 +1,13 @@
+﻿''' <summary>
+''' Indexed of target compound by m/z
+''' </summary>
+Public Structure MassIndexKey
+
+    Dim mz As Double
+    Dim precursorType As String
+
+    Public Overrides Function ToString() As String
+        Return $"{precursorType} {mz}"
+    End Function
+
+End Structure
