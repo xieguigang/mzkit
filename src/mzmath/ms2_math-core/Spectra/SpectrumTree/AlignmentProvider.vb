0266443751db9e1e8a1a1041459f5d614fa7b0e7
xieguigang <xie.guigang@gcmodeller.org>
2021-02-27 00:40:24 +0800

    run mass spectrum data alignment

--- src/mzmath/ms2_math-core/Spectra/SpectrumTree/AlignmentProvider.vb
+++ src/mzmath/ms2_math-core/Spectra/SpectrumTree/AlignmentProvider.vb
@@ -1,69 +1,72 @@
 ﻿#Region "Microsoft.VisualBasic::c9a844eff93634351c0d5c3755e04bee, ms2_math-core\Spectra\SpectrumTree\AlignmentProvider.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class AlignmentProvider
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '     Enum ScoreAggregates
-    ' 
-    '         max, min, sum
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    '     Class CosAlignment
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: CreateAlignment, GetScore
-    ' 
-    '     Class JaccardAlignment
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: CreateAlignment, GetScore
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class AlignmentProvider
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'     Enum ScoreAggregates
+' 
+'         max, min, sum
+' 
+'  
+' 
+' 
+' 
+'     Class CosAlignment
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: CreateAlignment, GetScore
+' 
+'     Class JaccardAlignment
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: CreateAlignment, GetScore
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.Xml
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports stdNum = System.Math
+Imports vec = Microsoft.VisualBasic.Math
 
 Namespace Spectra
 
@@ -145,6 +148,38 @@ Namespace Spectra
 
             Return min
         End Function
+
+        Public Overrides Function GetScore(alignment() As SSM2MatrixFragment) As (forward As Double, reverse As Double)
+            Dim a, b As Vector
+            Dim align As SSM2MatrixFragment()
+            Dim forward, reverse As Double
+
+            ' forward
+            align = alignment _
+                .Where(Function(x)
+                           Return Not x.query.IsNaNImaginary AndAlso x.query > 0
+                       End Function) _
+                .ToArray
+
+            a = align.Select(Function(x) x.query).AsVector
+            b = align.Select(Function(x) If(x.ref.IsNaNImaginary, 0, x.ref)).AsVector
+
+            forward = vec.SSM(a, b)
+
+            ' reverse
+            align = alignment _
+                .Where(Function(x)
+                           Return Not x.ref.IsNaNImaginary AndAlso x.ref > 0
+                       End Function) _
+                .ToArray
+
+            a = align.Select(Function(x) If(x.query.IsNaNImaginary, 0, x.query)).AsVector
+            b = align.Select(Function(x) x.ref).AsVector
+
+            reverse = vec.SSM(a, b)
+
+            Return (forward, reverse)
+        End Function
     End Class
 
     Public Class JaccardAlignment : Inherits AlignmentProvider
@@ -159,6 +194,19 @@ Namespace Spectra
 
         Public Overrides Function GetScore(a As ms2(), b As ms2()) As Double
             Return GlobalAlignment.JaccardIndex(a, b, mzwidth, topSet)
+        End Function
+
+        Public Overrides Function GetScore(alignment() As SSM2MatrixFragment) As (forward As Double, reverse As Double)
+            Static NA As Index(Of String) = {"NA", "n/a", "NaN"}
+
+            Dim intersect As Integer = Aggregate a As SSM2MatrixFragment
+                                       In alignment
+                                       Where Not a.da Like NA
+                                       Into Sum(1)
+            Dim union As Integer = alignment.Length
+            Dim J As Double = intersect / union
+
+            Return (J, J)
         End Function
     End Class
 End Namespace
