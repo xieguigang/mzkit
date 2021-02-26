880dd8ab8a7ac64043fbe9c439a62a3b60605f1a
xieguigang <xie.guigang@gcmodeller.org>
2021-02-26 23:05:25 +0800

    infer link data model

--- src/metadna/metaDNA/Models/InferLink.vb
+++ src/metadna/metaDNA/Models/InferLink.vb
@@ -1,3 +1,14 @@
-﻿Public Class InferLink
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.Xml
+
+Public Class InferLink : Inherits AlignmentOutput
+
+    Public Property level As InferLevel
 
 End Class
+
+Public Enum InferLevel
+
+    Ms1
+    Ms2
+
+End Enum
