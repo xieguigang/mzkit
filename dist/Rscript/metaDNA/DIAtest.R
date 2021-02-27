aa16534ae9f6b3ffb1896fd8d634a6d2883640f5
xieguigang <xie.guigang@gcmodeller.org>
2021-02-27 11:10:42 +0800

    fix of script test

--- dist/Rscript/metaDNA/DIAtest.R
+++ dist/Rscript/metaDNA/DIAtest.R
@@ -9,8 +9,10 @@ metadna(
 :> range(["[M]+", "[M+H]+", "[M+H+H2O]+"])
 :> load.kegg(kegg.library(repo = "E:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd"))
 :> load.kegg_network(kegg.network(repo = "E:\biodeep\biodeepdb_v3\KEGG\reaction_class"))
-:> load.raw(
+# :> load.raw(
+	# sample = assembly::mzxml.mgf("E:\biodeep\biodeepDB\lxy-CID30.mzML")
+# )
+:> DIA.infer(
 	sample = assembly::mzxml.mgf("E:\biodeep\biodeepDB\lxy-CID30.mzML")
 )
-:> DIA.infer
 ;
