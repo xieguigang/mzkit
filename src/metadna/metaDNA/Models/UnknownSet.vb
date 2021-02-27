487edeb6e3cdb709bd9a62fe810f37faa2e83e25
xieguigang <xie.guigang@gcmodeller.org>
2021-02-27 12:54:50 +0800

    metadna seeding

--- src/metadna/metaDNA/Models/UnknownSet.vb
+++ src/metadna/metaDNA/Models/UnknownSet.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::9424e4961b07ec67605a6700159b3485, metaDNA\Models\UnknownSet.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class UnknownSet
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: CreateTree, MassQuery, QueryByParentMz
-    ' 
-    ' /********************************************************************************/
+' Class UnknownSet
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: CreateTree, MassQuery, QueryByParentMz
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -53,9 +53,14 @@ Imports Microsoft.VisualBasic.ComponentModel.Algorithm.BinaryTree
 Public Class UnknownSet
 
     Dim features As AVLTree(Of Double, PeakMs2)
+    Dim spectrumIndex As Dictionary(Of String, PeakMs2)
 
     Private Sub New()
     End Sub
+
+    Public Function QueryByKey(key As String) As PeakMs2
+        Return spectrumIndex.TryGetValue(key)
+    End Function
 
     Public Function QueryByParentMz(mz As Double) As PeakMs2()
         Return features.Find(mz)?.Members
@@ -81,17 +86,20 @@ Public Class UnknownSet
     ''' <returns></returns>
     Public Shared Function CreateTree(raw As IEnumerable(Of PeakMs2), tolerance As Tolerance) As UnknownSet
         Dim tree As New AVLTree(Of Double, PeakMs2)(MassQuery(tolerance), Function(mz) mz.ToString("F4"))
+        Dim index As New Dictionary(Of String, PeakMs2)
 
         For Each product As PeakMs2 In raw
             If product.lib_guid Is Nothing Then
                 product.lib_guid = product.ToString
             End If
 
+            Call index.Add(product.lib_guid, product)
             Call tree.Add(product.mz, product, valueReplace:=False)
         Next
 
         Return New UnknownSet With {
-            .features = tree
+            .features = tree,
+            .spectrumIndex = index
         }
     End Function
 
--- src/metadna/metaDNA/SeedsProvider.vb
+++ src/metadna/metaDNA/SeedsProvider.vb
@@ -1,4 +1,8 @@
-﻿Public Class SeedsProvider
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports stdnum = System.Math
+
+Public Class SeedsProvider
 
     ReadOnly unknowns As UnknownSet
 
@@ -7,6 +11,37 @@
     End Sub
 
     Public Iterator Function Seeding(infer As IEnumerable(Of InferLink)) As IEnumerable(Of AnnotatedSeed)
+        Dim kegg = infer.GroupBy(Function(i) i.kegg.kegg_id).ToArray
 
+        For Each compound As IGrouping(Of String, InferLink) In kegg
+            Dim bestOrder = compound _
+                .Select(Function(i)
+                            Return (score:=Score(i), i)
+                        End Function) _
+                .OrderByDescending(Function(i) i.score) _
+                .ToArray
+            Dim best = bestOrder.First
+            Dim bestLib As New LibraryMatrix With {
+                .ms2 = unknowns.QueryByKey(best.Item2.query.id).mzInto,
+                .name = best.Item2.query.id
+            }
+
+            Yield New AnnotatedSeed With {
+                .id = best.Item2.query.id,
+                .kegg_id = compound.Key,
+                .parent = New ms1_scan With {
+                    .mz = best.Item2.query.mz,
+                    .scan_time = best.Item2.query.rt,
+                    .intensity = 0
+                },
+                .products = New Dictionary(Of String, LibraryMatrix) From {
+                    {best.Item2.query.id, bestLib}
+                }
+            }
+        Next
+    End Function
+
+    Public Function Score(infer As InferLink) As Double
+        Return stdnum.Min(infer.forward, infer.reverse)
     End Function
 End Class
