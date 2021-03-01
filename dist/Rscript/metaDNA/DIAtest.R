f1b98abbd29564a285c25157ebd912cb7dca1c64
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-01 11:42:48 +0800

    config script for run multiple file test

--- dist/Rscript/metaDNA/DIAtest.R
+++ dist/Rscript/metaDNA/DIAtest.R
@@ -7,17 +7,26 @@ const metadna = metadna(
 	mzwidth   = tolerance(0.3, "da")
 )
 :> range(["[M]+", "[M+H]+", "[M+H+H2O]+"])
-:> load.kegg(kegg.library(repo = "E:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd"))
-:> load.kegg_network(kegg.network(repo = "E:\biodeep\biodeepdb_v3\KEGG\reaction_class"))
+:> load.kegg(kegg.library(repo = "D:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd"))
+:> load.kegg_network(kegg.network(repo = "D:\biodeep\biodeepdb_v3\KEGG\reaction_class"))
 # :> load.raw(
 	# sample = assembly::mzxml.mgf("E:\biodeep\biodeepDB\lxy-CID30.mzML")
 # )
 ;
 
-const rawSample as string = "E:\biodeep\biodeepDB\lxy-CID30.mzML";
+const rawSample as string = ["F:\111\P201200404.mzXML",
+"F:\111\P201200406.mzXML",
+"F:\111\P201200407.mzXML",
+"F:\111\P201200408.mzXML",
+"F:\111\P201200400.mzXML",
+"F:\111\P201200401.mzXML",
+"F:\111\P201200402.mzXML",
+"F:\111\P201200403.mzXML"]; # "E:\biodeep\biodeepDB\lxy-CID30.mzML";
 const infer = metadna :> DIA.infer(
-	sample = rawSample
-		:> assembly::mzxml.mgf 
+	sample = rawSample 
+		:> sapply(assembly::mzxml.mgf) 
+		:> as.vector
+		:> unlist 
 		:> centroid 
 		:> make.ROI_names
 )
@@ -25,12 +34,12 @@ const infer = metadna :> DIA.infer(
 
 metadna 
 :> as.table(infer) 
-:> write.csv(file = `${dirname(@script)}/${basename(rawSample)}_raw.csv`)
+:> write.csv(file = `F:/111/all.csv`)
 ;
 
 metadna
 :> as.table(infer, unique = TRUE)
-:> write.csv(file = `${dirname(@script)}/${basename(rawSample)}.csv`)
+:> write.csv(file = `F:/111/metaDNA.csv`)
 ;
 
 require(igraph);
@@ -38,5 +47,5 @@ require(igraph);
 metadna
 :> as.table(infer)
 :> as.graph
-:> save.network(file = `${dirname(@script)}/${basename(rawSample)}_infers/`)
+:> save.network(file = `F:/111/infers/`)
 ;
