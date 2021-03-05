577e66016a213da7d83f2369819365410c2c639c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-05 14:51:29 +0800

    R# demo script for TIC overlaps plot

--- src/mzkit/setup/demo_script/TIC_overlaps.R
+++ src/mzkit/setup/demo_script/TIC_overlaps.R
@@ -1,4 +1,6 @@
 ﻿imports "gtk" from "Rstudio";
+imports "visual" from "mzkit.plot";
+imports ["assembly", "mzweb"] from "mzkit";
 
 const rawfiles as string = gtk::selectFiles(
     title  = "Select raw data files in mzXML/mzML formats.", 
@@ -8,7 +10,29 @@ const rawfiles as string = gtk::selectFiles(
 
 let TIC_overlaps as function() {
     const savefile as string = `${dirname(rawfiles[1])}/TIC_overlaps.png`;
-    
+    const TIC = new overlaps();
+	
+	print("loading TIC data of raw data files...");
+	
+	for(path in rawfiles) {		
+		TIC[[basename(path)]] = path 
+		:> raw.scans 
+		:> load.chromatogram
+		;
+		
+		print(path);
+	}
+	
+	bitmap(file = savefile) {
+		TIC :> plot(
+			bpc     = FALSE, 
+			opacity = 100, 
+			colors  = "Paired:c12"
+		)
+		;
+	}
+	
+	print("TIC plot job done!");
 }
 
 if (length(rawfiles) == 0) {
