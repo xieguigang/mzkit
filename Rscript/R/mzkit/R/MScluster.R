47b97c702bcb3e4295ccc29a9e2af6850dc012ee
xieguigang <xie.guigang@gcmodeller.org>
2021-03-06 16:46:42 +0800

    improvements of the algorithm of create MS tree cluster

--- Rscript/R/mzkit/R/MScluster.R
+++ Rscript/R/mzkit/R/MScluster.R
@@ -3,28 +3,25 @@
 #' @param peak_ms2 a small bundle of MS matrix. MS matrix items
 #' in this collection should be pre-processed by \code{centroid}
 #' 
-MScluster = function(peak_ms2, tolerance = mzkit::tolerance(0.3, "da")) {
-    cos = lapply(peak_ms2, function(ref) {
-        # get distance
-        1 - alignVector(ref, peak_ms2, tolerance);
-    });
-    cos  = t(as.data.frame(cos));
-    d    = dist(cos);
-    tree = hclust(d, method = 'average');
-
-    # plot(tree, hang = -1, cex=.8, main = 'average linkage clustering');
-
-    # returns MS tree clusters
-    cutree(tree, k = 3 + floor(length(peak_ms2) / 9));
-}
-
-alignVector = function(ref, peak_ms2, tolerance = mzkit::tolerance(0.3, "da")) {
-    as.vector(sapply(peak_ms2, function(query) {
+MScluster = function(peak_ms2, identical = 0.85, greaterThan = 0.6, tolerance = mzkit::tolerance(0.3, "da")) {
+    cos = function(query, ref) {
         query2  = globalAlign(query, ref, tolerance);
         forward = MScos(query2, ref);
         ref     = globalAlign(ref, query, tolerance);
         reverse = MScos(query, ref);
 
-        min(forward, reverse);
-    }));
+        # gets the final global alignment score
+        alignScore = min(forward, reverse);
+
+        if (alignScore >= identical) {
+            0;
+        } else if (alignScore >= greaterThan) {
+            1;
+        } else {
+            -1;
+        }
+    }
+
+    # returns MS tree clusters
+    bclusterTree::bcluster(peak_ms2, compares = cos);
 }
