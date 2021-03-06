aa6b2105baa82b53518e043a4dcc14ae449a900a
xieguigang <xie.guigang@gcmodeller.org>
2021-03-06 11:09:33 +0800

    Update globalAlignment.R

---
+++ Rscript/R/mzkit/R/globalAlignment.R
@@ -0,0 +1,21 @@
+# global alignment of two MS spectrum
+
+#'
+#' @details the MS matrix of \code{query} and \code{ref} should 
+#'     have been both pre-processed.
+#'
+MScos = function(query, ref, tolerance = mzkit::tolerance(0.3, "da")) {
+
+}
+
+globalAlign = function(x, y, tolerance = mzkit::tolerance(0.3, "da")) {
+
+}
+
+#'
+#' @details the MS matrix of \code{query} and \code{ref} should 
+#'     have been both pre-processed.
+#'
+MSjaccard = function(query, ref, tolerance = mzkit::tolerance(0.3, "da")) {
+
+}
