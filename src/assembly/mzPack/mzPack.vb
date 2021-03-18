e6f14441c5525dd8415bd2bbfb18ab831c6fbe7b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-18 17:11:04 +0800

    a unify buffer region model for CDF, mzpack?

--- src/assembly/mzPack/mzPack.vb
+++ src/assembly/mzPack/mzPack.vb
@@ -1,3 +1,22 @@
-﻿Public Class mzPack
+﻿Imports System.Drawing
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
+
+''' <summary>
+''' mzPack文件格式模型
+''' </summary>
+Public Class mzPack
+
+    ''' <summary>
+    ''' 一般为二维散点图
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property thumbnail As Image
+    Public Property MS As ScanMS1()
+
+    ''' <summary>
+    ''' 其他的扫描器数据，例如紫外扫描
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Scanners As Dictionary(Of String, ChromatogramOverlap)
 
 End Class
