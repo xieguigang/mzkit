56c16dac1d2b9bf260a4dba8ef2f3ae134eeca8e
xieguigang <xie.guigang@gcmodeller.org>
2021-03-19 22:30:33 +0800

    UV scan data model

---
+++ src/assembly/assembly/MarkupData/mzML/UVScan.vb
@@ -0,0 +1,31 @@
+﻿
+Imports Microsoft.VisualBasic.Math.SignalProcessing
+
+Namespace MarkupData.mzML
+
+    Public Class UVScan
+
+        Public Property wavelength As Double()
+        Public Property intensity As Double()
+        Public Property total_ion_current As Double
+        Public Property scan_time As Double
+
+        Public Overrides Function ToString() As String
+            Return $"total_ions:{total_ion_current.ToString("G3")} at {CInt(scan_time)} sec"
+        End Function
+
+        Public Function GetSignalModel() As GeneralSignal
+            Return New GeneralSignal With {
+                .description = ToString(),
+                .Measures = wavelength,
+                .measureUnit = "wavelength",
+                .reference = ToString(),
+                .Strength = intensity,
+                .meta = New Dictionary(Of String, String) From {
+                    {"title", .reference}
+                }
+            }
+        End Function
+
+    End Class
+End Namespace
--- src/mzkit/Task/Imports/Raw.vb
+++ src/mzkit/Task/Imports/Raw.vb
@@ -132,29 +132,3 @@ Public Class ScanEntry : Inherits MsScanEntry
         Return id
     End Function
 End Class
-
-Public Class UVScan
-
-    Public Property wavelength As Double()
-    Public Property intensity As Double()
-    Public Property total_ion_current As Double
-    Public Property scan_time As Double
-
-    Public Overrides Function ToString() As String
-        Return $"total_ions:{total_ion_current.ToString("G3")} at {CInt(scan_time)} sec"
-    End Function
-
-    Public Function GetSignalModel() As GeneralSignal
-        Return New GeneralSignal With {
-            .description = ToString(),
-            .Measures = wavelength,
-            .measureUnit = "wavelength",
-            .reference = ToString(),
-            .Strength = intensity,
-            .meta = New Dictionary(Of String, String) From {
-                {"title", .reference}
-            }
-        }
-    End Function
-
-End Class
