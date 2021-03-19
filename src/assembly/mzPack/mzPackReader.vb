17d9340177e8193d43fb6a5bc06384aa5864facd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-19 08:45:45 +0800

    read mzpack

--- src/assembly/mzPack/mzPackReader.vb
+++ src/assembly/mzPack/mzPackReader.vb
@@ -1,9 +1,12 @@
 ﻿Imports System.Drawing
 Imports System.IO
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
+Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Net.Http
 
 Public Class mzPackReader : Inherits BinaryStreamReader
+
+    ReadOnly otherScanners As New Dictionary(Of String, Long)
 
     Public Sub New(file As String)
         MyBase.New(file)
@@ -13,11 +16,24 @@ Public Class mzPackReader : Inherits BinaryStreamReader
         MyBase.loadIndex()
 
         ' load other scanner
-        loadScannerIndex()
+        Call loadScannerIndex()
     End Sub
 
     Private Sub loadScannerIndex()
+        Dim nsize As Integer
+        Dim scanPos As Long
+        Dim scanId As String
 
+        Using file.TemporarySeek
+            file.Seek(MSscannerIndex.nextBlock, SeekOrigin.Begin)
+            nsize = file.ReadInt32
+
+            For i As Integer = 0 To nsize - 1
+                scanPos = file.ReadInt64
+                scanId = file.ReadString(BinaryStringFormat.ZeroTerminated)
+                otherScanners(scanId) = scanPos
+            Next
+        End Using
     End Sub
 
     Public Function GetThumbnail() As Bitmap
