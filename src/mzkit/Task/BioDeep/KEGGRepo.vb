69a300ae19804040101489b48f06a1c66aa18d57
xieguigang <xie.guigang@gcmodeller.org>
2021-03-20 16:37:20 +0800

    kegg repository on biodeep

---
+++ src/mzkit/Task/BioDeep/KEGGRepo.vb
@@ -0,0 +1,12 @@
+﻿Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+
+Module KEGGRepo
+
+    Public Function RequestKEGGcompounds() As Compound()
+        Const url As String = "http://query.biodeep.cn/kegg/repository/compounds"
+    End Function
+
+    Public Function RequestKEGGReactions() As ReactionClass()
+        Const url As String = "http://query.biodeep.cn/kegg/repository/reactions"
+    End Function
+End Module
