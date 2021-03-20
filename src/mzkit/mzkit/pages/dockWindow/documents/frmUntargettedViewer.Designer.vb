405958663391c9aba84af96e7f0110ed5d4403cd
xieguigang <xie.guigang@gcmodeller.org>
2021-03-20 13:10:19 +0800

    Update frmUntargettedViewer.Designer.vb

--- src/mzkit/mzkit/pages/dockWindow/documents/frmUntargettedViewer.Designer.vb
+++ src/mzkit/mzkit/pages/dockWindow/documents/frmUntargettedViewer.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class frmUntargettedViewer
-    Inherits System.Windows.Forms.Form
+    Inherits DocumentWindow
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
