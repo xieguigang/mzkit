9457977bc5b094e2ce1af401af41bca637cd475a
xieguigang <xie.guigang@gcmodeller.org>
2021-03-21 20:38:18 +0800

    Update frmRawFeaturesList.vb

--- src/mzkit/mzkit/pages/dockWindow/explorer/frmRawFeaturesList.vb
+++ src/mzkit/mzkit/pages/dockWindow/explorer/frmRawFeaturesList.vb
@@ -642,7 +642,7 @@ Public Class frmRawFeaturesList
 
     Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
         Dim mz As Double = Val(Strings.Trim(ToolStripSpringTextBox1.Text))
-        Dim ppm As Double
+        Dim ppm As Double = MyApplication.host.GetPPMError
 
         If mz = 0.0 Then
             Call MyApplication.host.showStatusMessage($"M/z value expression '{ToolStripSpringTextBox1.Text}' is not a valid number expression, please input a valid m/z value...", My.Resources.StatusAnnotations_Warning_32xLG_color)
@@ -659,7 +659,7 @@ Public Class frmRawFeaturesList
         Dim mzNode As TreeNode = treeView1.Nodes.Add($"M/z {mz.ToString("F4")}, {Ms2.Length} hits")
 
         For Each item In Ms2
-            Call mzNode.Nodes.Add(New TreeNode(item.ToString) With {.Tag = item, .ImageIndex = 2, .SelectedImageIndex = 2})
+            Call mzNode.Nodes.Add(New TreeNode(item.ToString) With {.Tag = item, .ImageIndex = 1, .SelectedImageIndex = 1})
         Next
     End Sub
 
