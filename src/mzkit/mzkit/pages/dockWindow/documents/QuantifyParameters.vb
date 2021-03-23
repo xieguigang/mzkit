886c790d786ed1b06205d5b3fe0fd4b839829625
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-23 14:05:41 +0800

    Update QuantifyParameters.vb

--- src/mzkit/mzkit/pages/dockWindow/documents/QuantifyParameters.vb
+++ src/mzkit/mzkit/pages/dockWindow/documents/QuantifyParameters.vb
@@ -4,9 +4,15 @@ Public Class QuantifyParameters
 
     <Category("Peak Width")>
     <DisplayName("min")>
+    <Description("the min peak width in rt(seconds).")>
     Public Property peakMin As Double
     <Category("Peak Width")>
     <DisplayName("max")>
+    <Description("the max peak width in rt(seconds).")>
     Public Property peakMax As Double
+
+    <Category("Peak Finding")>
+    <Description("The threshold value of sin(alpha) angle value, value of this parameter should be in range of [0,90]")>
+    Public Property angle_threshold As Double = 8
 
 End Class
