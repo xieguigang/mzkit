e3dff7a4d58b93ff74cd2eaa04a98aadab57961f
xieguigang <xie.guigang@gcmodeller.org>
2021-03-27 11:35:40 +0800

    Update cdfTest.R

--- dist/Rscript/GCMS/GCxGC_TopMS/cdfTest.R
+++ dist/Rscript/GCMS/GCxGC_TopMS/cdfTest.R
@@ -6,6 +6,6 @@ using cdf as open.netCDF("P:\MTBLS212\CZ1_5m.cdf") {
 	
 	print("all of the data variables in cdf data file:");
 	print(variables(cdf));
-	pause();
-	print(var(cdf, "") :> getValue);
+pause();
+	print(var(cdf, "mass_values") :> getValue);
 }
