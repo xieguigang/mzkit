c191803bfc2d735165082986753fab496f407089
xieguigang <xie.guigang@gcmodeller.org>
2021-03-27 13:13:28 +0800

    Update cdfTest.R

--- dist/Rscript/GCMS/GCxGC_TopMS/cdfTest.R
+++ dist/Rscript/GCMS/GCxGC_TopMS/cdfTest.R
@@ -4,8 +4,11 @@ using cdf as open.netCDF("P:\MTBLS212\CZ1_5m.cdf") {
 	print("all of the global attributes in cdf data file:");
 	print(globalAttributes(cdf));
 	
+	print("all of the data dimensions:");
+	print(dimensions(cdf));
+	
 	print("all of the data variables in cdf data file:");
 	print(variables(cdf));
-pause();
+
 	print(var(cdf, "mass_values") :> getValue);
 }
