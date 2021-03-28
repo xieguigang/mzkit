c644485b8c83511d22620accc6d64366501ba4fc
xieguigang <xie.guigang@gcmodeller.org>
2021-03-28 16:48:39 +0800

    convert GCxGC to mzpack data

--- dist/Rscript/GCMS/GCxGC_TopMS/cdfTest.R
+++ dist/Rscript/GCMS/GCxGC_TopMS/cdfTest.R
@@ -10,5 +10,49 @@ using cdf as open.netCDF("P:\MTBLS212\CZ1_5m.cdf") {
 	print("all of the data variables in cdf data file:");
 	print(variables(cdf));
 
-	print(var(cdf, "mass_values") :> getValue);
+	print("a_d_sampling_rate");
+	print(var(cdf, "a_d_sampling_rate") :> getValue);
+	
+	print("a_d_coaddition_factor");
+	print(var(cdf, "a_d_coaddition_factor") :> getValue);
+	
+	print("scan_acquisition_time");
+	print(var(cdf, "scan_acquisition_time") :> getValue);
+	
+	print("scan_duration");
+	print(var(cdf, "scan_duration") :> getValue);
+	
+	print("inter_scan_time");
+	print(var(cdf, "inter_scan_time") :> getValue);
+	
+	print("resolution");
+	print(var(cdf, "resolution") :> getValue);
+	
+	print("actual_scan_number");
+	print(var(cdf, "actual_scan_number") :> getValue);
+	
+	print("total_intensity");
+	print(var(cdf, "total_intensity") :> getValue);
+	
+	print("mass_range_min");
+	print(var(cdf, "mass_range_min") :> getValue);
+	
+	print("mass_range_max");
+	print(var(cdf, "mass_range_max") :> getValue);
+	
+	print("time_range_min");
+	print(var(cdf, "time_range_min") :> getValue);
+	
+	print("time_range_max");
+	print(var(cdf, "time_range_max") :> getValue);
+	
+	print("scan_index");
+	print(var(cdf, "scan_index") :> getValue);
+	
+	print("point_count");
+	print(var(cdf, "point_count") :> getValue);
+	
+	print("flag_count");
+	print(var(cdf, "flag_count") :> getValue);
+	
 }
---
+++ src/assembly/Comprehensive/GC2Dimensional.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.IO.netCDF
+Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
+
+''' <summary>
+''' GCxGC assembly data
+''' </summary>
+Public Module GC2Dimensional
+
+    <Extension>
+    Public Function ToMzPack(agilentGC As netCDFReader) As mzPack
+        Dim scan_numbers As Dimension = agilentGC.dimensions.KeyItem("scan_number")
+        Dim blockSize As Integer = agilentGC.recordDimension.length / scan_numbers.size
+        Dim scan_time As doubles = agilentGC.getDataVariable("scan_acquisition_time")
+        Dim totalIons As doubles = agilentGC.getDataVariable("total_intensity")
+        Dim mz As shorts = agilentGC.getDataVariable("mass_values")
+        Dim into As shorts = agilentGC.getDataVariable("intensity_values")
+
+        Return New mzPack With {
+            .MS = blockSize.CreateMSScans(scan_time, totalIons, mz, into).ToArray
+        }
+    End Function
+
+    <Extension>
+    Private Iterator Function CreateMSScans(blockSize As Integer, scan_time As Double(), totalIons As Double(), mz As Short(), into As Short()) As IEnumerable(Of ScanMS1)
+        Dim mzMatrix As Double()() = mz.Select(Function(i) CDbl(i)).Split(blockSize).ToArray
+        Dim intoMatrix As Double()() = into.Select(Function(i) CDbl(i)).Split(blockSize).ToArray
+
+        For i As Integer = 0 To scan_time.Length - 1
+            Yield New ScanMS1 With {
+                .TIC = totalIons(i),
+                .BPC = .TIC,
+                .rt = scan_time(i),
+                .scan_id = i + 1,
+                .mz = mzMatrix(i),
+                .into = intoMatrix(i)
+            }
+        Next
+    End Function
+End Module
