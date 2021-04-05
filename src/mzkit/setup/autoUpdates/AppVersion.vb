f90b83f3f544fad987fd16935a3424f2ad770470
xieguigang <xie.guigang@gcmodeller.org>
2021-04-05 22:44:07 +0800

    how to create auto updates

---
+++ src/mzkit/setup/autoUpdates/AppVersion.vb
@@ -0,0 +1,15 @@
+﻿Public Class AppVersion
+
+    ''' <summary>
+    ''' version of mzkit_win32
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property version As String
+    ''' <summary>
+    ''' network url of the latest package file
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property url As String
+    Public Property changlog As String
+
+End Class
