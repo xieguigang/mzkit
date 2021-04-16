0d285df478df65ba2bf46c81b423daf95cba998f
xieguigang <xie.guigang@gcmodeller.org>
2021-04-16 22:07:14 +0800

    Update frmUserAgreement.vb

--- src/mzkit/mzkit/forms/frmUserAgreement.vb
+++ src/mzkit/mzkit/forms/frmUserAgreement.vb
@@ -1,6 +1,8 @@
 ﻿Public Class frmUserAgreement
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.DialogResult = DialogResult.OK
+
         Globals.Settings.licensed = True
         Globals.Settings.Save()
 
@@ -8,6 +10,7 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        End
+        Me.DialogResult = DialogResult.Cancel
+        Close()
     End Sub
 End Class
