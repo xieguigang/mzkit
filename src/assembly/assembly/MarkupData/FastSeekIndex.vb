31c8d1beabf5ba594cc1c4ead25d4a18edd74626
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-16 19:14:57 +0800

    fast load mzXML test success

--- src/assembly/assembly/MarkupData/FastSeekIndex.vb
+++ src/assembly/assembly/MarkupData/FastSeekIndex.vb
@@ -2,6 +2,7 @@
 Imports System.IO
 Imports System.Text.RegularExpressions
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.DataReader
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports Microsoft.VisualBasic.Text.Xml.Linq
 
 Namespace MarkupData
@@ -13,15 +14,21 @@ Namespace MarkupData
 
         Public Property indexId As String()
         Public Property Ms2Index As Dictionary(Of String, Double)
+        Public Property fileName As String
 
         Public Shared Function LoadIndex(file As String) As FastSeekIndex
             Select Case XmlSeek.ParseFileType(file)
                 Case XmlFileTypes.imzML, XmlFileTypes.mzML
+                    Return LoadIndex_mzML(file)
                 Case XmlFileTypes.mzXML
                     Return LoadIndex_mzXML(file)
                 Case Else
                     Throw New NotImplementedException(file)
             End Select
+        End Function
+
+        Public Shared Function LoadIndex_mzML(file As String) As FastSeekIndex
+
         End Function
 
         Public Shared Function LoadIndex_mzXML(file As String) As FastSeekIndex
@@ -46,14 +53,18 @@ Namespace MarkupData
             Dim TICRegexp As New Regex("totIonCurrent[=]"".+?""", RegexOptions.Singleline Or RegexOptions.Compiled)
 
             For Each block As String In NodeIterator.CreateBlockReader("scan")(stream())
-                Dim idKey As String = numRegexp.Match(block).Value
+                Dim idKey As String = numRegexp.Match(block).Value.GetTagValue("=", trim:=""" ").Value
                 Dim level As String = msLevelRegexp.Match(block).Value.GetTagValue("=").Value
-                Dim time As String = timeRegexp.Match(block).Value.GetTagValue("=").Value
+                Dim time As Double = PeakMs2.RtInSecond(timeRegexp.Match(block).Value.GetTagValue("=", trim:=""" ").Value)
+
+                Call keys.Add(idKey)
 
                 If level = """1""" Then
-
+                    scan_time.Add(time)
+                    TIC.Add(Double.Parse(TICRegexp.Match(block).Value.GetTagValue("=", trim:=""" ").Value))
+                    BPC.Add(Double.Parse(BPCRegexp.Match(block).Value.GetTagValue("=", trim:=""" ").Value))
                 Else
-
+                    Ms2Time.Add(idKey, time)
                 End If
             Next
 
@@ -62,7 +73,8 @@ Namespace MarkupData
                 .scan_time = scan_time.ToArray,
                 .TIC = TIC.ToArray,
                 .indexId = keys.ToArray,
-                .Ms2Index = Ms2Time
+                .Ms2Index = Ms2Time,
+                .fileName = file.GetFullPath
             }
         End Function
     End Class
