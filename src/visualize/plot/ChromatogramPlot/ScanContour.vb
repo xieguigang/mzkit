017b87b071472137f5fc3483dd0e2fc3f8ab7d31
xieguigang <xie.guigang@gcmodeller.org>
2021-04-17 20:47:24 +0800

    Update ScanContour.vb

--- src/visualize/plot/ChromatogramPlot/ScanContour.vb
+++ src/visualize/plot/ChromatogramPlot/ScanContour.vb
@@ -1,15 +1,20 @@
-﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 
 Public Class ScanContour : Inherits Plot
 
-    Public Sub New(theme As Theme)
+    Dim scans As ms1_scan()
+
+    Public Sub New(scans As ms1_scan(), theme As Theme)
         MyBase.New(theme)
+
+        Me.scans = scans
     End Sub
 
     Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
-        Throw New NotImplementedException()
+
     End Sub
 End Class
