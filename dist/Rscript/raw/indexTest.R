29dcff2bab09b508b27a95db81562b3c941ff50b
xieguigang <xie.guigang@gcmodeller.org>
2021-04-17 20:01:08 +0800

    Update indexTest.R

--- dist/Rscript/raw/indexTest.R
+++ dist/Rscript/raw/indexTest.R
@@ -1,5 +1,7 @@
 imports "assembly" from "mzkit";
 
-print(load_index("E:\biodeep\biodeepDB\protocols\biodeepMSMS1\biodeepMSMS\test\lxy-CID30.mzXML"));
+print(load_index("E:\D065.mzXML"));
+
+print("parse mzML file:");
 
 print(load_index("E:\D065.mzML"));
