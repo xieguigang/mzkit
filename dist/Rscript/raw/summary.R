4b5e1c3991db75efe3983c441b9a0472c003d8e5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-21 16:35:30 +0800

    summary of the raw data files

--- dist/Rscript/raw/summary.R
+++ dist/Rscript/raw/summary.R
@@ -1,7 +1,52 @@
+require(mzkit);
+
 imports "assembly" from "mzkit";
 
-"D:\plot\M151T200\spectrum.mgf"
-:> read.mgf
-:> as.data.frame
-:> print
+# title: Summary of the mzXML raw scan numbers
+#
+# description: 
+# Summary of the mzXML raw scan numbers: counting 
+# of MS1 scans and MS2 scans.
+#
+
+# docker run -it -v "$PWD:$PWD" -w "$PWD" dotnet:npsearch Rscript  ./summary.R
+
+[@info "A folder path that contains the mzXML raw data files."]
+const folder as string = ?"--mzXML" || "./";
+
+[@info "the file path of the summary result table output."]
+const out as string = ?"--out" || `${folder}/summary.csv`;
+
+let countIndex as function(index) {
+	const totalScans = length(index$indexId);
+	const countMs2   = length(index$Ms2Index);
+	const countMs1   = totalScans - countMs2;
+	
+	print(index$fileName);
+	
+	list(
+		fileName = basename(index$fileName),
+		Ms1      = countMs1,
+		MS2      = countMs2,
+		allScans = totalScans
+	);
+}
+
+const summaryList = folder
+:> list.files(pattern = "*.mzXML")
+:> lapply(load_index, names = basename)
+:> lapply(as.object)
+:> lapply(countIndex)
 ;
+
+const summary = data.frame(
+	fileName = sapply(summaryList, file -> file$fileName),
+	Ms1      = sapply(summaryList, file -> file$Ms1),
+	MS2      = sapply(summaryList, file -> file$MS2),
+	allScans = sapply(summaryList, file -> file$allScans),
+);
+
+print("View of the raw data file summary data:");
+print(summary);
+
+write.csv(summary, file = out);
---
+++ dist/Rscript/raw/summaryMgf.R
@@ -0,0 +1,7 @@
+imports "assembly" from "mzkit";
+
+"D:\plot\M151T200\spectrum.mgf"
+:> read.mgf
+:> as.data.frame
+:> print
+;
