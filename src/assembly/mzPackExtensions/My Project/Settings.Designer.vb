6c51c7d3974f544807a20918b9c326113729ff15
xieguigang <xie.guigang@gcmodeller.org>
2021-04-29 18:30:48 +0800

    how to load raw?

--- src/assembly/mzPackExtensions/My Project/Settings.Designer.vb
+++ src/assembly/mzPackExtensions/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.BioNovoGene.Analytical.MassSpectrometry.[Assembly].mzPack.My.MySettings
+        Friend ReadOnly Property Settings() As Global.BioNovoGene.Analytical.MassSpectrometry.[Assembly].My.MySettings
             Get
-                Return Global.BioNovoGene.Analytical.MassSpectrometry.[Assembly].mzPack.My.MySettings.Default
+                Return Global.BioNovoGene.Analytical.MassSpectrometry.[Assembly].My.MySettings.Default
             End Get
         End Property
     End Module
--- src/assembly/mzPackExtensions/RawStream.vb
+++ src/assembly/mzPackExtensions/RawStream.vb
@@ -1,6 +1,19 @@
-﻿Public Module RawStream
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ThermoRawFileReader
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ThermoRawFileReader.DataObjects
 
-    Public Function LoadFromXRaw()
+Public Module RawStream
 
+    <Extension>
+    Public Function LoadFromXRaw(raw As MSFileReader) As mzPack
+        Dim pack As New mzPack
+
+        Call raw.LoadFile()
+
+        For Each scan As RawLabelData In raw.GetLabelData
+
+        Next
+
+        Return pack
     End Function
 End Module
