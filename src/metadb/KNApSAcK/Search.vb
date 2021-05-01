2e002e730fc543c4fd6fff2a927652cc94978edb
xieguigang <xie.guigang@gcmodeller.org>
2021-05-01 16:12:45 +0800

    parse html table

--- src/metadb/KNApSAcK/Search.vb
+++ src/metadb/KNApSAcK/Search.vb
@@ -3,12 +3,29 @@ Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 
 Public Class Search
 
-    Public Shared Function Search(word As String, Optional type As Types = Types.metabolite) As ResultEntry()
+    Public Shared Iterator Function Search(word As String, Optional type As Types = Types.metabolite) As IEnumerable(Of ResultEntry)
         Dim content = sprintf(My.Resources.knapsack_search, type.Description, word.UrlEncode) _
             .GET _
             .GetTablesHTML _
-            .FirstOrDefault
+            .FirstOrDefault _
+            .GetRowsHTML
 
+        For Each rowText As String In content
+            Dim cells As String() = rowText.GetColumnsHTML
+            Dim names As String() = cells(2) _
+                .HtmlLines _
+                .Select(Function(str) str.StripHTMLTags) _
+                .ToArray
+            Dim entry As New ResultEntry With {
+                .C_ID = cells(Scan0).StripHTMLTags,
+                .CAS_ID = cells(1).StripHTMLTags,
+                .Metabolite = names,
+                .formula = cells(3).StripHTMLTags,
+                .Mw = cells(4).StripHTMLTags.ParseDouble
+            }
+
+            Yield entry
+        Next
     End Function
 End Class
 
