ffac838935ef87a789e0f44d1cf751babb4f2bdf
xieguigang <xie.guigang@gcmodeller.org>
2021-05-01 16:08:07 +0800

    get html table

--- src/metadb/KNApSAcK/Search.vb
+++ src/metadb/KNApSAcK/Search.vb
@@ -1,3 +1,21 @@
-﻿Public Class Search
+﻿Imports Microsoft.VisualBasic.Language.C
+Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 
+Public Class Search
+
+    Public Shared Function Search(word As String, Optional type As Types = Types.metabolite) As ResultEntry()
+        Dim content = sprintf(My.Resources.knapsack_search, type.Description, word.UrlEncode) _
+            .GET _
+            .GetTablesHTML _
+            .FirstOrDefault
+
+    End Function
+End Class
+
+Public Class ResultEntry
+    Public Property C_ID As String
+    Public Property CAS_ID As String
+    Public Property Metabolite As String()
+    Public Property formula As String
+    Public Property Mw As Double
 End Class
---
+++ src/metadb/KNApSAcK/Types.vb
@@ -0,0 +1,11 @@
+﻿Public Enum Types
+    all
+    organism
+    metabolite
+    formula
+    C_ID
+    CAS_ID
+    INCHIKEY
+    INCHI_CD
+    SMILES
+End Enum
