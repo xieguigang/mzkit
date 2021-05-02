fd50c63ae9551f737c77a9278ccfaccaf576070d
xieguigang <xie.guigang@gcmodeller.org>
2021-05-02 20:34:10 +0800

    Update MSSelector.Designer.vb

--- src/mzkit/ControlLibrary/MSSelector.Designer.vb
+++ src/mzkit/ControlLibrary/MSSelector.Designer.vb
@@ -14,13 +14,10 @@ Partial Class MSSelector
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
-    Protected components As System.ComponentModel.IContainer
-
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MSSelector))
@@ -38,6 +35,7 @@ Partial Class MSSelector
         '
         'RtRangeSelector1
         '
+        Me.RtRangeSelector1.ContextMenuStrip = Me.ContextMenuStrip1
         Me.RtRangeSelector1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.RtRangeSelector1.FillColor = System.Drawing.Color.Blue
         Me.RtRangeSelector1.Location = New System.Drawing.Point(0, 0)
@@ -52,60 +50,60 @@ Partial Class MSSelector
         '
         Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ResetToolStripMenuItem, Me.PinToolStripMenuItem, Me.ToolStripMenuItem2, Me.TICToolStripMenuItem, Me.BPCToolStripMenuItem, Me.ToolStripMenuItem1, Me.FilterMs2ToolStripMenuItem})
         Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(181, 148)
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(126, 126)
         '
         'ResetToolStripMenuItem
         '
         Me.ResetToolStripMenuItem.Image = CType(resources.GetObject("ResetToolStripMenuItem.Image"), System.Drawing.Image)
         Me.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem"
-        Me.ResetToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ResetToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.ResetToolStripMenuItem.Text = "Reset"
         '
         'PinToolStripMenuItem
         '
         Me.PinToolStripMenuItem.Image = CType(resources.GetObject("PinToolStripMenuItem.Image"), System.Drawing.Image)
         Me.PinToolStripMenuItem.Name = "PinToolStripMenuItem"
-        Me.PinToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PinToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.PinToolStripMenuItem.Text = "Pin"
         Me.PinToolStripMenuItem.ToolTipText = "Pin of RT Range"
         '
         'ToolStripMenuItem2
         '
         Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
-        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(177, 6)
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(122, 6)
         '
         'TICToolStripMenuItem
         '
         Me.TICToolStripMenuItem.Checked = True
         Me.TICToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked
         Me.TICToolStripMenuItem.Name = "TICToolStripMenuItem"
-        Me.TICToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TICToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.TICToolStripMenuItem.Text = "TIC"
         '
         'BPCToolStripMenuItem
         '
         Me.BPCToolStripMenuItem.Name = "BPCToolStripMenuItem"
-        Me.BPCToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.BPCToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.BPCToolStripMenuItem.Text = "BPC"
         '
         'ToolStripMenuItem1
         '
         Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(177, 6)
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(122, 6)
         '
         'FilterMs2ToolStripMenuItem
         '
         Me.FilterMs2ToolStripMenuItem.Image = CType(resources.GetObject("FilterMs2ToolStripMenuItem.Image"), System.Drawing.Image)
         Me.FilterMs2ToolStripMenuItem.Name = "FilterMs2ToolStripMenuItem"
-        Me.FilterMs2ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FilterMs2ToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.FilterMs2ToolStripMenuItem.Text = "Filter Ms2"
         '
-        'UserControl1
+        'MSSelector
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.RtRangeSelector1)
-        Me.Name = "UserControl1"
+        Me.Name = "MSSelector"
         Me.Size = New System.Drawing.Size(621, 241)
         Me.ContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
@@ -121,4 +119,5 @@ Partial Class MSSelector
     Friend WithEvents BPCToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
     Friend WithEvents FilterMs2ToolStripMenuItem As ToolStripMenuItem
+    Private components As System.ComponentModel.IContainer
 End Class
