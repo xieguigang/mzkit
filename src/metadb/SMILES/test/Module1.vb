26e07a85de2e9b977e7e8c3d15ac85320dfe1df8
xieguigang <xie.guigang@gcmodeller.org>
2021-05-06 22:55:42 +0800

    found a multiple stack bug

--- src/metadb/SMILES/test/Module1.vb
+++ src/metadb/SMILES/test/Module1.vb
@@ -3,22 +3,30 @@ Imports BioNovoGene.BioDeep.Chemoinformatics.SMILES
 
 Module Module1
 
+    Sub simpleTest()
+        Call runTest("CC", "ethane CH3CH3")
+        Call runTest("C=O", "formaldehyde (CH2O)")
+        Call runTest("C=C", "ethene (CH2=CH2)")
+        Call runTest("O=C=O", "carbon dioxide (CO2)")
+        Call runTest("COC", "dimethyl ether (CH3OCH3)")
+        Call runTest("C#N", "hydrogen cyanide (HCN)")
+        Call runTest("CCO", "ethanol (CH3CH2OH)")
+        Call runTest("[H][H]", "molecular hydrogen (H2)")
+        Call runTest("C=C-C-C=C-C-O", "6-hydroxy-1,4-hexadiene CH2=CH-CH2-CH=CH-CH2-OH")
+    End Sub
+
     Sub Main()
 
-        '  Call runTest("CC", "ethane CH3CH3")
-        ' Call runTest("C=O", "formaldehyde (CH2O)")
-        'Call runTest("C=C", "ethene (CH2=CH2)")
-        'Call runTest("O=C=O", "carbon dioxide (CO2)")
-        ' Call runTest("COC", "dimethyl ether (CH3OCH3)")
-        ' Call runTest("C#N", "hydrogen cyanide (HCN)")
-        ' Call runTest("CCO", "ethanol (CH3CH2OH)")
-        ' Call runTest("[H][H]", "molecular hydrogen (H2)")
-
-        ' Call runTest("C=C-C-C=C-C-O", "6-hydroxy-1,4-hexadiene CH2=CH-CH2-CH=CH-CH2-OH")
-
-        Call runTest("CCN(CC)CC", "Triethylamine")
+        Call simpleTest()
+        Call branchTest()
 
         Pause()
+    End Sub
+
+    Sub branchTest()
+        Call runTest("CCN(CC)CC", "Triethylamine  C6H15N")
+        Call runTest("CC(C)C(=O)O", "Isobutyric acid  C4H8O2")
+        Call runTest("C=CC(CCC)C(C(C)C)CCC", "3-propyl-4-isopropyl-1-heptene C10H20")
     End Sub
 
     Sub runTest(SMILES As String, prompt As String)
