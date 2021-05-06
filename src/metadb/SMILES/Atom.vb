2f5f9c90eacb8388e8adcde29c5eaf937dc8ba7f
xieguigang <xie.guigang@gcmodeller.org>
2021-05-06 21:43:50 +0800

    improvements of get formula composition

---
+++ src/metadb/SMILES/Atom.vb
@@ -0,0 +1,17 @@
+﻿Public Class Atom
+
+    Public Property label As String
+    Public Property maxKeys As Integer
+
+    Sub New(label As String, max As Integer)
+        Me.label = label
+        Me.maxKeys = max
+    End Sub
+
+    Public Shared Iterator Function DefaultElements() As IEnumerable(Of Atom)
+        Yield New Atom("C", 4)
+        Yield New Atom("O", 2)
+        Yield New Atom("N", 3)
+    End Function
+
+End Class
--- src/metadb/SMILES/FormulaBuilder.vb
+++ src/metadb/SMILES/FormulaBuilder.vb
@@ -8,9 +8,13 @@ Public Class FormulaBuilder
     Dim empirical As New StringBuilder
     Dim composition As New Dictionary(Of String, Integer)
     Dim visited As New Index(Of String)
+    Dim atomProfile As Dictionary(Of String, Atom)
 
     Sub New(graph As ChemicalFormula)
         Me.graph = graph
+        Me.atomProfile = Atom _
+            .DefaultElements _
+            .ToDictionary(Function(a) a.label)
     End Sub
 
     Public Function GetComposition(ByRef empirical As String) As Dictionary(Of String, Integer)
@@ -32,59 +36,20 @@ Public Class FormulaBuilder
         End If
 
         Select Case element.elementName
-            Case "C"
-                Call Push("C")
-
-                If element.Keys = 1 Then
-                    ' X-CH3
-                    Call Push("H", 4 - bond)
-                ElseIf element.Keys = 2 Then
-
-                    Call Push("H", 2 - bond + 1)
-
-                Else
-                    Throw New NotImplementedException
-                End If
-
             Case "H" : Call Push("H")
-
-            Case "O"
-                Call Push("O")
-
-                If element.Keys = 1 Then
-
-                    Call Push("H", 2 - bond)
-
-                ElseIf element.Keys = 2 Then
-                    ' do nothing
-                    ' no additional H element
-                Else
-                    Throw New InvalidConstraintException
-                End If
-
-            Case "N"
-
-                Call Push("N")
-
-                If element.Keys = 1 Then
-
-                    Call Push("H", 3 - bond)
-
-                ElseIf element.Keys = 2 Then
-
-                    Call Push("H", 1 - bond)
-
-                ElseIf element.Keys = 3 Then
-
-                    ' no additional H element
-                Else
-                    Throw New InvalidConstraintException
-
-                End If
-
             Case Else
-                Throw New NotImplementedException(element.elementName)
+
+                If atomProfile.ContainsKey(element.elementName) Then
+                    Call Push(atomProfile(element.elementName), element, bond)
+                Else
+                    Throw New NotImplementedException(element.elementName)
+                End If
         End Select
+    End Sub
+
+    Private Sub Push(atom As Atom, element As ChemicalElement, bond As Integer)
+        Call Push(atom.label)
+        Call Push("H", atom.maxKeys - element.Keys - bond)
     End Sub
 
     Private Sub Push(element As String, Optional n As Integer = 1)
