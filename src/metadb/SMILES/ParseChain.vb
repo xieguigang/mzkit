165d3c00ea77840024b27d1dd92a494597eb3849
xieguigang <xie.guigang@gcmodeller.org>
2021-05-06 19:01:22 +0800

    delete old source file

---
+++ src/metadb/SMILES/ParseChain.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Text.Parser
+
+Public Class ParseChain
+
+    ReadOnly SMILES As CharPtr
+
+    Sub New(SMILES As String)
+        Me.SMILES = SMILES
+    End Sub
+
+    Public Function ParseGraph() As networkgraph
+
+    End Function
+End Class
--- src/metadb/SMILES/old/ChemicalFormula.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class ChemicalFormula
-
-    ''' <summary>
-    ''' @return
-    ''' </summary>
-    Public Property formula As List(Of ChemicalElement)
-
-End Class
-
