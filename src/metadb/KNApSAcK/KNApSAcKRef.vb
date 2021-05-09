8219a0c67063ee18dbde584e229cca3e5268f2b2
xieguigang <xie.guigang@gcmodeller.org>
2021-05-09 19:34:57 +0800

    move source file to web query folder

---
+++ src/metadb/KNApSAcK/KNApSAcKRef.vb
@@ -0,0 +1,18 @@
+﻿Public Class KNApSAcKRef
+
+    Public Property xrefId As String
+    Public Property name As String
+    Public Property formula As String
+    Public Property exact_mass As Double
+    Public Property CAS As String
+    Public Property SMILES As String
+    Public Property InChI As String
+    Public Property InChIKey As String
+
+    ''' <summary>
+    ''' glycosyl count n
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property glycosyl As String()
+
+End Class
