053bf60c8c4fe344a55a3665edb7e6f9b4415195
guigang xie <gg.xie@bionovogene.com>
2021-05-13 15:00:24 +0800

    parse name stack

--- src/metadb/Chemoinformatics/NaturalProduct/GlycosylNameSolver.vb
+++ src/metadb/Chemoinformatics/NaturalProduct/GlycosylNameSolver.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 
 Namespace NaturalProduct
@@ -104,6 +105,8 @@ Namespace NaturalProduct
                            End If
                        End Function) _
                 .ToArray
+            Dim blocks As Token()() = SplitByTopLevelStack(tokens).ToArray
+
 
             For Each token As Token In tokens
                 If token.name = NameTokens.number Then
@@ -125,6 +128,39 @@ Namespace NaturalProduct
 
                 n = 1
             Next
+        End Function
+
+        Private Shared Function isNumber(buf As List(Of Token)) As Boolean
+            Return buf = 1 AndAlso buf(Scan0).name = NameTokens.number
+        End Function
+
+        Private Iterator Function SplitByTopLevelStack(tokens As IEnumerable(Of Token)) As IEnumerable(Of Token())
+            Dim buf As New List(Of Token)
+            Dim stack As New Stack(Of String)
+
+            For Each t As Token In tokens
+                If t.name = NameTokens.open Then
+                    stack.Push("(")
+
+                    If Not isNumber(buf) Then
+                        Yield buf.PopAll
+                    End If
+                ElseIf t.name = NameTokens.close Then
+                    stack.Pop()
+                ElseIf t.name = NameTokens.number Then
+                    buf.Add(t)
+                Else
+                    If stack.Count = 0 Then
+                        Yield {t}
+                    Else
+                        buf.Add(t)
+                    End If
+                End If
+            Next
+
+            If buf > 0 Then
+                Yield buf.PopAll
+            End If
         End Function
     End Class
 End Namespace
