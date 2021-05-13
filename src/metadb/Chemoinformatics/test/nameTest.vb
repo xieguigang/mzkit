02bf7b1c8d15daeb3c4aa031619fadb6e0f56f40
guigang xie <gg.xie@bionovogene.com>
2021-05-13 13:39:18 +0800

    add name parser test

---
+++ src/metadb/Chemoinformatics/test/nameTest.vb
@@ -0,0 +1,14 @@
+﻿Imports BioNovoGene.BioDeep.Chemoinformatics.NaturalProduct
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Module nameTest
+
+    Sub Main()
+
+        Dim parser As New GlycosylNameSolver
+
+        Call Console.WriteLine(parser.GlycosylNameParser("Malvidin 3,7-di-(6-malonylglucoside)").ToArray.GetJson)
+
+        Pause()
+    End Sub
+End Module
---
+++ src/metadb/FormulaSearch.Extensions/Annotation.vb
@@ -0,0 +1,25 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
+
+Public Class Annotation
+
+    Public Property products As ms2()
+    Public Property formula As FormulaComposition
+
+    Sub New(formula As FormulaComposition, products As ms2())
+        Me.formula = formula
+        Me.products = products _
+            .Select(Function(a)
+                        Return New ms2 With {
+                            .mz = a.mz,
+                            .intensity = a.intensity,
+                            .Annotation = If(a.Annotation.IsInteger, "", a.Annotation)
+                        }
+                    End Function) _
+            .ToArray
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return formula.ToString
+    End Function
+End Class
