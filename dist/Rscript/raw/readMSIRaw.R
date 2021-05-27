bf3919ac14c1fc97fd35cd599b3f3d928c0da2e8
guigang xie <gg.xie@bionovogene.com>
2021-05-27 13:22:13 +0800

    load raw data file

--- dist/Rscript/raw/readMSIRaw.R
+++ dist/Rscript/raw/readMSIRaw.R
@@ -1,12 +1,23 @@
 library("mzkit/ThermoRaw");
+library("mzkit/mzweb");
 
-const raw = open.raw("F:\MSI\MPIBremen_Bputeoserpentis_MALDI-FISH_DHB_233x233pixel_3um_mz400-1200_240k@200.RAW");
+const rawfile = "F:\MSI\MPIBremen_Bputeoserpentis_MALDI-FISH_DHB_233x233pixel_3um_mz400-1200_240k@200.RAW";
 
-for(i in 1:100) {
-  const scan =  raw :> read.rawscan(i);
+using raw as open.raw(rawfile) {
+
+# for(i in 1:100) {
+  # const scan =  raw :> read.rawscan(i);
   
-  print(scan :> events);
-  print(scan :> logs);
+  # print(scan :> events);
+  # print(scan :> logs);
   
-  pause();
+  # pause();
+# }
+
+	raw 
+	|> load_MSI(pixels = [233, 233])
+	|> write.mzPack(file = `${dirname(rawfile)}/${basename(rawfile)}.mzpack`)
+	;
+
 }
+
