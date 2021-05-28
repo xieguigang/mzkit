a074c842bbdeaa0f5acdfea8beb7d9759ef27f0e
guigang xie <gg.xie@bionovogene.com>
2021-05-28 14:52:36 +0800

    write meta data after index data

--- src/assembly/assembly/mzPack/Binary/BinaryStreamWriter.vb
+++ src/assembly/assembly/mzPack/Binary/BinaryStreamWriter.vb
@@ -1,51 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::92325367fed627da200914861ad7c132, src\assembly\assembly\mzPack\Binary\BinaryStreamWriter.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class BinaryStreamWriter
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Sub: (+2 Overloads) Dispose, (+2 Overloads) Write, (+2 Overloads) WriteBuffer, writeIndex
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class BinaryStreamWriter
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Sub: (+2 Overloads) Dispose, (+2 Overloads) Write, (+2 Overloads) WriteBuffer, writeIndex
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
 Namespace mzData.mzWebCache
@@ -199,12 +200,35 @@ Namespace mzData.mzWebCache
                 Call file.Write(entry.Key, BinaryStringFormat.ZeroTerminated)
             Next
 
+            Call file.Flush()
             Call writeMetaData()
         End Sub
 
         Protected Sub writeMetaData()
+            Dim startPos As Long = file.Position
+
+            ' 8 zero bytes as indicator
+            Call file.Write(0&)
+            ' meta data region size in byte
+            Call file.Write(0&)
+            ' should be less than or equals to index count/MS1 count
             Call file.Write(scanMetaData.Count)
 
+            For Each meta In scanMetaData
+                Call file.Write(meta.Key, BinaryStringFormat.ZeroTerminated)
+                Call file.Write(meta.Value.GetJson, BinaryStringFormat.ZeroTerminated)
+            Next
+
+            Call file.Flush()
+
+            Dim size As Long = file.Position - (startPos + 8 + 8)
+
+            Using file.TemporarySeek(startPos + 8, SeekOrigin.Begin)
+                ' write data size offset of ms1 
+                Call file.Write(size)
+            End Using
+
+            Call file.Flush()
         End Sub
 
         Protected Overridable Sub Dispose(disposing As Boolean)
