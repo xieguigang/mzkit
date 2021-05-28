0c993444a1219a8bf1d5648f2dbb8058455aa20f
xieguigang <xie.guigang@gcmodeller.org>
2021-05-28 16:55:17 +0800

    convert a specific single raw data file

--- Rscript/Library/mzkit_app/pipeline/mzPack.R
+++ Rscript/Library/mzkit_app/pipeline/mzPack.R
@@ -9,16 +9,13 @@ require(mzkit);
 
 [@info "A directory path that contains the raw data files that needs to be convert."]
 const sourceDir as string = ?"--scan"   || stop("You must provides a directory path for load the raw data files!");
+const israwFile = file.exists(sourceDir);
 [@info "A file extension name list with ',' or ';' symbol as delimiter."]
 const filter as string    = ?"--filter" || "*.raw;*.mzXML;*.mzML";
 [@info "A directory for output the converted mzPack raw data files."]
-const outputdir as string = ?"--output" || sourceDir;
+const outputdir as string = ?"--output" || ifelse(israwFile, dirname(sourceDir), sourceDir);
 
-const rawfiles as string = sourceDir 
-|> list.files(pattern = strsplit(filter, "[;,]"))
-;
-
-for(file in rawfiles) {
+const convertRawTask as function(file) {
 	cat(basename(file));
 	
 	file
@@ -29,6 +26,18 @@ for(file in rawfiles) {
 	;
 	
 	cat(" ... done!\n");
+}
+
+if (israwFile) {
+	convertRawTask(sourceDir);
+} else {
+	const rawfiles as string = sourceDir 
+	|> list.files(pattern = strsplit(filter, "[;,]"))
+	;
+
+	for(file in rawfiles) {
+		convertRawTask(file);
+	}
 }
 
 print("all data file has been converted!");
