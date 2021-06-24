4d8d36bb62dfc52093e2fadd0515e368c00aa387
guigang xie <gg.xie@bionovogene.com>
2021-06-24 18:06:14 +0800

    Update MSICache.R

--- dist/workbench/MSICache.R
+++ dist/workbench/MSICache.R
@@ -7,4 +7,4 @@ const cache = write.MSI_XIC(
 	ibd    = raw$ibd
 ); 
 
-write.MSI(cache, file = `${dirname(@script)}/demo.MSI`);
+write.MSI(cache, file = "D:\mzkit\DATA\test\imzML\S042_Continuous_imzML1.1.1.MSI");
