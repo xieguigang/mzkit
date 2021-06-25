f8e81702326d2fb11038fa9ece214527b98d73a9
guigang xie <gg.xie@bionovogene.com>
2021-06-25 11:45:26 +0800

    how to run task pipeline?

---
+++ src/mzkit/Task/Studio/TaskScript.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("task")>
+Public Class TaskScript
+
+    <ExportAPI("cache.MSI")>
+    Public Sub CreateMSIIndex(imzML As String, cacheFile As String)
+
+    End Sub
+
+End Class
