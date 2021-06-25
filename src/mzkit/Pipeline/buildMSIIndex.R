e8343153662606e951359553a9650af8171de177
xieguigang <xie.guigang@gcmodeller.org>
2021-06-26 01:25:43 +0800

    write pipeline script

--- src/mzkit/Pipeline/buildMSIIndex.R
+++ src/mzkit/Pipeline/buildMSIIndex.R
@@ -1,1 +1,5 @@
 imports "task" from "Mzkit_win32.Task";
+
+[@info "the file path of the imzML raw data file to create cache index."]
+[@type "*.imzML"]
+const imzML as string = ?"--imzML" || stop("no raw data file provided!");
--- src/mzkit/mzkit/forms/Task/RscriptPipelineTask.vb
+++ src/mzkit/mzkit/forms/Task/RscriptPipelineTask.vb
@@ -3,10 +3,12 @@
     Public Shared Function GetRScript(filename As String) As String
         Dim filepath As String = $"{App.HOME}/Rstudio/Pipeline/{filename}"
 
+        ' product version
         If filepath.FileLength > 10 Then
             Return filepath
         End If
 
+        ' development version
         filepath = $"{App.HOME}/../../src/mzkit/Pipeline/{filename}".GetFullPath
 
         Return filepath
