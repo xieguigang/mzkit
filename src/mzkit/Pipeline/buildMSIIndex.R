83e3632ac2e224ba1d0394b75ef919a58ab0ad9a
xieguigang <xie.guigang@gcmodeller.org>
2021-06-26 02:00:04 +0800

    Update buildMSIIndex.R

--- src/mzkit/Pipeline/buildMSIIndex.R
+++ src/mzkit/Pipeline/buildMSIIndex.R
@@ -9,3 +9,6 @@ const imzML as string = ?"--imzML" || stop("no raw data file provided!");
 [@info "the file path of the MSI indexed cache file."]
 [@type "filepath"]
 const cache as string = ?"--cache" || stop("a cache file path must be provided!");
+
+sleep(1);
+task::cache.MSI(imzML, cache);
