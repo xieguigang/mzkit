715b61a9aba4ca6d74a427f4da578a839a75988d
xieguigang <xie.guigang@gcmodeller.org>
2021-06-28 15:38:38 +0800

    Update Extensions.vb

--- src/visualize/MsImaging/Extensions.vb
+++ src/visualize/MsImaging/Extensions.vb
@@ -20,10 +20,18 @@ Public Module Extensions
     ''' pixels sampling
     ''' </summary>
     ''' <param name="pixels"></param>
-    ''' <param name="scaleTo"></param>
+    ''' <param name="samplingSize"></param>
     ''' <returns></returns>
     <Extension>
-    Public Iterator Function Sampling(pixels As IEnumerable(Of PixelScan), scaleTo As Size) As IEnumerable(Of mzPackPixel)
+    Public Iterator Function Sampling(pixels As IEnumerable(Of PixelScan), samplingSize As Size) As IEnumerable(Of PixelScan)
+        For Each pixel As PixelScan In pixels
+            If pixel.X Mod samplingSize.Width <> 0 Then
+                Continue For
+            ElseIf pixel.Y Mod samplingSize.Height <> 0 Then
+                Continue For
+            End If
 
+            Yield pixel
+        Next
     End Function
 End Module
