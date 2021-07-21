dc47e432278761941542336bfdadc696ec7bed85
guigang xie <gg.xie@bionovogene.com>
2021-07-21 13:41:28 +0800

    fix of NMR test file

---
+++ Rscript/Library/mzkit/assembly/NMR.vb
@@ -0,0 +1,14 @@
+﻿
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("NMR")>
+Module NMR
+
+    <ExportAPI("read.nmrML")>
+    Public Function readSmall(file As String) As nmrML.XML
+        Return file.LoadXml(Of nmrML.XML)
+    End Function
+
+End Module
---
+++ dist/Rscript/raw/NMR/readRaw.R
@@ -0,0 +1,3 @@
+imports "NMR" from "mzkit";
+
+print(NMR::read.nmrML("D:\mzkit\DATA\nmr\FAM013_AHTM.PROTON_04.nmrML"));
