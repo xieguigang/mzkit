ada84a8a5c1b225075be4fa06a6fc56e4ae90ec0
guigang xie <gg.xie@bionovogene.com>
2021-07-22 15:31:04 +0800

    parse formula

--- Rscript/R/mzkit/R/Formula.R
+++ Rscript/R/mzkit/R/Formula.R
@@ -33,6 +33,54 @@ Enumerate.Formula.MZ <- function(mz, tolerance = function(mz1, mz2) ppm(mz1 - mz
 #' 
 #' @return a key-value pair list of \code{atom} -> \code{count}
 #' 
-parseFormula = function(str) {
-
+parseFormula = function(formula) {
+	chars    = Strings.Split(formula, "");
+	elements = list();
+	buffer   = c();
+	buf_int  = c();
+	isLower  = function(x) utf8ToInt(x) >= utf8ToInt("a") && utf8ToInt(x) <= utf8ToInt("z");
+	isUpper  = function(x) utf8ToInt(x) >= utf8ToInt("A") && utf8ToInt(x) <= utf8ToInt("Z");
+	isInt    = function(x) utf8ToInt(x) >= utf8ToInt("0") && utf8ToInt(x) <= utf8ToInt("9");
+	work     = environment();
+	push     = function() {
+		atom = paste(buffer, collapse = "");
+				
+		if (is.null(buf_int)) {
+			n = 1;
+		} else {
+			n = paste(buf_int, collapse = "");
+			n = as.numeric(n);
+		}
+						
+		assign("buffer", NULL, envir = work);
+		assign("buf_int", NULL, envir = work);
+		
+		if (atom %in% names(elements)) {
+			elements[[atom]] = elements[[atom]] + n;
+		} else {
+			elements[[atom]] = n;
+		}
+		
+		assign("elements", elements, envir = work);
+	}
+	
+	for(c in chars) {
+		if (isUpper(c)) {
+			if (!is.null(buffer)) {
+				push();
+			}
+			
+			buffer = append(buffer, c);
+		} else if (isLower(c)) {
+			buffer = append(buffer, c);
+		} else if (isInt(c)) {
+			buf_int = append(buf_int, c);
+		}
+	}
+	
+	if (!IsNothing(buffer)) {
+		push();
+	}
+	
+	elements;
 }
