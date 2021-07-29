54e006c88f0e64ff7e11078caad2a50109fbf661
guigang xie <gg.xie@bionovogene.com>
2021-07-30 00:41:34 +0800

    add Q2 line

--- src/mzkit/mzkit/pages/dockWindow/tools/MSIPixelPropertyWindow.vb
+++ src/mzkit/mzkit/pages/dockWindow/tools/MSIPixelPropertyWindow.vb
@@ -3,6 +3,7 @@ Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Pixel
 Imports Microsoft.VisualBasic.Data.ChartPlots
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Legend
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Shapes
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.Quantile
 Imports Task
@@ -16,7 +17,7 @@ Public Class MSIPixelPropertyWindow
 
         Dim q As QuantileEstimationGK = pixel.GetMs.Select(Function(i) i.intensity).GKQuantile
         Dim serial As New SerialData With {
-            .color = Color.Blue,
+            .color = Color.SteelBlue,
             .lineType = DashStyle.Dash,
             .pointSize = 10,
             .shape = LegendStyles.Triangle,
@@ -28,8 +29,9 @@ Public Class MSIPixelPropertyWindow
                         End Function) _
                 .ToArray
         }
+        Dim Q2line As New Line(New PointF(0, q.Query(0.5)), New PointF(1, q.Query(0.5)), New Pen(Color.Red, 10))
 
-        PictureBox1.BackgroundImage = {serial}.Plot(fill:=True).AsGDIImage
+        PictureBox1.BackgroundImage = {serial}.Plot(size:="2700,2000", fill:=True, ablines:={Q2line}).AsGDIImage
         PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
     End Sub
 
