90e841333a26f75da9a8d066319dfa700f9d79d2
xieguigang <xie.guigang@gcmodeller.org>
2021-07-30 00:21:24 +0800

    show pixel info

--- src/mzkit/mzkit/pages/dockWindow/tools/MSIPixelPropertyWindow.vb
+++ src/mzkit/mzkit/pages/dockWindow/tools/MSIPixelPropertyWindow.vb
@@ -1,3 +1,33 @@
-﻿Public Class MSIPixelPropertyWindow
+﻿Imports System.Drawing.Drawing2D
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Pixel
+Imports Microsoft.VisualBasic.Data.ChartPlots
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Legend
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.Quantile
+Imports Task
+
+Public Class MSIPixelPropertyWindow
+
+    Public Sub SetPixel(pixel As PixelScan)
+        PropertyGrid1.SelectedObject = New PixelProperty(pixel)
+
+        Dim q As QuantileEstimationGK = pixel.GetMs.Select(Function(i) i.intensity).GKQuantile
+        Dim serial As New SerialData With {
+            .color = Color.Blue,
+            .lineType = DashStyle.Dash,
+            .pointSize = 10,
+            .shape = LegendStyles.Triangle,
+            .title = "Intensity",
+            .width = 5,
+            .pts = seq(0, 1, 0.1) _
+                .Select(Function(lv)
+                            Return New PointData(lv, q.Query(lv))
+                        End Function) _
+                .ToArray
+        }
+
+        PictureBox1.BackgroundImage = {serial}.Plot(fill:=True).AsGDIImage
+    End Sub
 
 End Class
