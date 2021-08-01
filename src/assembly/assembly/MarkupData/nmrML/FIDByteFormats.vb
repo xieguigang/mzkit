0b2ba2593d4831405aa5ea66aeb2d0d7f85e1bd4
xieguigang <xie.guigang@gcmodeller.org>
2021-08-02 01:29:19 +0800

    Update FIDByteFormats.vb
    still not working

--- src/assembly/assembly/MarkupData/nmrML/FIDByteFormats.vb
+++ src/assembly/assembly/MarkupData/nmrML/FIDByteFormats.vb
@@ -29,8 +29,8 @@ Namespace MarkupData.nmrML
                 Return bytes.ToArray _
                     .Split(8) _
                     .Select(Function(byts)
-                                Array.Reverse(byts)
-                                Return BitConverter.ToDouble(byts, Scan0)
+                                'Array.Reverse(byts)
+                                Return CDbl(BitConverter.ToInt64(byts, Scan0))
                             End Function) _
                     .ToArray
             End Using
@@ -42,7 +42,7 @@ Namespace MarkupData.nmrML
                 Return bytes.ToArray _
                     .Split(4) _
                     .Select(Function(byts)
-                                Array.Reverse(byts)
+                                'Array.Reverse(byts)
                                 Return CDbl(BitConverter.ToInt32(byts, Scan0))
                             End Function) _
                     .ToArray
