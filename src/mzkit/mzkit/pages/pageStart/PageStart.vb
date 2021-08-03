b144da0f5c176a15ced0f84a28a90e5c2ade7b14
guigang xie <gg.xie@bionovogene.com>
2021-08-03 17:39:04 +0800

    show raw file explorer

--- src/mzkit/mzkit/pages/pageStart/PageStart.vb
+++ src/mzkit/mzkit/pages/pageStart/PageStart.vb
@@ -1,46 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::7227bede08546142de7b4c93fffa4434, src\mzkit\mzkit\pages\pageStart\PageStart.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class PageStart
-    ' 
-    '     Sub: BackgroundWorker_DoWork, hideNewsFeeds, LinkLabel1_LinkClicked, LinkLabel2_LinkClicked, LinkLabel3_LinkClicked
-    '          LinkLabel4_LinkClicked, PageStart_DragDrop, PageStart_DragEnter, PageStart_Load, PageStart_Resize
-    '          showNewsFeeds
-    ' 
-    ' /********************************************************************************/
+' Class PageStart
+' 
+'     Sub: BackgroundWorker_DoWork, hideNewsFeeds, LinkLabel1_LinkClicked, LinkLabel2_LinkClicked, LinkLabel3_LinkClicked
+'          LinkLabel4_LinkClicked, PageStart_DragDrop, PageStart_DragEnter, PageStart_Load, PageStart_Resize
+'          showNewsFeeds
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -142,10 +142,15 @@ Public Class PageStart
         Dim firstFile As String = files.ElementAtOrDefault(Scan0)
 
         If Not firstFile Is Nothing Then
-            Dim page As frmSeeMs = VisualStudio.ShowDocument(Of frmSeeMs)
+            If firstFile.ExtensionSuffix("raw") Then
+                Call MyApplication.host.OpenFile(firstFile, showDocument:=True)
+                Call VisualStudio.ShowDocument(Of frmUntargettedViewer)().loadRaw(WindowModules.rawFeaturesList.CurrentRawFile)
+            Else
+                Dim page As frmSeeMs = VisualStudio.ShowDocument(Of frmSeeMs)
 
-            page.TabText = "SeeMS: " & firstFile.FileName
-            page.LoadRaw(firstFile)
+                page.TabText = "SeeMS: " & firstFile.FileName
+                page.LoadRaw(firstFile)
+            End If
         End If
     End Sub
 
