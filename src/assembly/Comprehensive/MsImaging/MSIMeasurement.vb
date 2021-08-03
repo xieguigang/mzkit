0989e8dff1de3a9f3eeac683bcabe44071d9157c
guigang xie <gg.xie@bionovogene.com>
2021-08-03 13:26:06 +0800

    measuring of the MSI raw data files for data correction

---
+++ src/assembly/Comprehensive/MsImaging/MSIMeasurement.vb
@@ -0,0 +1,72 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ThermoRawFileReader
+Imports stdNum = System.Math
+
+Namespace MsImaging
+
+    ''' <summary>
+    ''' combine helper for row scans MSI raw data files
+    ''' </summary>
+    Public Class MSIMeasurement
+
+        ReadOnly maxScans As Integer
+        ReadOnly maxRt As Double
+        ReadOnly hasMs2 As Boolean
+
+        Sub New(totalTime As Double, pixels As Integer, Optional hasMs2 As Boolean = False)
+            Me.maxScans = pixels
+            Me.hasMs2 = hasMs2
+            Me.maxRt = totalTime
+        End Sub
+
+        Public Function GetCorrection() As Correction
+            If hasMs2 Then
+                Return New ScanMs2Correction(maxRt, maxScans)
+            Else
+                Return New ScanTimeCorrection(maxRt, maxScans)
+            End If
+        End Function
+
+        Public Shared Function Measure(raw As IEnumerable(Of mzPack)) As MSIMeasurement
+            Dim scans As New List(Of Integer)
+            Dim maxrt As New List(Of Double)
+            Dim maxScan As Integer
+            Dim scanMs2 As Boolean = False
+
+            For Each file As mzPack In raw
+                maxScan = file.MS.Length
+                scans.Add(maxScan)
+                maxrt.Add(file.MS.Select(Function(scan) scan.rt).Max)
+
+                If Not scanMs2 Then
+                    scanMs2 = file.hasMs2
+                End If
+            Next
+
+            Return New MSIMeasurement(maxrt.Average, scans.Average, hasMs2:=scanMs2)
+        End Function
+
+        Public Shared Function Measure(raw As IEnumerable(Of MSFileReader), Optional scanMs2 As Boolean = False) As MSIMeasurement
+            Dim scans As New List(Of Integer)
+            Dim maxrt As New List(Of Double)
+            Dim maxScan As Integer
+
+            For Each file As MSFileReader In raw
+                maxScan = file.ThermoReader.GetNumScans
+                scans.Add(maxScan)
+                maxrt.Add(file.ScanTimeMax * 60)
+
+                If Not scanMs2 Then
+                    For i As Integer = 0 To stdNum.Min(64, maxScan)
+                        If file.ThermoReader.GetMSLevel(scan:=i) > 1 Then
+                            scanMs2 = True
+                            Exit For
+                        End If
+                    Next
+                End If
+            Next
+
+            Return New MSIMeasurement(maxrt.Average, scans.Average, hasMs2:=scanMs2)
+        End Function
+
+    End Class
+End Namespace
--- src/assembly/Comprehensive/MsImaging/ScanMs2Correction.vb
+++ src/assembly/Comprehensive/MsImaging/ScanMs2Correction.vb
@@ -1,5 +1,6 @@
 ﻿
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
+Imports Microsoft.VisualBasic.Linq
 
 Namespace MsImaging
 
@@ -8,8 +9,48 @@ Namespace MsImaging
     ''' </summary>
     Public Class ScanMs2Correction : Inherits Correction
 
+        Dim scans As ScanMS1()
+        Dim scanId As Dictionary(Of String, Integer)
+        Dim rtPixel As ScanTimeCorrection
+
+        Sub New(totalTime As Double, pixels As Integer)
+            rtPixel = New ScanTimeCorrection(totalTime, pixels)
+        End Sub
+
+        Public Sub SetMs1Scans(scans As IEnumerable(Of ScanMS1))
+            Me.scans = scans.OrderBy(Function(i) i.rt).ToArray
+            Me.scanId = Me.scans _
+                .SeqIterator _
+                .ToDictionary(Function(scan) scan.value.scan_id,
+                              Function(scan)
+                                  Return scan.i
+                              End Function)
+        End Sub
+
         Public Overrides Function GetPixelRowX(scanMs1 As ScanMS1) As Integer
-            Throw New NotImplementedException()
+            Dim i As Integer = scanId(scanMs1.scan_id)
+            Dim nMs1 As Integer = -1
+            Dim total As Integer = GetTotalScanNumbers(index:=i, ms1Count:=nMs1)
+            Dim skipMs2 As Integer = total - nMs1
+
+            Return rtPixel.GetPixelRow(scanMs1.rt) - skipMs2
+        End Function
+
+        Public Function GetTotalScanNumbers(index As Integer, Optional ByRef ms1Count As Integer = -1) As Integer
+            If index = 0 Then
+                Return 0
+            End If
+
+            Dim before As ScanMS1() = scans _
+                .Take(index) _
+                .ToArray
+
+            ms1Count = before.Length
+
+            Return Aggregate scan As ScanMS1
+                   In before
+                   Let total As Integer = GetTotalScanNumbers(scan)
+                   Into Sum(total)
         End Function
 
         Public Shared Function GetTotalScanNumbers(scan As ScanMS1) As Integer
