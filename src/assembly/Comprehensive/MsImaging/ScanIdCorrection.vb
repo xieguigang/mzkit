56539dc296207cdbce6d7cf2c04ac1dc315eac97
guigang xie <gg.xie@bionovogene.com>
2021-08-03 10:59:36 +0800

    correction by scan id?

---
+++ src/assembly/Comprehensive/MsImaging/ScanIdCorrection.vb
@@ -0,0 +1,12 @@
+﻿
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
+
+Namespace MsImaging
+
+    Public Class ScanIdCorrection : Inherits Correction
+
+        Public Overrides Function GetPixelRowX(scanMs1 As ScanMS1) As Integer
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
