82fe1e4eaa18bca0999bdee76417a67d8505ef45
guigang xie <gg.xie@bionovogene.com>
2021-08-05 10:53:15 +0800

    convert mzpack to netcdf

--- Rscript/Library/mzkit/assembly/MzWeb.vb
+++ Rscript/Library/mzkit/assembly/MzWeb.vb
@@ -1,47 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::94faccce3791997206d6e7b0606647de, Rscript\Library\mzkit\assembly\MzWeb.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module MzWeb
-    ' 
-    '     Function: GetChromatogram, loadStream, Ms1ScanPoints, Ms2ScanPeaks, Open
-    '               setMzpackThumbnail, ToMzPack, writeMzpack, writeStream
-    ' 
-    '     Sub: WriteCache
-    ' 
-    ' /********************************************************************************/
+' Module MzWeb
+' 
+'     Function: GetChromatogram, loadStream, Ms1ScanPoints, Ms2ScanPeaks, Open
+'               setMzpackThumbnail, ToMzPack, writeMzpack, writeStream
+' 
+'     Sub: WriteCache
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -186,6 +186,19 @@ Module MzWeb
         End If
 
         Return mzpack.Write(filestream.TryCast(Of Stream))
+    End Function
+
+    <ExportAPI("write.cdf")>
+    Public Function writeToCDF(mzpack As mzPack, file As Object, Optional env As Environment = Nothing) As Object
+        Dim filestream As [Variant](Of Stream, Message) = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Write, env)
+
+        If filestream Like GetType(Message) Then
+            Return filestream.TryCast(Of Message)
+        Else
+            Call mzpack.WriteCDF(filestream.TryCast(Of Stream))
+        End If
+
+        Return Nothing
     End Function
 
     ''' <summary>
