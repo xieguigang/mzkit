9d201167e757e714f7bf44b22fc50b0e200e9c77
guigang xie <gg.xie@bionovogene.com>
2021-08-05 10:45:53 +0800

    convert mzpack to netcdf

--- src/assembly/mzPackExtensions/ToCDFWriter.vb
+++ src/assembly/mzPackExtensions/ToCDFWriter.vb
@@ -1,7 +1,10 @@
 ﻿Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
 Imports Microsoft.VisualBasic.Data.IO.netCDF
 Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 
 Public Module ToCDFWriter
 
@@ -13,11 +16,53 @@ Public Module ToCDFWriter
     <Extension>
     Public Sub WriteCDF(mzpack As mzPack, file As Stream)
         Using writer As New CDFWriter(file)
-            Dim fileAttr As attribute() = {
-                New attribute With {.name = "application", .type = CDFDataTypes.CHAR, .value = mzpack.Application.Description}
-            }
+            Dim fileAttr As attribute() = mzpack.getFileAttributes.ToArray
+            Dim i As i32 = 1
+
+            Call writer.GlobalAttributes(fileAttr)
+
+            For Each scan As ScanMS1 In mzpack.MS
+                Dim data As New doubles(scan.mz.JoinIterates(scan.into))
+                Dim meta = scan.meta _
+                    .SafeQuery _
+                    .Select(Function(t)
+                                Return New attribute With {
+                                    .name = t.Key,
+                                    .type = CDFDataTypes.CHAR,
+                                    .value = t.Value
+                                }
+                            End Function) _
+                    .ToList
+                Dim size As New Dimension With {.name = $"sizeof_scan{++i}", .size = data.Length}
+
+                meta.Add(New attribute With {.name = "n_products", .type = CDFDataTypes.INT, .value = scan.products.TryCount})
+                meta.Add(New attribute With {.name = "n_points", .type = CDFDataTypes.INT, .value = scan.size})
+                meta.Add(New attribute With {.name = "tic", .type = CDFDataTypes.DOUBLE, .value = scan.TIC})
+                meta.Add(New attribute With {.name = "bpc", .type = CDFDataTypes.DOUBLE, .value = scan.BPC})
+                meta.Add(New attribute With {.name = "retention_time", .type = CDFDataTypes.DOUBLE, .value = scan.rt})
+
+                writer.AddVariable(scan.scan_id, data, size, meta.ToArray)
+
+                For Each ms2 As ScanMS2 In scan.products
+                    data = New doubles(ms2.mz.JoinIterates(ms2.into))
+                    size = New Dimension With {.name = $"sizeof_scan{++i}", .size = data.Length}
+                    meta.Clear()
+                    meta.Add(New attribute With {.name = "mz", .type = CDFDataTypes.DOUBLE, .value = ms2.parentMz})
+                    meta.Add(New attribute With {.name = "rt", .type = CDFDataTypes.DOUBLE, .value = ms2.rt})
+                    meta.Add(New attribute With {.name = "n_fragments", .type = CDFDataTypes.DOUBLE, .value = ms2.size})
+                    meta.Add(New attribute With {.name = "scan_ms1", .type = CDFDataTypes.CHAR, .value = scan.scan_id})
+
+                    writer.AddVariable(ms2.scan_id, data, size, meta.ToArray)
+                Next
+            Next
         End Using
     End Sub
 
-
+    <Extension>
+    Private Iterator Function getFileAttributes(file As mzPack) As IEnumerable(Of attribute)
+        Yield New attribute With {.name = "application", .type = CDFDataTypes.CHAR, .value = file.Application.Description}
+        Yield New attribute With {.name = "url", .type = CDFDataTypes.CHAR, .value = "https://mzkit.org"}
+        Yield New attribute With {.name = "n_scans", .type = CDFDataTypes.INT, .value = file.MS.Length}
+        Yield New attribute With {.name = "timestamp", .type = CDFDataTypes.CHAR, .value = Now.ToString}
+    End Function
 End Module
