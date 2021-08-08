121b74877e3c187d2f3fa873d21acf309c8cf9cc
xieguigang <xie.guigang@gcmodeller.org>
2021-08-08 22:53:32 +0800

    Update matrix100.R

--- dist/Rscript/raw/matrix100.R
+++ dist/Rscript/raw/matrix100.R
@@ -2,6 +2,6 @@ imports ["mzweb", "MSI"] from "mzkit";
 
 "E:\demo\HR2MSI mouse urinary bladder S096.mzPack"
 |> open.mzpack
-|> peakSamples(resolution = 75, mzError = "ppm:20", cutoff = 0.01)
+|> peakSamples(resolution = 175, mzError = "da:0.05", cutoff = 0.005)
 |> write.csv(file = "E:\demo\HR2MSI mouse urinary bladder S096_top3.csv")
 ;
