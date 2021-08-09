ba8603f12edaf0b2f24966e4fedf400325949cd7
guigang xie <gg.xie@bionovogene.com>
2021-08-09 21:29:51 +0800

    read ms2 data

--- Rscript/R/mzkit/R/mzpackCDFReader.R
+++ Rscript/R/mzkit/R/mzpackCDFReader.R
@@ -18,5 +18,21 @@ readMzPackCDF = function(cdf, verbose = TRUE) {
         print(paste("The file [", cdf ,"]has",nc$nvars,"variables,",nc$ndims,"dimensions and",nc$natts,"NetCDF attributes"));
     }
 
-    
+    scans = names(nc$var);
+	ms2i = sapply(scans, function(name) {
+		meta = ncatt_get(nc, name);
+		as.integer(meta$mslevel) != 1
+	});
+	
+	names(scans) = scans;
+	
+	# read ms2 data
+	lapply(scans[ms2i], function(name) {
+		data = ncvar_get(nc, name);
+		n = length(data) / 2;
+		mz = data[1:n];
+		into = data[(n+1):length(data)];
+		
+		data.frame(mz = mz, into = into);
+	});	
 }
