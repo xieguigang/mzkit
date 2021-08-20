182eb839ad7a6c8b10faeeea9ad54bca47d97c5f
xieguigang <xie.guigang@gcmodeller.org>
2021-08-21 02:19:25 +0800

    start server

--- src/mzkit/ServiceHub/Protocols.vb
+++ src/mzkit/ServiceHub/Protocols.vb
@@ -1,6 +1,20 @@
-﻿Public Module Protocols
+﻿Imports System.Threading
+Imports Microsoft.VisualBasic.CommandLine.InteropService.Pipeline
+Imports Task
 
-    Public Function StartServer() As Process
+Public Module Protocols
 
+    Public Function StartServer(Rscript As String) As RunSlavePipeline
+        Dim cli As String = Rscript
+        Dim pipeline As New RunSlavePipeline(RscriptPipelineTask.Rscript.Path, $"""{cli}""")
+
+        Call pipeline.CommandLine.__DEBUG_ECHO
+
+        ' AddHandler pipeline.SetProgress, AddressOf Progress.SetProgress
+        ' AddHandler pipeline.Finish, Sub() Progress.Invoke(Sub() Progress.Close())
+
+        Call New Thread(AddressOf pipeline.Run).Start()
+
+        Return pipeline
     End Function
 End Module
--- src/mzkit/mzkit/application/ServiceHub.vb
+++ src/mzkit/mzkit/application/ServiceHub.vb
@@ -1,11 +1,12 @@
-﻿Imports ServiceHub
+﻿Imports Microsoft.VisualBasic.CommandLine.InteropService.Pipeline
+Imports Task
 
 Module ServiceHub
 
-    Dim services As Process
+    Dim services As RunSlavePipeline
 
     Public Sub Start()
-        services = Global.ServiceHub.Protocols.StartServer
+        services = Global.ServiceHub.Protocols.StartServer(RscriptPipelineTask.GetRScript("ServiceHub/MSI-host.R"))
     End Sub
 
 End Module
