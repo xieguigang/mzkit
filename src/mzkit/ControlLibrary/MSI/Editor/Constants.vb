cecee656a22f21fbf0b777499f1bc9c5ee34a2f0
xieguigang <xie.guigang@gcmodeller.org>
2021-08-29 12:08:37 +0800

    add polygon editor source

---
+++ src/mzkit/ControlLibrary/MSI/Editor/Constants.vb
@@ -0,0 +1,26 @@
+﻿Namespace PolygonEditor
+
+    Friend Enum MenuOption
+        MoveComponent
+        AddVertex
+        DeleteVertex
+        MovePolygon
+        RemovePolygon
+        HalveEdge
+        AddRelation
+        RemoveRelation
+    End Enum
+
+    Friend Enum Relation
+        None
+        Equality
+        Perpendicular
+    End Enum
+
+    Friend Enum Algorithm
+        Bresenham
+        Library
+        Antialiasing
+        BresenhamSymmetric
+    End Enum
+End Namespace
---
+++ src/mzkit/ControlLibrary/MSI/Editor/Polygon.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Collections.Generic
+
+Namespace PolygonEditor
+    Friend Class Polygon
+        Public Property Vertices As List(Of Vertex)
+        Public Property Edges As List(Of Edge)
+
+        Public Sub New()
+            Vertices = New List(Of Vertex)()
+            Edges = New List(Of Edge)()
+        End Sub
+
+        Public Function HasEdge(ByVal e As Edge) As Boolean
+            For Each edge In Edges
+                If edge Is e Then Return True
+            Next
+
+            Return False
+        End Function
+
+        Public Sub New(ByVal vertices As List(Of Vertex), ByVal edges As List(Of Edge))
+            Me.Vertices = vertices
+            Me.Edges = edges
+        End Sub
+    End Class
+End Namespace
