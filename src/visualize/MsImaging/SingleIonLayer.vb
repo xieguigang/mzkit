cca792fcb17e660adce9eb462381da42dd650b97
guigang xie <gg.xie@bionovogene.com>
2021-09-02 13:48:05 +0800

    Update SingleIonLayer.vb

--- src/visualize/MsImaging/SingleIonLayer.vb
+++ src/visualize/MsImaging/SingleIonLayer.vb
@@ -1,11 +1,17 @@
 ﻿Imports System.Drawing
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Reader
 Imports Microsoft.VisualBasic.Math.Quantile
 
 Public Class SingleIonLayer
 
     Public Property IonMz As Double
     Public Property MSILayer As PixelData()
+
+    ''' <summary>
+    ''' the canvas size of the MSI plot output
+    ''' </summary>
+    ''' <returns></returns>
     Public Property DimensionSize As Size
 
     Public Function GetIntensity() As Double()
@@ -14,6 +20,18 @@ Public Class SingleIonLayer
 
     Public Function GetQuartile() As DataQuartile
         Return GetIntensity.Quartile
+    End Function
+
+    Public Shared Function GetLayer(mz As Double, viewer As PixelReader, mzErr As Tolerance) As SingleIonLayer
+        Dim pixels As PixelData() = viewer _
+            .LoadPixels({mz}, mzErr) _
+            .ToArray
+
+        Return New SingleIonLayer With {
+            .IonMz = mz,
+            .DimensionSize = viewer.dimension,
+            .MSILayer = pixels
+        }
     End Function
 
     Public Shared Function GetLayer(mz As Double, viewer As Drawer, mzErr As Tolerance) As SingleIonLayer
@@ -27,5 +45,4 @@ Public Class SingleIonLayer
             .MSILayer = pixels
         }
     End Function
-
 End Class
