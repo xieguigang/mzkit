03019d210415674f22b9451bf90d41b61ec92caa
guigang xie <gg.xie@bionovogene.com>
2021-09-02 20:16:59 +0800

    take by regions

--- src/visualize/MsImaging/SingleIonLayer.vb
+++ src/visualize/MsImaging/SingleIonLayer.vb
@@ -21,8 +21,50 @@ Public Class SingleIonLayer
         End Get
     End Property
 
-    Public Function Trim(region As Polygon2D, unionSize As Integer) As SingleIonLayer
+    Public Function MeasureUninSize(sampling As Integer) As Size
+        Return New Size(DimensionSize.Width / sampling, DimensionSize.Height / sampling)
+    End Function
 
+    ''' <summary>
+    ''' take part of the pixels array from the current layer with given region polygon data.
+    ''' </summary>
+    ''' <param name="region"></param>
+    ''' <param name="unionSize"></param>
+    ''' <returns></returns>
+    Public Function Take(region As Polygon2D, unionSize As Size) As SingleIonLayer
+        Dim takes As New List(Of PixelData)
+        Dim xy = MSILayer _
+            .GroupBy(Function(p) p.x) _
+            .ToDictionary(Function(xr) xr.Key,
+                          Function(xr)
+                              Return xr _
+                                  .GroupBy(Function(p) p.y) _
+                                  .ToDictionary(Function(p) p.Key,
+                                                Function(p)
+                                                    Return p.First
+                                                End Function)
+                          End Function)
+
+        For i As Integer = 0 To region.length - 1
+            Dim x As Integer = region.xpoints(i)
+            Dim y As Integer = region.ypoints(i)
+
+            For xi As Integer = x To x + unionSize.Width
+                If xy.ContainsKey(xi) Then
+                    For yi As Integer = y To y + unionSize.Height
+                        If xy(xi).ContainsKey(yi) Then
+                            takes.Add(xy(xi)(yi))
+                        End If
+                    Next
+                End If
+            Next
+        Next
+
+        Return New SingleIonLayer With {
+            .IonMz = IonMz,
+            .DimensionSize = DimensionSize,
+            .MSILayer = takes.Distinct.ToArray
+        }
     End Function
 
     Public Function GetIntensity() As Double()
