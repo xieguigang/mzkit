204132499150900dcadb31acc2260ba314279a9f
xieguigang <xie.guigang@gcmodeller.org>
2021-09-22 17:34:03 +0800

    split source file

--- src/mzkit/ServiceHub/ServiceProtocols/MSI.vb
+++ src/mzkit/ServiceHub/ServiceProtocols/MSI.vb
@@ -1,7 +1,5 @@
-﻿Imports System.Drawing
-Imports System.IO
+﻿Imports System.IO
 Imports System.Text
-Imports System.Threading
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.imzML
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
@@ -201,31 +199,3 @@ Public Class MSI : Implements ITaskDriver
     End Function
 End Class
 
-Public Class LayerLoader
-
-    Public Property mz As Double()
-    Public Property mzErr As Double
-    Public Property method As String
-    Public Property densityCut As Double
-
-    Public Function GetTolerance() As Tolerance
-        Return Tolerance.ParseScript($"{method}:{mzErr}")
-    End Function
-
-    Shared Sub New()
-        Call MsgPackSerializer.DefaultContext.RegisterSerializer(New Schema)
-    End Sub
-
-    Private Class Schema : Inherits SchemaProvider(Of LayerLoader)
-
-        Protected Overrides Iterator Function GetObjectSchema() As IEnumerable(Of (obj As Type, schema As Dictionary(Of String, NilImplication)))
-            Yield (GetType(LayerLoader), New Dictionary(Of String, NilImplication) From {
-                {NameOf(mz), NilImplication.MemberDefault},
-                {NameOf(mzErr), NilImplication.MemberDefault},
-                {NameOf(method), NilImplication.MemberDefault},
-                {NameOf(densityCut), NilImplication.MemberDefault}
-            })
-        End Function
-    End Class
-
-End Class
---
+++ src/mzkit/ServiceHub/ServiceProtocols/Payloads/LayerLoader.vb
@@ -0,0 +1,32 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports Microsoft.VisualBasic.Data.IO.MessagePack
+Imports Microsoft.VisualBasic.Data.IO.MessagePack.Serialization
+
+Public Class LayerLoader
+
+    Public Property mz As Double()
+    Public Property mzErr As Double
+    Public Property method As String
+    Public Property densityCut As Double
+
+    Public Function GetTolerance() As Tolerance
+        Return Tolerance.ParseScript($"{method}:{mzErr}")
+    End Function
+
+    Shared Sub New()
+        Call MsgPackSerializer.DefaultContext.RegisterSerializer(New Schema)
+    End Sub
+
+    Private Class Schema : Inherits SchemaProvider(Of LayerLoader)
+
+        Protected Overrides Iterator Function GetObjectSchema() As IEnumerable(Of (obj As Type, schema As Dictionary(Of String, NilImplication)))
+            Yield (GetType(LayerLoader), New Dictionary(Of String, NilImplication) From {
+                {NameOf(mz), NilImplication.MemberDefault},
+                {NameOf(mzErr), NilImplication.MemberDefault},
+                {NameOf(method), NilImplication.MemberDefault},
+                {NameOf(densityCut), NilImplication.MemberDefault}
+            })
+        End Function
+    End Class
+
+End Class
