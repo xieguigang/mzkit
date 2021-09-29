1fefcd3242c1c12dd591a8b472fcac4930bd59b6
xieguigang <xie.guigang@gcmodeller.org>
2021-09-29 11:04:49 +0800

    mzdiff tolerance

---
+++ Rscript/Library/mzkit_app/R/math.R
@@ -0,0 +1,10 @@
+imports "math" from "mzkit";
+
+#' Create mzdiff tolerance value
+#' 
+#' @param kind da method or ppm method.
+#' @param mzdiff the m/z tolerance error value.
+#' 
+const tolerance as function(kind as string, mzdiff as double) {
+    math::tolerance(mzdiff, kind);
+}
