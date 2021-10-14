ce967e26e13f175695bdd9c63d4ecafa8848081d
xieguigang <xie.guigang@gcmodeller.org>
2021-10-14 16:07:43 +0800

    run lipidsearch

--- dist/Rscript/lipidmaps/searchlipids.R
+++ dist/Rscript/lipidmaps/searchlipids.R
@@ -3,12 +3,14 @@ imports "package_utils" from "devkit";
 package_utils::attach(`${@dir}/../../../Rscript/Library/mzkit_app`);
 
 const lipids = lipidmaps_repo();
-const engine = metadb::ms1_handler(lipids, ["[M]+", "[M+H]+", "[M+H+H2O]+"], "da:0.1");
+const engine = metadb::ms1_handler(lipids, ["[M]+", "[M+H]+", "[M+H+H2O]+"], "ppm:20");
 const query = ms1_search(engine, [798.5349, 820.5200, 772.5208]);
 
 for (mz in names(query)) {
-	print(`query result for m/z: ${mz}:`);
+	print(`${length(query[[mz]])} query result for m/z: ${mz}:`);
 	print(as.data.frame(query[[mz]]));
+	
+	write.csv(as.data.frame(query[[mz]]), file = `${@dir}/lipids_${mz}.csv`, row.names = FALSE);
 }
 
 pause();
