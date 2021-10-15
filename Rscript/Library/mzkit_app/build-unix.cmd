1a149a611df9abca0d4828d9e27e69363274c3db
xieguigang <xie.guigang@gcmodeller.org>
2021-10-16 00:11:41 +0800

    config build script

--- Rscript/Library/mzkit_app/build-unix.cmd
+++ Rscript/Library/mzkit_app/build-unix.cmd
@@ -1,6 +1,7 @@
 @echo off
 
-SET Rscript="D:\GCModeller\src\R-sharp\App\net5.0\Rscript.exe"
+SET drive=%~d0
+SET Rscript="%drive%\GCModeller\src\R-sharp\App\net5.0\Rscript.exe"
 
 %Rscript% --build
 
