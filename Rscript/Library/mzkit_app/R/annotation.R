141fa3b78dec295723e9e8cde0523faad60404b5
xieguigang <xie.guigang@gcmodeller.org>
2021-10-15 10:14:49 +0800

    add help docs

--- Rscript/Library/mzkit_app/R/annotation.R
+++ Rscript/Library/mzkit_app/R/annotation.R
@@ -2,9 +2,19 @@ imports ["metadb", "massbank", "math"] from "mzkit";
 
 #' Load lipidmaps data repository from internal data pack
 #' 
+#' @param repofile the file path of the messagepack repository
+#'     data file. default file path is the internal data file
+#'     in mzkit package. 
+#' 
 #' @return a data seuqnece of lipidmaps metadata
 #' 
 const lipidmaps_repo as function(repofile = system.file("data/LIPIDMAPS.msgpack", package = "mzkit")) {
-    const repo = massbank::read.lipidmaps(filepath);
-    repo;
+    if (!file.exists(repofile)) {
+        stop(`no repository data file at location: ${repofile}!`);
+    } else {
+        # read messagepack repository data file
+        #
+        const repo = massbank::read.lipidmaps(repofile);
+        repo;
+    }
 }
