df290fe2b8b69e8375e0af3b996265ba9b7a36c1
xieguigang <xie.guigang@gcmodeller.org>
2021-10-17 22:55:19 +0800

    unique of search result table test success

--- dist/Rscript/lipidmaps/searchlipids.R
+++ dist/Rscript/lipidmaps/searchlipids.R
@@ -4,7 +4,7 @@ package_utils::attach(`${@dir}/../../../Rscript/Library/mzkit_app`);
 
 const lipids = lipidmaps_repo();
 const engine = metadb::ms1_handler(lipids, ["[M]+", "[M+H]+", "[M+H+H2O]+"], "ppm:20");
-const query = ms1_search(engine, [798.5349, 820.5200, 772.5208]);
+const query = ms1_search(engine, [798.5349, 820.5200, 772.5208, 985.6011, 743.5468, 798.5414, 741.5303]);
 
 for (mz in names(query)) {
 	print(`${length(query[[mz]])} query result for m/z: ${mz}:`);
@@ -12,6 +12,8 @@ for (mz in names(query)) {
 	
 	write.csv(as.data.frame(query[[mz]]), file = `${@dir}/lipids_${mz}.csv`, row.names = FALSE);
 }
+
+print(uniqueFeatures(query));
 
 pause();
 
