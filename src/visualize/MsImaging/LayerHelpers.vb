fc2b607f7ec05dd637230c9cb4ebae42ce077f8d
xieguigang <xie.guigang@gcmodeller.org>
2021-10-22 14:15:35 +0800

    filtering of the pixels

--- src/visualize/MsImaging/LayerHelpers.vb
+++ src/visualize/MsImaging/LayerHelpers.vb
@@ -10,6 +10,7 @@ Imports Microsoft.VisualBasic.ComponentModel.TagData
 Imports Microsoft.VisualBasic.Data.GraphTheory
 Imports Microsoft.VisualBasic.DataMining.DensityQuery
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.Statistics.Linq
 
 Public Module LayerHelpers
@@ -25,28 +26,52 @@ Public Module LayerHelpers
     <Extension>
     Public Iterator Function GetMSIIons(raw As mzPack,
                                         Optional mzdiff As Tolerance = Nothing,
-                                        Optional gridSize As Integer = 5) As IEnumerable(Of DoubleTagged(Of SingleIonLayer))
+                                        Optional gridSize As Integer = 5,
+                                        Optional qcut As Double = 0.5) As IEnumerable(Of DoubleTagged(Of SingleIonLayer))
 
         Dim cellSize As New Size(gridSize, gridSize)
         Dim graph As Grid(Of ScanMS1) = Grid(Of ScanMS1).Create(raw.MS, Function(scan) scan.GetMSIPixel)
         Dim mzErr As Tolerance = mzdiff Or Tolerance.DefaultTolerance
         Dim reader As PixelReader = New ReadRawPack(raw)
+        Dim ncut As Integer = graph.size * qcut
         Dim allMz = raw.MS _
             .AsParallel _
             .Select(Function(scan)
+                        Dim pt As Point = scan.GetMSIPixel
+
                         Return scan _
                             .GetMs _
                             .ToArray _
-                            .Centroid(mzErr, New RelativeIntensityCutoff(0.05))
+                            .Centroid(mzErr, New RelativeIntensityCutoff(0.05)) _
+                            .Select(Function(mzi) (mzi, pt))
                     End Function) _
             .IteratesALL _
-            .ToArray _
-            .Centroid(mzErr, New RelativeIntensityCutoff(0)) _
+            .GroupBy(Function(d) d.mzi.mz, mzErr) _
+            .Where(Function(d) d.Length > ncut) _
             .ToArray
 
         For i As Integer = 0 To allMz.Length - 1
-            Dim mz As Double = allMz(i).mz
-            Dim layer As SingleIonLayer = reader.LoadLayer(mz, mzErr)
+            Dim mz As Double = allMz(i) _
+                .OrderByDescending(Function(d) d.mzi.intensity) _
+                .First _
+                .mzi.mz
+            Dim layer As New SingleIonLayer With {
+                .IonMz = mz,
+                .DimensionSize = New Size(graph.width, graph.height),
+                .MSILayer = Grid(Of (mzi As ms2, pt As Point)) _
+                    .Create(allMz(i), Function(d) d.Item2) _
+                    .EnumerateData _
+                    .Select(Function(d)
+                                Return New PixelData With {
+                                    .intensity = d.mzi.intensity,
+                                    .level = 0,
+                                    .mz = d.mzi.mz,
+                                    .x = d.pt.X,
+                                    .y = d.pt.Y
+                                }
+                            End Function) _
+                    .ToArray
+            }
             Dim density As NamedValue(Of Double)() = layer.MSILayer _
                 .Density(
                     getName:=Function(pt) $"{pt.x},{pt.y}",
