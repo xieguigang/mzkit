a863520ae6bc4488d77f4ade623f3d952cf80bf3
guigang xie <gg.xie@bionovogene.com>
2021-10-27 11:42:21 +0800

    split source file modules

--- Rscript/Library/mzkit/math/Math.vb
+++ Rscript/Library/mzkit/math/Math.vb
@@ -190,61 +190,6 @@ Module MzMath
     End Function
 
     ''' <summary>
-    ''' Chromatogram data deconvolution
-    ''' </summary>
-    ''' <param name="ms1"></param>
-    ''' <param name="tolerance"></param>
-    ''' <returns></returns>
-    <ExportAPI("mz.deco")>
-    <RApiReturn(GetType(PeakFeature()))>
-    Public Function mz_deco(<RRawVectorArgument>
-                            ms1 As Object,
-                            Optional tolerance As Object = "ppm:20",
-                            Optional baseline# = 0.65,
-                            <RRawVectorArgument>
-                            Optional peakwidth As Object = "3,20",
-                            Optional env As Environment = Nothing) As Object
-
-        Dim ms1_scans As IEnumerable(Of IMs1Scan) = ms1Scans(ms1)
-        Dim errors As [Variant](Of Tolerance, Message) = Math.getTolerance(tolerance, env)
-        Dim rtRange = ApiArgumentHelpers.GetDoubleRange(peakwidth, env, [default]:="3,20")
-
-        If errors Like GetType(Message) Then
-            Return errors.TryCast(Of Message)
-        ElseIf rtRange Like GetType(Message) Then
-            Return rtRange.TryCast(Of Message)
-        End If
-
-        Return ms1_scans _
-            .GetMzGroups(errors) _
-            .DecoMzGroups(rtRange, quantile:=baseline) _
-            .ToArray
-    End Function
-
-    Private Function ms1Scans(ms1 As Object) As IEnumerable(Of IMs1Scan)
-        If ms1 Is Nothing Then
-            Return {}
-        ElseIf ms1.GetType Is GetType(ms1_scan()) Then
-            Return DirectCast(ms1, ms1_scan()).Select(Function(t) DirectCast(t, IMs1Scan))
-        Else
-            Throw New NotImplementedException
-        End If
-    End Function
-
-    ''' <summary>
-    ''' do ``m/z`` grouping under the given tolerance
-    ''' </summary>
-    ''' <param name="ms1"></param>
-    ''' <param name="tolerance"></param>
-    ''' <param name="env"></param>
-    ''' <returns></returns>
-    <ExportAPI("mz.groups")>
-    <RApiReturn(GetType(MzGroup()))>
-    Public Function mz_groups(<RRawVectorArgument> ms1 As Object, Optional tolerance As Object = "ppm:20", Optional env As Environment = Nothing) As Object
-        Return ms1Scans(ms1).GetMzGroups(Math.getTolerance(tolerance, env)).ToArray
-    End Function
-
-    ''' <summary>
     ''' calculate ppm value between two mass vector
     ''' </summary>
     ''' <param name="a">mass a</param>
