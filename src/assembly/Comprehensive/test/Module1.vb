511700b479205237980004f8107f5c8da5fe74a5
guigang xie <gg.xie@bionovogene.com>
2021-11-29 18:13:23 +0800

    config test for data export

--- src/assembly/Comprehensive/test/Module1.vb
+++ src/assembly/Comprehensive/test/Module1.vb
@@ -1,6 +1,12 @@
-﻿Module Module1
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.Comprehensive
+Imports Microsoft.VisualBasic.Data.IO.netCDF
+
+Module Module1
 
     Sub Main()
+        Dim demo As String = "F:\20211123_CDF\P210702366.netcdf"
+        Dim gcxgc = netCDFReader.Open(demo).ToMzPack
 
+        Pause()
     End Sub
 End Module
