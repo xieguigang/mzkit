b867f5468c17963dbdeb67acdb3dc8d4c149b268
guigang xie <gg.xie@bionovogene.com>
2021-11-30 16:35:28 +0800

    Update Module1.vb

--- src/assembly/Comprehensive/test/Module1.vb
+++ src/assembly/Comprehensive/test/Module1.vb
@@ -1,9 +1,13 @@
 ﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.Comprehensive
 Imports Microsoft.VisualBasic.Data.IO.netCDF
+Imports Microsoft.VisualBasic.My
+Imports Microsoft.VisualBasic.My.FrameworkInternal
 
 Module Module1
 
     Sub Main()
+        FrameworkInternal.ConfigMemory(MemoryLoads.Heavy)
+
         Dim demo As String = "F:\20211123_CDF\P210702366.netcdf"
         Dim gcxgc = netCDFReader.Open(demo).ToMzPack
 
