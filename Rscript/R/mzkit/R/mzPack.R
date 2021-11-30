c8b035bc91a4c14e0312b3fba0cc21ade2989456
guigang xie <gg.xie@bionovogene.com>
2021-11-30 17:19:29 +0800

    Create mzPack.R

---
+++ Rscript/R/mzkit/R/mzPack.R
@@ -0,0 +1,11 @@
+#' Read mzpack file
+#'
+#' @param file the file path of the mzpack raw
+#'    data file.
+#' 
+#' @return a list of scan content data that read from
+#'    the mzpack raw data file.
+#'
+mzPack = function(file) {
+
+}
