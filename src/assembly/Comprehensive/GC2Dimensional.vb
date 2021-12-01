7a8dc5ee797756a63247805b7132b2d5cde01cdf
guigang xie <gg.xie@bionovogene.com>
2021-12-01 17:27:12 +0800

    Demodulate2DShape

--- src/assembly/Comprehensive/GC2Dimensional.vb
+++ src/assembly/Comprehensive/GC2Dimensional.vb
@@ -1,53 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::e4e1f9b9a5fdf6b9c7b6a622d4f741c4, src\assembly\Comprehensive\GC2Dimensional.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module GC2Dimensional
-    ' 
-    '     Function: CreateMSScans, readIntoMatrix, readMzMatrix, ToMzPack
-    ' 
-    ' /********************************************************************************/
+' Module GC2Dimensional
+' 
+'     Function: CreateMSScans, readIntoMatrix, readMzMatrix, ToMzPack
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Drawing
 Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.IO.netCDF
 Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.SignalProcessing
 Imports stdNum = System.Math
 
 ''' <summary>
@@ -56,16 +59,55 @@ Imports stdNum = System.Math
 Public Module GC2Dimensional
 
     <Extension>
-    Public Function ToMzPack(agilentGC As netCDFReader) As mzPack
+    Public Function ToMzPack(agilentGC As netCDFReader, modtime As Double) As mzPack
         Dim scan_time As doubles = agilentGC.getDataVariable("scan_acquisition_time")
         Dim totalIons As doubles = agilentGC.getDataVariable("total_intensity")
         Dim point_count As integers = agilentGC.getDataVariable("point_count")
         Dim into As Double()() = agilentGC.readIntoMatrix(point_count).ToArray
         Dim mz As Double()() = agilentGC.readMzMatrix(point_count).ToArray
+        Dim sig As ScanMS1() = scan_time.Array.CreateMSScans(totalIons, mz, into).ToArray
 
         Return New mzPack With {
-            .MS = scan_time.Array.CreateMSScans(totalIons, mz, into).ToArray
+            .MS = sig.Demodulate2D(modtime)
         }
+    End Function
+
+    ''' <summary>
+    ''' converts 1D signal into 2D with xp x yp dimensions
+    ''' </summary>
+    ''' <param name="sig"></param>
+    ''' <param name="modtime">modulation period in seconds</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Demodulate2D(sig As ScanMS1(), modtime As Double) As ScanMS1()
+        Dim size As Size = sig.Demodulate2DShape(modtime)
+
+        Throw New NotImplementedException
+    End Function
+
+    ''' <summary>
+    ''' converts 1D signal into 2D with xp x yp dimensions
+    ''' </summary>
+    ''' <param name="sig">
+    ''' data should be re-order by scan time
+    ''' </param>
+    ''' <param name="modtime">modulation period in seconds</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Demodulate2DShape(Of Tick As ITimeSignal)(sig As Tick(), modtime As Double) As Size
+        Dim numpoints As Integer = sig.Length
+        Dim runtime As Double = sig.Last.time
+        Dim rate As Double = numpoints / runtime
+
+        Call Console.WriteLine($"Found ${numpoints} data points")
+        Call Console.WriteLine($"Runtime is ${(runtime / 60).ToString("F2")} minutes")
+        Call Console.WriteLine($"Acquisition rate is ${rate} Hz")
+
+        ' Pad matrix so it can be resized
+        Dim xp = CInt(modtime * rate)
+        Dim yp = CInt(stdNum.Ceiling(numpoints / xp))
+
+        Return New Size(xp, yp)
     End Function
 
     <Extension>
--- src/assembly/assembly/mzPack/Stream/ScanMS1.vb
+++ src/assembly/assembly/mzPack/Stream/ScanMS1.vb
@@ -1,53 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::8e57e5bd762c4ed7a6f4f51a8c88b428, src\assembly\assembly\mzPack\Stream\ScanMS1.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class ScanMS1
-    ' 
-    '         Properties: BPC, meta, products, TIC
-    ' 
-    '         Function: GetIntensity, GetMs1Scans, GetMSIPixel, hasMetaKeys
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class ScanMS1
+' 
+'         Properties: BPC, meta, products, TIC
+' 
+'         Function: GetIntensity, GetMs1Scans, GetMSIPixel, hasMetaKeys
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports BioNovoGene.Analytical.MassSpectrometry.Math
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports Microsoft.VisualBasic.Math.SignalProcessing
 Imports Microsoft.VisualBasic.Scripting.Runtime
 
 Namespace mzData.mzWebCache
@@ -56,8 +57,10 @@ Namespace mzData.mzWebCache
     ''' MS scan
     ''' </summary>
     Public Class ScanMS1 : Inherits MSScan
+        Implements ITimeSignal
 
-        Public Property TIC As Double
+        Public Overrides Property rt As Double Implements ITimeSignal.time
+        Public Property TIC As Double Implements ITimeSignal.intensity
         Public Property BPC As Double
         Public Property products As ScanMS2()
 
