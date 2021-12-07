775207649e44530b11c0ccd019e5a220254ec419
guigang xie <gg.xie@bionovogene.com>
2021-12-07 14:26:09 +0800

    try to run test

--- Rscript/R/mzkit/R/peakMs2.R
+++ Rscript/R/mzkit/R/peakMs2.R
@@ -1,23 +1,23 @@
 #' Create peakms2 data schema model
-#' 
+#'
 .class_peakMs2 = function() {
     setClass("PeakMs2", representation(
         mz = "numeric",
         rt = "numeric",
-        intensity = "numeric",
-        file = "character",
-        scan = "character",
-        activation = "character",
+        intensity       = "numeric",
+        file            = "character",
+        scan            = "character",
+        activation      = "character",
         collisionEnergy = "numeric",
-        lib_guid = "character",
-        precursor_type = "character",
-        meta = "list",
-        mzInto = "mzInto"
+        lib_guid        = "character",
+        precursor_type  = "character",
+        meta            = "list",
+        mzInto          = "mzInto"
     ));
 }
 
 #' Create peak matrix data schema model
-#' 
+#'
 .class_mzInto = function() {
     setClass("mzInto", representation(
         mz         = "numeric",
