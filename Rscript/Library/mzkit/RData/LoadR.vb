b526ec1ce0e7ccfaa3f95b5eb5b8ca993c0bab98
guigang xie <gg.xie@bionovogene.com>
2021-12-07 15:51:23 +0800

    how to load peakms2 dataset?

---
+++ Rscript/Library/mzkit/RData/LoadR.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports SMRUCC.Rsharp.RDataSet.Struct.LinkedList
+
+Module LoadR
+
+    <Extension>
+    Public Function LoadPeakMs2(obj As RObject) As PeakMs2
+
+    End Function
+
+End Module
