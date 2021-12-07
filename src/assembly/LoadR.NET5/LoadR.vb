c86c8b07641d377f1b669113ccc2f18d3b5e3af8
guigang xie <gg.xie@bionovogene.com>
2021-12-07 16:57:43 +0800

    peaks R data parser test success

--- src/assembly/LoadR.NET5/LoadR.vb
+++ src/assembly/LoadR.NET5/LoadR.vb
@@ -1,12 +1,71 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.Rsharp.RDataSet.Convertor
 Imports SMRUCC.Rsharp.RDataSet.Struct.LinkedList
 
-Module LoadR
+Public Module LoadR
 
     <Extension>
     Public Function LoadPeakMs2(obj As RObject) As PeakMs2
+        Dim peakData As New PeakMs2
+        Dim schema As Dictionary(Of String, PropertyInfo) = DataFramework.Schema(Of PeakMs2)(
+            flag:=PropertyAccess.Writeable,
+            nonIndex:=True
+        )
 
+        If obj.attributes Is Nothing Then
+            ' do nothing
+            ' obj = obj
+        Else
+            obj = obj.attributes
+        End If
+
+        Do While obj.value.nodeType = ListNodeType.LinkedList
+            Dim name As String = obj.symbolName
+
+            If schema.ContainsKey(name) Then
+                Dim val As RList = obj.value.CAR.value
+                Dim data As Array = val.data
+                Dim target As PropertyInfo = schema(name)
+                Dim dataVal As Object
+
+                If target.PropertyType Is GetType(String) Then
+                    dataVal = DirectCast(data(0), RObject).characters
+                ElseIf DataFramework.IsPrimitive(target.PropertyType) Then
+                    dataVal = CTypeDynamic(data(0), target.PropertyType)
+                ElseIf target.PropertyType Is GetType(ms2()) Then
+                    dataVal = obj.value.CAR.LoadProductMatrix
+                Else
+                    dataVal = Nothing
+                End If
+
+                Call target.SetValue(peakData, dataVal)
+            End If
+
+            obj = obj.value.CDR
+        Loop
+
+        Return peakData
+    End Function
+
+    <Extension>
+    Public Function LoadProductMatrix(data As RObject) As ms2()
+        Dim mz = data.attributes.LinkValue("mz").DoCall(AddressOf RStreamReader.ReadNumbers)
+        Dim into = data.attributes.LinkValue("intensity").DoCall(AddressOf RStreamReader.ReadNumbers)
+        Dim annotation = data.attributes.LinkValue("annotation").DoCall(AddressOf RStreamReader.ReadStrings)
+
+        Return mz _
+            .Select(Function(mzi, i)
+                        Return New ms2 With {
+                            .mz = mzi,
+                            .intensity = into(i),
+                            .Annotation = annotation.ElementAtOrDefault(i)
+                        }
+                    End Function) _
+            .ToArray
     End Function
 
 End Module
--- src/assembly/LoadR.NET5/test/Program.vb
+++ src/assembly/LoadR.NET5/test/Program.vb
@@ -1,13 +1,20 @@
-Imports System
 Imports System.IO
+Imports BioNovoGene.Analytical.MassSpectrometry
+Imports Microsoft.VisualBasic.My
 Imports SMRUCC.Rsharp.RDataSet
+Imports SMRUCC.Rsharp.RDataSet.Convertor
+Imports SMRUCC.Rsharp.RDataSet.Struct.LinkedList
 
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
 
-        Using buffer As Stream = "F:\PlantMAT.rda".Open
-            Dim data = Reader.ParseData(buffer)
+        FrameworkInternal.ConfigMemory(FrameworkInternal.MemoryLoads.Heavy)
+
+        Using buffer As Stream = "D:\mzkit\src\assembly\LoadR.NET5\PeaksMs2.rda".Open(FileMode.Open, doClear:=False, [readOnly]:=True)
+            Dim data = Reader.ParseData(buffer).object.LinkVisitor("x")
+            Dim list As RObject() = data.value.CAR.value.data
+            Dim peaksData = list.Select(AddressOf LoadR.LoadPeakMs2).ToArray
 
             Pause()
         End Using
