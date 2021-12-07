5e82dd21c1d3d5a98fc5609f514947e24260b117
guigang xie <gg.xie@bionovogene.com>
2021-12-07 16:07:43 +0800

    config test

--- src/assembly/LoadR.NET5/test/Program.vb
+++ src/assembly/LoadR.NET5/test/Program.vb
@@ -1,7 +1,15 @@
 Imports System
+Imports System.IO
+Imports SMRUCC.Rsharp.RDataSet
 
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+
+        Using buffer As Stream = "F:\PlantMAT.rda".Open
+            Dim data = Reader.ParseData(buffer)
+
+            Pause()
+        End Using
     End Sub
 End Module
