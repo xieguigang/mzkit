bf1569068cdab3eb4efa57a364f60d60d2c7b3e4
guigang xie <gg.xie@bionovogene.com>
2021-12-14 13:46:26 +0800

    tolerance object

---
+++ Rscript/R/mzkit/R/tolerance.S4.R
@@ -0,0 +1,10 @@
+.class_tolerance = function() {
+    setClass("mzdiff", representation(
+        threshold         = "numeric",
+        method            = "character",
+        massErr           = "function",
+        assert            = "function",
+        is.low.resolution = "logical",
+        toString          = "character"        
+    ));
+}
