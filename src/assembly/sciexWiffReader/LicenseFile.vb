dfbf25fb7c4b1a7c4bf2941786810efd7b390f2b
xieguigang <xie.guigang@gcmodeller.org>
2022-01-11 19:45:10 +0800

    apply license file

--- src/assembly/sciexWiffReader/LicenseFile.vb
+++ src/assembly/sciexWiffReader/LicenseFile.vb
@@ -1,3 +1,67 @@
-﻿Public Class LicenseFile
+﻿Imports System.Xml.Serialization
+Imports Clearcore2.Licensing
+Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.Text.Xml
+
+''' <summary>
+''' license file helper for mzkit using clearcore2 system
+''' </summary>
+''' 
+<XmlType("license_key")>
+<XmlRoot("license_key")>
+Public Class LicenseFile
+
+    Shared ReadOnly filepath As String
+
+    Public Property company_name As String
+    Public Property product_name As String
+    Public Property features As String
+
+    ''' <summary>
+    ''' base64 string of the sciex license
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property key_data As String
+
+    ''' <summary>
+    ''' install the license key and test for license valid or not
+    ''' </summary>
+    ''' <param name="key"></param>
+    ''' <returns></returns>
+    Public Shared Function Install(person As String,
+                                   key As String,
+                                   Optional product As String = "ProcessingFramework",
+                                   Optional features As String = "WiffReaderSDK") As Boolean
+
+        Dim license As New LicenseFile With {
+           .company_name = person,
+           .features = features,
+           .key_data = key,
+           .product_name = product
+        }
+
+        Call license _
+            .GetXml(xmlEncoding:=XmlEncodings.UTF8) _
+            .SaveTo(
+                path:=LicenseFile.filepath,
+                encoding:=Encodings.UTF8.CodePage
+            )
+
+        Return ApplyLicense()
+    End Function
+
+    Public Shared Function ApplyLicense() As Boolean
+        If LicenseFile.filepath.FileLength <= 0 Then
+            Return False
+        End If
+
+        Try
+            LicenseKeys.Keys = {LicenseFile.filepath.ReadAllText}
+        Catch ex As Exception
+            Return False
+        End Try
+
+        Return True
+    End Function
 
 End Class
