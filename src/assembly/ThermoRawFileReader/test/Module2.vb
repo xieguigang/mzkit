739ca0e70be9bcb36b73dc031bb347691312beb9
xieguigang <xie.guigang@gcmodeller.org>
2022-01-11 21:52:30 +0800

    Update Module2.vb

--- src/assembly/ThermoRawFileReader/test/Module2.vb
+++ src/assembly/ThermoRawFileReader/test/Module2.vb
@@ -5,6 +5,8 @@ Module Module2
     Sub Main()
         Dim wiff As New WiffScanFileReader("E:\mzkit\DATA\test\wiff\MTBLS691\KIT2-0-5504_1020804070_01_0_1_1_01_10000001.wiff")
 
+        Dim ms = wiff.GetCentroidFromScanNum(10)
+
         Pause()
     End Sub
 End Module
