1207829596db0b9a07147f8bd3aa3b58f3097f41
xieguigang <xie.guigang@gcmodeller.org>
2022-01-11 20:37:25 +0800

    add demo test data file

--- src/assembly/ThermoRawFileReader/test/Module2.vb
+++ src/assembly/ThermoRawFileReader/test/Module2.vb
@@ -3,6 +3,8 @@
 Module Module2
 
     Sub Main()
-        Dim wiff As New WiffScanFileReader()
+        Dim wiff As New WiffScanFileReader("E:\mzkit\DATA\test\wiff\MTBLS691\KIT2-0-5504_1020804070_01_0_1_1_01_10000001.wiff")
+
+        Pause()
     End Sub
 End Module
