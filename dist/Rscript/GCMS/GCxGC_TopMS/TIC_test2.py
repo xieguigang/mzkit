416c0e29f018f29b7549255942a3e1773a45b2e7
guigang xie <gg.xie@bionovogene.com>
2022-02-08 15:53:35 +0800

    plot GCxGC

--- dist/Rscript/GCMS/GCxGC_TopMS/TIC_test2.py
+++ dist/Rscript/GCMS/GCxGC_TopMS/TIC_test2.py
@@ -3,12 +3,17 @@ import mzkit
 from mzkit import GCxGC, mzweb
 from mzplot import visual
 
-raw   = open.mzpack("D:\web\Lu6-1.mzPack")
-# gcxgc = GCxGC::extract_2D_peaks(raw)
+inputfile = "F:\GCxGC-plot\YCH-G-G-3.mzPack"
+image_TIC = `${dirname(inputfile)}/${basename(inputfile)}_TIC2D.png`
 
-# bitmap(plot(gcxgc, size = [4800,3300], padding = "padding: 250px 500px 250px 250px;"), file = "D:\web\Lu6-1.png")
+raw   = open.mzpack("F:\GCxGC-plot\YCH-G-G-3.mzPack")
+gcxgc = GCxGC::extract_2D_peaks(raw)
+plt = plot(gcxgc, size = [4800,3300], padding = "padding: 250px 500px 250px 250px;")
+
+bitmap(plt, file = image_TIC)
 
 # XIC
-gcxgc = GCxGC::extract_2D_peaks(raw, mz = 43.036646)
+# gcxgc = GCxGC::extract_2D_peaks(raw, mz = 43.036646)
+# plt = plot(gcxgc, size = [4800,3300], padding = "padding: 250px 500px 250px 250px;")
 
-bitmap(plot(gcxgc, size = [4800,3300], padding = "padding: 250px 500px 250px 250px;"), file = "D:\web\Lu6-1_XIC-43.0366.png")
+# bitmap(plt, file = "D:\web\Lu6-1_XIC-43.0366.png")
