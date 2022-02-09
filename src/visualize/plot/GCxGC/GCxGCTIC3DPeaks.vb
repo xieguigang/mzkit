0cd114aa24fb3468140e7d26e77d0d665ea01abd
xieguigang <xie.guigang@gcmodeller.org>
2022-02-09 20:57:21 +0800

    render colors for surface

--- src/visualize/plot/GCxGC/GCxGCTIC3DPeaks.vb
+++ src/visualize/plot/GCxGC/GCxGCTIC3DPeaks.vb
@@ -1,10 +1,14 @@
-﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.Comprehensive
+﻿Imports System.Drawing
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.Comprehensive
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
+Imports Microsoft.VisualBasic.Language
 
 ''' <summary>
 ''' plot GCxGC TIC in 3D peaks style
@@ -13,16 +17,32 @@ Public Class GCxGCTIC3DPeaks : Inherits Plot
 
     ReadOnly raw As D2Chromatogram()
     ReadOnly sampling As Integer
+    ReadOnly mapLevels As Integer
 
-    Public Sub New(gcxgc As D2Chromatogram(), sampling As Integer, theme As Theme)
+    Public Sub New(gcxgc As D2Chromatogram(), sampling As Integer, mapLevels As Integer, theme As Theme)
         MyBase.New(theme)
 
         Me.raw = gcxgc
         Me.sampling = sampling
+        Me.mapLevels = mapLevels
     End Sub
 
     Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
         Dim mesh3D As Surface() = MeshGrid(gcxgc:=raw, sampling:=sampling).ToArray
+        Dim colors As SolidBrush() = Designer.GetColors(theme.colorSet, mapLevels).Select(Function(c) New SolidBrush(c)).ToArray
+        Dim z As Double() = mesh3D.Select(Function(s) s.Select(Function(p) p.Z).Average).ToArray
+        Dim range As New DoubleRange(z)
+        Dim indexRange As New DoubleRange(0, mapLevels - 1)
+
+        ' render color
+        For i As Integer = 0 To mesh3D.Length - 1
+            mesh3D(i) = New Surface With {
+                .brush = colors(CInt(range.ScaleMapping(z(i), indexRange))),
+                .vertices = mesh3D(i).vertices
+            }
+        Next
+
+
     End Sub
 
     Public Shared Iterator Function MeshGrid(gcxgc As D2Chromatogram(), sampling As Integer) As IEnumerable(Of Surface)
