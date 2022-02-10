07a89f28cdfecb62436bb37179b0dbd5308f8c0c
guigang xie <gg.xie@bionovogene.com>
2022-02-10 16:17:07 +0800

    there is a new syntax error

--- Rscript/Library/mzkit/math/mz_deco.vb
+++ Rscript/Library/mzkit/math/mz_deco.vb
@@ -10,4 +10,9 @@ Public Module mz_deco
     Public Function readXcmsPeaks(file As String) As PeakSet
         Return New PeakSet With {.peaks = xcms2.Load(file)}
     End Function
+
+    <ExportAPI("peak_subset")>
+    Public Function peakSubset(peaktable As PeakSet, sampleNames As String()) As PeakSet
+        Return peaktable.Subset(sampleNames)
+    End Function
 End Module
--- dist/Rscript/plotPeaktable.R
+++ dist/Rscript/plotPeaktable.R
@@ -13,7 +13,7 @@ qc = names[names == $"QC\d+"];
 print("get qc names:");
 print(qc);
 
-QC = as.object(data)$Subset(qc);
+QC = data |> peak_subset(qc);
 
 bitmap(file = `${@dir}/QC_peaks.png`) {
 	plot(QC);
