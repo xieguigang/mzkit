a708f01712603d663543c3102a6fa120a2699cbf
guigang xie <gg.xie@bionovogene.com>
2022-02-16 15:01:55 +0800

    helper module for processing haematoxylin and eosin staining image

---
+++ src/visualize/MsImaging/HistologicalImage.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+
+''' <summary>
+''' helper module for processing haematoxylin and eosin staining image
+''' </summary>
+Public Module HistologicalImage
+
+    ''' <summary>
+    ''' convert to gray scale
+    ''' </summary>
+    ''' <param name="HE"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function MonoScale(HE As Image) As Image
+        Return HE.Grayscale
+    End Function
+
+    <Extension>
+    Public Function HeatMap(HE As Image,
+                            Optional scale As ScalerPalette = ScalerPalette.turbo,
+                            Optional mapLevels As Integer = 64) As Image
+
+    End Function
+
+End Module
