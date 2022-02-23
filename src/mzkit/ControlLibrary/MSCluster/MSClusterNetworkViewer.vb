f3229ba1d48f9d4fa1ebc174a274e90df41a60e7
guigang xie <gg.xie@bionovogene.com>
2022-02-23 13:10:25 +0800

    add network viewer

---
+++ src/mzkit/ControlLibrary/MSCluster/MSClusterNetworkViewer.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Public Class MSClusterNetworkViewer
+
+    Public Overloads Sub Load(graph As NetworkGraph)
+
+    End Sub
+
+End Class
