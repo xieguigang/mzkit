8c09f0a6f12f6ee5af91e6f67216228da19f693c
guigang xie <gg.xie@bionovogene.com>
2022-02-25 11:44:20 +0800

    do pixel stats

---
+++ src/visualize/MsImaging/IonStat.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Drawing
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.Quantile
+
+Public Class IonStat
+
+    Public Property mz As Double
+    Public Property pixels As Integer
+    Public Property density As Double
+    Public Property maxIntensity As Double
+    Public Property basePixel As Point
+    Public Property Q1Intensity As Double
+    Public Property Q2Intensity As Double
+    Public Property Q3Intensity As Double
+
+    Public Shared Iterator Function DoStat(raw As mzPack) As IEnumerable(Of IonStat)
+        Dim allIons = raw.MS.Select(Function(scan) scan.GetMs.Select(Function(ms1) (scan.GetMSIPixel, ms1))).IteratesALL.ToArray
+        Dim ions = allIons.GroupBy(Function(d) d.ms1.mz, Tolerance.DeltaMass(0.05)).ToArray
+        Dim A As Double = 5 ^ 2
+
+        For Each ion In ions
+            Dim pixels = Grid(Of (Point, ms2)).Create(ion, Function(x) x.Item1)
+            Dim basePixel = ion.OrderByDescending(Function(i) i.ms1.intensity).First
+            Dim Q = ion.Select(Function(i) i.ms1.intensity).Quartile
+            Dim counts As New List(Of Double)
+
+            For Each top In ion.OrderByDescending(Function(i) i.ms1.intensity).Take(30)
+                Dim count As Integer = pixels.Query(top.GetMSIPixel.X, top.GetMSIPixel.Y, 5).Where(Function(i) Not i.Item2 Is Nothing).Count
+                Dim density As Double = count / A
+
+                counts.Add(density)
+            Next
+
+            Yield New IonStat With {
+                .mz = Val(ion.name),
+                .basePixel = basePixel.GetMSIPixel,
+                .maxIntensity = basePixel.ms1.intensity,
+                .pixels = pixels.size,
+                .Q1Intensity = Q.Q1,
+                .Q2Intensity = Q.Q2,
+                .Q3Intensity = Q.Q3,
+                .density = counts.Average
+            }
+        Next
+    End Function
+End Class
