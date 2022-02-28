ddfc33862b83fa33ea1e9f5d06c3f1da84ae744a
guigang xie <gg.xie@bionovogene.com>
2022-02-28 18:33:53 +0800

    Update banner_prompt.cmd

--- src/mzkit/setup/Rstudio/banner_prompt.cmd
+++ src/mzkit/setup/Rstudio/banner_prompt.cmd
@@ -1,4 +1,23 @@
 @echo off
 
+title BioNovoGene MZKit developer tool
+
 echo Welcome to the BioNovoGene MZKit developer tool
-echo type "R# some_script.R" or "R# some_script.py" to start automation work
+@echo:
+echo **********************************************************************
+echo ** BioNovoGene MZKit developer tool v1.0
+echo ** Copyright (c) 2022 BioNovoGene Corporation
+echo **********************************************************************
+@echo:
+echo Automation environment initialized done~
+@echo:
+echo RStudio pkg repository: "%pkg_attach%"
+echo R# engine config file: "%renv_config%"
+@echo:
+echo Run R# script or python.NET script at here,
+echo you can try:
+@echo:
+echo type "R# some_script.R" or
+echo      "R# some_script.py" 
+@echo:
+echo in the command console to start automation work
