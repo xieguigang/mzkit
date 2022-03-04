6fecedf614f26fd3f6e1212dd0c7ad5e10261196
xieguigang <xie.guigang@gcmodeller.org>
2022-03-04 23:47:52 +0800

    formula search test success

--- dist/Rscript/chemoinformatics/searchFormula.py
+++ dist/Rscript/chemoinformatics/searchFormula.py
@@ -4,3 +4,13 @@ from mzkit import formula
 
 D_glucose = formula::scan("C6H12O6")
 print(D_glucose)
+print(formula::eval(D_glucose))
+
+print("run search test")
+formulas = formula::candidates(formula::eval(D_glucose), ppm = 30, C= [3,12], H = [3,12], O = [3,12], N = [0, 4])
+
+print(formulas)
+
+test2 =  formula::candidates(formula::eval(C19H20O9), ppm = 5, C= [3,32], H = [3,32], O = [3,12], N = [0, 4]) 
+
+print(test2)
