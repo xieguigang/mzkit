4ddc256ec9782effcd4ffcf48f7624b958916fa9
xieguigang <xie.guigang@gcmodeller.org>
2022-03-04 23:20:05 +0800

    do ion stats

--- dist/workbench/IonStat.R
+++ dist/workbench/IonStat.R
@@ -9,16 +9,32 @@ file = ?"--file" || stop("no raw data file!");
 
 print(file);
 
-file 
+data = file 
 |> open.mzpack()
 |> ionStat()
 |> as.data.frame()
-|> write.csv(file = `${dirname(file)}/${basename(file)}-ionstat.csv`)
 ;
+
+data[, "percentage"] = data[, "pixels"] / max(data[, "pixels"]) * 100;
+data = data[ order(data[, "pixels"], decreasing = TRUE) ,  ];
+
+write.csv(data, file = `${dirname(file)}/${basename(file)}-ionstat.csv`);
 
 # bar plot of the pixels stat
 bitmap(file = `${dirname(file)}/${basename(file)}-ionstat.png`) {
 	
+	data[, "log(pixels)"] = log(data[, "pixels"]);
+	
+	print(data, max.print = 13);
+
+
+	ggplot(data, aes(x = "log(pixels)"), padding = "padding:250px 600px 250px 300px;", size = [3600, 2100])
+	 + geom_histogram(bins = 64,  color = "steelblue")
+	 + ggtitle("Ion pixel numbers")
+	        + scale_x_continuous(labels = "F2")
+       + scale_y_continuous(labels = "F0")
+	 + theme_default()
+	 ;
 	
 
 }
