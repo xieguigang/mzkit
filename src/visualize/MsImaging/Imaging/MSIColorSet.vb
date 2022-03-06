b1a873bba1242a251d12164b5283ac1ddb57678e
xieguigang <xie.guigang@gcmodeller.org>
2022-03-06 21:44:28 +0800

    fix of namespace

--- src/visualize/MsImaging/Imaging/MSIColorSet.vb
+++ src/visualize/MsImaging/Imaging/MSIColorSet.vb
@@ -3,42 +3,44 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 
-Public Module MSIColorSet
+Namespace Imaging
 
-    ReadOnly scales As String()
+    Public Module MSIColorSet
 
-    Sub New()
-        scales = {
-            "#070711",
-            "#110071",
-            "#1202a2",
-            "#185eff",
-            "#11d6ea",
-            "#20fdda",
-            "#11f9a6",
-            "#10f97a",
-            "#3df31b",
-            "#bbf50c",
-            "#e7f116",
-            "#f9bb1b",
-            "#f17408",
-            "#e23f00",
-            "#f50961",
-            "#f410a3",
-            "#e333d1",
-            "#f87bfd",
-            "#ffffff"
-        }
-    End Sub
+        ReadOnly scales As String()
 
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    Public Function GetColors() As Color()
-        Return (From str As String In scales Select str.TranslateColor).ToArray
-    End Function
+        Sub New()
+            scales = {
+                "#070711",
+                "#110071",
+                "#1202a2",
+                "#185eff",
+                "#11d6ea",
+                "#20fdda",
+                "#11f9a6",
+                "#10f97a",
+                "#3df31b",
+                "#bbf50c",
+                "#e7f116",
+                "#f9bb1b",
+                "#f17408",
+                "#e23f00",
+                "#f50961",
+                "#f410a3",
+                "#e333d1",
+                "#f87bfd",
+                "#ffffff"
+            }
+        End Sub
 
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    Public Sub DoRegister()
-        Call Designer.Register("MSImaging", GetColors)
-    End Sub
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function GetColors() As Color()
+            Return (From str As String In scales Select str.TranslateColor).ToArray
+        End Function
 
-End Module
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub DoRegister()
+            Call Designer.Register("MSImaging", GetColors)
+        End Sub
+    End Module
+End Namespace
