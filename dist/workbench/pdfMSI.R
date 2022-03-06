c6a04b7d7a98e8952a3360bf7e2ba7daa03c13ad
xieguigang <xie.guigang@gcmodeller.org>
2022-03-06 18:44:26 +0800

    Update pdfMSI.R

--- dist/workbench/pdfMSI.R
+++ dist/workbench/pdfMSI.R
@@ -1,8 +1,16 @@
 require(mzkit);
 
+imports "MsImaging" from "mzplot";
+
 setwd(@dir);
 
 data = "E:\mzkit\DATA\test\HR2MSI mouse urinary bladder S096 - Figure1.cdf";
 data = open.mzpack(data);
 
 print(data);
+
+pdf(file = "./msi1.pdf") {
+	viewer(data)
+	|> layer([741.55, 743.54, 798.54])
+	;
+}
