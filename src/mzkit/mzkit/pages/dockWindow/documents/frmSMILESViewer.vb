caed8b6eba6d1cc2c0ad57451d55c7c4425cd954
xieguigang <xie.guigang@gcmodeller.org>
2022-03-16 01:48:39 +0800

    fix of build network

--- src/mzkit/mzkit/pages/dockWindow/documents/frmSMILESViewer.vb
+++ src/mzkit/mzkit/pages/dockWindow/documents/frmSMILESViewer.vb
@@ -9,14 +9,17 @@ Public Class frmSMILESViewer
         Dim network As New NetworkGraph
 
         For Each v In graph.vertex
-            network.CreateNode($"{v.ID}-{v.elementName}", New NodeData With {.label = v.elementName})
+            network.CreateNode(v.label, New NodeData With {.label = v.elementName, .color = Brushes.Black})
         Next
         For Each l In graph.graphEdges
-            network.CreateEdge(
-                u:=$"{l.U.ID}-{l.U.elementName}",
-                v:=$"{l.V.ID}-{l.V.elementName}",
-                weight:=l.bond
-            )
+            Dim url = network.CreateEdge(
+                  u:=l.U.label,
+                  v:=l.V.label,
+                  weight:=l.bond
+              )
+
+            url.data.style = New Pen(Color.Red, 2)
+            network.AddEdge(url)
         Next
 
         Canvas1.Graph = network
