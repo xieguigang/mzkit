88baba1aef945ea4e57224ac6270b551883b7a35
xieguigang <xie.guigang@gcmodeller.org>
2022-03-16 01:38:31 +0800

    show chemical structrue as graph

--- src/mzkit/mzkit/pages/dockWindow/documents/frmSMILESViewer.vb
+++ src/mzkit/mzkit/pages/dockWindow/documents/frmSMILESViewer.vb
@@ -6,6 +6,19 @@ Public Class frmSMILESViewer
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim smilesStr As String = Strings.Trim(TextBox1.Text)
         Dim graph = ParseChain.ParseGraph(smilesStr)
-        Dim network As NetworkGraph
+        Dim network As New NetworkGraph
+
+        For Each v In graph.vertex
+            network.CreateNode($"{v.ID}-{v.elementName}", New NodeData With {.label = v.elementName})
+        Next
+        For Each l In graph.graphEdges
+            network.CreateEdge(
+                u:=$"{l.U.ID}-{l.U.elementName}",
+                v:=$"{l.V.ID}-{l.V.elementName}",
+                weight:=l.bond
+            )
+        Next
+
+        Canvas1.Graph = network
     End Sub
 End Class
