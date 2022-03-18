89be16521dcfd6062ad263a356b6d205e3059d58
guigang xie <gg.xie@bionovogene.com>
2022-03-18 15:42:09 +0800

    implements heatmap pixel data

--- src/visualize/MsImaging/PixelData.vb
+++ src/visualize/MsImaging/PixelData.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::09e1f2c86f808b0e0155b6ec8d4f4a72, mzkit\src\visualize\MsImaging\PixelData.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 167
-    '    Code Lines: 119
-    ' Comment Lines: 28
-    '   Blank Lines: 20
-    '     File Size: 6.01 KB
+' Summaries:
 
 
-    ' Class PixelData
-    ' 
-    '     Properties: intensity, level, mz, x, y
-    ' 
-    '     Constructor: (+2 Overloads) Sub New
-    '     Function: GetBuffer, getIntensityAuto, Parse, ScalePixels, SequenceIndex
-    '               ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 167
+'    Code Lines: 119
+' Comment Lines: 28
+'   Blank Lines: 20
+'     File Size: 6.01 KB
+
+
+' Class PixelData
+' 
+'     Properties: intensity, level, mz, x, y
+' 
+'     Constructor: (+2 Overloads) Sub New
+'     Function: GetBuffer, getIntensityAuto, Parse, ScalePixels, SequenceIndex
+'               ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -63,16 +63,17 @@ Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.imzML
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Data.GraphTheory
 Imports Microsoft.VisualBasic.Data.IO
+Imports HeatMapPixel = Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap.Pixel
 Imports stdNum = System.Math
 
 ''' <summary>
 ''' a pixels point of [x,y,color]
 ''' </summary>
-Public Class PixelData : Implements IMSIPixel, IPoint2D
+Public Class PixelData : Implements IMSIPixel, IPoint2D, HeatMapPixel
 
-    Public Property x As Integer Implements IMSIPixel.x, IPoint2D.X
-    Public Property y As Integer Implements IMSIPixel.y, IPoint2D.Y
-    Public Property intensity As Double Implements IMSIPixel.intensity
+    Public Property x As Integer Implements IMSIPixel.x, IPoint2D.X, HeatMapPixel.X
+    Public Property y As Integer Implements IMSIPixel.y, IPoint2D.Y, HeatMapPixel.Y
+    Public Property intensity As Double Implements IMSIPixel.intensity, HeatMapPixel.Scale
     Public Property level As Double
     Public Property mz As Double
 
