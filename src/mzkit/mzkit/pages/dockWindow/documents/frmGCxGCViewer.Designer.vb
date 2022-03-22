b9af1ec8b7a836869d6eeee388c233b512ad406c
xieguigang <xie.guigang@gcmodeller.org>
2022-03-23 00:09:50 +0800

    Update frmGCxGCViewer.Designer.vb

--- src/mzkit/mzkit/pages/dockWindow/documents/frmGCxGCViewer.Designer.vb
+++ src/mzkit/mzkit/pages/dockWindow/documents/frmGCxGCViewer.Designer.vb
@@ -76,16 +76,28 @@ Partial Class frmGCxGCViewer : Inherits DocumentWindow
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Me.PeakSelector1 = New ControlLibrary.PeakSelector()
         Me.SuspendLayout()
+        '
+        'PeakSelector1
+        '
+        Me.PeakSelector1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PeakSelector1.Location = New System.Drawing.Point(0, 0)
+        Me.PeakSelector1.Name = "PeakSelector1"
+        Me.PeakSelector1.Size = New System.Drawing.Size(945, 587)
+        Me.PeakSelector1.TabIndex = 0
         '
         'frmGCxGCViewer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(945, 587)
+        Me.Controls.Add(Me.PeakSelector1)
         Me.DoubleBuffered = True
         Me.Name = "frmGCxGCViewer"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PeakSelector1 As ControlLibrary.PeakSelector
 End Class
