4d440a17e3c1b086e9298c15bfa7372ee256f405
xieguigang <xie.guigang@gcmodeller.org>
2022-03-23 12:43:26 +0800

    fix of the zip resource reference problem

--- src/mzkit/Task/BioDeep/KEGGRepo.vb
+++ src/mzkit/Task/BioDeep/KEGGRepo.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::08566adaa29ccccb4611eb4bd31ad1a4, mzkit\src\mzkit\Task\BioDeep\KEGGRepo.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 105
-    '    Code Lines: 85
-    ' Comment Lines: 0
-    '   Blank Lines: 20
-    '     File Size: 4.07 KB
+' Summaries:
 
 
-    ' Module KEGGRepo
-    ' 
-    '     Function: RequestKEGGcompounds, RequestKEGGCompounds, RequestKEGGMaps, RequestKEGGReactions, RequestLipidMaps
-    '               unzip
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 105
+'    Code Lines: 85
+' Comment Lines: 0
+'   Blank Lines: 20
+'     File Size: 4.07 KB
+
+
+' Module KEGGRepo
+' 
+'     Function: RequestKEGGcompounds, RequestKEGGCompounds, RequestKEGGMaps, RequestKEGGReactions, RequestLipidMaps
+'               unzip
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -106,7 +106,7 @@ Module KEGGRepo
 
     Public Function RequestKEGGCompounds() As Compound()
         Using zip As New ZipArchive(getMZKitPackage.Open(FileMode.Open, doClear:=False))
-            Using pack = zip.GetEntry("data\KEGG_compounds.msgpack").Open
+            Using pack = If(zip.GetEntry("data\KEGG_compounds.msgpack"), zip.GetEntry("data/KEGG_compounds.msgpack")).Open
                 Return KEGGCompoundPack.ReadKeggDb(pack)
             End Using
         End Using
@@ -114,7 +114,7 @@ Module KEGGRepo
 
     Public Function RequestKEGGMaps() As Map()
         Using zip As New ZipArchive(getMZKitPackage.Open(FileMode.Open, doClear:=False))
-            Using pack = zip.GetEntry("data\KEGG_maps.msgpack").Open
+            Using pack = If(zip.GetEntry("data\KEGG_maps.msgpack"), zip.GetEntry("data/KEGG_maps.msgpack")).Open
                 Return KEGGMapPack.ReadKeggDb(pack)
             End Using
         End Using
@@ -136,7 +136,7 @@ Module KEGGRepo
 
     Public Function RequestLipidMaps() As LipidMaps.MetaData()
         Using zip As New ZipArchive(getMZKitPackage.Open(FileMode.Open, doClear:=False))
-            Using pack = zip.GetEntry("data\LIPIDMAPS.msgpack").Open
+            Using pack = If(zip.GetEntry("data\LIPIDMAPS.msgpack"), zip.GetEntry("data/LIPIDMAPS.msgpack")).Open
                 Return LipidMaps.ReadRepository(pack)
             End Using
         End Using
