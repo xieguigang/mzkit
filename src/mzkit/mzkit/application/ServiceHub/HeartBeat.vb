f1474e44c7cbc75a39bb913be99a51e045784fd3
xieguigang <xie.guigang@gcmodeller.org>
2022-03-27 05:43:47 +0800

    start heartbeats services

--- src/mzkit/mzkit/application/ServiceHub/HeartBeat.vb
+++ src/mzkit/mzkit/application/ServiceHub/HeartBeat.vb
@@ -6,14 +6,16 @@ Public Module HeartBeat
 
     Dim socket As TcpServicesSocket
 
-    Public Sub Start()
+    Public Function Start() As Integer
         If socket Is Nothing OrElse Not socket.Running Then
             socket = New TcpServicesSocket(localPort:=TCPExtensions.GetFirstAvailablePort(5000))
             socket.ResponseHandler = AddressOf HeartBeatRequestHandler
 
             Call New Thread(AddressOf socket.Run).Start()
         End If
-    End Sub
+
+        Return socket.LocalPort
+    End Function
 
     Private Function HeartBeatRequestHandler(request As RequestStream, remoteAddress As System.Net.IPEndPoint) As BufferPipe
         Return New DataPipe("OK!")
