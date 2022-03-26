219e7290342b37618d6754c6dfb42aa39cd8cae5
guigang xie <gg.xie@bionovogene.com>
2022-03-26 22:57:06 +0800

    parse chebi data

--- Rscript/Library/mzkit/Massbank.vb
+++ Rscript/Library/mzkit/Massbank.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::1123296c5f4287cc50cf1e85060d5757, mzkit\Rscript\Library\mzkit\Massbank.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 362
-    '    Code Lines: 254
-    ' Comment Lines: 56
-    '   Blank Lines: 52
-    '     File Size: 13.74 KB
+' Summaries:
 
 
-    ' Module Massbank
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: chebiSecondary2Main, createIdMapping, createLipidMapTable, GlycosylNameSolver, GlycosylTokens
-    '               hmdbSecondary2Main, KEGGPathwayCoverages, lipidnameMapping, name2, readHMDB
-    '               readLipidMapsRepo, readMoNA, readSDF, saveIDMapping, toLipidMaps
-    '               writeLipidMapsRepo
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 362
+'    Code Lines: 254
+' Comment Lines: 56
+'   Blank Lines: 52
+'     File Size: 13.74 KB
+
+
+' Module Massbank
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: chebiSecondary2Main, createIdMapping, createLipidMapTable, GlycosylNameSolver, GlycosylTokens
+'               hmdbSecondary2Main, KEGGPathwayCoverages, lipidnameMapping, name2, readHMDB
+'               readLipidMapsRepo, readMoNA, readSDF, saveIDMapping, toLipidMaps
+'               writeLipidMapsRepo
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -70,6 +70,8 @@ Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.genomics.Assembly.ELIXIR.EBI.ChEBI.WebServices
+Imports SMRUCC.genomics.Assembly.ELIXIR.EBI.ChEBI.XML
 Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
 Imports SMRUCC.Rsharp
 Imports SMRUCC.Rsharp.Runtime
@@ -416,5 +418,17 @@ Module Massbank
         End If
 
         Return New GlycosylNameSolver(custom)
+    End Function
+
+    <ExportAPI("parseChEBIEntity")>
+    <RApiReturn(GetType(ChEBIEntity))>
+    Public Function ParseChebiEntity(xml As String) As Object
+        Dim data = REST.ParsingRESTData(xml)
+
+        If data.Length = 1 Then
+            Return data(Scan0)
+        Else
+            Return data
+        End If
     End Function
 End Module
