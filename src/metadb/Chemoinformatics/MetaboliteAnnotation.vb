20c89a8fdadc56f505dd94370f88652a670ad087
xieguigang <xie.guigang@gcmodeller.org>
2022-03-27 05:05:35 +0800

    create a general metabolite annotation model

---
+++ src/metadb/Chemoinformatics/MetaboliteAnnotation.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+
+Public Class MetaboliteAnnotation
+    Implements IReadOnlyId, ICompoundNameProvider, IExactMassProvider, IFormulaProvider
+
+    Public Property UniqueId As String Implements IReadOnlyId.Identity
+    Public Property CommonName As String Implements ICompoundNameProvider.CommonName
+    Public Property ExactMass As Double Implements IExactMassProvider.ExactMass
+    Public Property Formula As String Implements IFormulaProvider.Formula
+
+    Public Overrides Function ToString() As String
+        Return $"[{UniqueId}] {CommonName}"
+    End Function
+
+End Class
