6f4c1f084660972af741968d4598ba445f442420
xieguigang <xie.guigang@gcmodeller.org>
2022-03-27 05:10:13 +0800

    create annotation dataset

--- Rscript/Library/mzkit/MetaDbXref.vb
+++ Rscript/Library/mzkit/MetaDbXref.vb
@@ -1,61 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::f28990e0d8883cd3b1b670d51954d1a9, mzkit\Rscript\Library\mzkit\MetaDbXref.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 230
-    '    Code Lines: 176
-    ' Comment Lines: 28
-    '   Blank Lines: 26
-    '     File Size: 10.94 KB
+' Summaries:
 
 
-    ' Module MetaDbXref
-    ' 
-    '     Function: cbindMeta, CreateMs1Handler, createTable, ms1Search, searchTable
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 230
+'    Code Lines: 176
+' Comment Lines: 28
+'   Blank Lines: 26
+'     File Size: 10.94 KB
+
+
+' Module MetaDbXref
+' 
+'     Function: cbindMeta, CreateMs1Handler, createTable, ms1Search, searchTable
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports BioNovoGene.BioDeep.Chemistry
+Imports BioNovoGene.BioDeep.Chemoinformatics
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
 Imports BioNovoGene.BioDeep.MetaDNA
 Imports BioNovoGene.BioDeep.MSEngine
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
@@ -94,6 +96,25 @@ Module MetaDbXref
             .columns = columns,
             .rownames = columns!unique_id
         }
+    End Function
+
+    <ExportAPI("annotationStream")>
+    <RApiReturn(GetType(MetaboliteAnnotation))>
+    Public Function AnnotationStream(id As String(), name As String(), formula As String(), Optional env As Environment = Nothing) As Object
+        If id.Length <> name.Length OrElse name.Length <> formula.Length Then
+            Return Internal.debug.stop("vector size of the annotation data should be equals to each other!", env)
+        Else
+            Return id _
+                .Select(Function(ref, i)
+                            Return New MetaboliteAnnotation With {
+                                .UniqueId = ref,
+                                .CommonName = name(i),
+                                .Formula = formula(i),
+                                .ExactMass = FormulaScanner.EvaluateExactMass(.Formula)
+                            }
+                        End Function) _
+                .ToArray
+        End If
     End Function
 
     ''' <summary>
