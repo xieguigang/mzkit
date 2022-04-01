3b71927b72da8431e94d969789a7c8a1f397f5f4
xieguigang <xie.guigang@gcmodeller.org>
2022-04-01 20:55:31 +0800

    allow add custom data

--- src/metadb/FormulaSearch.Extensions/AtomGroups/AtomGroupHandler.vb
+++ src/metadb/FormulaSearch.Extensions/AtomGroups/AtomGroupHandler.vb
@@ -1,62 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::336f884666a379c1b0f8d9111cd53f85, mzkit\src\metadb\FormulaSearch.Extensions\AtomGroups\AtomGroupHandler.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 53
-    '    Code Lines: 42
-    ' Comment Lines: 0
-    '   Blank Lines: 11
-    '     File Size: 2.07 KB
+' Summaries:
 
 
-    ' Class AtomGroupHandler
-    ' 
-    '     Function: FindDelta, GetByMass, loadGroup
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 53
+'    Code Lines: 42
+' Comment Lines: 0
+'   Blank Lines: 11
+'     File Size: 2.07 KB
+
+
+' Class AtomGroupHandler
+' 
+'     Function: FindDelta, GetByMass, loadGroup
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Reflection
 Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-Imports Microsoft.VisualBasic.Language
-Imports stdnum = System.Math
+Imports Microsoft.VisualBasic.My
+Imports stdNum = System.Math
 
 Public Class AtomGroupHandler
 
@@ -78,12 +78,28 @@ Public Class AtomGroupHandler
                         End Function)
     End Function
 
-    Public Shared Function GetByMass(mass As Double) As NamedValue(Of Formula)
-        Static all_groups As List(Of KeyValuePair(Of String, Formula)) = New List(Of KeyValuePair(Of String, Formula)) + alkyl + ketones + amines + alkenyl + others
+    Sub New()
+        Call SingletonList(Of NamedValue(Of (Double, Formula))).Add(From i In alkyl Select New NamedValue(Of (Double, Formula))(i.Key, (i.Value.ExactMass, i.Value)))
+        Call SingletonList(Of NamedValue(Of (Double, Formula))).Add(From i In ketones Select New NamedValue(Of (Double, Formula))(i.Key, (i.Value.ExactMass, i.Value)))
+        Call SingletonList(Of NamedValue(Of (Double, Formula))).Add(From i In amines Select New NamedValue(Of (Double, Formula))(i.Key, (i.Value.ExactMass, i.Value)))
+        Call SingletonList(Of NamedValue(Of (Double, Formula))).Add(From i In alkenyl Select New NamedValue(Of (Double, Formula))(i.Key, (i.Value.ExactMass, i.Value)))
+        Call SingletonList(Of NamedValue(Of (Double, Formula))).Add(From i In others Select New NamedValue(Of (Double, Formula))(i.Key, (i.Value.ExactMass, i.Value)))
+    End Sub
 
-        For Each group In all_groups
-            If stdnum.Abs(group.Value.ExactMass - mass) <= 0.1 Then
-                Return New NamedValue(Of Formula)(group.Key, group.Value)
+    Public Sub Register(name As String, formula As String)
+        Dim chemical As Formula = FormulaScanner.ScanFormula(formula)
+        Dim exactMass As Double = chemical.ExactMass
+
+        Call SingletonList(Of NamedValue(Of (Double, Formula))).Add(New NamedValue(Of (Double, Formula))(name, (exactMass, chemical)))
+    End Sub
+
+    Public Shared Function GetByMass(mass As Double, Optional da As Double = 0.1) As NamedValue(Of Formula)
+        For Each group As NamedValue(Of (ExactMass As Double, Formula)) In SingletonList(Of NamedValue(Of (Double, Formula))).ForEach
+            If stdNum.Abs(group.Value.ExactMass - mass) <= da Then
+                Return New NamedValue(Of Formula) With {
+                    .Name = group.Name,
+                    .Value = group.Value.Item2
+                }
             End If
         Next
 
@@ -92,7 +108,7 @@ Public Class AtomGroupHandler
 
     Public Shared Function FindDelta(mz1 As Double, mz2 As Double, Optional ByRef delta As Integer = 0) As NamedValue(Of Formula)
         Dim d As Double = mz1 - mz2
-        Dim dmass As Double = stdnum.Abs(d)
+        Dim dmass As Double = stdNum.Abs(d)
 
         If dmass <= 0.1 Then
             Return Nothing
@@ -100,7 +116,7 @@ Public Class AtomGroupHandler
 
         Dim group = GetByMass(dmass)
 
-        delta = stdnum.Sign(d)
+        delta = stdNum.Sign(d)
 
         Return group
     End Function
