f2c91440f413c0367e581bf785f0ef133bdc503a
guigang xie <gg.xie@bionovogene.com>
2022-05-02 02:17:25 +0800

    add test data

--- dist/Rscript/lipidmaps/test/test_annotation.R
+++ dist/Rscript/lipidmaps/test/test_annotation.R
@@ -28,3 +28,23 @@ plot(ms2, title = "LSM(d16:1)+H", label.intensity = 0)
 ;
 
 }
+
+input = read.csv("./Cer(m17-1_25-2)+H.csv", row.names = NULL);
+
+print(input);
+
+bitmap(file = "./Cer(m17-1_25-2)+H.png") {
+
+input[, "annotation"] = NULL;
+ms2 = input
+|> libraryMatrix(, parentMz = 630.618455)
+|> peakAnnotations(massDiff = 0.005, adducts = math::precursor_types(["[M+H]+","[M+H-2H2O]+","[M-H2O]+"]))
+;
+
+print("view of the predicted ion fragment annotations:");
+print(as.data.frame(ms2));
+
+plot(ms2, title = "LSM(d16:1)+H", label.intensity = 0)
+;
+
+}
