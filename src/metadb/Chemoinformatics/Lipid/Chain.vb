f3a411b2954b32d35f501ca459c7195cf99879eb
guigang xie <gg.xie@bionovogene.com>
2022-05-05 09:42:17 +0800

    how to parse lipid name

---
+++ src/metadb/Chemoinformatics/Lipid/Chain.vb
@@ -0,0 +1,25 @@
+﻿Public Class Chain
+
+    Public Property carbons As Integer
+    Public Property doubleBonds As Integer
+    Public Property position As BondPosition()
+    Public Property groups As Group()
+
+End Class
+
+Public Class Group : Inherits BondPosition
+
+    Public Property groupName As String
+
+End Class
+
+Public Class BondPosition
+
+    Public Property index As Integer
+    ''' <summary>
+    ''' E/Z
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property [structure] As Char
+
+End Class
---
+++ src/metadb/Chemoinformatics/Lipid/LipidName.vb
@@ -0,0 +1,22 @@
+﻿Public Class LipidName
+
+    Public Property className As String
+    Public Property chains As Chain()
+
+    Public Overrides Function ToString() As String
+        Return ToSystematicName()
+    End Function
+
+    Public Function ToSystematicName() As String
+
+    End Function
+
+    Public Function ToOverviewName() As String
+
+    End Function
+
+    Public Shared Function ParseName(name As String) As LipidName
+
+    End Function
+
+End Class
