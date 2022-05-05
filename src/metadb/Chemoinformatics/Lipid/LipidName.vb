a3f389ab9e37146d1ba35e6f983b768f4ef2e007
guigang xie <gg.xie@bionovogene.com>
2022-05-05 10:02:08 +0800

    parse lipidname

--- src/metadb/Chemoinformatics/Lipid/LipidName.vb
+++ src/metadb/Chemoinformatics/Lipid/LipidName.vb
@@ -1,4 +1,4 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+﻿Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.Linq
 
 Public Class LipidName
@@ -24,6 +24,32 @@ Public Class LipidName
     ''' <param name="name"></param>
     ''' <returns></returns>
     Public Shared Function ParseLipidName(name As String) As LipidName
+        Static namePattern1 As New Regex("[a-zA-Z0-9]+\(.+\)")
+        Static namePattern2 As New Regex("[a-zA-Z0-9]+\s+.+")
+
+        Dim className As String
+        Dim components As String
+
+        If name.IsPattern(namePattern1) Then
+            Dim tokens = name.GetTagValue("(", trim:=True)
+
+            className = tokens.Name
+            components = tokens.Value
+            components = components.Substring(0, components.Length - 1)
+        Else
+            Dim tokens = name.GetTagValue(" ", trim:=True)
+
+            className = tokens.Name
+            components = tokens.Value.Trim
+        End If
+
+        Return New LipidName With {
+            .className = className,
+            .chains = ChainParser(components).ToArray
+        }
+    End Function
+
+    Private Shared Iterator Function ChainParser(components As String) As IEnumerable(Of Chain)
 
     End Function
 
