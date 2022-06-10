6aae0567b24f2fa0c5f1d1668ac21a0924a76b18
guigang xie <gg.xie@bionovogene.com>
2022-06-10 17:29:53 +0800

    Normalize matrix sample data

--- Rscript/Library/mzkit_app/R/math.R
+++ Rscript/Library/mzkit_app/R/math.R
@@ -8,3 +8,43 @@ imports "math" from "mzkit";
 const tolerance = function(kind as string, mzdiff as double) {
     math::tolerance(mzdiff, kind);
 }
+
+#' Normalize matrix sample data
+#'
+#' @return a result data matrix has been normalized 
+#'     via total sum of the peak area.
+#'
+const normData = function(mat, factor = 10^12) {
+	const min = mzkit::.minPos(mat) / 2;
+	const dat = as.data.frame(mat);
+	
+	v = [];
+	
+	for(name in colnames(mat)) {
+		v = append(v, mat[, name]);
+	}
+	
+	tpa = sum(as.numeric(v));
+	
+	for(name in colnames(mat)) {
+		v = as.numeric(mat[, name]);
+		v[v == 0.0] = min;
+		v = v / tpa * factor;
+		
+		mat[, name] = v;
+	}
+	
+	mat;
+}
+
+const .minPos = function(mat) {
+	v = [];
+	
+	for(name in colnames(mat)) {
+		v = append(v, mat[, name]);
+	}
+	
+	v = as.numeric(v);
+	v = min(v[v > 0]);
+	v;
+}
