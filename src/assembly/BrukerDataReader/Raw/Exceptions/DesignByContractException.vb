f86f4f06947a58e8eb25ab02b1fb005a8d4119e0
xieguigang <xie.guigang@gcmodeller.org>
2022-06-26 14:56:02 +0800

    split exception code

---
+++ src/assembly/BrukerDataReader/Raw/Exceptions/DesignByContractException.vb
@@ -0,0 +1,27 @@
+﻿
+Imports System
+
+Namespace Raw
+    ''' <summary>
+    ''' Exception raised when a contract is broken.
+    ''' Catch this exception type if you wish to differentiate between
+    ''' any DesignByContract exception and other runtime exceptions.
+    '''
+    ''' </summary>
+    <CoverageExclude>
+    <Serializable>
+    Public Class DesignByContractException
+        Inherits ApplicationException
+
+        Protected Sub New()
+        End Sub
+
+        Protected Sub New(message As String)
+            MyBase.New(message)
+        End Sub
+
+        Protected Sub New(message As String, inner As Exception)
+            MyBase.New(message, inner)
+        End Sub
+    End Class
+End Namespace
