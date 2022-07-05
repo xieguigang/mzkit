3879a01a21c6882d83dc33c5731969ed326384af
xieguigang <xie.guigang@gcmodeller.org>
2022-07-05 11:00:58 +0800

    Create read_v2.R

---
+++ dist/Rscript/raw/mzpack_v2/read_v2.R
@@ -0,0 +1,15 @@
+require(mzkit);
+require(HDS);
+require(JSON);
+
+imports "mzPack" from "mzkit";
+imports "mzweb" from "mzkit";
+
+const v2raw = "V:\project\CRO\Test_Brain_chca(10MG0.1%FA)_Tri_L15_15%_10mmmin20220628\20220705-1\analysis\06.MZKit数据查看\CleanSample.mzPack";
+
+data = HDS::openStream(v2raw);
+
+print(HDS::tree(data, showReadme = FALSE));
+close(data);
+
+data = open.mzpack(v2raw);
