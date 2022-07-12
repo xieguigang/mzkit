236b72bcbfade4b1697e093f4f4f0bafaaf5b2a6
guigang xie <gg.xie@bionovogene.com>
2022-07-12 16:10:36 +0800

    get ms cluster bins

--- Rscript/Library/mzkit/math/MoleculeNetworking.vb
+++ Rscript/Library/mzkit/math/MoleculeNetworking.vb
@@ -2,7 +2,9 @@
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.DataMining.BinaryTree
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
 <Package("MoleculeNetworking")>
 Module MoleculeNetworking
@@ -14,5 +16,24 @@ Module MoleculeNetworking
                          Optional equals As Double = 0.85) As ClusterTree
 
         Return ions.Tree(mzdiff, intocutoff, equals)
+    End Function
+
+    <ExportAPI("msBin")>
+    Public Function MsBin(tree As ClusterTree, ions As PeakMs2()) As list
+        Dim clusters As IEnumerable(Of ClusterTree) = ClusterTree.GetClusters(tree)
+        Dim list = ions.ToDictionary(Function(i) i.lib_guid)
+        Dim payload As New list With {
+            .slots = New Dictionary(Of String, Object)
+        }
+        Dim members As New List(Of PeakMs2)
+
+        For Each cluster As ClusterTree In clusters
+            Call members.Clear()
+            Call members.Add(list(cluster.Data))
+            Call members.AddRange(cluster.Members.SafeQuery.Select(Function(r) list(r)))
+            Call payload.add(cluster.Data, members.ToArray)
+        Next
+
+        Return payload
     End Function
 End Module
