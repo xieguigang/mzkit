583bea9c3998f0bf567e42c9ecbaf17bbf7489e8
guigang xie <gg.xie@bionovogene.com>
2022-07-18 13:29:14 +0800

    code perfermance improvements

--- src/assembly/mzPack/v2.0/mzStream.vb
+++ src/assembly/mzPack/v2.0/mzStream.vb
@@ -147,7 +147,7 @@ Public Class mzStream : Implements IMzPackReader
         Dim n As Integer = dir.attributes.GetValue("products")
         Dim id2 As String() = Nothing
 
-        If Not skipProducts Then
+        If n > 0 AndAlso Not skipProducts Then
             id2 = dir.attributes.GetValue("id")
             ms1.products = New ScanMS2(n - 1) {}
 
