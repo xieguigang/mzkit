efa2cfeb4b60d18f6ff3e838d9c7b00bcc6c1abb
guigang xie <gg.xie@bionovogene.com>
2022-07-21 11:27:34 +0800

    helper for imports data

---
+++ src/assembly/BrukerDataReader/SCiLSLab/ImportsUtils.vb
@@ -0,0 +1,19 @@
+﻿Imports System.IO
+
+Namespace SCiLSLab
+
+    ''' <summary>
+    ''' tools handler for import SCiLSLab csv data
+    ''' </summary>
+    Public Module ImportsUtils
+
+        Public Function CheckFileHandler(file As String) As Type
+
+        End Function
+
+        Public Function CheckSpotFile(msDatafile As String) As String
+
+        End Function
+
+    End Module
+End Namespace
