d6032fab7f31b2bd8e008c352086ede21e4a3aab
xieguigang <xie.guigang@gcmodeller.org>
2022-07-29 18:49:36 +0800

    read blob bytes data

--- src/assembly/BrukerDataReader/test/Program.vb
+++ src/assembly/BrukerDataReader/test/Program.vb
@@ -4,9 +4,14 @@ Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.BrukerDataReader
 Module Program
     Sub Main(args As String())
         Dim mcf_idx As New IndexParser("F:\MSI\YP202130530-V.d\b6ad08c2-7356-4a7c-88a5-47809c687c81_2.mcf_idx")
+        Dim bytes As Byte()
+        Dim blob As New mcfParser("F:\MSI\YP202130530-V.d\b6ad08c2-7356-4a7c-88a5-47809c687c81_2.mcf")
 
         For Each index In mcf_idx.GetContainerIndex
-            Call Console.WriteLine(index.ToString)
+            Console.WriteLine(index.ToString)
+            bytes = blob.GetBlob(index)
+
+            Pause()
         Next
     End Sub
 End Module
