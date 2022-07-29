312ce607d13e45907eeeed72234888e5e372177b
xieguigang <xie.guigang@gcmodeller.org>
2022-07-29 19:04:54 +0800

    Update Spectra.vb

--- src/assembly/BrukerDataReader/flexImaging/spectrum_sqlite/Spectra.vb
+++ src/assembly/BrukerDataReader/flexImaging/spectrum_sqlite/Spectra.vb
@@ -1,3 +1,30 @@
 ﻿Public Class Spectra
 
+    ' CREATE TABLE Spectra (
+    ' Id INTEGER PRIMARY KEY,
+    ' Chip INTEGER NOT NULL,
+    ' SpotName TEXT,
+    ' RegionNumber INTEGER,
+    ' XIndexPos INTEGER,
+    ' YIndexPos INTEGER,
+    ' AcquisitionKey INTEGER NOT NULL,
+    ' ParentMass REAL,
+    ' DateTime TEXT,
+    ' CalibrationDateTime TEXT,
+    ' MotorPositionX REAL,
+    ' MotorPositionY REAL,
+    ' NumSummations INTEGER,
+    ' LaserPower REAL,
+    ' LaserRepRate REAL,
+    ' NumPeaks INTEGER NOT NULL,
+    ' PeakMzValues BLOB NOT NULL,
+    ' PeakIntensityValues BLOB NOT NULL,
+    ' PeakFwhmValues BLOB NOT NULL,
+    ' PeakSnrValues BLOB,
+    ' PeakFlags BLOB,
+    ' BH BLOB,
+    ' BC TEXT,
+    ' FOREIGN KEY (AcquisitionKey) REFERENCES AcquisitionKeys(Id)
+    ' )
+
 End Class
