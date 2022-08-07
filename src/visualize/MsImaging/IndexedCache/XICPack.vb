6306922e945cea6c23afbcbccc03b4ab64fe926d
xieguigang <xie.guigang@gcmodeller.org>
2022-08-07 10:18:08 +0800

    removes old source code

---
+++ src/visualize/MsImaging/IndexedCache/XICPack.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+
+Namespace IndexedCache
+
+    Public Class XICPack
+
+        ReadOnly stream As StreamPack
+
+    End Class
+End Namespace
