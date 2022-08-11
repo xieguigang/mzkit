941dc8ec333898e32d871e3c1231b67f929664e3
xieguigang <xie.guigang@gcmodeller.org>
2022-08-11 11:17:48 +0800

    move to vendors folder

---
+++ src/assembly/BrukerDataReader/flexImaging/Storage.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+''' <summary>
+''' Storage.mcf_idx
+''' </summary>
+Public Module Storage
+
+    Public Function GetMetaData(mcf_file As String) As NamedValue(Of String)()
+        Dim parser As New mcfParser(mcf_file)
+    End Function
+End Module
