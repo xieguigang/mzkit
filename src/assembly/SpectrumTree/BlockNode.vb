5f45899c6436ecb64c635cab4a65cdca733d68b8
xieguigang <xie.guigang@gcmodeller.org>
2022-08-12 23:48:15 +0800

    improvements of the tree node cluster model;

--- src/assembly/SpectrumTree/BlockNode.vb
+++ src/assembly/SpectrumTree/BlockNode.vb
@@ -1,82 +1,20 @@
-﻿#Region "Microsoft.VisualBasic::f31ce37d9ff492e909e530f0731d80ca, mzkit\src\assembly\SpectrumTree\BlockNode.vb"
-
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
-
-
-
-    ' /********************************************************************************/
-
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 26
-    '    Code Lines: 20
-    ' Comment Lines: 0
-    '   Blank Lines: 6
-    '     File Size: 747.00 B
-
-
-    ' Class BlockNode
-    ' 
-    '     Properties: left, right, scan0
-    ' 
-    '     Function: ReadNode
-    ' 
-    ' /********************************************************************************/
-
-#End Region
-
-Imports System.IO
-Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
-Imports Microsoft.VisualBasic.Data.IO
+﻿Imports Microsoft.VisualBasic.Data.IO
 
 Public Class BlockNode
 
-    Public Property scan0 As Long
-    Public Property left As BlockNode
-    Public Property right As BlockNode
+    Public Property Id As String
+    Public Property Block As BufferRegion
+    ''' <summary>
+    ''' 得分0.9以上的都算作为当前节点的等价谱图
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Members As Integer()
 
-    Public Iterator Function ReadNode(file As Stream) As IEnumerable(Of PeakMs2)
-        Dim infile As New BinaryDataReader(file)
-        Dim size As Long
-        Dim nspectra As Integer
-
-        infile.Seek(scan0, SeekOrigin.Begin)
-        size = infile.ReadInt64
-        infile.ReadString(BinaryStringFormat.ZeroTerminated)
-        nspectra = infile.ReadInt32
-
-        For i As Integer = 0 To nspectra - 1
-            Yield Reader.ReadSpectra(infile)
-        Next
-    End Function
+    ''' <summary>
+    ''' 总共10个元素，分别表示[0,1]区间内的10个阈值等级
+    ''' 0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property childs As Integer()
 
 End Class
---
+++ src/assembly/SpectrumTree/ReferenceTree.vb
@@ -0,0 +1,5 @@
+﻿Public Class ReferenceTree
+
+    ReadOnly tree As New List(Of )
+
+End Class
