0e798d49191eac757d6718edd96a1e13ed5eb515
xieguigang <xie.guigang@gcmodeller.org>
2022-08-13 00:58:52 +0800

    create spectrum tree file

---
+++ Rscript/Library/mzkit/ReferenceTree.vb
@@ -0,0 +1,40 @@
+﻿Imports System.IO
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports BioNovoGene.Analytical.MassSpectrometry.SpectrumTree
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("spectrumTree")>
+Module ReferenceTreePkg
+
+    <ExportAPI("new")>
+    <RApiReturn(GetType(ReferenceTree))>
+    Public Function CreateNew(file As Object, Optional env As Environment = Nothing) As Object
+        Dim buffer = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Write, env)
+
+        If buffer Like GetType(Message) Then
+            Return buffer.TryCast(Of Message)
+        End If
+
+        Return New ReferenceTree(buffer.TryCast(Of Stream))
+    End Function
+
+    <ExportAPI("addBucket")>
+    Public Function addBucket(tree As ReferenceTree, <RRawVectorArgument> x As Object, Optional env As Environment = Nothing) As Object
+        Dim list As pipeline = pipeline.TryCreatePipeline(Of PeakMs2)(x, env)
+
+        If list.isError Then
+            Return list.getError
+        End If
+
+        For Each spectrum As PeakMs2 In list.populates(Of PeakMs2)(env)
+            Call tree.Push(spectrum)
+        Next
+
+        Return tree
+    End Function
+End Module
