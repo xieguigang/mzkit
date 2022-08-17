c316e41c26decc6ddf0192b1ca13af25eea5a7a1
guigang xie <xie.guigang@gcmodeller.org>
2022-08-17 18:47:06 +0800

    config for run test

--- src/metadb/Massbank/Public/NCBI/MeSH/Reader.vb
+++ src/metadb/Massbank/Public/NCBI/MeSH/Reader.vb
@@ -1,11 +1,18 @@
 ﻿Imports System.IO
+Imports Microsoft.VisualBasic.Language
 
 Namespace NCBI.MeSH
 
     Public Module Reader
 
-        Public Function ParseRDF(file As Stream) As RDF
+        Public Function ParseRDF(file As StreamReader) As RDF
+            Dim line As Value(Of String) = ""
+            Dim str As String
 
+            Do While Not (line = file.ReadLine) Is Nothing
+                str = line.Value
+
+            Loop
         End Function
     End Module
 End Namespace
