baad7d61c27ca37560ea6979f2384ba10be426bd
xieguigang <xie.guigang@gcmodeller.org>
2022-08-18 22:04:36 +0800

    evaluate cell target

--- src/visualize/TissueMorphology/HEMap/Cell.vb
+++ src/visualize/TissueMorphology/HEMap/Cell.vb
@@ -1,4 +1,10 @@
-﻿Public Class Cell
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+Public Class Cell
 
     ''' <summary>
     ''' the location X of the grid
@@ -37,5 +43,32 @@ Public Class [Object]
 
     Public Property Pixels As Integer
     Public Property Density As Double
+
+    Public Shared Function Eval(rect As Grid(Of Color),
+                                target As Color,
+                                gridSize As Integer,
+                                Optional tolerance As Integer = 5,
+                                Optional densityGrid As Integer = 5) As [Object]
+
+        Dim hits As New List(Of Integer)
+
+        rect = rect _
+            .Cells _
+            .Where(Function(c)
+                       Return c.data.Equals(target, tolerance:=tolerance)
+                   End Function) _
+            .DoCall(AddressOf Grid(Of Color).CreateReadOnly)
+
+        For x As Integer = 1 To gridSize Step densityGrid
+            For y As Integer = 1 To gridSize Step densityGrid
+                hits.Add(rect.Query(x, y, densityGrid).Count)
+            Next
+        Next
+
+        Return New [Object] With {
+            .Pixels = rect.Cells.Count,
+            .Density = (New Vector(hits) / (densityGrid ^ 2)).Average
+        }
+    End Function
 
 End Class
--- src/visualize/TissueMorphology/HEMap/HistologicalImage.vb
+++ src/visualize/TissueMorphology/HEMap/HistologicalImage.vb
@@ -1,63 +1,65 @@
 ﻿#Region "Microsoft.VisualBasic::6c3d4fc2faa55528e764af758d51626c, mzkit\src\visualize\MsImaging\HistologicalImage.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 34
-    '    Code Lines: 21
-    ' Comment Lines: 8
-    '   Blank Lines: 5
-    '     File Size: 1.03 KB
+' Summaries:
 
 
-    ' Module HistologicalImage
-    ' 
-    '     Function: HeatMap, MonoScale
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 34
+'    Code Lines: 21
+' Comment Lines: 8
+'   Blank Lines: 5
+'     File Size: 1.03 KB
+
+
+' Module HistologicalImage
+' 
+'     Function: HeatMap, MonoScale
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.ChartPlots
+Imports Microsoft.VisualBasic.Data.GraphTheory
 Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Linq
 
 ''' <summary>
 ''' helper module for processing haematoxylin and eosin staining image
@@ -85,8 +87,40 @@ Public Module HistologicalImage
         )
     End Function
 
-    Public Iterator Function GridScan(target As Image, Optional gridSize As Integer = 25) As IEnumerable(Of Cell)
+    Public Iterator Function GridScan(target As Image,
+                                      Optional gridSize As Integer = 25,
+                                      Optional tolerance As Integer = 5,
+                                      Optional densityGrid As Integer = 5) As IEnumerable(Of Cell)
 
+        Dim A As Double = gridSize ^ 2
+
+        Using bitmap As BitmapBuffer = BitmapBuffer.FromImage(target)
+            For i As Integer = 1 To bitmap.Width Step gridSize
+                For j As Integer = 1 To bitmap.Height Step gridSize
+                    Dim block As Color()() = bitmap _
+                        .GetPixel(New Rectangle(i, j, gridSize, gridSize)) _
+                        .ToArray
+                    Dim r = block.IteratesALL.Select(Function(c) CDbl(c.R)).Average
+                    Dim g = block.IteratesALL.Select(Function(c) CDbl(c.G)).Average
+                    Dim b = block.IteratesALL.Select(Function(c) CDbl(c.B)).Average
+                    Dim matrix As Grid(Of Color) = block _
+                        .Select(Function(row, y)
+                                    Return row.Select(Function(c, x) (c, x, y))
+                                End Function) _
+                        .IteratesALL _
+                        .DoCall(AddressOf Grid(Of Color).Create)
+
+                    Yield New Cell With {
+                        .X = i,
+                        .Y = j,
+                        .B = b,
+                        .G = g,
+                        .R = r,
+                        .Black = [Object].Eval(matrix, Color.Black, gridSize, tolerance, densityGrid)
+                    }
+                Next
+            Next
+        End Using
     End Function
 
 End Module
