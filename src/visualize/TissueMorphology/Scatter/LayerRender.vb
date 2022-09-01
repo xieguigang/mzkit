3af4193ddc65074796cc07740257186d741dde3b
guigang xie <xie.guigang@gcmodeller.org>
2022-09-01 13:12:47 +0800

    improvements of the layer render

--- src/visualize/TissueMorphology/Scatter/LayerRender.vb
+++ src/visualize/TissueMorphology/Scatter/LayerRender.vb
@@ -1,16 +1,30 @@
 ﻿Imports System.Drawing
 Imports System.Drawing.Drawing2D
 Imports System.Drawing.Imaging
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Linq
 
 Public Class LayerRender
 
     Public Property alphaLevel As Double = 0.8
     Public Property dotSize As New Size(3, 3)
 
+    ''' <summary>
+    ''' a list of the region labels for highlights
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property highlights As String()
+
     Public Sub Rendering(g As Graphics, regions As IEnumerable(Of TissueRegion))
+        Dim highlights As Index(Of String) = Me.highlights.SafeQuery.Distinct.Indexing
+
         For Each region As TissueRegion In regions
             Dim fill As New SolidBrush(region.color.Alpha(255 * alphaLevel))
+
+            If region.label Like highlights Then
+                fill = New SolidBrush(region.color.Darken(0.25))
+            End If
 
             For Each p As Point In region.points
                 p = New Point(p.X * dotSize.Width, p.Y * dotSize.Height)
@@ -22,7 +36,8 @@ Public Class LayerRender
     Public Shared Function Draw(regions As IEnumerable(Of TissueRegion),
                                 layerSize As Size,
                                 Optional alphaLevel As Double = 0.8,
-                                Optional dotSize As Integer = 3) As Bitmap
+                                Optional dotSize As Integer = 3,
+                                Optional highlights As String() = Nothing) As Bitmap
 
         Dim layer As New Bitmap(
             width:=layerSize.Width * dotSize,
@@ -32,7 +47,8 @@ Public Class LayerRender
         Dim g As Graphics = Graphics.FromImage(layer)
         Dim blender As New LayerRender With {
             .alphaLevel = alphaLevel,
-            .dotSize = New Size(dotSize, dotSize)
+            .dotSize = New Size(dotSize, dotSize),
+            .highlights = highlights
         }
 
         g.CompositingQuality = CompositingQuality.HighQuality
