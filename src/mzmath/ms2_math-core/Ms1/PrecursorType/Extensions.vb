5511513ced9d38cca15cecec375a102fbf934206
xieguigang <xie.guigang@gcmodeller.org>
2022-09-23 11:46:43 +0800

    populate details error message

--- src/mzmath/ms2_math-core/Ms1/PrecursorType/Extensions.vb
+++ src/mzmath/ms2_math-core/Ms1/PrecursorType/Extensions.vb
@@ -1,59 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::a507d7951623a4f0e84e0fd4190c891f, mzkit\src\mzmath\ms2_math-core\Ms1\PrecursorType\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 182
-    '    Code Lines: 134
-    ' Comment Lines: 21
-    '   Blank Lines: 27
-    '     File Size: 7.00 KB
+' Summaries:
 
 
-    '     Module Parser
-    ' 
-    '         Function: Formula, ParseCharge, ParseMzCalculator, ParseMzCalculatorInternal, ToString
-    ' 
-    '     Module Extensions
-    ' 
-    '         Sub: PrintTable
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 182
+'    Code Lines: 134
+' Comment Lines: 21
+'   Blank Lines: 27
+'     File Size: 7.00 KB
+
+
+'     Module Parser
+' 
+'         Function: Formula, ParseCharge, ParseMzCalculator, ParseMzCalculatorInternal, ToString
+' 
+'     Module Extensions
+' 
+'         Sub: PrintTable
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -115,6 +115,10 @@ Namespace Ms1.PrecursorType
             }
 
             SyncLock cache
+                If Not cache.ContainsKey(ionMode) Then
+                    Throw New InvalidExpressionException($"invalid ion mode value '{ionMode}', please check the precursor type '{precursor_type}' is valid or not?")
+                End If
+
                 If cache(ionMode).ContainsKey(precursor_type) Then
                     Return cache(ionMode)(precursor_type)
                 End If
