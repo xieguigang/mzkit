9c25712d2c6b7fcf85598eb2a8e4c3b8efa66f8b
xieguigang <xie.guigang@gcmodeller.org>
2022-10-28 10:19:08 +0800

    fix of the namespace reference

--- src/metadb/Chemoinformatics/MetaboliteAnnotation.vb
+++ src/metadb/Chemoinformatics/MetaboliteAnnotation.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.Annotations
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 
 Public Class MetaboliteAnnotation
     Implements IReadOnlyId, ICompoundNameProvider, IExactMassProvider, IFormulaProvider
