5ac7152e6effacff643bbda33a897262f4adf62f
guigang xie <xie.guigang@gcmodeller.org>
2022-11-09 17:15:43 +0800

    add method handler for the spot plot

--- src/visualize/TissueMorphology/Scatter/PhenographSpot.vb
+++ src/visualize/TissueMorphology/Scatter/PhenographSpot.vb
@@ -1,7 +1,12 @@
 Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 
 Public Class PhenographSpot
 
+    ''' <summary>
+    ''' the spot pixel id, in string format of ``[x,y]``.
+    ''' </summary>
+    ''' <returns></returns>
     Public Property id As String
     Public Property phenograph_cluster As String
 
@@ -17,4 +22,15 @@ Public Class PhenographSpot
         Return New Point(x, y)
     End Function
 
+    Public Shared Function GetSpotColorIndex(phenograph As IEnumerable(Of PhenographSpot), Optional colorSet As String = "paper") As Dictionary(Of String, Color)
+        Dim classes As String() = phenograph.Select(Function(p) p.phenograph_cluster).Distinct.ToArray
+        Dim colors As Color() = Designer.GetColors(colorSet, n:=classes.Length)
+        Dim index As New Dictionary(Of String, Color)
+
+        For i As Integer = 0 To classes.Length - 1
+            Call index.Add(classes(i), colors(i))
+        Next
+
+        Return index
+    End Function
 End Class
