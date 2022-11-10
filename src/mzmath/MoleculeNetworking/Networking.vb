9ccf57e52fae387146ee9818b04357d899f97822
guigang xie <xie.guigang@gcmodeller.org>
2022-11-10 11:05:09 +0800

    code style improvements of processing ms2 data

--- src/mzmath/MoleculeNetworking/Networking.vb
+++ src/mzmath/MoleculeNetworking/Networking.vb
@@ -2,6 +2,25 @@
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports Microsoft.VisualBasic.DataMining.BinaryTree
+Imports Microsoft.VisualBasic.Imaging.Physics.World
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports System.Runtime.CompilerServices
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.MoleculeNetworking
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.DataMining.BinaryTree
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.Distributions
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 Public Module Networking
 
@@ -19,5 +38,70 @@ Public Module Networking
         Next
 
         Return clustering
+    End Function
+
+    <Extension>
+    Public Function RepresentativeSpectrum(cluster As PeakMs2(),
+                                           tolerance As Tolerance,
+                                           zero As RelativeIntensityCutoff,
+                                           Optional key As String = Nothing) As PeakMs2
+        Dim union As ms2() = cluster _
+            .Select(Function(i)
+                        Dim maxinto As Double = i.mzInto _
+                            .Select(Function(mzi) mzi.intensity) _
+                            .Max
+
+                        Return i.mzInto _
+                            .Select(Function(mzi)
+                                        Return New ms2 With {
+                                            .mz = mzi.mz,
+                                            .intensity = mzi.intensity / maxinto
+                                        }
+                                    End Function)
+                    End Function) _
+            .IteratesALL _
+            .ToArray _
+            .Centroid(tolerance, cutoff:=zero) _
+            .ToArray
+        Dim rt As Double = cluster _
+            .Select(Function(c) c.rt) _
+            .TabulateBin _
+            .Average
+        Dim mz1 As Double
+        Dim metadata = cluster _
+            .Select(Function(c) c.meta) _
+            .IteratesALL _
+            .GroupBy(Function(t) t.Key) _
+            .ToDictionary(Function(t) t.Key,
+                            Function(t)
+                                Return t _
+                                    .Select(Function(ti) ti.Value) _
+                                    .Distinct _
+                                    .JoinBy("; ")
+                            End Function)
+
+        If cluster.Length = 1 Then
+            mz1 = cluster(Scan0).mz
+        Else
+            mz1 = 0
+            metadata("mz1") = cluster _
+                .Select(Function(c) c.mz) _
+                .ToArray _
+                .GetJson
+        End If
+
+        Return New PeakMs2 With {
+            .rt = rt,
+            .activation = "NA",
+            .collisionEnergy = 0,
+            .file = key,
+            .intensity = cluster.Sum(Function(c) c.intensity),
+            .lib_guid = key,
+            .mz = mz1,
+            .mzInto = union,
+            .precursor_type = "NA",
+            .scan = "NA",
+            .meta = metadata
+        }
     End Function
 End Module
