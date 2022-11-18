747bff9847c472144dbca0f04a1d4255cc8ed0d7
guigang xie <xie.guigang@gcmodeller.org>
2022-11-18 11:52:51 +0800

    Update imzML_converts.R

--- dist/workbench/ms_imaging/imzML_converts.R
+++ dist/workbench/ms_imaging/imzML_converts.R
@@ -5,3 +5,9 @@ package_utils::attach("D:\mzkit\Rscript\Library\mzkit_app");
 require(mzkit);
 
 imports "MSI" from "mzkit";
+imports "mzweb" from "mzkit";
+
+"D:\mzkit\DATA\test\HR2MSI mouse urinary bladder S096 - Figure1.cdf"
+|> open.mzpack()
+|> write.imzML(file = `${@dir}/HR2MSI mouse urinary bladder S096 - Figure1.imzML`)
+;
