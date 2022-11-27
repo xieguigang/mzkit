8e95906cf1c4b5f48af2178e56afd9ae71bd3abb
xieguigang <xie.guigang@gcmodeller.org>
2022-11-27 23:19:23 +0800

    do mzimage writer test

--- dist/workbench/ms_imaging/mzImageWrite.R
+++ dist/workbench/ms_imaging/mzImageWrite.R
@@ -4,3 +4,11 @@ package_utils::attach("E:\mzkit\Rscript\Library\mzkit_app");
 
 require(mzkit);
 
+imports "mzweb" from "mzkit";
+imports "MsImaging" from "mzplot";
+
+"E:\demo\HR2MSI mouse urinary bladder S096.mzPack"
+|> open.mzpack()
+|> viewer()
+|> write.mzImage(file = `${@dir}/demo.mzImage`)
+;
