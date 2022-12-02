b3d6a75ce73102f854819a03ab3c8ce452061dcb
guigang xie <xie.guigang@gcmodeller.org>
2022-12-02 13:30:09 +0800

    export of the spatial mapping matrix

---
+++ src/visualize/TissueMorphology/SpatialMapping/SpatialMapping.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Xml.Serialization
+
+Public Class SpatialMapping
+
+    ''' <summary>
+    ''' pixel x of the spatial transcriptomics spot
+    ''' </summary>
+    ''' <returns></returns>
+    <XmlAttribute> Public Property STX As Integer
+    ''' <summary>
+    ''' pixel y of the spatial transcriptomics spot
+    ''' </summary>
+    ''' <returns></returns>
+    <XmlAttribute> Public Property STY As Integer
+
+    <XmlAttribute> Public Property SMX As Integer()
+    <XmlAttribute> Public Property SMY As Integer()
+
+End Class
