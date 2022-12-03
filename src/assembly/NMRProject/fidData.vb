46a6e00815206922c719d6570e107e6a9c3a0c91
xieguigang <xie.guigang@gcmodeller.org>
2022-12-03 20:25:15 +0800

    how to do data transform

--- src/assembly/NMRProject/fidData.vb
+++ src/assembly/NMRProject/fidData.vb
@@ -6,4 +6,19 @@ Public Class fidData
     Public Property time As Double()
     Public Property intensity As Double()
 
+    ''' <summary>
+    ''' 基于傅里叶变换将时域数据转换为频域数据
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function FourierTransform() As FrequencyData
+
+    End Function
+
+End Class
+
+Public Class FrequencyData
+
+    Public Property frequency As Double()
+    Public Property intensity As Double()
+
 End Class
