5b2dbe8f20caa5a1e6c88c930dccfb8d87daf025
guigang xie <xie.guigang@gcmodeller.org>
2022-12-05 17:26:52 +0800

    create tables?

---
+++ src/metadb/Massbank/Public/TMIC/MarkerDB/Exports.vb
@@ -0,0 +1,9 @@
+Namespace TMIC.MarkerDB
+
+    ''' <summary>
+    ''' methods for export database tables
+    ''' </summary>
+    Public Module Exports
+
+    End Module
+End Namespace
---
+++ src/metadb/Massbank/Public/TMIC/MarkerDB/Tables.vb
@@ -0,0 +1,4 @@
+Namespace TMIC.MarkerDB
+
+
+End Namespace
