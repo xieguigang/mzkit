d9bf7d679e7f354d38238228fc7e33ff0e776541
xieguigang <xie.guigang@gcmodeller.org>
2022-12-10 21:09:27 +0800

    handling of the model file

--- src/assembly/Comprehensive/MsImaging/3D_MALDI/MALDIPointCloud.vb
+++ src/assembly/Comprehensive/MsImaging/3D_MALDI/MALDIPointCloud.vb
@@ -2,16 +2,13 @@
 Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.imzML
-Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.mzML.IonTargeted
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Data.IO
-Imports Microsoft.VisualBasic.DataStorage.netCDF.DataVector
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Imaging.Landscape.Ply
 Imports Microsoft.VisualBasic.Serialization.BinaryDumping
-Imports SMRUCC.Rsharp.Runtime.Internal.Invokes
 
 Namespace MsImaging.MALDI_3D
 
@@ -42,26 +39,26 @@ Namespace MsImaging.MALDI_3D
         End Sub
 
         Public Function FileConvert(xml As String, ply As String, Optional colors As ScalerPalette = ScalerPalette.turbo) As Boolean
+            Dim cachefile As String = ply.ChangeSuffix("pointcloud_cache")
+
+            Call saveCache(xml, cachefile)
+
+            Using file As Stream = ply.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
+                Return SimplePlyWriter.WriteAsciiText(ReadPointCloud(cachefile), file, colors)
+            End Using
+        End Function
+
+        Public Sub SaveCache(xml As String, cachefile As String)
             Dim scans As IEnumerable(Of Scan3DReader) = imzML.XML.Load3DScanData(imzML:=xml)
             Dim intensity As Func(Of ms2(), Double) = Function(scan) Aggregate i In scan Into Sum(i.intensity)
             Dim pointcloud As IEnumerable(Of PointCloud) = scans.LoadPointCloud(intensity)
-            Dim cachefile As String = ply.ChangeSuffix("pointcloud_cache")
 
             Using file As Stream = cachefile.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
                 Call pointcloud.cache(New BinaryDataWriter(file) With {.ByteOrder = ByteOrder.BigEndian})
             End Using
+        End Sub
 
-            Using file As Stream = ply.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
-                ' Return SimplePlyWriter.WriteAsciiText(pointcloud, file, colors)
-            End Using
-        End Function
-
-        Public Sub ExportHeatMapModel(cachefile As String, model As String,
-                                      Optional colors As ScalerPalette = ScalerPalette.turbo,
-                                      Optional levels As Integer = 255)
-
-            Dim points As New List(Of PointCloud)
-
+        Private Iterator Function ReadPointCloud(cachefile As String) As IEnumerable(Of PointCloud)
             Using file As Stream = cachefile.Open(FileMode.Open, doClear:=False, [readOnly]:=True)
                 Dim bin As New NetworkByteOrderBuffer
                 Dim buffer As Byte() = New Byte(4 * 8 - 1) {}
@@ -70,15 +67,22 @@ Namespace MsImaging.MALDI_3D
                 Do While file.Position < file.Length
                     file.Read(buffer, Scan0, buffer.Length)
                     dbls = bin.decode(buffer)
-                    points.Add(New PointCloud With {
+
+                    Yield New PointCloud With {
                         .x = dbls(0),
                         .y = dbls(1),
                         .z = dbls(2),
                         .intensity = dbls(3)
-                    })
+                    }
                 Loop
             End Using
+        End Function
 
+        Public Sub ExportHeatMapModel(cachefile As String, model As String,
+                                      Optional colors As ScalerPalette = ScalerPalette.turbo,
+                                      Optional levels As Integer = 255)
+
+            Dim points As PointCloud() = ReadPointCloud(cachefile).ToArray
             Dim colorSet As String() = Designer _
                 .GetColors(colors.Description, levels) _
                 .Select(Function(c) c.ToHtmlColor) _
@@ -89,7 +93,7 @@ Namespace MsImaging.MALDI_3D
             Using file As Stream = model.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
                 Dim buf As New BinaryDataWriter(file) With {.ByteOrder = ByteOrder.BigEndian}
 
-                Call buf.Write(points.Count)
+                Call buf.Write(points.Length)
                 Call buf.Write(colorSet.Length)
 
                 For Each color As String In colorSet
