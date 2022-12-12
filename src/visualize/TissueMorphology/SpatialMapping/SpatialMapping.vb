8a31865d05095e235ac686ef8dc0dca8e5a388d2
xieguigang <xie.guigang@gcmodeller.org>
2022-12-12 15:39:46 +0800

    how to record the spatial mapping operations?

--- src/visualize/TissueMorphology/SpatialMapping/SpatialMapping.vb
+++ src/visualize/TissueMorphology/SpatialMapping/SpatialMapping.vb
@@ -6,6 +6,7 @@ Public Class SpatialMapping : Inherits ListOf(Of SpotMap)
     <XmlElement> Public Property label As String
     <XmlElement("spot")>
     Public Property spots As SpotMap()
+    Public Property transform As Transform()
 
     Protected Overrides Function getSize() As Integer
         Return spots.TryCount
@@ -14,4 +15,20 @@ Public Class SpatialMapping : Inherits ListOf(Of SpotMap)
     Protected Overrides Function getCollection() As IEnumerable(Of SpotMap)
         Return spots
     End Function
+End Class
+
+Public Class Transform
+
+    Public Enum Operation
+        Rotate
+        Mirror
+    End Enum
+
+    <XmlAttribute> Public Property op As Operation
+    <XmlAttribute> Public Property argument As Double
+
+    Public Overrides Function ToString() As String
+        Return $"{op.Description}({argument})"
+    End Function
+
 End Class
