5e00f125ea8ad6aff533c8e8bbac2d4670f986f6
guigang xie <xie.guigang@gcmodeller.org>
2022-12-12 15:19:16 +0800

    make bugs fixed for the wiff file supports in combine row scan data

--- src/assembly/mzPackExtensions/MSImaging/Extensions.vb
+++ src/assembly/mzPackExtensions/MSImaging/Extensions.vb
@@ -1,13 +1,30 @@
 #If NET48 Then
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.sciexWiffReader
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ThermoRawFileReader
 Imports Microsoft.VisualBasic.Math
 
 Public Module Extensions
 
     Public Function CheckMatrixBaseIon(fileName As String) As (nscans As Integer, ion As Double)
-        Dim Xraw As New MSFileReader(fileName)
-        Dim n As Integer = Xraw.ScanMax
-        Dim allscans = New XRawStream(Xraw).StreamTo
+        Dim n As Integer
+        Dim allscans As mzPack
+
+        Select Case fileName.ExtensionSuffix.ToLower
+            Case "raw"
+                Dim Xraw As New MSFileReader(fileName)
+
+                n = Xraw.ScanMax
+                allscans = New XRawStream(Xraw).StreamTo
+            Case "wiff"
+                Dim wiffRaw As New WiffScanFileReader(fileName)
+                Dim println As Action(Of String) = AddressOf Console.WriteLine
+
+                allscans = wiffRaw.LoadFromWiffRaw(checkNoise:=False, println:=println)
+                n = allscans.size
+            Case Else
+                Throw New NotImplementedException(fileName.ExtensionSuffix)
+        End Select
+
         Dim basePeak As Double = allscans.MS _
             .Select(Function(a) a.GetMs.OrderByDescending(Function(i) i.intensity).FirstOrDefault) _
             .Where(Function(mzi) Not mzi Is Nothing) _
@@ -20,4 +37,4 @@ Public Module Extensions
         Return (n, basePeak)
     End Function
 End Module
-#End If  
+#End If
