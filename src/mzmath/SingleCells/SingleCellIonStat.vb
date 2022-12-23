d4fff9792c29aa7d6cbb788cdf696ef2a1f622d3
xieguigang <xie.guigang@gcmodeller.org>
2022-12-23 17:00:38 +0800

    source file renames

---
+++ src/mzmath/SingleCells/SingleCellIonStat.vb
@@ -0,0 +1,14 @@
+
+Public Class SingleCellIonStat
+
+    Public Property mz As Double
+    Public Property cells As Integer
+    Public Property density As Double
+    Public Property maxIntensity As Double
+    Public Property baseCell As String
+    Public Property Q1Intensity As Double
+    Public Property Q2Intensity As Double
+    Public Property Q3Intensity As Double
+    Public Property RSD As Double
+
+End Class
