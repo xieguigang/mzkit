00782b36118b5083ac2fc77ab265ace5ee6dc7dc
xieguigang <xie.guigang@gcmodeller.org>
2022-12-25 15:17:25 +0800

    add tissue map string of SMdata to the spot mapping data

--- src/visualize/TissueMorphology/SpatialMapping/SpotMap.vb
+++ src/visualize/TissueMorphology/SpatialMapping/SpotMap.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.Data.GraphTheory
 
@@ -20,7 +21,6 @@ Public Class SpotMap : Implements IPoint2D
     ''' <returns></returns>
     <XmlAttribute>
     Public Property flag As Integer
-
     ''' <summary>
     ''' the physical xy
     ''' </summary>
@@ -50,13 +50,21 @@ Public Class SpotMap : Implements IPoint2D
     <XmlAttribute> Public Property SMX As Integer()
     <XmlAttribute> Public Property SMY As Integer()
 
+    ''' <summary>
+    ''' tissue tag data of the SMdata
+    ''' </summary>
+    ''' <returns></returns>
+    <XmlText> Public Property TissueMorphology As String
+
     Private ReadOnly Property X As Integer Implements IPoint2D.X
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Get
             Return STX
         End Get
     End Property
 
     Private ReadOnly Property Y As Integer Implements IPoint2D.Y
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Get
             Return STY
         End Get
