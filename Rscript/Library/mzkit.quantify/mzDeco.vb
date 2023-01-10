b725481ee598dcfcc043f634588311cd99b73f85
guigang xie <xie.guigang@gcmodeller.org>
2023-01-10 13:26:12 +0800

    make improvements of the data deocd function accepted inputs

--- Rscript/Library/mzkit.quantify/mzDeco.vb
+++ Rscript/Library/mzkit.quantify/mzDeco.vb
@@ -1,48 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::fd5e50c47c702d05f5ba552d93385afe, Rscript\Library\mzkit.quantify\mzDeco.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module mzDeco
-    ' 
-    '     Function: ms1Scans, mz_deco, mz_groups
-    ' 
-    ' /********************************************************************************/
+' Module mzDeco
+' 
+'     Function: ms1Scans, mz_deco, mz_groups
+' 
+' /********************************************************************************/
 
 #End Region
 
 
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
 Imports BioNovoGene.Analytical.MassSpectrometry.Math
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports Microsoft.VisualBasic.CommandLine.Reflection
@@ -60,7 +61,9 @@ Module mzDeco
     ''' <summary>
     ''' Chromatogram data deconvolution
     ''' </summary>
-    ''' <param name="ms1"></param>
+    ''' <param name="ms1">
+    ''' a collection of the ms1 data or the mzpack raw data object
+    ''' </param>
     ''' <param name="tolerance"></param>
     ''' <returns></returns>
     <ExportAPI("mz.deco")>
@@ -111,6 +114,10 @@ Module mzDeco
             Return {}
         ElseIf ms1.GetType Is GetType(ms1_scan()) Then
             Return DirectCast(ms1, ms1_scan()).Select(Function(t) DirectCast(t, IMs1Scan))
+        ElseIf TypeOf ms1 Is mzPack Then
+            Return DirectCast(ms1, mzPack) _
+                .GetAllScanMs1 _
+                .Select(Function(t) DirectCast(t, IMs1Scan))
         Else
             Throw New NotImplementedException
         End If
