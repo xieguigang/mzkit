dd53e8f15422fbb20c2d395be2a89fec72692e5a
xieguigang <gg.xie@bionovogene.com>
2023-02-08 15:21:03 +0800

    add dev notes about SMILES structure string

--- src/metadb/SMILES/Language/ElementTypes.vb
+++ src/metadb/SMILES/Language/ElementTypes.vb
@@ -1,64 +1,96 @@
 ﻿#Region "Microsoft.VisualBasic::a73b0e76be8e497a549d3918193c4bbe, mzkit\src\metadb\SMILES\Language\ElementTypes.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 6
-    '    Code Lines: 6
-    ' Comment Lines: 0
-    '   Blank Lines: 0
-    '     File Size: 77.00 B
+' Summaries:
 
 
-    ' Enum ElementTypes
-    ' 
-    '     Close, Element, Key, Open
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 6
+'    Code Lines: 6
+' Comment Lines: 0
+'   Blank Lines: 0
+'     File Size: 77.00 B
+
+
+' Enum ElementTypes
+' 
+'     Close, Element, Key, Open
+' 
+'  
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Public Enum ElementTypes
+    ''' <summary>
+    ''' alphabet
+    ''' </summary>
     Element
+    ''' <summary>
+    ''' <see cref="Bonds"/>
+    ''' </summary>
     Key
+    ''' <summary>
+    ''' (
+    ''' </summary>
     Open
+    ''' <summary>
+    ''' )
+    ''' </summary>
     Close
+    ''' <summary>
+    ''' ##### Disconnected Structures
+    ''' 
+    ''' Disconnected compounds are written as individual structures separated by a "." (period). 
+    ''' The order in which ions or ligands are listed is arbitrary. There is no implied pairing 
+    ''' of one charge with another, nor is it necessary to have a net zero charge. If desired, 
+    ''' the SMILES of one ion may be imbedded within another as shown in the example of sodium 
+    ''' phenoxide.
+    ''' 
+    ''' Matching pairs of digits following atom specifications imply that the atoms are bonded 
+    ''' to each other. The bond may be explicit (bond symbol and/or direction preceding the ring 
+    ''' closure digit) or implicit (a nondirectional single or aromatic bond). This is true 
+    ''' whether or not the bond ends up as part of a ring.
+    ''' 
+    ''' Adjacent atoms separated by dot (.) implies that the atoms are Not bonded To Each other. 
+    ''' This Is True whether Or Not the atoms are In the same connected component.
+    ''' 
+    ''' For example, C1.C1 specifies the same molecule as CC(ethane)
+    ''' </summary>
+    Disconnected
 End Enum
