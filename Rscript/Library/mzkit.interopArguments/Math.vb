f3a05691385a6265fd4316c61756e6d4cb074f3c
guigang xie <xie.guigang@gcmodeller.org>
2023-02-17 16:34:24 +0800

    symbol renames

--- Rscript/Library/mzkit.interopArguments/Math.vb
+++ Rscript/Library/mzkit.interopArguments/Math.vb
@@ -86,21 +86,21 @@ Module Math
     ''' <summary>
     ''' 
     ''' </summary>
-    ''' <param name="val">
+    ''' <param name="adducts">
     ''' should be a collection of <see cref="MzCalculator"/> or 
     ''' a collection of <see cref="String"/> literal of the 
     ''' adducts
     ''' </param>
     ''' <param name="env"></param>
     ''' <returns></returns>
-    Public Function GetPrecursorTypes(val As Object, env As Environment) As MzCalculator()
-        Dim data As pipeline = pipeline.TryCreatePipeline(Of MzCalculator)(val, env, suppress:=True)
+    Public Function GetPrecursorTypes(adducts As Object, env As Environment) As MzCalculator()
+        Dim data As pipeline = pipeline.TryCreatePipeline(Of MzCalculator)(adducts, env, suppress:=True)
 
         If Not data.isError Then
             Return data.populates(Of MzCalculator)(env).ToArray
         End If
 
-        data = pipeline.TryCreatePipeline(Of String)(val, env, suppress:=True)
+        data = pipeline.TryCreatePipeline(Of String)(adducts, env, suppress:=True)
 
         If Not data.isError Then
             Return data.populates(Of String)(env) _
