63e7a4e0cd849a1118e3ea5b48fc949d0c3fa37b
xieguigang <xie.guigang@gcmodeller.org>
2023-03-04 07:49:44 +0800

    parse raman spectrum data file in plain text

---
+++ src/assembly/assembly/ASCII/Raman/FileReader.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Drawing
+Imports System.IO
+Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.Values
+
+Namespace ASCII.Raman
+
+    Public Module FileReader
+
+        ReadOnly schema As Dictionary(Of String, PropertyInfo)
+
+        Sub New()
+            schema = DataFramework _
+                .Schema(Of Spectroscopy)(PropertyAccess.Writeable, nonIndex:=True, primitive:=True) _
+                .ToDictionary(Function(p)
+                                  Dim [alias] = p.Value.GetCustomAttribute(Of Field)
+
+                                  If [alias] Is Nothing Then
+                                      Return p.Key
+                                  Else
+                                      Return [alias].Name
+                                  End If
+                              End Function,
+                              Function(p)
+                                  Return p.Value
+                              End Function)
+        End Sub
+
+        Public Function ParseTextFile(txt As StreamReader) As Spectroscopy
+            Dim line As Value(Of String) = ""
+            Dim data As NamedValue(Of String)
+            Dim value As Object
+            Dim raman As New Spectroscopy
+            Dim type As Type
+
+            Do While Not (line = txt.ReadLine).StringEmpty
+                data = line.GetTagValue(vbTab, trim:=True)
+
+                If data.Name.TextEquals("xydata") Then
+                    Exit Do
+                End If
+
+                If schema.ContainsKey(data.Name) Then
+                    type = schema(data.Name).PropertyType
+                Else
+                    Continue Do
+                End If
+
+                If data.Value.StringEmpty Then
+                    value = Nothing
+                Else
+                    value = Scripting.CTypeDynamic(data.Value, type)
+                End If
+
+                Call schema(data.Name).SetValue(raman, value)
+            Loop
+
+            Dim pt As PointF
+            Dim spectroscopy As New List(Of PointF)
+            Dim metadata As New Dictionary(Of String, Dictionary(Of String, String))
+            Dim information As Dictionary(Of String, String) = Nothing
+
+            Do While Not (line = txt.ReadLine).StringEmpty
+                Dim t = line.Split(vbTab)
+                pt = New PointF(Val(t(0)), Val(t(1)))
+                spectroscopy.Add(pt)
+            Loop
+
+            raman.xyData = spectroscopy.ToArray
+
+            Do While Not (line = txt.ReadLine).StringEmpty
+                If line.First = "#"c Then
+                    Continue Do
+                End If
+                If line.Value.IsPattern("\[.+\]") Then
+                    information = New Dictionary(Of String, String)
+                    metadata.Add(line, information)
+                Else
+                    data = line.GetTagValue(vbTab, trim:=True)
+                    information.Add(data.Name, data.Value)
+                End If
+            Loop
+
+            raman.Comments = metadata.TryGetValue("[Comments]")
+            raman.DetailedInformation = metadata.TryGetValue("[Detailed Information]")
+            raman.MeasurementInformation = metadata.TryGetValue("[Measurement Information]")
+
+            Return raman
+        End Function
+    End Module
+End Namespace
--- src/assembly/assembly/ASCII/Raman/Spectroscopy.vb
+++ src/assembly/assembly/ASCII/Raman/Spectroscopy.vb
@@ -1,27 +1,28 @@
 ﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
 
 Namespace ASCII.Raman
 
     Public Class Spectroscopy
 
-        Public Property Title As String
-        Public Property DataType As String
-        Public Property Origin As String
-        Public Property Owner As String
-        Public Property [Date] As String
-        Public Property Time As String
-        Public Property Spectrometer As String
-        Public Property Locale As String
-        Public Property Resolution As String
-        Public Property Reltax As String
-        Public Property Xunits As String
-        Public Property Yunits As String
-        Public Property FirstX As String
-        Public Property LastX As String
-        Public Property nPoints As Integer
-        Public Property FirstY As String
-        Public Property MaxY As String
-        Public Property MinY As String
+        <Field("TITLE")> Public Property Title As String
+        <Field("DATA TYPE")> Public Property DataType As String
+        <Field("ORIGIN")> Public Property Origin As String
+        <Field("OWNER")> Public Property Owner As String
+        <Field("DATE")> Public Property [Date] As String
+        <Field("TIME")> Public Property Time As String
+        <Field("SPECTROMETER/DATA SYSTEM")> Public Property Spectrometer As String
+        <Field("LOCALE")> Public Property Locale As String
+        <Field("RESOLUTION")> Public Property Resolution As String
+        <Field("DELTAX")> Public Property Reltax As String
+        <Field("XUNITS")> Public Property Xunits As String
+        <Field("YUNITS")> Public Property Yunits As String
+        <Field("FIRSTX")> Public Property FirstX As String
+        <Field("LASTX")> Public Property LastX As String
+        <Field("NPOINTS")> Public Property nPoints As Integer
+        <Field("FIRSTY")> Public Property FirstY As String
+        <Field("MAXY")> Public Property MaxY As String
+        <Field("MINY")> Public Property MinY As String
         Public Property xyData As PointF()
         Public Property Comments As Dictionary(Of String, String)
         Public Property DetailedInformation As Dictionary(Of String, String)
