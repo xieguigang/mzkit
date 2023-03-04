a224b508b31b3838451ff1b63986e6db8de4fcf5
xieguigang <xie.guigang@gcmodeller.org>
2023-03-04 08:16:48 +0800

    export api for read ramana specterum data

---
+++ Rscript/Library/mzkit/assembly/RamanSpectroscopy.vb
@@ -0,0 +1,25 @@
+﻿
+Imports System.IO
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ASCII.Raman
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("RamanSpectroscopy")>
+Public Module RamanSpectroscopy
+
+    <ExportAPI("readFile")>
+    Public Function readFile(<RRawVectorArgument> file As Object,
+                             Optional env As Environment = Nothing) As Object
+
+        Dim buf = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Read, env)
+
+        If buf Like GetType(Message) Then
+            Return buf.TryCast(Of Message)
+        End If
+
+        Return FileReader.ParseTextFile(New StreamReader(buf))
+    End Function
+End Module
