4636a0836a88a1fb71683be6968344a13ccffcff
xieguigang <gg.xie@bionovogene.com>
2023-03-08 11:30:19 +0800

    updates of the help documents for the molecular networking tool

--- Rscript/Library/mzkit.insilicons/MoleculeNetworking.vb
+++ Rscript/Library/mzkit.insilicons/MoleculeNetworking.vb
@@ -69,9 +69,45 @@ Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports REnv = SMRUCC.Rsharp.Runtime
 
+''' <summary>
+''' Molecular Networking (MN) is a computational strategy that may help visualization and interpretation of the complex data arising from MS analysis. 
+''' </summary>
+''' <remarks>
+''' MN is able to identify potential similarities among all MS/MS spectra within 
+''' the dataset and to propagate annotation to unknown but related molecules 
+''' (Wang et al., 2016). This approach exploits the assumption that structurally
+''' related molecules produce similar fragmentation patterns, and therefore they 
+''' should be related within a network (Quinn et al., 2017). In MN, MS/MS data 
+''' are represented in a graphical form, where each node represents an ion with 
+''' an associated fragmentation spectrum; the links among the nodes indicate 
+''' similarities of the spectra. By propagation of the structural information within
+''' the network, unknown but structurally related molecules can be highlighted
+''' and successful dereplication can be obtained (Yang et al., 2013); this may
+''' be particularly useful for metabolite and NPS identification.
+''' 
+''' MN has been implemented In different fields, particularly metabolomics And 
+''' drug discovery (Quinn et al., 2017); MN In forensic toxicology was previously
+''' used by Allard et al. (2019) For the retrospective analysis Of routine 
+''' cases involving biological sample analysis. Yu et al. (2019) also used MN 
+''' analysis For the detection Of designer drugs such As NBOMe derivatives And 
+''' they showed that unknown compounds could be recognized As NBOMe-related 
+''' substances by MN.
+''' 
+''' In the present work the Global Natural Products Social platform (GNPS) was 
+''' exploited to analyze HRMS/MS data obtained from the analysis of seizures 
+''' collected by the Italian Department of Scientific Investigation of Carabinieri 
+''' (RIS). The potential of MN to highlight And support the identification of
+''' unknown NPS belonging to chemical classes such as fentanyls And synthetic
+''' cannabinoids has been demonstrated.
+''' </remarks>
 <Package("MoleculeNetworking")>
 Module MoleculeNetworking
 
+    ''' <summary>
+    ''' makes the spectrum data its unique id reference uniqued!
+    ''' </summary>
+    ''' <param name="ions"></param>
+    ''' <returns></returns>
     <ExportAPI("uniqueNames")>
     Public Function unqiueNames(ions As PeakMs2()) As PeakMs2()
         Dim id As String() = ions.SafeQuery.Select(Function(i) i.lib_guid).uniqueNames
@@ -83,6 +119,14 @@ Module MoleculeNetworking
         Return ions
     End Function
 
+    ''' <summary>
+    ''' convert the cluster tree into the graph model
+    ''' </summary>
+    ''' <param name="tree">A cluster tree which is created via the ``tree`` function.</param>
+    ''' <param name="ions">
+    ''' the source data for create the cluster tree
+    ''' </param>
+    ''' <returns></returns>
     <ExportAPI("as.graph")>
     Public Function createGraph(tree As ClusterTree, ions As PeakMs2()) As NetworkGraph
         Dim bins As list = tree.MsBin(ions)
@@ -99,6 +143,20 @@ Module MoleculeNetworking
         Return g
     End Function
 
+    ''' <summary>
+    ''' do spectrum data clustering
+    ''' </summary>
+    ''' <param name="ions">A set of the spectrum data</param>
+    ''' <param name="mzdiff">
+    ''' the ms2 fragment mass tolerance when used for compares 
+    ''' ms2 spectrum data
+    ''' </param>
+    ''' <param name="intocutoff">
+    ''' intensity cutoff value that used for make the spectrum 
+    ''' centroid and noise cleanup
+    ''' </param>
+    ''' <param name="equals"></param>
+    ''' <returns></returns>
     <ExportAPI("tree")>
     Public Function Tree(ions As PeakMs2(),
                          Optional mzdiff As Double = 0.3,
@@ -114,7 +172,7 @@ Module MoleculeNetworking
     ''' <param name="tree"></param>
     ''' <param name="ions"></param>
     ''' <returns>
-    ''' a set of ms2 data groups, in format of guid => peakms2 vector tuples
+    ''' a set of ms2 data groups, in format of ``[guid => peakms2]`` vector tuples
     ''' </returns>
     <ExportAPI("msBin")>
     <Extension>
