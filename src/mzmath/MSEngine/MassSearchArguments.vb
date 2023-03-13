c072444494eaf090515f8d4f1dff1844fd5a60c1
xieguigang <xie.guigang@gcmodeller.org>
2023-03-13 23:24:12 +0800

    annotation arguments

---
+++ src/mzmath/MSEngine/MassSearchArguments.vb
@@ -0,0 +1,10 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
+
+Public Class MassSearchArguments
+
+    Public Property IonMode As IonModes
+    Public Property PPM As Integer
+    Public Property Adducts As String()
+    Public Property Optionals As Dictionary(Of String, String)
+
+End Class
