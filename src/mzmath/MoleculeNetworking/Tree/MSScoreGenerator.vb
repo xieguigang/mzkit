cfab0b467c29232f4fdba50e32a9c9375caf9758
xieguigang <gg.xie@bionovogene.com>
2023-03-15 13:55:27 +0800

    make improvement of the spectrum tree helper

--- src/mzmath/MoleculeNetworking/Tree/MSScoreGenerator.vb
+++ src/mzmath/MoleculeNetworking/Tree/MSScoreGenerator.vb
@@ -2,6 +2,9 @@
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports Microsoft.VisualBasic.DataMining.BinaryTree
 
+''' <summary>
+''' A score evaluator that contains the cache
+''' </summary>
 Public Class MSScoreGenerator : Inherits ComparisonProvider
 
     ReadOnly getSpectrum As Func(Of String, PeakMs2)
@@ -14,15 +17,21 @@ Public Class MSScoreGenerator : Inherits ComparisonProvider
     ''' </summary>
     ''' <param name="align"></param>
     ''' <param name="getSpectrum">
-    ''' get spectrum data by unique reference id
+    ''' get spectrum data by unique reference id[cache system was implemented in this module]
     ''' </param>
     ''' <param name="equals"></param>
     ''' <param name="gt"></param>
-    Sub New(align As AlignmentProvider, getSpectrum As Func(Of String, PeakMs2), equals As Double, gt As Double)
+    Sub New(align As AlignmentProvider, getSpectrum As Func(Of String, PeakMs2), Optional equals As Double = 1, Optional gt As Double = 0)
         Call MyBase.New(equals, gt)
 
         Me.align = align
         Me.getSpectrum = getSpectrum
+    End Sub
+
+    Public Sub Add(spectral As PeakMs2)
+        If Not cache.ContainsKey(spectral.lib_guid) Then
+            Call cache.Add(spectral.lib_guid, spectral)
+        End If
     End Sub
 
     Private Function GetSpectral(guid As String) As PeakMs2
