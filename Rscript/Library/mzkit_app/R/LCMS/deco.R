d12e3ff44c6fd6ca0e8c45b99149f493ef4800af
xieguigang <xie.guigang@gcmodeller.org>
2023-03-18 13:03:28 +0800

    Update deco.R

--- Rscript/Library/mzkit_app/R/LCMS/deco.R
+++ Rscript/Library/mzkit_app/R/LCMS/deco.R
@@ -1,3 +1,9 @@
-const run.Deconvolution = function(data_dir = "./") {
+imports "mzDeco" from "mz_quantify";
+
+#' extract peak ms1 from a set of raw data files
+#' 
+#' @param data_dir a directory path that contains multiple raw data files
+#' 
+const run.Deconvolution = function(data_dir = "./", n_threads = 8) {
 
 }
