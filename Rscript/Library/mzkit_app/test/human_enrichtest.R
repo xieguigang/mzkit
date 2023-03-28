e59c52f5ccffe7f07623f5f339ff571d87000f68
xieguigang <xie.guigang@gcmodeller.org>
2023-03-28 14:19:30 +0800

    inspect of the gsea model

--- Rscript/Library/mzkit_app/test/human_enrichtest.R
+++ Rscript/Library/mzkit_app/test/human_enrichtest.R
@@ -1,5 +1,11 @@
 require(GCModeller);
 require(mzkit);
 
+imports ["background", "GSEA"] from "gseakit";
+
 data = system.file("data/hsa_metpa.json", package = "mzkit");
-data = data |> readText() |> json_decode(typeof = "");
+data = data |> readText() |> json_decode(typeof = "metpa");
+
+print("view of the loaded background model:");
+str(data);
+
