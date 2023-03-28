6c6a13e46b257479e9b302a27beadd5ab2be70be
xieguigang <xie.guigang@gcmodeller.org>
2023-03-28 14:33:45 +0800

    run enrichment test

--- Rscript/Library/mzkit_app/test/human_enrichtest.R
+++ Rscript/Library/mzkit_app/test/human_enrichtest.R
@@ -3,9 +3,24 @@ require(mzkit);
 
 imports ["background", "GSEA"] from "gseakit";
 
+
+cid = read.csv("D:\mzkit\Rscript\demo\Mummichog\mz_annotations.csv")$hits;
+cid = unlist(strsplit(cid, "[;]\s+"));
+cid = unlist(strsplit(cid, "\s+"));
+cid = unique(cid[cid == $"C\d+"]);
+
+print(cid);
+
+
 data = system.file("data/hsa_metpa.json", package = "mzkit");
+
+print(data);
+
 data = data |> readText() |> json_decode(typeof = "metpa");
 
 print("view of the loaded background model:");
 str(data);
 
+enrich = metpa_enrich(cid, data);
+
+print(enrich, max.print = 13);
