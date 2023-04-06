a87628bb3285a01fe888798ed4fd1a4a64b24172
xieguigang <gg.xie@bionovogene.com>
2023-04-06 09:38:00 +0800

    script for create kegg reference library

--- Rscript/Library/mzkit_app/pipeline/KEGG_reference.R
+++ Rscript/Library/mzkit_app/pipeline/KEGG_reference.R
@@ -1,2 +1,20 @@
 require(mzkit);
 
+# controls of the server memory usage in the data analysis workflow.
+options(memory.load = "max");
+
+#' the spectrum tree reference library tools
+imports "spectrumTree" from "mzkit";
+
+rawdir = "E:\reference_ms\DIA\pos";
+graph_pack = `${dirname(rawdir)}/lib.${basename(rawdir)}`;
+
+const stdlib = spectrumTree::new(graph_pack, type = "Pack");
+
+for(dir in list.dirs(rawdir,recursive = FALSE)) {
+    kegg_id = basename(dir);
+
+    print(dir);
+    print(kegg_id);
+    
+}
--- Rscript/Library/mzkit_app/test/convert_sciex.R
+++ Rscript/Library/mzkit_app/test/convert_sciex.R
@@ -1,5 +1,8 @@
 require(mzkit);
 
+# controls of the server memory usage in the data analysis workflow.
+options(memory.load = "max");
+
 #' biodeep mzweb data viewer raw data file helper
 imports "mzweb" from "mzkit";
 #' raw data accessor for the mzpack data object
