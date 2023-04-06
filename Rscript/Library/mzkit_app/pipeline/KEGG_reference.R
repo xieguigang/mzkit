89eb753a662a80d56cbefe3d2a443c0f6e35b33a
xieguigang <gg.xie@bionovogene.com>
2023-04-06 10:01:01 +0800

    how to get the formula information?

--- Rscript/Library/mzkit_app/pipeline/KEGG_reference.R
+++ Rscript/Library/mzkit_app/pipeline/KEGG_reference.R
@@ -13,11 +13,19 @@ const stdlib = spectrumTree::new(graph_pack, type = "Pack");
 
 for(dir in list.dirs(rawdir,recursive = FALSE)) {
     kegg_id = basename(dir);
-    raw = open.mzpack(`${dir}/reference.mzPack`);
+    raw = open.mzpack(`${dir}/reference.mzPack`) |> ms2_peaks(tag.source = FALSE);
 
     print(dir);
     print(kegg_id);
 
     # do spectrum clustering and then get the max cluster
-    
+
+    stdlib |> spectrumTree::addBucket(
+        x = raw,
+        ignore_error = TRUE,
+        uuid = kegg_id,
+        formula = 
+    );
 }
+
+close(stdlib );
