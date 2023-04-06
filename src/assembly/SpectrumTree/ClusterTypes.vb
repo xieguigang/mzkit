2a090fe7587a03afe06492db71e15e0237a6d4bf
xieguigang <gg.xie@bionovogene.com>
2023-04-06 09:39:31 +0800

    Update ClusterTypes.vb

--- src/assembly/SpectrumTree/ClusterTypes.vb
+++ src/assembly/SpectrumTree/ClusterTypes.vb
@@ -1,6 +1,8 @@
 ﻿Public Enum ClusterTypes
     [Default]
-    Tree = [Default]
+    ' 20230406
+    ' ArgumentException: An item with the same key has already been added. Key: Tree
+    Tree ' = [Default]
     Binary
     Pack
 End Enum
