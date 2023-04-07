645c126e1cdec2a39fd9aae2166a75a430ba9aba
guigang xie <xie.guigang@gcmodeller.org>
2023-04-07 10:17:24 +0800

    read xcms rdata

---
+++ src/assembly/LoadR.NET5/ReadXcmsRData.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports SMRUCC.Rsharp.RDataSet
+Imports SMRUCC.Rsharp.RDataSet.Convertor
+Imports SMRUCC.Rsharp.RDataSet.Struct.LinkedList
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Vectorization
+
+Public Class XcmsRData
+
+    Public Property mz As Double()
+    Public Property rt As Double()
+    Public Property into As Double()
+    Public Property ms2 As Dictionary(Of String, ms2())
+
+    Private Shared Function readNumeric(root As RObject, node As String) As Double()
+        Dim query = root.LinkVisitor(node)
+        Dim car As RObject = query.value.CAR
+        Dim vec As Double() = RStreamReader.ReadNumbers(car)
+
+        Return vec
+    End Function
+
+    Public Shared Function ReadRData(buffer As Stream) As XcmsRData
+        Dim root = Reader.ParseData(buffer).object
+        Dim mz = readNumeric(root, "mz1")
+        Dim rt = readNumeric(root, "rt2")
+        Dim into = readNumeric(root, "into")
+        Dim matrix As New Dictionary(Of String, ms2())
+        Dim raw As list = ConvertToR.ToRObject(root.LinkVisitor("ms2"))
+
+        For Each name As String In raw.getNames
+            Dim vec As Double() = CLRVector.asNumeric(raw(name))
+            Dim msms As ms2() = New ms2(CInt(vec.Length / 2) - 1) {}
+            Dim tuple = vec.Split(msms.Length)
+
+            For i As Integer = 0 To msms.Length - 1
+                msms(i) = New ms2 With {
+                    .mz = tuple(0)(i),
+                    .intensity = tuple(1)(i)
+                }
+            Next
+
+            Call matrix.Add(name, msms)
+        Next
+
+        Return New XcmsRData With {
+            .mz = mz, .rt = rt, .into = into,
+            .ms2 = matrix
+        }
+    End Function
+
+End Class
--- src/assembly/LoadR.NET5/test/Program.vb
+++ src/assembly/LoadR.NET5/test/Program.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::b1935ba4ea53d18e507fbedafbb98d6c, mzkit\src\assembly\LoadR.NET5\test\Program.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 23
-    '    Code Lines: 19
-    ' Comment Lines: 0
-    '   Blank Lines: 4
-    '     File Size: 827 B
+' Summaries:
 
 
-    ' Module Program
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 23
+'    Code Lines: 19
+' Comment Lines: 0
+'   Blank Lines: 4
+'     File Size: 827 B
+
+
+' Module Program
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -59,17 +59,50 @@ Imports Microsoft.VisualBasic.My.FrameworkInternal
 Imports SMRUCC.Rsharp.RDataSet
 Imports SMRUCC.Rsharp.RDataSet.Convertor
 Imports SMRUCC.Rsharp.RDataSet.Struct.LinkedList
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
 
         FrameworkInternal.ConfigMemory(MemoryLoads.Heavy)
+        readtest2()
 
         Using buffer As Stream = "D:\mzkit\src\assembly\LoadR.NET5\PeaksMs2.rda".Open(FileMode.Open, doClear:=False, [readOnly]:=True)
             Dim data = Reader.ParseData(buffer).object.LinkVisitor("x")
             Dim list As RObject() = data.value.CAR.value.data
             Dim peaksData = list.Select(AddressOf LoadR.LoadPeakMs2).ToArray
+
+            Pause()
+        End Using
+    End Sub
+
+    Sub readtest2()
+        ' mz1, rt2, into, ms2
+        Using buffer As Stream = "D:\mzkit\src\assembly\LoadR.NET5\003_Ex2_Orbitrap_CID.mzXML.RData".Open(FileMode.Open, doClear:=False, [readOnly]:=True)
+            Dim root = Reader.ParseData(buffer).object
+            Dim data = root.LinkVisitor("mz1")
+            Dim data2 = root.LinkVisitor("rt2")
+            Dim data3 = root.LinkVisitor("into")
+            Dim mz1 As Double() = data.value.CAR.value.data
+            Dim rt2 As Double() = data2.value.CAR.value.data
+            Dim into As Double() = data3.value.CAR.value.data
+
+            Dim all = ConvertToR.ToRObject(root)
+
+            ' > ms2[["M476T1, 476.3065@0min"]]
+            ' mz intensity
+            ' [1,] 175.1938  6.712281
+            ' [2,] 417.5124 20.837078
+            ' [3,] 419.4352 10.454858
+            ' [4,] 431.6585 28.139475
+            ' [5,] 465.2776  4.005749
+            ' [6,] 467.3275  9.030666
+            ' [7,] 489.1284  8.694260
+
+            Dim test = DirectCast(all!ms2, list)
+            Dim names = test.getNames
+            Dim data_ms2 = test("M476T1, 476.3065@0min")
 
             Pause()
         End Using
