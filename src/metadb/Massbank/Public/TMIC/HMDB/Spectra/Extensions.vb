f856444913a50547d549cff47cbf1bcf99c861c8
guigang xie <xie.guigang@gcmodeller.org>
2023-04-14 14:01:11 +0800

    make bugs fixed for the header parser

--- src/metadb/Massbank/Public/TMIC/HMDB/Spectra/Extensions.vb
+++ src/metadb/Massbank/Public/TMIC/HMDB/Spectra/Extensions.vb
@@ -1,62 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::a73c972cad135f611c2b6246e07a2777, mzkit\src\metadb\Massbank\Public\TMIC\HMDB\Spectra\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 52
-    '    Code Lines: 45
-    ' Comment Lines: 0
-    '   Blank Lines: 7
-    '     File Size: 2.27 KB
+' Summaries:
 
 
-    '     Module Extensions
-    ' 
-    '         Function: ParseIonizationMode, PopulateSpectras
-    ' 
-    '     Enum IonizationModes
-    ' 
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 52
+'    Code Lines: 45
+' Comment Lines: 0
+'   Blank Lines: 7
+'     File Size: 2.27 KB
+
+
+'     Module Extensions
+' 
+'         Function: ParseIonizationMode, PopulateSpectras
+' 
+'     Enum IonizationModes
+' 
+' 
+'  
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -77,23 +77,22 @@ Namespace TMIC.HMDB.Spectra
         <Extension>
         Public Iterator Function PopulateSpectras(repository As String) As IEnumerable(Of NamedValue(Of SpectraFile))
             For Each file As String In repository.EnumerateFiles("*.xml")
-                Dim header = file.IterateAllLines _
-                    .Take(2) _
-                    .Last
-                Dim rootNodeName$ = header _
-                    .Match("[<]\S+") _
-                    .Trim("<"c) _
-                    .ToLower
+                Dim header = file.IterateAllLines.Take(2).Last
+                Dim rootNodeName$ = header.Trim("<"c, ">"c).ToLower
+                Dim fileName As String = file.BaseName
+                Dim spectra As SpectraFile
 
                 Select Case rootNodeName
-                    Case "c-ms" : Yield New NamedValue(Of SpectraFile)(file.FileName, file.LoadXml(Of CMS))
-                    Case "ms-ms" : Yield New NamedValue(Of SpectraFile)(file.FileName, file.LoadXml(Of MSMS))
-                    Case "ei-ms" : Yield New NamedValue(Of SpectraFile)(file.FileName, file.LoadXml(Of EIMS))
-                    Case "nmr-one-d" : Yield New NamedValue(Of SpectraFile)(file.FileName, file.LoadXml(Of NMR1D))
-                    Case "nmr-two-d" : Yield New NamedValue(Of SpectraFile)(file.FileName, file.LoadXml(Of NMR2D))
+                    Case "c-ms" : spectra = file.LoadXml(Of CMS)
+                    Case "ms-ms" : spectra = file.LoadXml(Of MSMS)
+                    Case "ei-ms" : spectra = file.LoadXml(Of EIMS)
+                    Case "nmr-one-d" : spectra = file.LoadXml(Of NMR1D)
+                    Case "nmr-two-d" : spectra = file.LoadXml(Of NMR2D)
                     Case Else
                         Throw New NotImplementedException($"{rootNodeName} @ {file}")
                 End Select
+
+                Yield New NamedValue(Of SpectraFile)(fileName, spectra)
             Next
         End Function
 
