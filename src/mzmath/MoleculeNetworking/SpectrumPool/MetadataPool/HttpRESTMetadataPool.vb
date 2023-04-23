ba1325d7d3c795a523a36d37a1074f021a6cd933
guigang xie <xie.guigang@gcmodeller.org>
2023-04-23 15:08:11 +0800

    fix of the missing property data

--- src/mzmath/MoleculeNetworking/SpectrumPool/MetadataPool/HttpRESTMetadataPool.vb
+++ src/mzmath/MoleculeNetworking/SpectrumPool/MetadataPool/HttpRESTMetadataPool.vb
@@ -124,7 +124,8 @@ Namespace PoolData
                 .organism = fetch!organism,
                 .rt = Val(fetch!rt),
                 .sample_source = fetch!biosample,
-                .source_file = fetch!filename
+                .source_file = fetch!filename,
+                .instrument = fetch!instrument
             }
         End Function
 
