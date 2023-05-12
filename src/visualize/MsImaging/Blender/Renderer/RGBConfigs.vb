9a06c5578cb703d74b486a0c333cb9432c8a9c77
guigang xie <xie.guigang@gcmodeller.org>
2023-05-12 18:09:26 +0800

    configs of the rgb layers

---
+++ src/visualize/MsImaging/Blender/Renderer/RGBConfigs.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace Blender
+
+    Public Class RGBConfigs
+
+        Public Property R As NamedValue(Of Double)
+        Public Property G As NamedValue(Of Double)
+        Public Property B As NamedValue(Of Double)
+
+        Public Function GetJSON() As String
+            Dim json As New Dictionary(Of String, Dictionary(Of String, String))
+
+            json!r = New Dictionary(Of String, String) From {{"m/z", R.Value}, {"annotation", R.Name}}
+            json!g = New Dictionary(Of String, String) From {{"m/z", G.Value}, {"annotation", G.Name}}
+            json!b = New Dictionary(Of String, String) From {{"m/z", B.Value}, {"annotation", B.Name}}
+
+            Return json.GetJson
+        End Function
+
+    End Class
+End Namespace
