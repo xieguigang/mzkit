a7394e1ed6c3412c4cf900c793e29dcbc69e8db2
xieguigang <xie.guigang@gcmodeller.org>
2023-05-19 11:11:33 +0800

    try to make bugs fixed for the reference name parser

--- src/mzmath/TargetedMetabolomics/Contents/ContentTable.vb
+++ src/mzmath/TargetedMetabolomics/Contents/ContentTable.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::87fd1576a49ede7a0620995ae30b64e9, mzkit\src\mzmath\TargetedMetabolomics\Contents\ContentTable.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 122
-    '    Code Lines: 74
-    ' Comment Lines: 33
-    '   Blank Lines: 15
-    '     File Size: 4.62 KB
+' Summaries:
 
 
-    '     Class ContentTable
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetIS, GetStandards, hasDefined, hasISDefined, StripMaxCommonNames
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 122
+'    Code Lines: 74
+' Comment Lines: 33
+'   Blank Lines: 15
+'     File Size: 4.62 KB
+
+
+'     Class ContentTable
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetIS, GetStandards, hasDefined, hasISDefined, StripMaxCommonNames
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -158,7 +158,7 @@ Namespace Content
 
             If names.All(Function(str) Char.IsDigit(str.First)) Then
                 names = names _
-                    .Select(Function(str) $"L{str}") _
+                    .Select(Function(str) $"L{str.Match("\d+")}") _
                     .ToArray
             End If
 
