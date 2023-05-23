0b221d74dc99c12f1344351063be76e574d2774d
xieguigang <xie.guigang@gcmodeller.org>
2023-05-23 10:07:00 +0800

    update generic function

--- Rscript/Library/mzkit_app/@export/NMR.d.ts
+++ Rscript/Library/mzkit_app/@export/NMR.d.ts
@@ -1,7 +1,6 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
 // ref=mzkit.NMRTool@mzkit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// ref=mzkit.plotNMR@mzplot, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
  * 
@@ -24,12 +23,6 @@ declare namespace NMR {
    /**
    */
    function nmr_dft(fidData: object): object;
-   /**
-     * @param size default value Is ``'3600,2400'``.
-     * @param padding default value Is ``'padding: 200px 400px 300px 100px'``.
-     * @param env default value Is ``null``.
-   */
-   function plot_nmr(nmr: object, size?: any, padding?: any, env?: object): any;
    module read {
       /**
       */
