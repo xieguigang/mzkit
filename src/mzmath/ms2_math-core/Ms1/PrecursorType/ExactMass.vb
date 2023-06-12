dc39af341a9131adae977dabe28416927368e63b
xieguigang <xie.guigang@gcmodeller.org>
2023-06-12 14:48:49 +0800

    try to handling of the missing ms1 precursor type symbols

--- src/mzmath/ms2_math-core/Ms1/PrecursorType/ExactMass.vb
+++ src/mzmath/ms2_math-core/Ms1/PrecursorType/ExactMass.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::ad183c7f7cf2fb1e5bb121bd306c8ee8, mzkit\src\mzmath\ms2_math-core\Ms1\PrecursorType\ExactMass.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 145
-    '    Code Lines: 112
-    ' Comment Lines: 14
-    '   Blank Lines: 19
-    '     File Size: 4.80 KB
+' Summaries:
 
 
-    '     Module ExactMass
-    ' 
-    '         Function: Eval, Mul, Weight
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 145
+'    Code Lines: 112
+' Comment Lines: 14
+'   Blank Lines: 19
+'     File Size: 4.80 KB
+
+
+'     Module ExactMass
+' 
+'         Function: Eval, Mul, Weight
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -83,6 +83,8 @@ Namespace Ms1.PrecursorType
             {"C2H3O", C * 2 + H * 3 + O},
             {"O", O},
             {"P", P},
+            {"OH", O + H},
+            {"CO2", C + O * 2},
             {"Methylcarbonyl", 43.018},
             {"Propyldioxy", 75.045},
             {"C12H20O9", 308.111},
@@ -92,10 +94,12 @@ Namespace Ms1.PrecursorType
             {"N", N},
             {"Na", 22.98976928},
             {"NH4", N + H * 4},
+            {"NH3", N + H * 3},
             {"K", 39.0983},
             {"F", 18.998},
             {"Li", 6.941},
             {"H2O", H2O},
+            {"HCO3", H + C + O * 3},
             {"ACN", 41.04746},      ' Acetonitrile (CH3CN)
             {"CH3OH", C + H * 3 + O + H},
             {"C2H3O2", C * 2 + H * 3 + O * 2},     ' Acetate
