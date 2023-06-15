09a9892951b0852864dd394fb3cbe5e0fcb17116
xieguigang <xie.guigang@gcmodeller.org>
2023-06-15 09:28:05 +0800

    add function comment documentation

--- Rscript/Library/mzkit_app/R/pubchem/graph_json.R
+++ Rscript/Library/mzkit_app/R/pubchem/graph_json.R
@@ -1,5 +1,16 @@
 require(JSON);
 
+#' Parse the pugview xml as the metabolite data object
+#'
+#' @param dataXml the xml content of the pugview data object
+#' @param process a callable function that used for additional object processing, example like
+#'    attach a additional database cross reference id to the xrefs of the generated pugview 
+#'    metabolite data object
+#' @param extensionCache a cache dir for save the extension data of the current metabolite object,
+#'    example as pathway query, reaction query, and reaction query.
+#'
+#' @return A generated metabolite object that could be used for dump well formatted json
+#'
 const pubchem_graphjson = function(dataXml, process = NULL, extensionCache = "./.cache/extdata/") {
     let pugView = as.list(metadata.pugView(read.pugView(dataXml)));
     let data = {
