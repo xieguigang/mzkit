4976fdbca5ebeccda09dece56f3b56ac9d1394d0
xieguigang <gg.xie@bionovogene.com>
2023-07-05 14:11:10 +0800

    create smiles graph?

---
+++ src/metadb/FormulaSearch.Extensions/SMILESGraph.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.BioDeep.Chemoinformatics.SMILES
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Public Module SMILESGraph
+
+    ''' <summary>
+    ''' convert the smiles chemical formula graph data to a network graph object
+    ''' </summary>
+    ''' <param name="f"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function AsGraph(f As ChemicalFormula) As NetworkGraph
+
+
+    End Function
+
+End Module
