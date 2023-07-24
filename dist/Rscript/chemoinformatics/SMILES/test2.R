0257af877cf0323acab5970b27c2ab0b2402be4f
guigang xie <xie.guigang@gcmodeller.org>
2023-07-24 14:22:28 +0800

    try to test for the parser error

---
+++ dist/Rscript/chemoinformatics/SMILES/test2.R
@@ -0,0 +1,9 @@
+require(mzkit);
+imports "SMILES" from "mzkit";
+imports "formula" from "mzkit";
+
+const debug = "C(C=4)(=O)c(c3OC4c(c5)ccc(c5)O)c(O)cc(c3)OC(C(O)1)OC(COC(C2O)OCC2(CO)O)C(O)C1O";
+const g = SMILES::parse(debug);
+
+print(as.formula(g));
+print(SMILES::links(smiles));
