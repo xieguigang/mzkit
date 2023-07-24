fe1ec35a0886b744a61e769cd17cd497b39da321
guigang xie <xie.guigang@gcmodeller.org>
2023-07-24 11:32:38 +0800

    fix of the data symbol reference

---
+++ src/metadb/SMILES/Embedding/SmilesAtom.vb
@@ -0,0 +1,14 @@
+﻿Namespace Embedding
+
+    ''' <summary>
+    ''' A csv table represents of the smiles atom data
+    ''' </summary>
+    Public Class SmilesAtom
+        Public Property id As String
+        Public Property atom As String
+        Public Property group As String
+        Public Property ion_charge As Integer
+        Public Property links As Integer
+        Public Property connected As String
+    End Class
+End Namespace
