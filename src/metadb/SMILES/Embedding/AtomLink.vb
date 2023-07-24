d05939c207c6410f345be9af378f45448406b745
guigang xie <xie.guigang@gcmodeller.org>
2023-07-24 13:18:41 +0800

    create graph embedding vector

---
+++ src/metadb/SMILES/Embedding/AtomLink.vb
@@ -0,0 +1,11 @@
+﻿Namespace Embedding
+
+    Public Class AtomLink
+
+        Public Property atom1 As String
+        Public Property atom2 As String
+        Public Property score As Double
+        Public Property vertex As Dictionary(Of String, String())
+
+    End Class
+End Namespace
--- src/metadb/SMILES/Embedding/Embedding.vb
+++ src/metadb/SMILES/Embedding/Embedding.vb
@@ -1,4 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
 Namespace Embedding
 
@@ -28,6 +31,54 @@ Namespace Embedding
                 }
 
                 Yield atom
+            Next
+        End Function
+
+        <Extension>
+        Public Iterator Function GraphEmbedding(g As ChemicalFormula, Optional kappa As Double = 1) As IEnumerable(Of AtomLink)
+            Dim links = g.AllBonds _
+                .GroupBy(Function(l)
+                             Return New String() {l.U.group, l.V.group} _
+                                .OrderBy(Function(v) v) _
+                                .JoinBy("|")
+                         End Function) _
+                .ToArray
+            Dim v0 As New Vector(links.Length)
+            Dim vk As New Vector(links.Length)
+
+            For i As Integer = 0 To links.Length - 1
+                Dim group As IGrouping(Of String, ChemicalKey) = links(i)
+
+                v0.Array(i) = group.Count
+                vk.Array(i) = (-kappa * New Vector(group.Select(Function(a) CDbl(a.bond)))).Exp().Sum
+            Next
+
+            Dim sgv As Double() = (vk / v0) ^ (1 / kappa)
+
+            For i As Integer = 0 To links.Length - 1
+                Dim t = links(i).Key.Split("|"c)
+                Dim gv As New Dictionary(Of String, String())
+
+                For Each v As ChemicalElement In links(i) _
+                    .Select(Function(l) {l.U, l.V}) _
+                    .IteratesALL _
+                    .GroupBy(Function(vi) vi.label) _
+                    .Select(Function(vg) vg.First)
+
+                    gv(v.label) = ChemicalElement _
+                        .GetConnection(g, v) _
+                        .Select(Function(a)
+                                    Return $"{CInt(a.keys)}({a.Item2.group})"
+                                End Function) _
+                        .ToArray
+                Next
+
+                Yield New AtomLink With {
+                    .atom1 = t(0),
+                    .atom2 = t(1),
+                    .score = sgv(i),
+                    .vertex = gv
+                }
             Next
         End Function
     End Module
