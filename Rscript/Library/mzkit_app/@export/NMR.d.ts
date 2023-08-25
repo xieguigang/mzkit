89e9b4023aae4fb793232b7c456be750524261f4
xieguigang <xie.guigang@gcmodeller.org>
2023-08-25 17:45:41 +0800

    run msbuild

--- Rscript/Library/mzkit_app/@export/NMR.d.ts
+++ Rscript/Library/mzkit_app/@export/NMR.d.ts
@@ -1,10 +1,14 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
 //    imports "NMR" from "mzkit";
+//    imports "NMR" from "mzplot";
 //
 // ref=mzkit.NMRTool@mzkit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// ref=mzkit.plotNMR@mzplot, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
+ * 
+ * plot NMR spectrum data
  * 
 */
 declare namespace NMR {
@@ -25,6 +29,12 @@ declare namespace NMR {
    /**
    */
    function nmr_dft(fidData: object): object;
+   /**
+     * @param size default value Is ``'3600,2400'``.
+     * @param padding default value Is ``'padding: 200px 400px 300px 100px'``.
+     * @param env default value Is ``null``.
+   */
+   function plot_nmr(nmr: object, size?: any, padding?: any, env?: object): any;
    module read {
       /**
       */
