adf1625e910e361093c2f35f61056b463f3df824
guigang xie <xie.guigang@gcmodeller.org>
2023-09-07 09:32:38 +0800

    fix of the syntax error

--- src/metadb/Lipidomics/FacadeLipidParser.vb
+++ src/metadb/Lipidomics/FacadeLipidParser.vb
@@ -1,45 +1,44 @@
-﻿Imports System.Collections.Generic
+﻿Public Class FacadeLipidParser
+    Implements ILipidParser
+    Private ReadOnly map As New Dictionary(Of String, List(Of ILipidParser))()
 
+    Public ReadOnly Property Target As String = String.Empty Implements ILipidParser.Target
 
-Public Class FacadeLipidParser
-        Implements ILipidParser
-        Private ReadOnly map As Dictionary(Of String, List(Of ILipidParser)) = New Dictionary(Of String, List(Of ILipidParser))()
+    Public Function Parse(lipidStr As String) As ILipid Implements ILipidParser.Parse
+        Dim key = lipidStr.Split()(0)
+        Dim parsers As List(Of ILipidParser) = Nothing, lipid As ILipid = Nothing
 
-        Public ReadOnly Property Target As String = String.Empty Implements ILipidParser.Target
+        If map.TryGetValue(key, parsers) Then
+            For Each parser In parsers
 
-        Public Function Parse(lipidStr As String) As ILipid Implements ILipidParser.Parse
-            Dim key = lipidStr.Split()(0)
-            Dim parsers As List(Of ILipidParser) = Nothing, lipid As ILipid = Nothing
+                lipid = TryCast(parser.Parse(lipidStr), ILipid)
 
-            If map.TryGetValue(key, parsers) Then
-                For Each parser In parsers
+                If lipid IsNot Nothing Then
+                    Return lipid
+                End If
+            Next
+        End If
+        Return Nothing
+    End Function
 
-                    If CSharpImpl.__Assign(lipid, TryCast(parser.Parse(lipidStr), ILipid)) IsNot Nothing Then
-                        Return lipid
-                    End If
-                Next
-            End If
-            Return Nothing
-        End Function
+    Public Sub Add(parser As ILipidParser)
+        If Not map.ContainsKey(parser.Target) Then
+            map.Add(parser.Target, New List(Of ILipidParser)())
+        End If
+        map(parser.Target).Add(parser)
+    End Sub
 
-        Public Sub Add(parser As ILipidParser)
-            If Not map.ContainsKey(parser.Target) Then
-                map.Add(parser.Target, New List(Of ILipidParser)())
-            End If
-            map(parser.Target).Add(parser)
-        End Sub
+    Public Sub Remove(parser As ILipidParser)
+        If map.ContainsKey(parser.Target) Then
+            map(parser.Target).Remove(parser)
+        End If
+    End Sub
 
-        Public Sub Remove(parser As ILipidParser)
-            If map.ContainsKey(parser.Target) Then
-                map(parser.Target).Remove(parser)
-            End If
-        End Sub
-
-        Public Shared ReadOnly Property [Default] As ILipidParser
-            Get
-                If defaultField Is Nothing Then
-                    Dim parser = New FacadeLipidParser()
-                    Call New List(Of ILipidParser) From {
+    Public Shared ReadOnly Property [Default] As ILipidParser
+        Get
+            If defaultField Is Nothing Then
+                Dim parser = New FacadeLipidParser()
+                Call New ILipidParser() {
                         New BMPLipidParser(),
                         New CLLipidParser(),
                         New DGLipidParser(),
@@ -92,14 +91,13 @@ Public Class FacadeLipidParser
                         New DGd5LipidParser(),
                         New TGd5LipidParser(),
                         New CEd7LipidParser()
-
-                    }.ForEach(New Action(Of ILipidParser)(AddressOf parser.Add))
-                    defaultField = parser
-                End If
-                Return defaultField
-            End Get
-        End Property
-        Private Shared defaultField As ILipidParser
+                    }.ForEach(Sub(par, nil) parser.Add(par))
+                defaultField = parser
+            End If
+            Return defaultField
+        End Get
+    End Property
+    Private Shared defaultField As ILipidParser
 
 End Class
 
