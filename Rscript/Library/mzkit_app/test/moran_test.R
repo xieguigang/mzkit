90b7afcf82b3657939da2d0fbdfb2136fc697710
xieguigang <xie.guigang@gcmodeller.org>
2023-09-27 11:35:01 +0800

    make code style improvements

--- Rscript/Library/mzkit_app/test/moran_test.R
+++ Rscript/Library/mzkit_app/test/moran_test.R
@@ -5,7 +5,7 @@ imports "MsImaging" from "mzplot";
 
 const rawdata = open.mzpack("D:\biodeep\biodeepdb_v3\spatial\msi_conv\test\rotate-gan-4.mzPack");
 const index = MsImaging::viewer(rawdata);
-const target = MSIlayer(index,  611.9421, tolerance = "da:0.05");
+const target = MSIlayer(index,  650.4379, tolerance = "da:0.05");
 
 str(as.list(ionStat(target)));
 
