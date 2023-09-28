69baa7357db1f2e54e4be5fc7b939e50d268d393
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-09-28 08:31:41 +0800

    make improvements of the ruler layout

--- src/visualize/MsImaging/Blender/Ruler.vb
+++ src/visualize/MsImaging/Blender/Ruler.vb
@@ -1,62 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::1e7b5eecc046a407e3891290b6392553, mzkit\src\visualize\MsImaging\Blender\Ruler.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 60
-    '    Code Lines: 49
-    ' Comment Lines: 0
-    '   Blank Lines: 11
-    '     File Size: 2.36 KB
+' Summaries:
 
 
-    '     Class Ruler
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Sub: DrawOnCanvas, DrawOnImage
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 60
+'    Code Lines: 49
+' Comment Lines: 0
+'   Blank Lines: 11
+'     File Size: 2.36 KB
+
+
+'     Class Ruler
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Sub: DrawOnCanvas, DrawOnImage
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports System.Drawing.Drawing2D
 Imports System.Drawing.Text
+Imports System.Runtime.InteropServices
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.MIME.Html.CSS
@@ -107,6 +108,21 @@ Namespace Blender
             End If
         End Function
 
+        Private Sub layout(rect As Rectangle, rulerWidth As Double, bottom As Double,
+                           <Out> ByRef left As PointF,
+                           <Out> ByRef right As PointF)
+
+            Dim offsetX As Double = rect.Width * 0.05
+            Dim padding As Double = 12.5
+
+            If offsetX < padding Then
+                offsetX = padding
+            End If
+
+            left = New PointF(rect.Left + offsetX, bottom)
+            right = New PointF(left.X + rulerWidth, bottom)
+        End Sub
+
         Public Sub DrawOnCanvas(g As IGraphics, dimsize As Size, rect As Rectangle, color As Color, resolution As Double)
             ' width drawing on the canvas
             Dim rulerWidth As Double = 0
@@ -115,9 +131,11 @@ Namespace Blender
             ' width of the ruler standards for
             Dim physical As String = eval(rect, dimsize, resolution, rulerWidth).ToString("F2") & " um"
             Dim fontsize As SizeF = g.MeasureString(physical, font)
+            Dim left, right As PointF
             Dim bottom As Double = rect.Bottom - fontsize.Height * 2
-            Dim left As New PointF(rect.Left + rect.Width * 0.05, bottom)
-            Dim right As New PointF(left.X + rulerWidth, bottom)
+
+            Call layout(rect, rulerWidth, bottom, left, right)
+
             Dim center As New PointF(left.X + (rulerWidth - fontsize.Width) / 2, bottom + 10)
 
 #Disable Warning
