272bbed3607e895898cc4c3051857e9389f4f553
guigang xie <xie.guigang@gcmodeller.org>
2023-10-09 09:38:44 +0800

    split the source function for the new algorithm

---
+++ src/mzmath/Mummichog/Annealing/ContextFitness.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF
+
+Public Class ContextFitness : Implements Fitness(Of AnnotationSet)
+
+    Public ReadOnly Property Cacheable As Boolean Implements Fitness(Of AnnotationSet).Cacheable
+        Get
+            Return True
+        End Get
+    End Property
+
+    Dim background As NamedValue(Of NetworkGraph)()
+
+    Public Function Calculate(chromosome As AnnotationSet, parallel As Boolean) As Double Implements Fitness(Of AnnotationSet).Calculate
+
+    End Function
+End Class
