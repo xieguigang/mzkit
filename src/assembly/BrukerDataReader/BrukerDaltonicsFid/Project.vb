8bff0b484b0565b3e4a6e0f535cfde2c025617ec
谢桂纲 <gg.xie@bionovogene.com>
2023-10-26 10:06:29 +0800

    load project data

--- src/assembly/BrukerDataReader/BrukerDaltonicsFid/Project.vb
+++ src/assembly/BrukerDataReader/BrukerDaltonicsFid/Project.vb
@@ -9,8 +9,22 @@ Public Class Project
     Public Property AnalysisParameter As AnalysisParameter
 
     Public Shared Function FromResultFolder(dir As String) As Project
+        Dim acqu = PropertyFileReader.ReadData($"{dir}/acqu".OpenReader).ToArray
+        Dim acqus = PropertyFileReader.ReadData($"{dir}/acqus".OpenReader).ToArray
+        Dim method As AnalysisMethod = $"{dir}/Analysis.FAmethod".LoadXml(Of AnalysisMethod)
+        Dim parms As AnalysisParameter = $"{dir}/AnalysisParameter.xml".LoadXml(Of AnalysisParameter)
+        Dim pdataList As New List(Of pdata)
+
+        For Each pdatadir As String In $"{dir}/pdata".ListDirectory
+            Call pdataList.Add(BrukerDataReader.pdata.LoadFolder(pdatadir))
+        Next
+
         Return New Project With {
-            .sptype = $"{dir}/sptype".ReadAllText
+            .sptype = $"{dir}/sptype".ReadAllText,
+            .acqu = acqu,
+            .acqus = acqus,
+            .AnalysisParameter = parms,
+            .pdata = pdataList.ToArray
         }
     End Function
 End Class
