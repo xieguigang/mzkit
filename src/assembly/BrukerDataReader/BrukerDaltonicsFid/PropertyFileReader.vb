9c4315819158f3712945a7028da7671551062cf6
谢桂纲 <gg.xie@bionovogene.com>
2023-10-26 09:36:23 +0800

    make improvements of the text parser helpers

---
+++ src/assembly/BrukerDataReader/BrukerDaltonicsFid/PropertyFileReader.vb
@@ -0,0 +1,9 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Public Class PropertyFileReader
+
+    Public Function ReadData(file As StreamReader) As NamedValue(Of String())
+
+    End Function
+End Class
