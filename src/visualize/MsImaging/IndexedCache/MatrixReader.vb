4bddaf963766789ff00239b9b7e2e0260b7066a1
guigang xie <xie.guigang@gcmodeller.org>
2023-11-14 17:05:17 +0800

    get spatial MSI layer

---
+++ src/visualize/MsImaging/IndexedCache/MatrixReader.vb
@@ -0,0 +1,37 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells.Deconvolute
+Imports System.IO
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Text
+Imports HeatMapPixel = Microsoft.VisualBasic.Imaging.Pixel
+Imports System.Drawing
+
+''' <summary>
+''' A spatial matrix reader
+''' </summary>
+Public Class MatrixReader
+
+    ReadOnly m As MzMatrix
+    ReadOnly index As BlockSearchFunction(Of (mz As Double, Integer))
+    ReadOnly mzdiff As Tolerance
+
+    Sub New(m As MzMatrix)
+        Me.m = m
+        Me.mzdiff = Tolerance.ParseScript(m.tolerance)
+        Me.index = m.mz.CreateMzIndex
+    End Sub
+
+    Public Function GetLayer(mz As Double, Optional dims As Size = Nothing) As SingleIonLayer
+        Dim spots = MzMatrix.GetLayer(Of PixelData)(m, mz, mzdiff, index)
+        Dim layer As New SingleIonLayer With {
+            .DimensionSize = dims,
+            .IonMz = mz.ToString,
+            .MSILayer = spots.ToArray
+        }
+
+        Return layer
+    End Function
+End Class
