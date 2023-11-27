07b81f99a6370b67e7cb4d10d2d4de1fd0b09672
guigang xie <xie.guigang@gcmodeller.org>
2023-11-27 14:27:41 +0800

    construct the metabolite data

--- dist/workbench/kegg.R
+++ dist/workbench/kegg.R
@@ -4,11 +4,37 @@ require(GCModeller);
 require(mzkit);
 
 imports "pubchem_kit" from "mzkit";
+imports "formula" from "mzkit";
+imports "repository" from "kegg_kit";
 
 const hits = read.webquery("E:\\PubChem_compound_text_kegg.xml");
 
 for(metabo in hits) {
     metabo = as.list(metabo);
     str(metabo);
+    # stop();
+
+    let names = metabo$cmpdsynonym$cmpdsynonym;
+
+    print(names);
+
+    let kegg_id = names[names == $"C\d{5}"];
+    let name = metabo$cmpdname;
+    let formula = metabo$mf; 
+
+    print(kegg_id);
+
+    let kegg_compound = repository::compound(
+        entry = kegg_id,
+        name = name,
+        formula = formula,
+        exactMass = formula::eval(formula),         
+        DBLinks = data.frame(
+        
+        )
+    );
+
+    str(xml(kegg_compound));
+
     stop();
 }
