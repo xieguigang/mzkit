675eb3e67ad313e4c8dd44c9ac6a9b030b4a849c
guigang xie <xie.guigang@gcmodeller.org>
2023-11-27 13:50:01 +0800

    Update kegg.R

--- dist/workbench/kegg.R
+++ dist/workbench/kegg.R
@@ -3,3 +3,11 @@
 require(GCModeller);
 require(mzkit);
 
+imports "pubchem_kit" from "mzkit";
+
+const hits = read.webquery("E:\\PubChem_compound_text_kegg.xml");
+
+for(metabo in hits) {
+    metabo = as.list(metabo);
+    str(metabo);
+}
