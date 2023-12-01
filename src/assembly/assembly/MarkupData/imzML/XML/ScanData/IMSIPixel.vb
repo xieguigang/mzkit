6f16708703a9b71d8bbead5c8eadbda94122b5f7
guigang xie <xie.guigang@gcmodeller.org>
2023-12-01 09:35:51 +0800

    split source file into multiple parts

---
+++ src/assembly/assembly/MarkupData/imzML/XML/ScanData/IMSIPixel.vb
@@ -0,0 +1,20 @@
+﻿Namespace MarkupData.imzML
+
+    Public Interface IMSIPixel
+
+        ReadOnly Property x As Integer
+        ReadOnly Property y As Integer
+        ReadOnly Property intensity As Double
+
+    End Interface
+
+    ''' <summary>
+    ''' a unify model of imzml image pixel scan data
+    ''' </summary>
+    Public Interface ImageScan
+
+        Property MzPtr As ibdPtr
+        Property IntPtr As ibdPtr
+
+    End Interface
+End Namespace
