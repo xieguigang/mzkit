25ed1a7916a26884228b6edf690d289d81e06fb7
guigang xie <xie.guigang@gcmodeller.org>
2023-12-13 09:51:35 +0800

    inspect of the rawdata

--- dist/workbench/ms_imaging/inspect_rawdata.R
+++ dist/workbench/ms_imaging/inspect_rawdata.R
@@ -3,6 +3,7 @@ require(mzkit);
 imports "chromatogram" from "mzkit";
 imports "visual" from "mzplot";
 
+# row scan raw data
 let rawdata = open.mzpack("E:\\01183.mzpack");
 let msdata = [rawdata]::MS;
 
@@ -15,16 +16,12 @@ print(max(rt));
 
 let rowdata = data.frame(rt, into = TIC);
 rowdata = rowdata[order(rt), ];
-rowdata = rowdata[rowdata$rt between [max(rt) * 0.35, max(rt) *0.65], ];
-rowdata[, "relative"] = rowdata$into / max(rowdata$into);
+# rowdata = rowdata[rowdata$rt between [max(rt) * 0.35, max(rt) *0.65], ];
+# rowdata[, "relative"] = rowdata$into / max(rowdata$into);
 
 print(rowdata, max.print = 6);
 
 write.csv(rowdata, file = `${@dir}/data_TIC.csv`);
 
-rowdata = as.chromatogram(rowdata$rt, rowdata$into);
 
-bitmap(file = `${@dir}/plot_TIC.png`) {
-    plot(rowdata);
-}
-
+print(diff(rowdata$rt), max.print = 100000);
