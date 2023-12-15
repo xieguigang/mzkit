bd32253ac379e4237b30a38905783d54c38be026
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-15 23:53:56 +0800

    print table

--- src/mzmath/Oligonucleotide_MS/OligonucleotideComposition/OligonucleotideCompositionOutput.vb
+++ src/mzmath/Oligonucleotide_MS/OligonucleotideComposition/OligonucleotideCompositionOutput.vb
@@ -1,55 +1,47 @@
 ﻿Imports System.IO
-Imports System.IO
-Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.TablePrinter
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.TablePrinter.Flags
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
 Imports Microsoft.VisualBasic.Linq
-Imports SMRUCC.Rsharp.Interpreter
-Imports SMRUCC.Rsharp.Runtime.Components
-Imports SMRUCC.Rsharp.Runtime.Internal.Object
-Imports SMRUCC.Rsharp.Runtime.Internal.[Object].Converts
-Imports SMRUCC.Rsharp.Runtime.Interop
-Imports stdNum = System.Math
 
 Public Class OligonucleotideCompositionOutput
 
     ''' <summary>
     ''' 1
     ''' </summary>
-    Public Property ObservedMass As Double
+    <Column("Observed Mass")> Public Property ObservedMass As Double
     ''' <summary>
     ''' 2
     ''' </summary>
-    Public Property TheoreticalMass As Double
+    <Column("Theoretical Mass")> Public Property TheoreticalMass As Double
     ''' <summary>
     ''' 3
     ''' </summary>
-    Public Property ErrorPpm As Double
+    <Column("Error (ppm)")> Public Property ErrorPpm As Double
     ''' <summary>
     ''' 4
     ''' </summary>
-    Public Property OfpA As Integer
+    <Column("# of pA")> Public Property OfpA As Integer
     ''' <summary>
     ''' 5
     ''' </summary>
-    Public Property OfpG As Integer
+    <Column("# of pG")> Public Property OfpG As Integer
     ''' <summary>
     ''' 6
     ''' </summary>
-    Public Property OfpC As Integer
+    <Column("# of pC")> Public Property OfpC As Integer
     ''' <summary>
     ''' 7
     ''' </summary>
-    Public Property OfpV As Integer
+    <Column("# of pV")> Public Property OfpV As Integer
     ''' <summary>
     ''' 8
     ''' </summary>
-    Public Property Modification As String
+    <Column("Modification")> Public Property Modification As String
     ''' <summary>
     ''' 10
     ''' </summary>
-    Public Property OfBases As Integer
+    <Column("# of Bases")> Public Property OfBases As Integer
 
     Friend Sub SetBaseNumber(i As Integer, n As Integer)
         Select Case i
@@ -77,7 +69,12 @@ Public Class OligonucleotideCompositionOutput
         )
 
         For Each hit As OligonucleotideCompositionOutput In outputs
-
+            Call content.AppendLine(
+                hit.ObservedMass, hit.TheoreticalMass, hit.ErrorPpm,
+                hit.OfpA, hit.OfpG, hit.OfpC, hit.OfpV, hit.Modification,
+                "",
+                hit.OfBases
+            )
         Next
 
         Call ConsoleTableBuilder _
