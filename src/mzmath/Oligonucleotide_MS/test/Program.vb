9dd39fe225cc400e05f31348b4d75806dc2f97f1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-15 23:31:39 +0800

    fix for test

--- src/mzmath/Oligonucleotide_MS/test/Program.vb
+++ src/mzmath/Oligonucleotide_MS/test/Program.vb
@@ -10,7 +10,7 @@ Module Program
 
     Sub Oligonucleotide_Composition_from_Mass_Calculator_v2()
         Dim test As New Composition(ppm:=5)
-        Dim result = test.FindCompositions({2247.316306, 2167.34997497712, 1838.297, 2798.42}, Monoisotopic:=True).ToArray
+        Dim result = test.FindCompositions(2247.316306, 2167.34997497712, 1838.297, 2798.42).ToArray
 
         ' Observed Mass	Theoretical Mass	Error (ppm)	# of pA	# of pG	# of pC	# of pV	Modification		# of Bases
         ' 2247.316	2247.316	0.0	1	1	3	2			7
