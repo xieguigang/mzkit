26e2157069fd64de6d1a42ad7a6d8aa1dd382870
guigang xie <xie.guigang@gcmodeller.org>
2023-12-15 13:15:44 +0800

    make improvements of the file format convertor handler

--- Rscript/Library/mzkit_app/test/test_demodulate_2D.R
+++ Rscript/Library/mzkit_app/test/test_demodulate_2D.R
@@ -2,5 +2,5 @@ require(mzkit);
 
 imports "GCxGC" from "mzkit";
 
-let rawdata = open.mzPack("E:\\D1.mzPack");
+let rawdata = open.mzpack("E:\\D1.mzPack");
 let gcxgc = GCxGC::demodulate_2D(rawdata, modtime = 4);
