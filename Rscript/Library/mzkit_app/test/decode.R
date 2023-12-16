8c51dc92fe55825dfebb7991678dc570cf8d5bca
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-17 00:02:30 +0800

    config parameters

--- Rscript/Library/mzkit_app/test/decode.R
+++ Rscript/Library/mzkit_app/test/decode.R
@@ -9,7 +9,7 @@ print(rawfiles);
 for(file in rawfiles) {
     let rawdata = open.mzpack(file);
     let ms1 = rawdata |> ms1_scans();
-    let peaks = mz_deco(ms1, tolerance = "da:0.01", peak.width = [3,30]);
+    let peaks = mz_deco(ms1, tolerance = "da:0.005", peak.width = [6,30], joint = FALSE);
     let debug_file = `${dirname(file)}/${basename(file)}.dat`;
 
     write.peaks(peaks, file = debug_file);
