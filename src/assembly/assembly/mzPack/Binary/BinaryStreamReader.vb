85369528b57f96debee0c36b2a647d7479652707
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-16 19:15:53 +0800

    split source files into m,ultiple parts

--- src/assembly/assembly/mzPack/Binary/BinaryStreamReader.vb
+++ src/assembly/assembly/mzPack/Binary/BinaryStreamReader.vb
@@ -82,34 +82,6 @@ Imports Microsoft.VisualBasic.Text
 Namespace mzData.mzWebCache
 
     ''' <summary>
-    ''' a unify reader interface for <see cref="BinaryStreamReader"/> read 
-    ''' data from file or read data from ``mzPack`` in-memory data object.
-    ''' </summary>
-    Public Interface IMzPackReader
-
-        ''' <summary>
-        ''' get all scan ms1 data its scan id collection
-        ''' </summary>
-        ''' <returns></returns>
-        ReadOnly Property EnumerateIndex As IEnumerable(Of String)
-        ''' <summary>
-        ''' the source file name of current raw data file
-        ''' </summary>
-        ''' <returns></returns>
-        ReadOnly Property source As String
-        ReadOnly Property rtmax As Double
-        Function ReadScan(scan_id As String, Optional skipProducts As Boolean = False) As ScanMS1
-        Function GetMetadata(id As String) As Dictionary(Of String, String)
-
-        Sub ReadChromatogramTick(scanId As String,
-                                 <Out> ByRef scan_time As Double,
-                                 <Out> ByRef BPC As Double,
-                                 <Out> ByRef TIC As Double)
-        Function hasMs2(Optional sampling As Integer = 64) As Boolean
-
-    End Interface
-
-    ''' <summary>
     ''' the binary mzpack data reader
     ''' </summary>
     Public Class BinaryStreamReader : Implements IMagicBlock, IMzPackReader
