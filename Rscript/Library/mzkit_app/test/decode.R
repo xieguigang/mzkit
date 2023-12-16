a4ce981128894a6a55c156c444658a4f5c955c0a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-16 22:15:52 +0800

    create debug script

--- Rscript/Library/mzkit_app/test/decode.R
+++ Rscript/Library/mzkit_app/test/decode.R
@@ -1,2 +1,16 @@
 require(mzkit);
 
+imports "mzDeco" from "mz_quantify";
+
+let rawfiles = list.files("G:\\tmp\\pos_mzPack", pattern = "*.mzPack");
+
+print(rawfiles);
+
+for(file in rawfiles) {
+    let rawdata = open.mzpack(file);
+    let ms1 = rawdata |> ms1_scans();
+    let peaks = mz_deco(ms1, tolerance = "da:0.01", peak.width = [3,30]);
+    let debug_file = `${dirname(file)}/${basename(file)}.dat`;
+
+    write.peaks(peaks, file = debug_file);
+}
