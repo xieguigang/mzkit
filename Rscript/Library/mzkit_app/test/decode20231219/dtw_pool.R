c7623995396bb77411a5a040fa60e02b0591c78e
谢桂纲 <gg.xie@bionovogene.com>
2023-12-20 11:09:42 +0800

    commit nothing

--- Rscript/Library/mzkit_app/test/decode20231219/dtw_pool.R
+++ Rscript/Library/mzkit_app/test/decode20231219/dtw_pool.R
@@ -2,11 +2,11 @@ require(mzkit);
 
 imports "mzDeco" from "mz_quantify";
 
-let files = list.files("E:\\lipids-XCMS3-rt\\raw\\pos_mzPack", pattern = "*.xic");
-let pool = xic_pool(files[1:10]);
-let mzbins = read.csv("E:\\lipids-XCMS3-rt\\raw\\pos_mzbins.csv", row.names = 1, check.names = FALSE);
+let files = list.files("E:\biodeep\lipids-XCMS3-rt\raw\raw_test_QC", pattern = "*.xic");
+let pool = xic_pool(files);
+# let mzbins = read.csv("E:\\lipids-XCMS3-rt\\raw\\pos_mzbins.csv", row.names = 1, check.names = FALSE);
 
-print(mzbins);
+# print(mzbins);
 
 let raw_xic = pull_xic(pool , mz = 711.5664, dtw = FALSE);
 let dtw_xic = pull_xic(pool, mz = 711.5664, dtw = TRUE);
