e38828edd578219f49ce752b0e7d350dc73b088b
谢桂纲 <gg.xie@bionovogene.com>
2023-12-20 17:38:59 +0800

    Update dtw_pool.R

--- Rscript/Library/mzkit_app/test/decode20231219/dtw_pool.R
+++ Rscript/Library/mzkit_app/test/decode20231219/dtw_pool.R
@@ -5,26 +5,46 @@ imports "visual" from "mzplot";
 
 let files = list.files("E:\biodeep\lipids-XCMS3-rt\raw\raw_test_QC", pattern = "*.xic");
 let pool = xic_pool(files);
-# let mzbins = read.csv("E:\\lipids-XCMS3-rt\\raw\\pos_mzbins.csv", row.names = 1, check.names = FALSE);
+let mzbins = read.csv("E:\biodeep\lipids-XCMS3-rt\raw\pos_mzbins.csv", row.names = 1, check.names = FALSE);
 
-# print(mzbins);
+print(mzbins);
 
-let raw_xic = pull_xic(pool , mz = 711.5664, dtw = FALSE);
-let dtw_xic = pull_xic(pool, mz = 711.5664, dtw = TRUE);
+let peaktable = [];
 
-for(name in names(dtw_xic)) {
-    writeBin(dtw_xic[[name]], con = `E:/biodeep/lipids-XCMS3-rt/raw/test_QC_dtw_xic/${name}.XIC`);
+for(mz in mzbins$mz) {
+    str(mz);
+
+    let dtw_xic = pull_xic(pool, mz = mz, dtw = TRUE);
+    let key = md5(as.character(mz));
+
+    bitmap(file = `E:/biodeep/lipids-XCMS3-rt/raw/test_QC_dtw_xic/${substr(key, 5,6)}/${mz}.png`) {
+        raw_snapshot3D(dtw_xic);
+    }
+
+    peaktable = append(peaktable, pool 
+    |> mz_deco(feature = mz, joint = TRUE, peak.width = [3,60]));
+
+    NULL;
 }
 
-bitmap(file = "E:\biodeep\lipids-XCMS3-rt\raw\test_QC_raw_xic.png") {
-    raw_snapshot3D(raw_xic);
-}
+write.csv(peaktable, file = "E:\biodeep\lipids-XCMS3-rt\raw\test_QC_dtw_xic.csv");
 
-bitmap(file = "E:\biodeep\lipids-XCMS3-rt\raw\test_QC_dtw_xic.png") {
-    raw_snapshot3D(dtw_xic);
-}
+# let raw_xic = pull_xic(pool , mz = 711.5664, dtw = FALSE);
+# let dtw_xic = pull_xic(pool, mz = 711.5664, dtw = TRUE);
 
-pool 
-|> mz_deco(feature = 711.5664, joint = TRUE, peak.width = [3,60])
-|> write.csv(file = "E:\biodeep\lipids-XCMS3-rt\raw\test_QC_dtw_xic.csv")
-;
+# for(name in names(dtw_xic)) {
+#     writeBin(dtw_xic[[name]], con = `E:/biodeep/lipids-XCMS3-rt/raw/test_QC_dtw_xic/${name}.XIC`);
+# }
+
+# bitmap(file = "E:\biodeep\lipids-XCMS3-rt\raw\test_QC_raw_xic.png") {
+#     raw_snapshot3D(raw_xic);
+# }
+
+# bitmap(file = "E:\biodeep\lipids-XCMS3-rt\raw\test_QC_dtw_xic.png") {
+#     raw_snapshot3D(dtw_xic);
+# }
+
+# pool 
+# |> mz_deco(feature = 711.5664, joint = TRUE, peak.width = [3,60])
+# |> write.csv(file = "E:\biodeep\lipids-XCMS3-rt\raw\test_QC_dtw_xic.csv")
+# ;
