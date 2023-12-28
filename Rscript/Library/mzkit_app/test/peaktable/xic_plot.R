2d099ad0130261deb5efa07e29662c2bfbc34977
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-28 09:12:41 +0800

    plot images

--- Rscript/Library/mzkit_app/test/peaktable/xic_plot.R
+++ Rscript/Library/mzkit_app/test/peaktable/xic_plot.R
@@ -8,5 +8,7 @@ options(memory.load = "max");
 let table = readBin("D:\\pos.xcms", what = "peak_set");
 let XIC = data::XIC(table, mz = 113.1632, tolerance = "da:0.01");
 
-
+bitmap(file = "D:\\pos_113.1632.png") {
+    raw_snapshot3D(XIC);
+}
 
