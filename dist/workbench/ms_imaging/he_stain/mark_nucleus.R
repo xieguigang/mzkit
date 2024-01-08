bba62e95379bfe4a6ce907bee173bf61134ea3e6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-08 10:58:57 +0800

    run scan test succsss

--- dist/workbench/ms_imaging/he_stain/mark_nucleus.R
+++ dist/workbench/ms_imaging/he_stain/mark_nucleus.R
@@ -1,12 +1,33 @@
 require(mzkit);
+require(clustering);
 
 imports "tissue" from "mzplot";
 
 let image_raster = readImage("D:\\demo_test.PNG");
-let nucleus_xy = mark_nucleus(image_raster);
+let nucleus_xy = mark_nucleus(image_raster, tolerance = 20);
 
 print(nucleus_xy, max.print = 6);
 
 write.csv(nucleus_xy, file = `${@dir}/raster_nucleus.csv`, row.names = FALSE);
 
+bitmap(file = `${@dir}/raw_raster.png`) {
+    plot(nucleus_xy$x, nucleus_xy$y, reverse = TRUE, grid.fill = "white", point.size = 5);
+}
 
+rownames(nucleus_xy) = `${nucleus_xy$x},${nucleus_xy$y}`;
+
+nucleus_xy = dbscan(nucleus_xy, eps = 1.125, minPts = 2);
+nucleus_xy = as.data.frame([nucleus_xy]::cluster);
+
+print(nucleus_xy, max.print = 6);
+
+# str(nucleus_xy);
+
+write.csv(nucleus_xy, file = `${@dir}/mark_nucleus.csv`, row.names = TRUE);
+
+bitmap(file = `${@dir}/mark_nucleus.png`, padding = [100, 2400, 200, 200], size = [4800, 2100]) {
+    plot(as.numeric(nucleus_xy$x), as.numeric(nucleus_xy$y), class= nucleus_xy$Cluster, 
+        reverse = TRUE, grid.fill = "white",  colorSet = "paper", point.size = 5, 
+        colorset.shuffles = TRUE,
+        legend.block = 30);
+}
