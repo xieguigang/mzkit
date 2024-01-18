705ca12133d230ad462ee75f6e975d2399ae91bd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-18 08:23:56 +0800

    Update volume.R

--- dist/workbench/ms_imaging/3d/volume.R
+++ dist/workbench/ms_imaging/3d/volume.R
@@ -5,7 +5,7 @@ imports "MsImaging" from "mzplot";
 
 options(memory.load = "max");
 
-let power = MsImaging::parseFilters(["TrIQ_clip(0.9,100)", "denoise(0.01)", "power(2)"]);
+let power = MsImaging::parseFilters(["TrIQ_clip(0.9,100)", "denoise(0.01)"]);
 let rawfiles = list.files("\\192.168.1.254\backup3\项目以外内容\2024\动物器官3D重建测试\raw_mzPack\single_ions\413.0958", pattern = "*.dat");
 let z = as.numeric(basename(rawfiles));
 
