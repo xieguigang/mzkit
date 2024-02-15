17186bd665352931a0b56200e012e1bf537ee1f9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-15 20:00:53 +0800

    test read kegg compounds library

--- Rscript/Library/mzkit_app/test/pubchem/reader.R
+++ Rscript/Library/mzkit_app/test/pubchem/reader.R
@@ -2,4 +2,4 @@ require(mzkit);
 
 imports ["pubchem_kit" "massbank"] from "mzkit";
 
-print(readBin("F:\compounds\kegg_metabolites.msgpack", what = "metalib"));
+print(readBin("F:\compounds\hmdb_metabolites.msgpack", what = "metalib"));
