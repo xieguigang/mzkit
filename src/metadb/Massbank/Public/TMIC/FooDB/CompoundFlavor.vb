5b520d1a5c33fce1c99251d4de2b122323be8c44
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-03 13:16:02 +0800

    Namespace TMIC.FooDB

--- src/metadb/Massbank/Public/TMIC/FooDB/CompoundFlavor.vb
+++ src/metadb/Massbank/Public/TMIC/FooDB/CompoundFlavor.vb
@@ -1,62 +1,65 @@
 ﻿#Region "Microsoft.VisualBasic::9f0930d05e051ab76db4a6fbec8720a7, mzkit\src\metadb\FooDB\FooDB\CompoundFlavor.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 8
-    '    Code Lines: 7
-    ' Comment Lines: 0
-    '   Blank Lines: 1
-    '     File Size: 249 B
+' Summaries:
 
 
-    ' Class CompoundFlavor
-    ' 
-    '     Properties: citations, compound_id, flavor_id, source_id, source_type
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 8
+'    Code Lines: 7
+' Comment Lines: 0
+'   Blank Lines: 1
+'     File Size: 249 B
+
+
+' Class CompoundFlavor
+' 
+'     Properties: citations, compound_id, flavor_id, source_id, source_type
+' 
+' /********************************************************************************/
 
 #End Region
 
-Public Class CompoundFlavor
+Namespace TMIC.FooDB
 
-    Public Property compound_id As String
-    Public Property flavor_id As String
-    Public Property source_id As String
-    Public Property source_type As String
-    Public Property citations As String
-End Class
+    Public Class CompoundFlavor
+
+        Public Property compound_id As String
+        Public Property flavor_id As String
+        Public Property source_id As String
+        Public Property source_type As String
+        Public Property citations As String
+    End Class
+End Namespace
