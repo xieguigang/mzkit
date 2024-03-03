290e293a6597f8dcfc965079ed4f6b94589f7670
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-03 20:27:32 +0800

    msbuild

---
+++ Rscript/Library/mzkit_app/@export/foodb.d.ts
@@ -0,0 +1,14 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "foodb" from "mzkit";
+//
+// ref=mzkit.foodbTools@mzkit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
+declare namespace foodb {
+   /**
+     * @param env default value Is ``null``.
+   */
+   function loadFoods(dir: string, env?: object): any;
+}
