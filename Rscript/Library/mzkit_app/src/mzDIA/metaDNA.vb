f1d60ede698dc9a028763a2a010e96762d3c4ca5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-04 15:17:38 +0800

    how to create nwtowkr solver from ontology tree?

--- Rscript/Library/mzkit_app/src/mzDIA/metaDNA.vb
+++ Rscript/Library/mzkit_app/src/mzDIA/metaDNA.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::dc8f09faca442980b6908d1f6f0008f4, mzkit\Rscript\Library\mzkit.insilicons\metaDNA.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 538
-    '    Code Lines: 377
-    ' Comment Lines: 88
-    '   Blank Lines: 73
-    '     File Size: 22.83 KB
+' Summaries:
 
 
-    ' Module metaDNAInfer
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: CreateKEGGSearch, DIAInfer, ExportNetwork, getResultTable, handleSample
-    '               InferTable, loadCompoundLibrary, loadKeggNetwork, loadMetaDNAInferNetwork, MetaDNAAlgorithm
-    '               MgfSeeds, readReactionClassTable, ResultAlignments, ResultTable, SaveAlgorithmPerfermance
-    '               SetInferNetwork, SetKeggLibrary, SetSearchRange
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 538
+'    Code Lines: 377
+' Comment Lines: 88
+'   Blank Lines: 73
+'     File Size: 22.83 KB
+
+
+' Module metaDNAInfer
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: CreateKEGGSearch, DIAInfer, ExportNetwork, getResultTable, handleSample
+'               InferTable, loadCompoundLibrary, loadKeggNetwork, loadMetaDNAInferNetwork, MetaDNAAlgorithm
+'               MgfSeeds, readReactionClassTable, ResultAlignments, ResultTable, SaveAlgorithmPerfermance
+'               SetInferNetwork, SetKeggLibrary, SetSearchRange
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -88,6 +88,8 @@ Imports ReactionClass = SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject.ReactionC
 Imports ReactionClassTbl = BioNovoGene.BioDeep.MetaDNA.Visual.ReactionClass
 Imports REnv = SMRUCC.Rsharp.Runtime
 Imports metadata = BioNovoGene.BioDeep.Chemistry.MetaLib.Models.MetaInfo
+Imports SMRUCC.genomics.foundation.OBO_Foundry.IO.Models
+Imports BioNovoGene.BioDeep.Chemistry.ChEBI
 
 ''' <summary>
 ''' Metabolic Reaction Network-based Recursive Metabolite Annotation for Untargeted Metabolomics
@@ -304,6 +306,18 @@ Module metaDNAInfer
         End If
 
         Return metadna.SetNetwork(network.populates(Of ReactionClass)(env))
+    End Function
+
+    ''' <summary>
+    ''' load the ontology tree as the network graph for search
+    ''' </summary>
+    ''' <param name="metadna"></param>
+    ''' <param name="obo"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("load.ontology")>
+    Public Function loadOntologyTree(metadna As Algorithm, obo As OBOFile, Optional env As Environment = Nothing) As Object
+
     End Function
 
     ''' <summary>
@@ -577,7 +591,11 @@ Module metaDNAInfer
             )
         End If
 
-        keggSet = pipeline.TryCreatePipeline(Of metadata)(kegg, env)
+        If TypeOf kegg Is OBOFile Then
+            keggSet = pipeline.CreateFromPopulator(ChEBIObo.ImportsMetabolites(DirectCast(kegg, OBOFile)))
+        Else
+            keggSet = pipeline.TryCreatePipeline(Of metadata)(kegg, env)
+        End If
 
         If Not keggSet.isError Then
             Return CompoundSolver.CreateIndex(
