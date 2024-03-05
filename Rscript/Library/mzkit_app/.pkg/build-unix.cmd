80ce3f37bed3f92ee66de09d4e0850fe73b84f43
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-05 09:42:28 +0800

    make package document upodates

--- Rscript/Library/mzkit_app/.pkg/build-unix.cmd
+++ Rscript/Library/mzkit_app/.pkg/build-unix.cmd
@@ -4,8 +4,9 @@ SET drive=%~d0
 SET R_HOME=%drive%\GCModeller\src\R-sharp\App\net6.0
 SET Rscript="%R_HOME%/Rscript.exe"
 SET REnv="%R_HOME%/R#.exe"
+SET js_url="https://mzkit.org/assets/js/R_syntax.js"
 
-%Rscript% --build /src ../ /save ./mzkit.zip --skip-src-build
+%Rscript% --build /src ../ /save ./mzkit.zip --skip-src-build  --github-page %js_url%
 %REnv% --install.packages ./mzkit.zip
 
 pause
