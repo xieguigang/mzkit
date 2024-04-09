b7b6137843cf53700a8a21a266aec3d3009b1301
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 22:57:20 +0800

    make improvements of the progress echo

--- src/mzmath/ms2_simulator/MolecularGraph.vb
+++ src/mzmath/ms2_simulator/MolecularGraph.vb
@@ -1,9 +1,40 @@
-﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+﻿Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.MachineLearning.Bootstrapping
 
 Public Module MolecularGraph
 
-    Public Function ToVector(molecular As NetworkGraph) As Double()
+    Dim embedding As New Graph2Vec(AddressOf getVocabulary)
 
+    Sub New()
+        Call embedding.Setup({
+            "-CH3",
+            "-CH=",
+            "-CH2-",
+            "-OH",
+            "-O-",
+            "-NH2",
+            "-NH3",
+            "-NH4"
+        })
+    End Sub
+
+    Private Function getVocabulary(atom_group As Vertex) As String
+        Return DirectCast(atom_group, Node).data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE)
+    End Function
+
+    Public Function ToVector(molecular As NetworkGraph) As Double()
+        Dim graph As New Graph
+
+        For Each v As Node In molecular.vertex
+            Call graph.AddVertex(v)
+        Next
+        For Each link As Edge In molecular.graphEdges
+            Call graph.AddEdge(link.U, link.V, link.weight)
+        Next
+
+        Return embedding.GraphVector(graph)
     End Function
 
 End Module
--- src/mzmath/ms2_simulator/test/graph_test.vb
+++ src/mzmath/ms2_simulator/test/graph_test.vb
@@ -1,16 +1,30 @@
 ﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Insilicon
 Imports BioNovoGene.BioDeep.Chemoinformatics.SMILES
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.DataMining.KMeans
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module graph_test
 
     Sub Main()
-        Dim test_smiles As String = "CC(C)C[C@H](N)C(O)=O"
-        Dim chemical_struct As ChemicalFormula = ParseChain.ParseGraph(test_smiles, strict:=False)
-        Dim molecular_graph As NetworkGraph = chemical_struct.AsGraph
-        Dim vec = MolecularGraph.ToVector(molecular_graph)
+        Dim data As New List(Of ClusterEntity)
+
+        data.Add(New ClusterEntity("leucine", vectorBuilder("CC(C)C[C@H](N)C(O)=O")))
+        data.Add(New ClusterEntity("3-Isopropylmalic acid", vectorBuilder("CC(C)[C@@H]([C@@H](O)C(O)=O)C(O)=O")))
+        data.Add(New ClusterEntity("Choline", vectorBuilder("C[N+](C)(C)CCO")))
 
         Pause()
     End Sub
+
+    Private Function vectorBuilder(test_smiles As String) As Double()
+        Dim chemical_struct As ChemicalFormula = ParseChain.ParseGraph(test_smiles, strict:=False)
+        Dim molecular_graph As NetworkGraph = chemical_struct.AsGraph
+
+        Call VBDebugger.WaitOutput()
+        Call Console.WriteLine(molecular_graph.vertex.Select(Function(v) v.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE)).GetJson)
+
+        Return MolecularGraph.ToVector(molecular_graph)
+    End Function
 End Module
