756221881b7c64f24cec4a060f92428718d3420f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 22:29:50 +0800

    try to embedding graph as vector

---
+++ src/mzmath/ms2_simulator/MolecularGraph.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Public Module MolecularGraph
+
+    Public Function ToVector(molecular As NetworkGraph) As Double()
+
+    End Function
+
+End Module
--- src/mzmath/ms2_simulator/test/graph_test.vb
+++ src/mzmath/ms2_simulator/test/graph_test.vb
@@ -1,6 +1,16 @@
-﻿Module graph_test
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Insilicon
+Imports BioNovoGene.BioDeep.Chemoinformatics.SMILES
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Module graph_test
 
     Sub Main()
+        Dim test_smiles As String = "CC(C)C[C@H](N)C(O)=O"
+        Dim chemical_struct As ChemicalFormula = ParseChain.ParseGraph(test_smiles, strict:=False)
+        Dim molecular_graph As NetworkGraph = chemical_struct.AsGraph
+        Dim vec = MolecularGraph.ToVector(molecular_graph)
 
+        Pause()
     End Sub
 End Module
