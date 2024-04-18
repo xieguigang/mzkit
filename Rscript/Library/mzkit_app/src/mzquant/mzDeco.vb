97ea9e67d94c8264badd71c6e884eaea7a22e64d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-19 00:39:26 +0800

    fix of the pssible missing key

--- Rscript/Library/mzkit_app/src/mzquant/mzDeco.vb
+++ Rscript/Library/mzkit_app/src/mzquant/mzDeco.vb
@@ -452,6 +452,9 @@ Module mzDeco
     ''' </summary>
     ''' <param name="peakdata">should be a collection of the peak data from a single sample file.</param>
     ''' <param name="RI">should be a collection of the <see cref="RIRefer"/> data.</param>
+    ''' <param name="C">
+    ''' the number of carbon atoms for kovats retention index
+    ''' </param>
     ''' <param name="env"></param>
     ''' <returns></returns>
     <ExportAPI("RI_cal")>
@@ -462,6 +465,7 @@ Module mzDeco
                             Optional dt As Double = 15,
                             Optional rawfile As String = Nothing,
                             Optional by_id As Boolean = False,
+                            Optional C As list = Nothing,
                             Optional env As Environment = Nothing) As Object
 
         Dim refer_points As New List(Of PeakFeature)
@@ -527,25 +531,48 @@ Module mzDeco
         Dim a As (rt As Double, ri As Double)
         Dim b As (rt As Double, ri As Double)
         Dim offset As Integer = 0
+        Dim id_a, id_b As String
+        Dim c_atoms As Dictionary(Of String, Integer) = Nothing
+
+        If Not C Is Nothing Then
+            c_atoms = C.AsGeneric(Of Integer)(env)
+
+            If Not c_atoms.ContainsKey(peakdata(0).xcms_id) Then
+                c_atoms.Add(peakdata(0).xcms_id, c_atoms.Values.Min - 1)
+            End If
+            If Not c_atoms.ContainsKey(peakdata.Last.xcms_id) Then
+                c_atoms.Add(peakdata.Last.xcms_id, c_atoms.Values.Max + 1)
+            End If
+        End If
 
         If peakdata(0).RI > 0 Then
             a = (peakdata(0).rt, peakdata(0).RI)
+            id_a = peakdata(0).xcms_id
             offset = 1
             b = (refer_points(1).rt, refer_points(1).RI)
+            id_b = refer_points(1).xcms_id
         Else
             a = (peakdata(0).rt, 0)
             b = (refer_points(0).rt, refer_points(0).RI)
+            id_a = peakdata(0).xcms_id
+            id_b = refer_points(0).xcms_id
         End If
 
         For i As Integer = offset To peakdata.Length - 1
             peakdata(i).rawfile = If(rawfile, peakdata(i).rawfile)
 
             If peakdata(i).RI = 0 Then
-                peakdata(i).RI = deco_math.RetentionIndex(peakdata(i), a, b)
+                If c_atoms Is Nothing Then
+                    peakdata(i).RI = deco_math.RetentionIndex(peakdata(i), a, b)
+                Else
+                    peakdata(i).RI = deco_math.KovatsRI(c_atoms(id_a), c_atoms(id_b), peakdata(i).rt, a.rt, b.rt)
+                End If
             Else
                 a = b
+                id_a = id_b
                 offset += 1
                 b = (refer_points(offset).rt, refer_points(offset).RI)
+                id_b = refer_points(offset).xcms_id
             End If
         Next
 
