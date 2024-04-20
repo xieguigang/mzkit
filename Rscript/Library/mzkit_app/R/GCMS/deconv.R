257557e50d61e1df228fb7e21ccfbe253ae4ac7d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-21 01:51:48 +0800

    how to expoprt p[eaktable for gcms data?

--- Rscript/Library/mzkit_app/R/GCMS/deconv.R
+++ Rscript/Library/mzkit_app/R/GCMS/deconv.R
@@ -1,13 +1,16 @@
 let deconv_gcms = function(rawdata, export_dir = "./", peak.width = [3, 90], n_threads = 16) {
     let files = list.files(rawdata, pattern = "*.mzPack");
+    let peaks_export = file.path(export_dir, "peaksdata");
     let decode_gcms = function(filepath) {
         require(mzkit);
 
         filepath 
         |> __deconv_gcms_single(peak.width)
-        |> writeBin(con = file.path(export_dir, "peaksdata", `${basename(filepath)}.peakdata`))
+        |> writeBin(con = file.path(peaks_export, `${basename(filepath)}.peakdata`))
         ;
     }
+
+    files = as.list(files, names = basename(files));
 
     parallel(path = files, n_threads = n_threads, 
                 ignoreError = TRUE) {
@@ -16,6 +19,8 @@ let deconv_gcms = function(rawdata, export_dir = "./", peak.width = [3, 90], n_t
         # data for each single sample data file.
         decode_gcms(path);
     }
+
+
 }
 
 let __deconv_gcms_single = function(file, peak.width = [3, 90]) {
