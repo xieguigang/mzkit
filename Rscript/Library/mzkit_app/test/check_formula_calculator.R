c86e1a0579895af28ae2bb9105db920ce63fc547
谢桂纲 <gg.xie@bionovogene.com>
2024-04-23 09:47:41 +0800

    formula evaluate check success

--- Rscript/Library/mzkit_app/test/check_formula_calculator.R
+++ Rscript/Library/mzkit_app/test/check_formula_calculator.R
@@ -2,4 +2,4 @@ require(mzkit);
 
 imports "formula" from "mzkit";
 
-print(formula::eval(["O62P2W18-63"]));
+print(formula::eval(["O62P2W18-63" "C7H19Fe7MoNO7S9-12" "H32Mo7N6O28-42" "Fe8H48O22+28" "C60H79F32N2O18S3---"]));
