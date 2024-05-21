fbeff77c838c1baf0515e511d61af9de0ca93bda
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-21 13:02:11 +0800

    MatchByExactMass

--- src/mzmath/MSFinder/PrecursorAnnotation/FeatureSearchHandler.vb
+++ src/mzmath/MSFinder/PrecursorAnnotation/FeatureSearchHandler.vb
@@ -25,8 +25,32 @@ Public Module FeatureSearchHandler
         Dim pool = rawdata.SafeQuery.ToArray
 
         If pool.Length = 1 Then
+            Dim adducts = GetAdductsTuple(exact_mass)
+            Dim source As String = pool(0).Name
+            Dim find = pool(0).Value _
+                .AsParallel _
+                .Select(Function(scan) scan.MatchByExactMass(adducts, source, ppm)) _
+                .IteratesALL _
+                .ToArray
 
+            Yield New NamedCollection(Of ParentMatch)(source, find)
+        Else
+            For Each find As NamedCollection(Of ParentMatch) In pool _
+                .AsParallel _
+                .Select(Function(file)
+                            Return New NamedCollection(Of ParentMatch)(file.Name, file.Value.MatchByExactMass(exact_mass, file.Name, ppm))
+                        End Function)
+
+                Yield find
+            Next
         End If
+    End Function
+
+    Private Function GetAdductsTuple(exact_mass As Double) As PolarityData(Of PrecursorInfo())
+        Return New PolarityData(Of PrecursorInfo())(
+            pos:=MzCalculator.EvaluateAll(exact_mass, "+", False).ToArray,
+            neg:=MzCalculator.EvaluateAll(exact_mass, "-", False).ToArray
+        )
     End Function
 
     ''' <summary>
@@ -41,15 +65,15 @@ Public Module FeatureSearchHandler
     ''' <returns></returns>
     ''' 
     <Extension>
-    Public Iterator Function MatchByExactMass(Of T As ISpectrumScanData)(raw As IEnumerable(Of T), exact_mass As Double, source As String, ppm As Tolerance) As IEnumerable(Of ParentMatch)
+    Public Iterator Function MatchByExactMass(Of T As ISpectrumScanData)(raw As IEnumerable(Of T),
+                                                                         exact_mass As Double,
+                                                                         source As String,
+                                                                         ppm As Tolerance) As IEnumerable(Of ParentMatch)
         ' C25H40N4O5
-        Dim adducts As New PolarityData(Of PrecursorInfo())(
-            pos:=MzCalculator.EvaluateAll(exact_mass, "+", False).ToArray,
-            neg:=MzCalculator.EvaluateAll(exact_mass, "-", False).ToArray
-        )
+        Dim adducts = GetAdductsTuple(exact_mass)
 
         For Each scan As ISpectrumScanData In raw
-            For Each match As ParentMatch In scan.MatchByExactMass(adducts, source, exact_mass, ppm)
+            For Each match As ParentMatch In scan.MatchByExactMass(adducts, source, ppm)
                 Yield match
             Next
             ' Call System.Windows.Forms.Application.DoEvents()
@@ -59,7 +83,6 @@ Public Module FeatureSearchHandler
     <Extension>
     Private Iterator Function MatchByExactMass(scan As ISpectrumScanData, adducts As PolarityData(Of PrecursorInfo()),
                                                source As String,
-                                               exact_mass As Double,
                                                ppm As Tolerance) As IEnumerable(Of ParentMatch)
 
         For Each mode As PrecursorInfo In adducts(scan.Polarity)
