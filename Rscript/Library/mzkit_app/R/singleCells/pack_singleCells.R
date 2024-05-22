89391cc5496be143a3a2413c7973a3960c6af3fc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-22 11:09:10 +0800

    pack cells data

--- Rscript/Library/mzkit_app/R/singleCells/pack_singleCells.R
+++ Rscript/Library/mzkit_app/R/singleCells/pack_singleCells.R
@@ -1,3 +1,9 @@
-const pack_singleCells = function() {
-    
+const pack_singleCells = function(rawdata) {
+    if (dir.exists(rawdata)) {
+        rawdata = list.files(rawdata, ["*.mzPack"]);
+    }
+
+    rawdata 
+    |> lapply(file -> open.mzpack(file))
+    |> 
 }
---
+++ Rscript/Library/mzkit_app/src/mzkit/comprehensive/SingleCellsPack.vb
@@ -0,0 +1,29 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.Comprehensive.SingleCells
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("cellsPack")>
+Module SingleCellsPack
+
+    <ExportAPI("pack_cells")>
+    Public Function PackSingleCells(<RRawVectorArgument>
+                                    rawdata As Object,
+                                    Optional source_tag As String = Nothing,
+                                    Optional env As Environment = Nothing) As Object
+
+        Dim cell_packs As pipeline = pipeline.TryCreatePipeline(Of mzPack)(rawdata, env)
+
+        If cell_packs.isError Then
+            Return cell_packs.getError
+        End If
+
+        Return cell_packs _
+            .populates(Of mzPack)(env) _
+            .PackRawData(source_tag)
+    End Function
+
+End Module
