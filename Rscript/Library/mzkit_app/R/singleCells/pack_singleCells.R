6141e2e619a712cdd8d699c070eaed51b4920a71
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-22 11:10:56 +0800

    packs the single cell rawdata

--- Rscript/Library/mzkit_app/R/singleCells/pack_singleCells.R
+++ Rscript/Library/mzkit_app/R/singleCells/pack_singleCells.R
@@ -1,9 +1,16 @@
-const pack_singleCells = function(rawdata) {
+imports "cellsPack" from "mzkit";
+
+const pack_singleCells = function(rawdata, tag = NULL) {
     if (dir.exists(rawdata)) {
-        rawdata = list.files(rawdata, ["*.mzPack"]);
+        if (is.empty(tag)) {
+            tag = basename(rawdata);
+        }
+
+        rawdata <- list.files(rawdata, ["*.mzPack"]);
     }
 
     rawdata 
     |> lapply(file -> open.mzpack(file))
-    |> 
+    |> cellsPack::pack_cells()
+    ;
 }
