fafe9d14fb8583815936e9726f48f51c66859fb9
谢桂纲 <gg.xie@bionovogene.com>
2024-06-03 16:10:26 +0800

    test success

--- Rscript/Library/mzkit_app/test/formula_test.R
+++ Rscript/Library/mzkit_app/test/formula_test.R
@@ -2,4 +2,7 @@ require(mzkit);
 
 imports "formula" from "mzkit";
 
-print(formula_calibration(C9H6O3KNaNH5, ["[M+H]+", "[M+K]+","[M+H-H2O]+","[M+NH3]-","[M]+"]));
+let demo  = C9H6O3KNaNH5;
+
+print(demo);
+print(formula_calibration(C9H6O3KNaNH5, ["[M+H]+", "[M+K]+","[M+H-H2O]+","[M+NH3]-","[M]+","[M]-","[M-H]-","[M+COO]-","[M-H2O]-"]));
