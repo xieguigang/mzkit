4196c9a23b4b0b30cc1c9d432407f245cc842a25
谢桂纲 <gg.xie@bionovogene.com>
2024-06-03 16:40:23 +0800

    create lipid name helper

---
+++ src/metadb/Chemoinformatics/Lipid/LipidClassAbbreviation.vb
@@ -0,0 +1,44 @@
+﻿Imports System.ComponentModel
+
+Namespace Lipidomics
+
+    Public Enum LipidClassAbbreviation
+
+        <Description("Sphingosine")> SPH
+
+    End Enum
+
+    Public Module AbbreviationNameMapper
+
+        Public Function ToFullName(abbr As String) As String
+            Static mapping As Dictionary(Of String, String) = Enums(Of LipidClassAbbreviation) _
+                .ToDictionary(Function(f) f.ToString.ToLower,
+                              Function(f)
+                                  Return f.Description
+                              End Function)
+            Dim lcase As String = abbr.ToLower
+
+            If mapping.ContainsKey(lcase) Then
+                Return mapping(lcase)
+            Else
+                Return abbr
+            End If
+        End Function
+
+        Public Function GetAbbreviationName(full As String) As String
+            Static mapping As Dictionary(Of String, String) = Enums(Of LipidClassAbbreviation) _
+                .ToDictionary(Function(f) f.Description.ToLower,
+                              Function(f)
+                                  Return f.ToString
+                              End Function)
+            Dim lcase As String = full.ToLower
+
+            If mapping.ContainsKey(lcase) Then
+                Return mapping(lcase)
+            Else
+                Return full
+            End If
+        End Function
+
+    End Module
+End Namespace
