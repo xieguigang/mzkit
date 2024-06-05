adfe3380e601b52ba8db830ee6dfb7f2675d831b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-05 15:25:38 +0800

    update source file banner headers![Rscript api package module]

--- Rscript/Library/mzkit_app/src/mzDIA/MoleculeNetworking.vb
+++ Rscript/Library/mzkit_app/src/mzDIA/MoleculeNetworking.vb
@@ -1,57 +1,57 @@
-﻿#Region "Microsoft.VisualBasic::fb0e2bbc2e6cf1e062a7a5588877d25a, Rscript\Library\mzkit_app\src\mzDIA\MoleculeNetworking.vb"
+﻿#Region "Microsoft.VisualBasic::b96983ed84e963ff53c2661181702829, Rscript\Library\mzkit_app\src\mzDIA\MoleculeNetworking.vb"
 
-' Author:
-' 
-'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-' 
-' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-' 
-' 
-' MIT License
-' 
-' 
-' Permission is hereby granted, free of charge, to any person obtaining a copy
-' of this software and associated documentation files (the "Software"), to deal
-' in the Software without restriction, including without limitation the rights
-' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-' copies of the Software, and to permit persons to whom the Software is
-' furnished to do so, subject to the following conditions:
-' 
-' The above copyright notice and this permission notice shall be included in all
-' copies or substantial portions of the Software.
-' 
-' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-' SOFTWARE.
-
-
-
-' /********************************************************************************/
-
-' Summaries:
+    ' Author:
+    ' 
+    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+    ' 
+    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+    ' 
+    ' 
+    ' MIT License
+    ' 
+    ' 
+    ' Permission is hereby granted, free of charge, to any person obtaining a copy
+    ' of this software and associated documentation files (the "Software"), to deal
+    ' in the Software without restriction, including without limitation the rights
+    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    ' copies of the Software, and to permit persons to whom the Software is
+    ' furnished to do so, subject to the following conditions:
+    ' 
+    ' The above copyright notice and this permission notice shall be included in all
+    ' copies or substantial portions of the Software.
+    ' 
+    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    ' SOFTWARE.
 
 
-' Code Statistics:
 
-'   Total Lines: 346
-'    Code Lines: 191 (55.20%)
-' Comment Lines: 123 (35.55%)
-'    - Xml Docs: 96.75%
-' 
-'   Blank Lines: 32 (9.25%)
-'     File Size: 15.33 KB
+    ' /********************************************************************************/
+
+    ' Summaries:
 
 
-' Module MoleculeNetworking
-' 
-'     Function: clustering, createGraph, MsBin, RepresentativeSpectrum, splitClusterRT
-'               Tree, unqiueNames
-' 
-' /********************************************************************************/
+    ' Code Statistics:
+
+    '   Total Lines: 347
+    '    Code Lines: 192 (55.33%)
+    ' Comment Lines: 123 (35.45%)
+    '    - Xml Docs: 96.75%
+    ' 
+    '   Blank Lines: 32 (9.22%)
+    '     File Size: 15.40 KB
+
+
+    ' Module MoleculeNetworking
+    ' 
+    '     Function: clustering, createGraph, MsBin, RepresentativeSpectrum, splitClusterRT
+    '               Tree, unqiueNames
+    ' 
+    ' /********************************************************************************/
 
 #End Region
 
