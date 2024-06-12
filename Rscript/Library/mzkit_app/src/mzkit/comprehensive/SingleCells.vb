a7d48ba4fc905609c52ca2b0eba9d59db50fe573
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-12 16:34:41 +0800

    make type mis-matched error of the mass window reference

--- Rscript/Library/mzkit_app/src/mzkit/comprehensive/SingleCells.vb
+++ Rscript/Library/mzkit_app/src/mzkit/comprehensive/SingleCells.vb
@@ -1,60 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::798383c22c6c8ff5369b402ce3b3387f, Rscript\Library\mzkit_app\src\mzkit\comprehensive\SingleCells.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 683
-    '    Code Lines: 403 (59.00%)
-    ' Comment Lines: 208 (30.45%)
-    '    - Xml Docs: 94.23%
-    ' 
-    '   Blank Lines: 72 (10.54%)
-    '     File Size: 26.53 KB
+' Summaries:
 
 
-    ' Module SingleCells
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: asHTSExpression, cell_clusters, cell_embedding, cellMatrix, cellStatsTable
-    '               dfMzMatrix, embedding_sample, getCellLabels, getSpatialLabels, mzMatrixDf
-    '               openMatrix, readMzmatrix, rowApplyScale, singleCellsIons, spatialLabels
-    '               spot_vector, writeMatrix
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 683
+'    Code Lines: 403 (59.00%)
+' Comment Lines: 208 (30.45%)
+'    - Xml Docs: 94.23%
+' 
+'   Blank Lines: 72 (10.54%)
+'     File Size: 26.53 KB
+
+
+' Module SingleCells
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: asHTSExpression, cell_clusters, cell_embedding, cellMatrix, cellStatsTable
+'               dfMzMatrix, embedding_sample, getCellLabels, getSpatialLabels, mzMatrixDf
+'               openMatrix, readMzmatrix, rowApplyScale, singleCellsIons, spatialLabels
+'               spot_vector, writeMatrix
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -346,7 +346,7 @@ Module SingleCells
                                Optional env As Environment = Nothing) As Object
 
         Dim singleCells As New List(Of DataFrameRow)
-        Dim mzSet As Double()
+        Dim mzSet As MassWindow()
         Dim source As String
 
         If raw Is Nothing Then
@@ -363,7 +363,7 @@ Module SingleCells
                 freq:=freq
             )
 
-            For Each cell_scan As DataFrameRow In SingleCellMatrix.ExportScans(Of DataFrameRow)(mzpack, mzSet)
+            For Each cell_scan As DataFrameRow In SingleCellMatrix.ExportScans(Of DataFrameRow)(mzpack, mzSet.Mass)
                 cell_scan.geneID = cell_scan.geneID _
                     .Replace("[MS1]", "") _
                     .Trim
@@ -371,16 +371,12 @@ Module SingleCells
             Next
         ElseIf TypeOf raw Is list Then
             Dim msdata As Dictionary(Of String, LibraryMatrix) = DirectCast(raw, list).AsGeneric(Of LibraryMatrix)(env)
+            Dim massList = msdata.Values.Select(Function(m) m.mz).AsList
 
             source = "msdata"
-            mzSet = msdata.Values _
-                .IteratesALL _
-                .ToArray _
-                .Centroid(Tolerance.DeltaMass(mzdiff), New RelativeIntensityCutoff(0)) _
-                .Select(Function(mzi) mzi.mz) _
-                .ToArray
+            mzSet = Deconvolute.Math.GetMzIndexFastBin(massList, mzdiff, freq)
 
-            For Each cell_scan As DataFrameRow In SingleCellMatrix.ExportScans(Of DataFrameRow)(msdata.Values, mzSet)
+            For Each cell_scan As DataFrameRow In SingleCellMatrix.ExportScans(Of DataFrameRow)(msdata.Values, mzSet.Mass)
                 cell_scan.geneID = cell_scan.geneID _
                     .Replace("[MS1]", "") _
                     .Trim
@@ -412,16 +408,18 @@ Module SingleCells
             Return New MzMatrix With {
                 .tolerance = $"da:{mzdiff}",
                 .matrixType = FileApplicationClass.SingleCellsMetabolomics,
-                .mz = mzSet,
+                .mz = mzSet.Mass,
                 .matrix = singleCells _
                     .Select(Function(v) PixelData.FromSingleCellExpression(v)) _
-                    .ToArray
+                    .ToArray,
+                .mzmin = mzSet.Select(Function(mzi) mzi.mzmin).ToArray,
+                .mzmax = mzSet.Select(Function(mzi) mzi.mzmax).ToArray
             }
         Else
             Return New HTSMatrix With {
                 .expression = singleCells.ToArray,
                 .sampleID = mzSet _
-                    .Select(Function(mzi) mzi.ToString("F4")) _
+                    .Select(Function(mzi) mzi.mass.ToString("F4")) _
                     .ToArray,
                 .tag = source
             }
--- src/mzmath/ms2_math-core/Ms1/Tolerance/MassWindow.vb
+++ src/mzmath/ms2_math-core/Ms1/Tolerance/MassWindow.vb
@@ -1,67 +1,68 @@
 ﻿#Region "Microsoft.VisualBasic::a3bd86d964afc1f2f7cdac43fdd118d2, mzmath\ms2_math-core\Ms1\Tolerance\MassWindow.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 79
-    '    Code Lines: 45 (56.96%)
-    ' Comment Lines: 18 (22.78%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 16 (20.25%)
-    '     File Size: 2.25 KB
+' Summaries:
 
 
-    '     Interface IMassBin
-    ' 
-    '         Properties: mass, max, min
-    ' 
-    '     Class MassWindow
-    ' 
-    '         Properties: annotation, mass, mzmax, mzmin
-    ' 
-    '         Constructor: (+3 Overloads) Sub New
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 79
+'    Code Lines: 45 (56.96%)
+' Comment Lines: 18 (22.78%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 16 (20.25%)
+'     File Size: 2.25 KB
+
+
+'     Interface IMassBin
+' 
+'         Properties: mass, max, min
+' 
+'     Class MassWindow
+' 
+'         Properties: annotation, mass, mzmax, mzmin
+' 
+'         Constructor: (+3 Overloads) Sub New
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports std = System.Math
 
@@ -77,6 +78,21 @@ Namespace Ms1
         Property max As Double
 
     End Interface
+
+    <HideModuleName>
+    Public Module MassExtensions
+
+        ''' <summary>
+        ''' get all mass value from the mass window data
+        ''' </summary>
+        ''' <param name="masslist"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function Mass(masslist As IEnumerable(Of MassWindow)) As Double()
+            Return masslist.Select(Function(mzi) mzi.mass).ToArray
+        End Function
+
+    End Module
 
     ''' <summary>
     ''' the m/z bin data
