e47b8cfa0864a41662778e246948038acdbda75e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-15 22:10:47 +0800

    evaluate the ion rsd

--- src/mzmath/SingleCells/Matrix/Math/MathExtensions.vb
+++ src/mzmath/SingleCells/Matrix/Math/MathExtensions.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells.Deconvolute
+Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.SIMD
 
 Namespace MatrixMath
@@ -37,6 +38,20 @@ Namespace MatrixMath
                 .mzmin = m.mzmin,
                 .tolerance = m.tolerance
             }
+        End Function
+
+        <Extension>
+        Public Function RSD(m As MzMatrix) As Double()
+            Dim rsd_vec As Double() = New Double(m.featureSize - 1) {}
+
+            For i As Integer = 0 To rsd_vec.Length - 1
+                Dim offset As Integer = i
+                Dim col As Double() = (From cell As PixelData In m.matrix Select cell(offset)).ToArray
+
+                rsd_vec(i) = col.RSD
+            Next
+
+            Return rsd_vec
         End Function
     End Module
 End Namespace
