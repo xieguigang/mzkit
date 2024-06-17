b837442107ce9274b9f941bd8c86e7a106d081a9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-17 14:48:12 +0800

    make improvements of the data model

--- src/metadb/Massbank/MetaLib/RefMet.vb
+++ src/metadb/Massbank/MetaLib/RefMet.vb
@@ -1,62 +1,65 @@
 ﻿#Region "Microsoft.VisualBasic::d93e99b82fc40da724d659448213a5b9, metadb\Massbank\MetaLib\RefMet.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 54
-    '    Code Lines: 16 (29.63%)
-    ' Comment Lines: 34 (62.96%)
-    '    - Xml Docs: 91.18%
-    ' 
-    '   Blank Lines: 4 (7.41%)
-    '     File Size: 3.14 KB
+' Summaries:
 
 
-    '     Class RefMet
-    ' 
-    '         Properties: exactmass, formula, inchi_key, main_class, pubchem_cid
-    '                     refmet_name, smiles, sub_class, super_class
-    ' 
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 54
+'    Code Lines: 16 (29.63%)
+' Comment Lines: 34 (62.96%)
+'    - Xml Docs: 91.18%
+' 
+'   Blank Lines: 4 (7.41%)
+'     File Size: 3.14 KB
+
+
+'     Class RefMet
+' 
+'         Properties: exactmass, formula, inchi_key, main_class, pubchem_cid
+'                     refmet_name, smiles, sub_class, super_class
+' 
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.Annotations
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 
 Namespace MetaLib
 
@@ -94,14 +97,14 @@ Namespace MetaLib
     ''' 
     ''' Some notes pertaining To different metabolite classes are outlined below.
     ''' </remarks>
-    Public Class RefMet
+    Public Class RefMet : Implements IReadOnlyId, IExactMassProvider, ICompoundNameProvider, IFormulaProvider, INamedValue
 
-        Public Property refmet_name As String
+        Public Property refmet_name As String Implements IReadOnlyId.Identity, ICompoundNameProvider.CommonName, INamedValue.Key
         Public Property super_class As String
         Public Property main_class As String
         Public Property sub_class As String
-        Public Property formula As String
-        Public Property exactmass As String
+        Public Property formula As String Implements IFormulaProvider.Formula
+        Public Property exactmass As Double Implements IExactMassProvider.ExactMass
         Public Property inchi_key As String
         Public Property smiles As String
         Public Property pubchem_cid As String
