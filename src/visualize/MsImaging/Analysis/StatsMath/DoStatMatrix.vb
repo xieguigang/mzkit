dcac2d2692ec0b3c2b4eecaff65907ad09d68aa5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-17 14:16:32 +0800

    update source file banner headers![MS data plot]

--- src/visualize/MsImaging/Analysis/StatsMath/DoStatMatrix.vb
+++ src/visualize/MsImaging/Analysis/StatsMath/DoStatMatrix.vb
@@ -1,4 +1,70 @@
-﻿Imports System.Runtime.CompilerServices
+﻿#Region "Microsoft.VisualBasic::700eb052c44b0f944465ff25c03e12e1, visualize\MsImaging\Analysis\StatsMath\DoStatMatrix.vb"
+
+    ' Author:
+    ' 
+    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+    ' 
+    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+    ' 
+    ' 
+    ' MIT License
+    ' 
+    ' 
+    ' Permission is hereby granted, free of charge, to any person obtaining a copy
+    ' of this software and associated documentation files (the "Software"), to deal
+    ' in the Software without restriction, including without limitation the rights
+    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    ' copies of the Software, and to permit persons to whom the Software is
+    ' furnished to do so, subject to the following conditions:
+    ' 
+    ' The above copyright notice and this permission notice shall be included in all
+    ' copies or substantial portions of the Software.
+    ' 
+    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    ' SOFTWARE.
+
+
+
+    ' /********************************************************************************/
+
+    ' Summaries:
+
+
+    ' Code Statistics:
+
+    '   Total Lines: 150
+    '    Code Lines: 122 (81.33%)
+    ' Comment Lines: 3 (2.00%)
+    '    - Xml Docs: 100.00%
+    ' 
+    '   Blank Lines: 25 (16.67%)
+    '     File Size: 6.41 KB
+
+
+    '     Module DoStatMatrix
+    ' 
+    '         Function: DoStat
+    '         Class IonFeatureTask
+    ' 
+    '             Constructor: (+1 Overloads) Sub New
+    ' 
+    '             Function: Result
+    ' 
+    '             Sub: Solve
+    ' 
+    ' 
+    ' 
+    ' 
+    ' /********************************************************************************/
+
+#End Region
+
+Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells.Deconvolute
