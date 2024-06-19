c364e1f96d7cea71f47465f970a6da2c037fbc1a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-19 14:49:27 +0800

    tweaks of the default plot options

--- src/visualize/plot/PeakAssign.vb
+++ src/visualize/plot/PeakAssign.vb
@@ -90,8 +90,8 @@ Public Class PeakAssign : Inherits Plot
     ''' <summary>
     ''' 
     ''' </summary>
-    ''' <param name="title$"></param>
-    ''' <param name="matrix"></param>
+    ''' <param name="title">the title display on the plot</param>
+    ''' <param name="matrix">the spectrum matrix data</param>
     ''' <param name="barHighlight"></param>
     ''' <param name="theme"></param>
     ''' <param name="images">the annotated molecular parts image</param>
@@ -189,7 +189,7 @@ Public Class PeakAssign : Inherits Plot
 
         Call Axis.DrawAxis(
             g, canvas, scaler,
-            showGrid:=True,
+            showGrid:=theme.drawGrid,
             xlabel:=xlabel,
             ylabel:=ylabel,
             XtickFormat:="F0",
