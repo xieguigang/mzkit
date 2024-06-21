1b811c5cbebc28e0daee010aff2351f1f3e5dbaf
谢桂纲 <gg.xie@bionovogene.com>
2024-06-21 08:33:49 +0800

    fix of the missing data fields

--- Rscript/Library/mzkit_app/src/mzkit/comprehensive/MSI.vb
+++ Rscript/Library/mzkit_app/src/mzkit/comprehensive/MSI.vb
@@ -1,67 +1,67 @@
 ﻿#Region "Microsoft.VisualBasic::95128e74070c237f28289037b423c8a2, Rscript\Library\mzkit_app\src\mzkit\comprehensive\MSI.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1857
-    '    Code Lines: 1116 (60.10%)
-    ' Comment Lines: 545 (29.35%)
-    '    - Xml Docs: 91.74%
-    ' 
-    '   Blank Lines: 196 (10.55%)
-    '     File Size: 75.86 KB
+' Summaries:
 
 
-    ' Module MSI
-    ' 
-    '     Function: asMSILayer, asRaster, basePeakMz, castSpatialLayers, Correction
-    '               dimension_size, getimzmlMetadata, GetIonsJointMatrix, GetMatrixIons, GetMSIMetadata
-    '               getmzpackFileMetadata, getmzPackMetadata, GetPeakMatrix, getStatTable, GetXySpatialFilter
-    '               IonStats, level_convolution, loadRowSummary, LoadSpotVectorDataFrame, moran_index
-    '               MSI_summary, MSIScanMatrix, open_imzML, packDf, packFile
-    '               packMatrix, PeakMatrix, peakSamples, pixelId, PixelIons
-    '               PixelMatrix, pixels, pixels2D, readPeaklayer, readSummarylayer
-    '               rowScans, SampleBootstraping, scale, scan, spatialConvolution
-    '               splice, write_imzML, writePeaklayer, writeSummarylayer, z_assembler
-    '               z_header, z_volume
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1857
+'    Code Lines: 1116 (60.10%)
+' Comment Lines: 545 (29.35%)
+'    - Xml Docs: 91.74%
+' 
+'   Blank Lines: 196 (10.55%)
+'     File Size: 75.86 KB
+
+
+' Module MSI
+' 
+'     Function: asMSILayer, asRaster, basePeakMz, castSpatialLayers, Correction
+'               dimension_size, getimzmlMetadata, GetIonsJointMatrix, GetMatrixIons, GetMSIMetadata
+'               getmzpackFileMetadata, getmzPackMetadata, GetPeakMatrix, getStatTable, GetXySpatialFilter
+'               IonStats, level_convolution, loadRowSummary, LoadSpotVectorDataFrame, moran_index
+'               MSI_summary, MSIScanMatrix, open_imzML, packDf, packFile
+'               packMatrix, PeakMatrix, peakSamples, pixelId, PixelIons
+'               PixelMatrix, pixels, pixels2D, readPeaklayer, readSummarylayer
+'               rowScans, SampleBootstraping, scale, scan, spatialConvolution
+'               splice, write_imzML, writePeaklayer, writeSummarylayer, z_assembler
+'               z_header, z_volume
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -203,6 +203,9 @@ Module MSI
         Call table.add(NameOf(IonStat.Q1Intensity), ions.Select(Function(i) i.Q1Intensity))
         Call table.add(NameOf(IonStat.Q2Intensity), ions.Select(Function(i) i.Q2Intensity))
         Call table.add(NameOf(IonStat.Q3Intensity), ions.Select(Function(i) i.Q3Intensity))
+        Call table.add(NameOf(IonStat.sparsity), ions.Select(Function(i) i.sparsity))
+        Call table.add(NameOf(IonStat.entropy), ions.Select(Function(i) i.entropy))
+        Call table.add(NameOf(IonStat.rsd), ions.Select(Function(i) i.rsd))
         Call table.add(NameOf(IonStat.moran), ions.Select(Function(i) i.moran))
         Call table.add(NameOf(IonStat.pvalue), ions.Select(Function(i) i.pvalue))
 
--- Rscript/Library/mzkit_app/test/single_cells/create_matrix.R
+++ Rscript/Library/mzkit_app/test/single_cells/create_matrix.R
@@ -4,7 +4,7 @@ imports "SingleCells" from "mzkit";
 
 options(n_threads = 40);
 
-let rawdata = open.mzpack("Y:\项目以外内容\2024\单细胞代谢分析流程测试\datafiles\bulk_group_2.mzPack");
+let rawdata = open.mzpack("Y:\项目以外内容\2024\单细胞代谢分析流程测试\datafiles\bulk_group_9.mzPack");
 let matrix = cell_matrix(rawdata, mz_matrix = TRUE, mzdiff = 0.01);
 
-write.matrix(matrix, file = "Y:\项目以外内容\2024\单细胞代谢分析流程测试\datafiles\bulk_group_2.mzImage");
+write.matrix(matrix, file = "Y:\项目以外内容\2024\单细胞代谢分析流程测试\datafiles\bulk_group_9.mzImage");
