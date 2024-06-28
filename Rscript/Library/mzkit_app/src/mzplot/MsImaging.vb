34db9e3dc6a457ae3352efe4e9eed1d312e69579
谢桂纲 <gg.xie@bionovogene.com>
2024-06-28 14:29:34 +0800

    make improvements of the filter pieline

--- Rscript/Library/mzkit_app/src/mzplot/MsImaging.vb
+++ Rscript/Library/mzkit_app/src/mzplot/MsImaging.vb
@@ -1,62 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::97ed41299e987216a018f8707b4ea305, Rscript\Library\mzkit_app\src\mzplot\MsImaging.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1194
-    '    Code Lines: 773 (64.74%)
-    ' Comment Lines: 286 (23.95%)
-    '    - Xml Docs: 94.06%
-    ' 
-    '   Blank Lines: 135 (11.31%)
-    '     File Size: 50.84 KB
+' Summaries:
 
 
-    ' Module MsImaging
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: (+2 Overloads) asPixels, AutoScaleMax, averageStep, defaultFilter, FilterMz
-    '               GetIntensityData, GetIonLayer, getMSIIons, GetMsMatrx, GetPixel
-    '               intensityFilter, KnnFill, layer, layerTable, LimitIntensityRange
-    '               LoadPixels, MSICoverage, openIndexedCacheFile, parseFilters, plotMSI
-    '               printLayer, renderRowScans, RGB, splitLayer, sumLayer
-    '               tagLayers, testLayer, TrIQRange, viewer, WriteXICCache
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1194
+'    Code Lines: 773 (64.74%)
+' Comment Lines: 286 (23.95%)
+'    - Xml Docs: 94.06%
+' 
+'   Blank Lines: 135 (11.31%)
+'     File Size: 50.84 KB
+
+
+' Module MsImaging
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: (+2 Overloads) asPixels, AutoScaleMax, averageStep, defaultFilter, FilterMz
+'               GetIntensityData, GetIonLayer, getMSIIons, GetMsMatrx, GetPixel
+'               intensityFilter, KnnFill, layer, layerTable, LimitIntensityRange
+'               LoadPixels, MSICoverage, openIndexedCacheFile, parseFilters, plotMSI
+'               printLayer, renderRowScans, RGB, splitLayer, sumLayer
+'               tagLayers, testLayer, TrIQRange, viewer, WriteXICCache
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -71,6 +71,7 @@ Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Blender
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Blender.Scaler
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Blender.Scaler.Scaler
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.IndexedCache
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Pixel
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Reader
@@ -279,6 +280,7 @@ Module MsImaging
     ''' <param name="max"></param>
     ''' <param name="min"></param>
     ''' <returns></returns>
+    ''' <keywords>filter</keywords>
     <ExportAPI("intensityLimits")>
     <RApiReturn(GetType(SingleIonLayer))>
     Public Function LimitIntensityRange(data As SingleIonLayer, max As Double, Optional min As Double = 0) As Object
@@ -295,6 +297,18 @@ Module MsImaging
             .ToArray
 
         Return data
+    End Function
+
+    ''' <summary>
+    ''' apply a single filter or a filter pipeline
+    ''' </summary>
+    ''' <param name="filter"></param>
+    ''' <param name="layer"></param>
+    ''' <returns></returns>
+    ''' <keywords>filter</keywords>
+    <ExportAPI("apply_raster_filter")>
+    Public Function apply_raster_filter(filter As LayerScaler, layer As SingleIonLayer) As SingleIonLayer
+        Return filter.DoIntensityScale(layer)
     End Function
 
     ''' <summary>
