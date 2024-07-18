9f10a854cf372744107442a7b548a3f8688083f1
谢桂纲 <gg.xie@bionovogene.com>
2024-07-18 15:47:49 +0800

    fix of the missing adducts type

--- Rscript/Library/mzkit_app/test/test_adduct_math.R
+++ Rscript/Library/mzkit_app/test/test_adduct_math.R
@@ -1,5 +1,10 @@
 require(mzkit);
 
+find_precursor(mass = 145.0738896, mz =190.96685,
+                                   libtype = -1,
+                                    da  = 0.3) |> str(); 
+
+
 find_precursor(mass = 100, mz =100.2231,
                                    libtype = -1,
                                     da  = 0.3) |> str(); 
