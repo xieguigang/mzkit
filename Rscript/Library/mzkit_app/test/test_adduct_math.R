8c8c4ba45d6693c97bebf8fdb02d2abe23f98c4e
谢桂纲 <gg.xie@bionovogene.com>
2024-07-18 15:23:32 +0800

    confg run test

--- Rscript/Library/mzkit_app/test/test_adduct_math.R
+++ Rscript/Library/mzkit_app/test/test_adduct_math.R
@@ -3,3 +3,7 @@ require(mzkit);
 find_precursor(mass = 100, mz =100.2231,
                                    libtype = -1,
                                     da  = 0.3) |> str(); 
+
+find_precursor(mass = 101, mz =100.2231,
+                                   libtype = -1,
+                                    da  = 0.3) |> str(); 
