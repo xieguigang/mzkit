2f105cf6bfd00455f9087d9efefab41f4eab6d2d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-05 14:50:30 +0800

    read ms2 data

--- src/mzmath/MSEngine/AnnotationPack/ReadPack.vb
+++ src/mzmath/MSEngine/AnnotationPack/ReadPack.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 
 Public Module ReadPack
 
@@ -7,7 +8,29 @@ Public Module ReadPack
     End Function
 
     Public Function ReadMs2Alignment(file As Stream) As Ms2Score
+        Dim bin As New BinaryReader(file)
+        Dim details As New Ms2Score With {
+            .precursor = bin.ReadDouble,
+            .rt = bin.ReadDouble,
+            .intensity = bin.ReadDouble,
+            .score = bin.ReadDouble,
+            .libname = bin.ReadString,
+            .source = bin.ReadString
+        }
+        Dim n As Integer = bin.ReadInt32
+        Dim peaks As ms2() = New ms2(n - 1) {}
 
+        For i As Integer = 0 To n - 1
+            peaks(i) = New ms2 With {
+                .mz = bin.ReadDouble,
+                .intensity = bin.ReadDouble,
+                .Annotation = bin.ReadString
+            }
+        Next
+
+        details.ms2 = peaks
+
+        Return details
     End Function
 
 End Module
