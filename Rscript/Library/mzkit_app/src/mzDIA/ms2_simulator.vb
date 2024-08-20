888c11462a29ea628a1f427ae4cbb0f471ff1d57
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-20 11:04:16 +0800

    parse the single smiles graph data

--- Rscript/Library/mzkit_app/src/mzDIA/ms2_simulator.vb
+++ Rscript/Library/mzkit_app/src/mzDIA/ms2_simulator.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::7da9277c8ecb9cbdc1244ed7f54d1caa, Rscript\Library\mzkit_app\src\mzDIA\ms2_simulator.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 89
-    '    Code Lines: 63 (70.79%)
-    ' Comment Lines: 12 (13.48%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 14 (15.73%)
-    '     File Size: 3.46 KB
+' Summaries:
 
 
-    ' Module ms2_simulator
-    ' 
-    '     Function: embed_graph2vector, loadKCF, MolecularGraph_func
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 89
+'    Code Lines: 63 (70.79%)
+' Comment Lines: 12 (13.48%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 14 (15.73%)
+'     File Size: 3.46 KB
+
+
+' Module ms2_simulator
+' 
+'     Function: embed_graph2vector, loadKCF, MolecularGraph_func
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -80,32 +80,47 @@ Module ms2_simulator
     ''' parse the smiles structure string as molecular network graph
     ''' </summary>
     ''' <param name="mol"></param>
+    ''' <param name="id">
+    ''' tag the id data with the corresponding smiles graph data, this character id vector
+    ''' length should be equals to the given molecules vector size.
+    ''' </param>
     ''' <param name="verbose"></param>
     ''' <returns></returns>
     <ExportAPI("molecular.graph")>
     <RApiReturn(GetType(NetworkGraph))>
     Public Function MolecularGraph_func(<RRawVectorArgument>
                                         mol As Object,
+                                        Optional id As Object = Nothing,
+                                        Optional digest_formula As Boolean = True,
                                         Optional verbose As Boolean = False,
                                         Optional env As Environment = Nothing) As Object
 
-        Return env.EvaluateFramework(Of String, NetworkGraph)(
+        Return env.EvaluateFramework(Of String, Object)(
             x:=mol,
             eval:=Function(smiles)
-                      If verbose Then
-                          Call VBDebugger.EchoLine(smiles)
-                      End If
-
-                      Dim chemical_struct As ChemicalFormula = ParseChain.ParseGraph(smiles, strict:=False)
-                      Dim molecular_graph As NetworkGraph = chemical_struct.AsGraph
-
-                      If verbose Then
-                          Call VBDebugger.WaitOutput()
-                          Call Console.WriteLine(molecular_graph.vertex.Select(Function(v) v.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE)).GetJson)
-                      End If
-
-                      Return molecular_graph
+                      Return parseSingleSmiles(smiles, verbose, digest_formula)
                   End Function)
+    End Function
+
+    Private Function parseSingleSmiles(smiles As String, verbose As Boolean, digest_formula As Boolean) As Object
+        If verbose Then
+            Call VBDebugger.EchoLine(smiles)
+        End If
+
+        Dim chemical_struct As ChemicalFormula = ParseChain.ParseGraph(smiles, strict:=False)
+
+        If digest_formula Then
+            Dim molecular_graph As NetworkGraph = chemical_struct.AsGraph
+
+            If verbose Then
+                Call VBDebugger.WaitOutput()
+                Call Console.WriteLine(molecular_graph.vertex.Select(Function(v) v.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE)).GetJson)
+            End If
+
+            Return molecular_graph
+        Else
+            Return chemical_struct
+        End If
     End Function
 
     ''' <summary>
