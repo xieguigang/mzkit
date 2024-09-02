4bedf4fe4261f03b4462b9b23cd484429f65c70c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-02 15:02:39 +0800

    tweakso  tf the plot styles

--- src/visualize/plot/PlotMassWindowXIC.vb
+++ src/visualize/plot/PlotMassWindowXIC.vb
@@ -181,7 +181,7 @@ Public Class PlotMassWindowXIC : Inherits Plot
             }
         Next
 
-        Call New Scatter2D({xic_dat}, theme).Plot(g, layout:=part1)
-        Call New Scatter2D(mass_scatter, theme).Plot(g, layout:=part2)
+        Call New Scatter2D({xic_dat}, theme) With {.xlabel = "Retention Time(s)", .ylabel = "Intensity"}.Plot(g, layout:=part1)
+        Call New Scatter2D(mass_scatter, theme) With {.xlabel = "Retention Time(s)", .ylabel = "M/Z"}.Plot(g, layout:=part2)
     End Sub
 End Class
