b51342bb90c9bbed356fa5c7d17451c59bfeb0ab
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-10 10:35:12 +0800

    add xml dev comment docs

--- src/mzmath/MSEngine/AnnotationPack/Report/IReportRender.vb
+++ src/mzmath/MSEngine/AnnotationPack/Report/IReportRender.vb
@@ -9,6 +9,13 @@
     Property colorSet As String()
 
     Function GetIon(xcms_id As String) As AlignmentHit
+
+    ''' <summary>
+    ''' implements of the html table report
+    ''' </summary>
+    ''' <param name="refSet"></param>
+    ''' <param name="rt_cell"></param>
+    ''' <returns>this function generates the html table code for view report</returns>
     Function Tabular(refSet As IEnumerable(Of String), Optional rt_cell As Boolean = False) As IEnumerable(Of String)
 
 End Interface
