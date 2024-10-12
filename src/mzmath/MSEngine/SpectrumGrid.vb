6a4f51ee7a9bbc988ab5bd530087cd3927b36e9b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-12 17:14:37 +0800

    binary clustering model

---
+++ src/mzmath/MSEngine/SpectrumGrid.vb
@@ -0,0 +1,13 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+''' <summary>
+''' Make sample spectrum aligns to the peaktable based on the pearson correlation method
+''' </summary>
+Public Class SpectrumGrid
+
+    Public Iterator Function Clustering(rawdata As IEnumerable(Of NamedCollection(Of PeakMs2))) As IEnumerable(Of NamedCollection(Of PeakMs2))
+
+    End Function
+
+End Class
---
+++ src/mzmath/MoleculeNetworking/Tree/BinaryClustering.vb
@@ -0,0 +1,45 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+
+''' <summary>
+''' Spectrum binary tree clustering helper
+''' </summary>
+Public Class BinaryClustering
+
+    Friend ReadOnly align As MSScoreGenerator
+    Friend ReadOnly equals_cutoff As Double = 0.85
+
+    Sub New(Optional mzdiff As Double = 0.3,
+            Optional intocutoff As Double = 0.05,
+            Optional equals As Double = 0.85,
+            Optional interval As Double = 0.1)
+
+        align = NetworkingTree.CreateAlignment(mzdiff, intocutoff, equals)
+        equals_cutoff = equals
+    End Sub
+
+    ''' <summary>
+    ''' clear the spectrum cache
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function Clear() As BinaryClustering
+        Call align.Clear()
+        Return Me
+    End Function
+
+    ''' <summary>
+    ''' Just add the spectrum data to memory cache
+    ''' </summary>
+    ''' <param name="spec"></param>
+    Public Sub Add(spec As PeakMs2)
+        Call align.Add(spec)
+    End Sub
+
+    ''' <summary>
+    ''' do spectrum tree alignment
+    ''' </summary>
+    ''' <param name="ions"></param>
+    ''' <returns></returns>
+    Public Function Tree(ions As IEnumerable(Of PeakMs2)) As BinaryClustering
+
+    End Function
+End Class
