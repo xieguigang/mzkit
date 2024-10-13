76d828771b5deafc4480c23912ea0c38e91ca325
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-13 11:41:32 +0800

    make peak assigned

--- src/mzmath/MoleculeNetworking/SpectrumGrid.vb
+++ src/mzmath/MoleculeNetworking/SpectrumGrid.vb
@@ -1,14 +1,20 @@
 ﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.Correlations
+Imports std = System.Math
 
 ''' <summary>
 ''' Make sample spectrum aligns to the peaktable based on the pearson correlation method
 ''' </summary>
 Public Class SpectrumGrid
 
-    Dim clusters As SpectrumLine()
+    ''' <summary>
+    ''' spectrum cluster which is indexed via the rt window
+    ''' </summary>
+    Dim clusters As BlockSearchFunction(Of SpectrumLine)
     Dim filenames As String()
 
     Sub New()
@@ -16,7 +22,7 @@ Public Class SpectrumGrid
     End Sub
 
     Public Function SetRawDataFiles(files As IEnumerable(Of NamedCollection(Of PeakMs2))) As SpectrumGrid
-        clusters = Clustering(files).ToArray
+        clusters = New BlockSearchFunction(Of SpectrumLine)(Clustering(files), Function(a) a.rt, 5, fuzzy:=True)
         Return Me
     End Function
 
@@ -63,9 +69,25 @@ Public Class SpectrumGrid
         Next
     End Function
 
-    Public Iterator Function AssignPeaks(peaks As IEnumerable(Of xcms2)) As IEnumerable(Of (peak As xcms2, ms2 As PeakMs2(), cor As Double, score As Double))
+    Public Iterator Function AssignPeaks(peaks As IEnumerable(Of xcms2)) As IEnumerable(Of (peak As xcms2, ms2 As PeakMs2(), cor As Double, score As Double, pval As Double))
+        Dim q As New SpectrumLine
+
         For Each peak As xcms2 In peaks
             Dim i1 As Double() = peak(filenames)
+            Dim candidates = clusters _
+                .Search(q.SetRT(peak.rt)) _
+                .Select(Function(c)
+                            Dim cor As Double, pval As Double
+                            cor = Correlations.GetPearson(i1, c.intensity, prob2:=pval)
+                            Return (c, cor, pval, score:=cor / (pval + 0.00001) / (std.Abs(peak.rt - c.rt) + 1))
+                        End Function) _
+                .OrderByDescending(Function(c) c.cor) _
+                .Take(3) _
+                .ToArray
+
+            For Each candidate In candidates
+                Yield (peak, candidate.c.cluster, candidate.cor, candidate.score, candidate.pval)
+            Next
         Next
     End Function
 
@@ -76,5 +98,10 @@ Public Class SpectrumLine
     Public Property cluster As PeakMs2()
     Public Property intensity As Double()
     Public Property rt As Double
+
+    Friend Function SetRT(rt As Double) As SpectrumLine
+        _rt = rt
+        Return Me
+    End Function
 
 End Class
