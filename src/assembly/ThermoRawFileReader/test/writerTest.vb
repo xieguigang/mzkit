d140eb7a2b80bdcab7858edcc8266aeb69ee4899
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-16 13:25:55 +0800

    writer is not working

---
+++ src/assembly/ThermoRawFileReader/test/writerTest.vb
@@ -0,0 +1,14 @@
+﻿Imports ThermoFisher.CommonCore.Data.Business
+Imports ThermoFisher.CommonCore.Data.Interfaces
+
+Module writerTest
+
+    Sub Main()
+        Dim worker As New RawFileFactory
+        Dim rawfile As IRawFileWriter = worker.OpenFile("./aaa.raw")
+
+        rawfile.UpdateCreationDate(Now)
+
+        Pause()
+    End Sub
+End Module
