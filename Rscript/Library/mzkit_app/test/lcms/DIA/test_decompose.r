4e89c7909b3b736de3d9bb929b660dc1c34e0505
谢桂纲 <gg.xie@bionovogene.com>
2024-10-21 11:05:26 +0800

    config for run demo test

--- Rscript/Library/mzkit_app/test/lcms/DIA/test_decompose.r
+++ Rscript/Library/mzkit_app/test/lcms/DIA/test_decompose.r
@@ -2,3 +2,16 @@ require(mzkit);
 
 imports "DIA" from "mzDIA";
 
+setwd(@dir);
+
+let testdata = read.csv("./test_ms2.csv", row.names = NULL, check.names = FALSE);
+
+print(testdata, max.print= 6);
+
+let spec = mzweb::parse_base64( mz = testdata$mz,
+                                  intensity= testdata$intensity,
+                                  id = testdata$splash_id);
+
+print(spec);
+
+let decompose = DIA::dia_nmf(spec, n = 5);
