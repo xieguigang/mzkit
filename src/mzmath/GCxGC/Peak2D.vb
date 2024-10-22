1a33be172a15833963ae912bda39987fad3a9372
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-22 15:14:19 +0800

    create GCxGC 2d peak model

---
+++ src/mzmath/GCxGC/Peak2D.vb
@@ -0,0 +1,13 @@
+Public Class Peak2D
+
+    Public Property id As String
+    Public Property rt1 As Double
+    Public Property rt2 As Double
+    Public Property rtmin1 As Double
+    Public Property rtmax1 As Double
+    Public Property rtmin2 As Double
+    Public Property rtmax2 As Double
+    Public Property volumn As Double
+    Public Property maxIntensity As Double
+
+End Class
