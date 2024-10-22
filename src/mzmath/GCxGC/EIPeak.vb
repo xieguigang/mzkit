bd3bc27409efd67a2696d18d33c212397fc6211c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-22 16:49:26 +0800

    add xml comment notes

--- src/mzmath/GCxGC/EIPeak.vb
+++ src/mzmath/GCxGC/EIPeak.vb
@@ -1,10 +1,26 @@
-﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 
 Public Class EIPeak(Of T)
 
+    ''' <summary>
+    ''' the ms1 peak
+    ''' </summary>
+    ''' <returns></returns>
     Public Property peak As T
+    ''' <summary>
+    ''' all spectrum that pick from current peak rt range.
+    ''' </summary>
+    ''' <returns></returns>
     Public Property spectrum As LibraryMatrix()
 
+    ''' <summary>
+    ''' make average spectrum of current <see cref="spectrum"/> collection.
+    ''' </summary>
+    ''' <param name="centroid"></param>
+    ''' <returns></returns>
+    ''' 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetRepresentativeSpectrum(Optional centroid As Double = 0.1) As LibraryMatrix
         Return spectrum.SpectrumSum(centroid, average:=True)
     End Function
