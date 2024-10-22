be72cc948c954fcdf5a5ea115959d8ebef7b2622
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-22 16:57:28 +0800

    add data model

---
+++ src/metadb/Massbank/Public/LOTUS/NaturalProduct.vb
@@ -0,0 +1,20 @@
+﻿Namespace LOTUS
+
+    ''' <summary>
+    ''' Natural Products Online is an open source project for Natural Products (NPs) storage,
+    ''' search and analysis. This page hosts LOTUS, one of the biggest and best annotated
+    ''' resources for NPs occurrences available free of charge and without any restriction. 
+    ''' LOTUS is a living database, which is hosted both here and on Wikidata.The Wikidata 
+    ''' version allows for community curation and addition of novel data. The current version
+    ''' allows a more user friendly experience (such as structural search, taxonomy oriented 
+    ''' query, flat table and structures exports). If you use LOTUS in your research, please 
+    ''' cite the following work: Adriano Rutz, Maria Sorokina, Jakub Galgonek, Daniel Mietchen,
+    ''' Egon Willighagen, Arnaud Gaudry, James G Graham, Ralf Stephan, Roderic Page, Jiří 
+    ''' Vondrášek, Christoph Steinbeck, Guido F Pauli, Jean-Luc Wolfender, Jonathan Bisson, 
+    ''' Pierre-Marie Allard (2022) The LOTUS initiative for open knowledge management in 
+    ''' natural products research. eLife 11:e70780. https://doi.org/10.7554/eLife.70780
+    ''' </summary>
+    Public Class NaturalProduct
+
+    End Class
+End Namespace
