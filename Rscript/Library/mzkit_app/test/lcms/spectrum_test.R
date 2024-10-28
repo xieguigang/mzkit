7fef5f04485f68002a274dd4dcf7adf4f83c8031
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-28 11:12:37 +0800

    test of the neg data

--- Rscript/Library/mzkit_app/test/lcms/spectrum_test.R
+++ Rscript/Library/mzkit_app/test/lcms/spectrum_test.R
@@ -3,12 +3,12 @@ require(mzkit);
 imports "MoleculeNetworking" from "mzDIA";
 imports "mzDeco" from "mz_quantify";
 
-let source_dir = "\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\POS\mzPack";
+let source_dir = "\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\NEG\mzPack";
 let rawfiles = list.files(source_dir, pattern = "*.mzPack");
 
 print(basename(rawfiles ));
 
-let peaks = read.xcms_peaks("\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\POS\mzXML\peak_tablenew.txt", 
+let peaks = read.xcms_peaks("\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\NEG\mzXML\peak_tablenew_nofill.txt", 
     tsv = TRUE);
 
 peaks <- mzkit::preprocessing_expression(peaks, 
@@ -40,19 +40,19 @@ print(`${100*as.integer(attr(unassigned,"unmapped_clusters"))/as.integer(attr(un
 print("spectrum unmapping ratio:");
 print(`${100*as.integer(attr(unassigned,"unmapped_spectrum"))/as.integer(attr(unassigned,"total_spectrum"))}% (${attr(unassigned,"unmapped_spectrum")}/${attr(unassigned,"total_spectrum")})`);
 
-write.csv(as.data.frame(assigned), file = "\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\POS\mzPack\test.csv");
+write.csv(as.data.frame(assigned), file = "\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\NEG\mzPack\test.csv");
 
 assigned
 |> JSON::json_encode()
-|> writeLines(con = "\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\POS\mzPack\test.json")
+|> writeLines(con = "\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\NEG\mzPack\test.json")
 ;
 
 assigned = unpack_assign(assigned);
 
 for(let name in names(assigned)) {
-    write.cache(assigned[[name]], file = `\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\POS\mzPack\cache/${name}.cache`);
+    write.cache(assigned[[name]], file = `\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\NEG\mzPack\cache/${name}.cache`);
 }
 
 for(let name in names(unassigned)) {
-    write.cache(unassigned[[name]], file = `\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\POS\mzPack\unmapped/${name}.cache`);
+    write.cache(unassigned[[name]], file = `\\192.168.1.254\backup3\项目以外内容\human_reference_metabolome\benchmark\MTBLS6039\FILES\RAW_FILES\NEG\mzPack\unmapped/${name}.cache`);
 }
