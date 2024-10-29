2b7023de0d1ea210d61546e87b722dd0c614ed25
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-29 18:39:14 +0800

    how to make conversion

--- src/metadb/Chemoinformatics/InChI/InChI.vb
+++ src/metadb/Chemoinformatics/InChI/InChI.vb
@@ -1,65 +1,66 @@
 ﻿#Region "Microsoft.VisualBasic::d04133506ca8980be7e02324aaa5bfd4, metadb\Chemoinformatics\InChI\InChI.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 64
-    '    Code Lines: 42 (65.62%)
-    ' Comment Lines: 12 (18.75%)
-    '    - Xml Docs: 83.33%
-    ' 
-    '   Blank Lines: 10 (15.62%)
-    '     File Size: 2.52 KB
+' Summaries:
 
 
-    '     Class InChI
-    ' 
-    '         Properties: Charge, FixedH, Isotopic, IsStandard, Key
-    '                     Main, Reconnected, Stereochemical, Version
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: Parse, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 64
+'    Code Lines: 42 (65.62%)
+' Comment Lines: 12 (18.75%)
+'    - Xml Docs: 83.33%
+' 
+'   Blank Lines: 10 (15.62%)
+'     File Size: 2.52 KB
+
+
+'     Class InChI
+' 
+'         Properties: Charge, FixedH, Isotopic, IsStandard, Key
+'                     Main, Reconnected, Stereochemical, Version
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: Parse, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports BioNovoGene.BioDeep.Chemoinformatics.SDF.Models
 Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
 
 Namespace IUPAC.InChI
@@ -105,6 +106,14 @@ Namespace IUPAC.InChI
             FixedH = Layer.ParseFixedHLayer(populator)
             Reconnected = Layer.ParseReconnectedLayer(populator)
         End Sub
+
+        ''' <summary>
+        ''' convert the inchi structure to SDF structure data
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function GetStruct() As [Structure]
+
+        End Function
 
         ''' <summary>
         ''' Generate A InChI string base on the layer information
