79c96498bf3d22a66daf87a86f93be58cd64175c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 13:31:38 +0800

    add cas metabolite data model

---
+++ src/metadb/Massbank/Public/CAS.vb
@@ -0,0 +1,35 @@
+﻿Public Class CAS
+
+    Public Property uri As String
+    Public Property rn As String
+    Public Property name As String
+    Public Property images As String()
+    Public Property inchi As String
+    Public Property inchikey As String
+    Public Property smile As String
+    Public Property canonicalSmile As String
+    Public Property molecularFormula As String
+    Public Property molecularMass As String
+    Public Property experimentalProperties As experimentalProperty()
+    Public Property propertyCitations As propertyCitation()
+    Public Property synonyms As String()
+    Public Property replacedRns As String()
+    Public Property hasMolfile As Boolean
+
+End Class
+
+Public Class propertyCitation
+
+    Public Property docUri As String
+    Public Property sourceNumber As Integer
+    Public Property source As String
+
+End Class
+
+Public Class experimentalProperty
+
+    Public Property name As String
+    Public Property [property] As String
+    Public Property sourceNumber As Integer
+
+End Class
