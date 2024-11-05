f96f4af5392f103bc394e1d1b52ef7951acfa937
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-05 13:35:29 +0800

    export and save the composition data

--- Rscript/Library/mzkit_app/test/lcms/DIA/test_dia2.R
+++ Rscript/Library/mzkit_app/test/lcms/DIA/test_dia2.R
@@ -14,6 +14,9 @@ for(let line in cluster |> spectrum_clusters()) {
     let decompose = line |> dia_nmf(n = 2);
     let sum = attr(decompose, "sum_spectrum");
 
+    write.csv(attr(decompose,"sample_composition"), file = `group_sample_composition.csv`);
+    write.csv(attr(decompose,"ionpeaks_composition"), file = `group_ionpeaks_composition.csv`);
+
     for(name in names(sum)) {
         svg(file = `group_${i}-LIPID_${name}.svg`, size = [3000 2000]) {
             plot(sum[[name]]);
