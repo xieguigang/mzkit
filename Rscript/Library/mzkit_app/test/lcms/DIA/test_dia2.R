2103c28dddf147555bb087c5bbe3b469061220aa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-05 10:23:01 +0800

    test script for make spectrum decompose

--- Rscript/Library/mzkit_app/test/lcms/DIA/test_dia2.R
+++ Rscript/Library/mzkit_app/test/lcms/DIA/test_dia2.R
@@ -9,4 +9,13 @@ setwd(@dir);
 let spectrum = read.cache("F:\testdata.spec");
 let cluster = spectrum_grid(spectrum);
 
+for(let line in cluster) {
+    let decompose = line |> dia_nmf(n = 2);
+    let sum = attr(decompose, "sum_spectrum");
 
+    for(name in names(sum)) {
+        svg(file = `LIPID_${name}.svg`) {
+            plot(sum[[name]]);
+        }
+    }
+}
