82d61bfbae59abb04155fb0edfe9b2200ecde567
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-08 11:43:21 +0800

    export table test success

--- Rscript/Library/mzkit_app/test/dataframe_dump.R
+++ Rscript/Library/mzkit_app/test/dataframe_dump.R
@@ -3,7 +3,39 @@ require(mzkit);
 imports "pubchem_kit" from "mzkit";
 imports "massbank" from "mzkit";
 
+let mapping = readText("F:\.pubchem_cache\mapping.json") |> JSON::json_decode();
 let files = list.files("F:\.pubchem_cache\pugViews", pattern = "*.xml", recursive = TRUE);
 let metadata = sapply(files, path -> read.pugView(path));
 
-write.csv(as.data.frame(metadata), file = "F:/metabolite_exports.csv");
+mapping = lapply(mapping, cid -> `PubChem:${cid}`);
+metadata = as.data.frame(metadata);
+
+let cols = colnames(metadata);
+
+metadata = as.list(metadata, byrow = TRUE);
+
+str(metadata);
+
+let join = [];
+
+for(let q in names(mapping)) {
+    for(let id in mapping[[q]]) {
+        let xi =as.list( metadata[[id]]);
+        if (!is.null(xi)) {
+            xi$query = q;
+            join = append(join, xi);
+        }
+    }
+}
+
+str(join);
+
+metadata = data.frame(
+    query = join@query
+);
+
+for(name in cols) {
+    metadata[, name] = sapply( join, i -> i[[name]]);
+}
+
+write.csv(metadata, file = "F:/metabolite_exports.csv");
