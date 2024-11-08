3e45ef2a362e3a27523ea74c4534864e38be401c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-08 11:09:52 +0800

    fix of the missing contents

--- src/metadb/Massbank/Public/NCBI/PubChem/Web/MetaInfoReader.vb
+++ src/metadb/Massbank/Public/NCBI/PubChem/Web/MetaInfoReader.vb
@@ -430,9 +430,16 @@ Namespace NCBI.PubChem
                 desc.FlashPoint = experiments.safeProject(key:="Flash Point", AddressOf getValues)
                 desc.Density = experiments.safeProject(key:="Density", AddressOf getValues)
                 desc.VaporPressure = experiments.safeProject(key:="Vapor Pressure", AddressOf getValues)
+
                 desc.Odor = experiments.safeProject(key:="Odor", AddressOf getValues)
                 desc.Taste = experiments.safeProject(key:="Taste", AddressOf getValues)
                 desc.Color = experiments.safeProject(key:="Color/Form", AddressOf getValues)
+
+                Dim physical = experiments.safeProject("Physical Description", AddressOf getValues)
+
+                If desc.Odor Is Nothing Then
+                    desc.Odor = physical
+                End If
             End If
 
             Return desc
