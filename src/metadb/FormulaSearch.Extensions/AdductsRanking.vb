011a13e1dde4e4368331f572367e04e792e54c7f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-12 11:38:53 +0800

    A helper tools for make adducts ions ranking

---
+++ src/metadb/FormulaSearch.Extensions/AdductsRanking.vb
@@ -0,0 +1,33 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
+
+''' <summary>
+''' A helper tools for make adducts ions ranking
+''' </summary>
+Public Class AdductsRanking
+
+    ReadOnly ion As IonModes
+
+    Sub New(ion As IonModes)
+        Me.ion = ion
+    End Sub
+
+    Public Function Rank(formula_str As String, adduct_str As String) As Double
+        Dim formula As Formula = FormulaScanner.ScanFormula(formula_str)
+        Dim adduct As MzCalculator = Provider.ParseAdductModel(adduct_str)
+
+        If ion = IonModes.Positive Then
+            Return RankPositive(formula, adduct)
+        Else
+            Return RankNegative(formula, adduct)
+        End If
+    End Function
+
+    Private Function RankPositive(formula As Formula, adduct As MzCalculator) As Double
+
+    End Function
+
+    Private Function RankNegative(formula As Formula, adduct As MzCalculator) As Double
+
+    End Function
+End Class
