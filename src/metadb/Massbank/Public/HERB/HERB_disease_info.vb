be28a4fac41fb90ab677372ed61e1acf41628c6d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-18 10:43:28 +0800

    add database fields

--- src/metadb/Massbank/Public/HERB/HERB_disease_info.vb
+++ src/metadb/Massbank/Public/HERB/HERB_disease_info.vb
@@ -2,5 +2,18 @@
 
     Public Class HERB_disease_info
 
+        Public Property Disease_id As String
+        Public Property DisGENet_disease_id As String
+        Public Property Disease_name As String
+        Public Property Disease_type As String
+        Public Property DiseaseClass_MeSH As String
+        Public Property DiseaseClassName_MeSH As String
+        Public Property HPO_ClassId As String
+        Public Property HPO_ClassName As String
+        Public Property DO_ClassId As String
+        Public Property DO_ClassName As String
+        Public Property UMLS_SemanticTypeId As String
+        Public Property UMLS_SemanticTypeName As String
+
     End Class
 End Namespace
