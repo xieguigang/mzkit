db55da540a789ab1e2e1f0e4afe1b030ea3c6667
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-23 19:27:41 +0800

    implements of the abstract graph model

--- src/metadb/SMILES/Graph/ChemicalFormula.vb
+++ src/metadb/SMILES/Graph/ChemicalFormula.vb
@@ -1,69 +1,71 @@
 ﻿#Region "Microsoft.VisualBasic::4e057b728c96e5b8aa7bc64faf6f73f0, metadb\SMILES\Graph\ChemicalFormula.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 135
-    '    Code Lines: 100 (74.07%)
-    ' Comment Lines: 16 (11.85%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 19 (14.07%)
-    '     File Size: 4.70 KB
+' Summaries:
 
 
-    ' Class ChemicalFormula
-    ' 
-    '     Properties: AllBonds, AllElements, EmpiricalFormula
-    ' 
-    '     Function: Decomposition, FindKeys, GetFormula, Join, ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 135
+'    Code Lines: 100 (74.07%)
+' Comment Lines: 16 (11.85%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 19 (14.07%)
+'     File Size: 4.70 KB
+
+
+' Class ChemicalFormula
+' 
+'     Properties: AllBonds, AllElements, EmpiricalFormula
+' 
+'     Function: Decomposition, FindKeys, GetFormula, Join, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
 Imports Microsoft.VisualBasic.Data.GraphTheory.Network
+Imports Microsoft.VisualBasic.Data.GraphTheory.SparseGraph
 Imports EmpiricalFormula = BioNovoGene.BioDeep.Chemoinformatics.Formula.Formula
 
 ''' <summary>
 ''' the molecule graph
 ''' </summary>
 Public Class ChemicalFormula : Inherits NetworkGraph(Of ChemicalElement, ChemicalKey)
+    Implements ISparseGraph
 
     ''' <summary>
     ''' the graph edges is the connection links between the atom groups
@@ -194,5 +196,11 @@ Public Class ChemicalFormula : Inherits NetworkGraph(Of ChemicalElement, Chemica
 
     Public Overrides Function ToString() As String
         Return GetFormula.ToString
+    End Function
+
+    Public Iterator Function GetGraph() As IEnumerable(Of IInteraction) Implements ISparseGraph.GetGraph
+        For Each edge As ChemicalKey In graphEdges
+            Yield edge
+        Next
     End Function
 End Class
