f7081f1af5f8d80b956d51495804bc9fb22d6f69
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-26 09:22:38 +0800

    extract content data success

--- src/mzmath/TargetedMetabolomics/LinearQuantitative/Models/DataFile.vb
+++ src/mzmath/TargetedMetabolomics/LinearQuantitative/Models/DataFile.vb
@@ -1,15 +1,19 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 
 Namespace LinearQuantitative
 
     Public Class DataFile
 
-        Public Property filename As String
-        Public Property compounds As IonPeakTableRow()
+        <XmlAttribute> Public Property filename As String
+        <XmlElement> Public Property compounds As IonPeakTableRow()
 
         Sub New(filename As String, compounds As IEnumerable(Of IonPeakTableRow))
             _filename = filename
             _compounds = compounds.ToArray
+        End Sub
+
+        Sub New()
         End Sub
 
         Public Overrides Function ToString() As String
