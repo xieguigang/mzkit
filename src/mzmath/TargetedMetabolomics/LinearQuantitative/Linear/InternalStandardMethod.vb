5e12a2dc14176305c0cbebbe9f731d05ec281007
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-26 17:53:06 +0800

    create linear models

--- src/mzmath/TargetedMetabolomics/LinearQuantitative/Linear/InternalStandardMethod.vb
+++ src/mzmath/TargetedMetabolomics/LinearQuantitative/Linear/InternalStandardMethod.vb
@@ -57,6 +57,7 @@
 #End Region
 
 Imports System.Drawing
+Imports System.Runtime.InteropServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Content
 Imports Microsoft.VisualBasic.Data.Bootstrapping
@@ -117,36 +118,29 @@ Namespace LinearQuantitative.Linear
         End Function
 
         ''' <summary>
-        ''' linear regression
+        ''' 
         ''' </summary>
-        ''' <param name="ionGroups"></param>
+        ''' <param name="ionGroups">
+        ''' single compound reference points data
+        ''' </param>
         ''' <param name="ionKey"></param>
-        ''' <param name="linearSamples"></param>
         ''' <returns></returns>
-        Private Function ToFeatureLinear(ionGroups As Dictionary(Of String, TargetPeakPoint()),
-                                         ionKey As String,
-                                         linearSamples As String()) As StandardCurve
-
+        Public Function ToFeatureLinear(ionGroups As IEnumerable(Of IonPeakTableRow), ionKey As String) As StandardCurve
             Dim define As Standards = contents.GetStandards(ionKey)
-            Dim rawPoints As TargetPeakPoint() = ionGroups(ionKey)
-            Dim points As New List(Of ReferencePoint)
-            Dim A As Double() = TPA(linearSamples, rawPoints)
-            Dim ISTPA As Double()
+            Dim compound As IonPeakTableRow() = ionGroups.ToArray
+            Dim A As Double() = compound.Select(Function(i) i.TPA).ToArray
+            Dim ISTPA As Double() = compound.Select(Function(i) i.TPA_IS).ToArray
+            Dim linearSamples As String() = compound.Select(Function(i) i.raw).ToArray
 
-            If Not define.ISTD.StringEmpty Then
-                If Not ionGroups.ContainsKey(define.ISTD) Then
-                    Call $"target linear required IS reference '{define.ISTD}', but ion is missing in sample data!".Warning
-                    ISTPA = Nothing
-                Else
-                    ISTPA = TPA(linearSamples, ionGroups(define.ISTD))
-                End If
-            Else
-                ISTPA = Nothing
-            End If
+            Return CreateModel(linearSamples, A, ISTPA, define)
+        End Function
 
+        Private Function CreateModel(linearSamples As String(), A#(), ISTPA#(), define As Standards) As StandardCurve
+            Dim ionKey As String = define.ID
             Dim C As Double() = linearSamples.Select(Function(level) contents(level, ionKey)).ToArray
             Dim CIS As Double = 1
             Dim invalids As New List(Of PointF)
+            Dim points As New List(Of ReferencePoint)
             Dim line As PointF() = QuantificationWorker _
                 .CreateModelPoints(C, A, ISTPA, CIS, ionKey, define.Name, linearSamples, points) _
                 .ToArray
@@ -184,6 +178,37 @@ Namespace LinearQuantitative.Linear
             Next
 
             Return out
+        End Function
+
+        ''' <summary>
+        ''' linear regression
+        ''' </summary>
+        ''' <param name="ionGroups"></param>
+        ''' <param name="ionKey"></param>
+        ''' <param name="linearSamples"></param>
+        ''' <returns></returns>
+        Private Function ToFeatureLinear(ionGroups As Dictionary(Of String, TargetPeakPoint()),
+                                         ionKey As String,
+                                         linearSamples As String()) As StandardCurve
+
+            Dim define As Standards = contents.GetStandards(ionKey)
+            Dim rawPoints As TargetPeakPoint() = ionGroups(ionKey)
+            Dim points As New List(Of ReferencePoint)
+            Dim A As Double() = TPA(linearSamples, rawPoints)
+            Dim ISTPA As Double()
+
+            If Not define.ISTD.StringEmpty Then
+                If Not ionGroups.ContainsKey(define.ISTD) Then
+                    Call $"target linear required IS reference '{define.ISTD}', but ion is missing in sample data!".Warning
+                    ISTPA = Nothing
+                Else
+                    ISTPA = TPA(linearSamples, ionGroups(define.ISTD))
+                End If
+            Else
+                ISTPA = Nothing
+            End If
+
+            Return CreateModel(linearSamples, A, ISTPA, define)
         End Function
 
         ''' <summary>
