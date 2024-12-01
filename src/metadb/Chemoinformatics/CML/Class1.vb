43140361f18eb0d6c5db69a0f370592790db2973
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-01 21:46:33 +0800

    create molecule model

--- src/metadb/Chemoinformatics/CML/Class1.vb
+++ src/metadb/Chemoinformatics/CML/Class1.vb
@@ -9,4 +9,42 @@ Namespace ChemicalMarkupLanguage
         <XmlAttribute> Public Property order As Integer
 
     End Class
+
+    Public Class atom
+
+        <XmlAttribute> Public Property id As String
+        <XmlAttribute> Public Property elementType As String
+        <XmlAttribute> Public Property hydrogenCount As Integer
+        <XmlAttribute> Public Property x2 As Double
+        <XmlAttribute> Public Property y2 As Double
+
+    End Class
+
+    Public Class molecule
+
+        <XmlAttribute>
+        Public Property id As String
+
+        Public Property atomArray As atomArray
+        Public Property bondArray As bondArray
+
+    End Class
+
+    Public Class ArrayList
+
+        <XmlAttribute> Public Property id As String
+
+    End Class
+
+    Public Class atomArray : Inherits ArrayList
+
+        <XmlElement("atom")> Public Property atoms As atom()
+
+    End Class
+
+    Public Class bondArray : Inherits ArrayList
+
+        <XmlElement("bond")> Public Property bonds As bond()
+
+    End Class
 End Namespace
