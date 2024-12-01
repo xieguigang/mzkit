d3a263214504bf31e1532b4b6e0ad90503e46538
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-01 21:50:35 +0800

    create markup model

---
+++ src/metadb/Chemoinformatics/CML/Class1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Xml.Serialization
+
+Namespace ChemicalMarkupLanguage
+
+    <XmlRoot("cml", [Namespace]:=MarkupFile.xmlns)>
+    <XmlType("cml", [Namespace]:=MarkupFile.xmlns)>
+    Public Class MarkupFile
+
+        Public Const xmlns As String = "http://www.xml-cml.org/schema"
+
+        <XmlAttribute>
+        Public Property title As String
+        Public Property molecule As molecule
+
+    End Class
+End Namespace
---
+++ src/metadb/SMILES/ChemicalMarkup.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.BioDeep.Chemoinformatics.ChemicalMarkupLanguage
+
+Public Module ChemicalMarkup
+
+    ''' <summary>
+    ''' cast the smiles graph as chemical markup language model data
+    ''' </summary>
+    ''' <param name="graph"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function AsCML(graph As ChemicalFormula) As molecule
+
+    End Function
+
+End Module
