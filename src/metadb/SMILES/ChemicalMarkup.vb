fb99dedb5e0dd7565361432aac3720750d7d7248
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-01 21:58:03 +0800

    cast smiles graph to chemical markup language data

--- src/metadb/SMILES/ChemicalMarkup.vb
+++ src/metadb/SMILES/ChemicalMarkup.vb
@@ -16,6 +16,22 @@ Public Module ChemicalMarkup
         Dim atoms As New List(Of ChemicalMarkupLanguage.atom)
         Dim bonds As New List(Of ChemicalMarkupLanguage.bond)
 
+        For Each element As ChemicalElement In graph.AllElements
+            Call atoms.Add(New ChemicalMarkupLanguage.atom With {
+                .elementType = element.elementName,
+                .hydrogenCount = element.hydrogen,
+                .id = element.label
+            })
+        Next
+
+        For Each key As ChemicalKey In graph.AllBonds
+            Call bonds.Add(New bond With {
+                .id = key.ID,
+                .order = 1,
+                .atomRefs2 = {key.U.label, key.V.label}
+            })
+        Next
+
         data.atomArray = New atomArray("atoms", atoms)
         data.bondArray = New bondArray("bonds", bonds)
 
