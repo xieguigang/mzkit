6ce4bd1afb673ca734fa99642197773879845dcc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-01 21:41:21 +0800

    create chemical markup language file model

---
+++ src/metadb/Chemoinformatics/CML/Class1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Xml.Serialization
+
+Namespace ChemicalMarkupLanguage
+
+    Public Class bond
+
+        <XmlAttribute> Public Property id As String
+        <XmlAttribute> Public Property atomRefs2 As String()
+        <XmlAttribute> Public Property order As Integer
+
+    End Class
+End Namespace
