617a9d4f7c8e0374fe7413d0b891039cdb331e8d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-11 21:47:31 +0800

    create tool script for make rawdata ascii file dumpping

--- Rscript/Library/mzkit_app/tools/ascii_cache.R
+++ Rscript/Library/mzkit_app/tools/ascii_cache.R
@@ -1,10 +1,38 @@
 require(mzkit);
 
+[@info "the file path to the rawdata file or a directory folder path that contains multiple rawdata files. supported formats: mzML,mzXML,imzML,raw,mzPack"]
 let rawdata = ?"--raw" || stop("no rawdata file was provided!");
-let outfile = ?"--out" || `${dirname(rawdata)}/${basename(rawdata)}.txt`;
+let is_dir  = dir.exists(rawdata);
+[@info "the file path of the dump text file for a single file input, or a directory path for output multiple rawdata file."]
+let outfile = ?"--out" || {
+    if (is_dir) {
+        rawdata;
+    } else {
+        `${dirname(rawdata)}/${basename(rawdata)}.txt`;
+    }
+};
 
-if (file.ext(rawdata) == "raw") {
+let dump_ascii = function(rawfile, cachefile) {
+    let rawdata = open.mzpack(rawfile,verbose =TRUE);
+    let ms1 = [rawdata]::MS;
+
+    write.text_cache(ms1, 
+        file = cachefile, 
+        tabular = TRUE);
+}
+
+if (is_dir) {
+    let rawfiles = list.files(rawdata, pattern = ["*.mzXML","*.mzML","*.raw","*.imzML"]);
+    let names = basename(rawfiles);
+
+    print("dumping raw data files as ascii text cache data:");
+    print(names);
+
+    for(let file in tqdm(as.list(rawfiles, names = names))) {
+        dump_ascii(file, `${outfile}/${basename(file)}.txt`);
+    }
 
 } else {
-    
+    dump_ascii(rawdata, outfile);
 }
+
