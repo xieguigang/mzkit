9b1594eab45980fac9e8f3df394c93e07f5a96c5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-16 19:50:07 +0800

    check of the null reference error

--- Rscript/Library/mzkit_app/src/mzkit/comprehensive/TissueMorphology.vb
+++ Rscript/Library/mzkit_app/src/mzkit/comprehensive/TissueMorphology.vb
@@ -1,60 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::3d378d248d2314352cf77a8f05de51f9, Rscript\Library\mzkit_app\src\mzkit\comprehensive\TissueMorphology.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 738
-    '    Code Lines: 544 (73.71%)
-    ' Comment Lines: 104 (14.09%)
-    '    - Xml Docs: 97.12%
-    ' 
-    '   Blank Lines: 90 (12.20%)
-    '     File Size: 29.11 KB
+' Summaries:
 
 
-    ' Module TissueMorphology
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: createCDF, createTissueData, createTissueTable, createUMAPsample, createUMAPTable
-    '               FillLabels, GetPointLabels, gridding, intersect, loadSpatialMapping
-    '               loadTissue, loadUMAP, (+2 Overloads) PlotTissueMap, SplitMapping, tag_samples
-    '               TagSampleLabels
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 738
+'    Code Lines: 544 (73.71%)
+' Comment Lines: 104 (14.09%)
+'    - Xml Docs: 97.12%
+' 
+'   Blank Lines: 90 (12.20%)
+'     File Size: 29.11 KB
+
+
+' Module TissueMorphology
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: createCDF, createTissueData, createTissueTable, createUMAPsample, createUMAPTable
+'               FillLabels, GetPointLabels, gridding, intersect, loadSpatialMapping
+'               loadTissue, loadUMAP, (+2 Overloads) PlotTissueMap, SplitMapping, tag_samples
+'               TagSampleLabels
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -552,6 +552,10 @@ Module TissueMorphology
         Dim labelClass As String() = labels.Distinct.ToArray
         Dim colors As New Dictionary(Of String, Color)
         Dim regions As New Dictionary(Of String, List(Of Point))
+
+        If colorSet Is Nothing Then
+            Return RInternal.debug.stop($"the color palette term is nothing, please check of the '{NameOf(colorSet)}' parameter!", env)
+        End If
 
         If TypeOf colorSet Is list Then
             Dim list As list = DirectCast(colorSet, list)
