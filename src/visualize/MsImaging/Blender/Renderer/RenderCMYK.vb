3092fac74fb54766f627d0d8a67e31530edaf6af
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-23 16:06:26 +0800

    create cmyk channels

--- src/visualize/MsImaging/Blender/Renderer/RenderCMYK.vb
+++ src/visualize/MsImaging/Blender/Renderer/RenderCMYK.vb
@@ -1,6 +1,38 @@
-﻿Namespace Blender
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
 
-    Public Class RenderCMYK
+Namespace Blender
 
+    Public Class RenderCMYK : Inherits CompositionBlender
+
+        ''' <summary>
+        ''' Cyan
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Cchannel As Func(Of Integer, Integer, Byte)
+        ''' <summary>
+        ''' Magenta
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Mchannel As Func(Of Integer, Integer, Byte)
+        ''' <summary>
+        ''' Yellow
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Ychannel As Func(Of Integer, Integer, Byte)
+        ''' <summary>
+        ''' Key/Black
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Kchannel As Func(Of Integer, Integer, Byte)
+
+        Public Sub New(defaultBackground As Color, Optional heatmapMode As Boolean = False)
+            MyBase.New(defaultBackground, heatmapMode)
+        End Sub
+
+        Public Overrides Sub Render(ByRef g As IGraphics, region As GraphicsRegion)
+
+        End Sub
     End Class
 End Namespace
