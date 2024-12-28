11ab8d3a40ed4794f4b2db36948bc189f910a7b9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-29 04:36:57 +0800

    try to deal with the empty dimension size error

--- src/assembly/mzPack/Converter.vb
+++ src/assembly/mzPack/Converter.vb
@@ -1,60 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::46e68467588651c6dcdca21e69e56451, assembly\mzPack\Converter.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 313
-    '    Code Lines: 256 (81.79%)
-    ' Comment Lines: 20 (6.39%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 37 (11.82%)
-    '     File Size: 13.30 KB
+' Summaries:
 
 
-    ' Module Converter
-    ' 
-    '     Function: GetUVScans, LoadAsciiFileAuto, LoadimzML, LoadMgf, LoadMsp
-    '               LoadMzML, LoadRawFileAuto
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 313
+'    Code Lines: 256 (81.79%)
+' Comment Lines: 20 (6.39%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 37 (11.82%)
+'     File Size: 13.30 KB
+
+
+' Module Converter
+' 
+'     Function: GetUVScans, LoadAsciiFileAuto, LoadimzML, LoadMgf, LoadMsp
+'               LoadMzML, LoadRawFileAuto
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Drawing
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ASCII.MGF
@@ -221,6 +222,13 @@ imzML:      Return LoadimzML(xml, intocutoff, IonModes.Positive, Sub(p, msg) pro
         Dim mz As Double() = Nothing
         Dim intensity As Double() = Nothing
         Dim maxinto As Double
+
+        If allscans.Any AndAlso (metadata.dims.Width <= 1 OrElse metadata.dims.Height <= 1) Then
+            metadata.dims = New Size(
+                allscans.Select(Function(a) a.x).Max,
+                allscans.Select(Function(a) a.y).Max
+            )
+        End If
 
         msiMetadata!width = metadata.dims.Width
         msiMetadata!height = metadata.dims.Height
