3a1081c336d96d37bfed95c7369111a408c429b9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-15 17:45:09 +0800

    Update dump_hmdb.R

--- Rscript/Library/mzkit_app/test/dump_hmdb.R
+++ Rscript/Library/mzkit_app/test/dump_hmdb.R
@@ -3,5 +3,25 @@ require(mzkit);
 imports "hmdb_kit" from "mzkit";
 
 let s = read.hmdb("E:\biodeep\hmdb_metabolites.xml");
+let tabular_file = "E:\biodeep\hmdb_metabolites.csv";
 
-s |> export.hmdb_table(file = "E:\biodeep\hmdb_metabolites.csv");
+# s |> export.hmdb_table(file = tabular_file);
+
+let table = read.csv(tabular_file, row.names = 1, check.names = FALSE);
+
+table[,"state"]=NULL;
+table[,"description"]=NULL;
+table[,"secondary_accessions"]=NULL;
+table[,"cellular_locations"]=NULL;
+table[,"synonyms"]=NULL;
+table[,"tissue"]=NULL;
+table[,"pathways"]=NULL;
+table[,"proteins"]=NULL;
+table[,"disease"]=NULL;
+table[,"Physiological_effects"]=NULL;
+table[,"Disposition"]=NULL;
+table[,"Process"]=NULL;
+table[,"Role"]=NULL;
+table[,"Biomarker"]=NULL;
+
+write.csv(table, file = file.path(@dir, "/../data/hmdb.csv"));
