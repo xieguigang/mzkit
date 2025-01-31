3349bd37dda9837c00681fb55759ca8e1eca57c9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-31 18:38:52 +0800

    how to  deal with the label bug

--- src/metadb/Chemoinformatics/SDF/Struct/GraphData.vb
+++ src/metadb/Chemoinformatics/SDF/Struct/GraphData.vb
@@ -14,8 +14,10 @@ Namespace SDF.Models
         Public Function AsMolecularGraph(Of Node As {New, Network.Node}, Edge As {New, Network.Edge(Of Node)}, G As {New, NetworkGraph(Of Node, Edge)})(mol As [Structure]) As G
             Dim g As New G
             Dim id As i32 = 0
+            Dim atom_keys As New List(Of String)
 
             For Each atom As Atom In mol.Atoms
+                Call atom_keys.Add($"{atom.Atom}_{atom.Coordination.ToString}_{atom.GetHashCode}")
                 Call g.AddVertex(New Node With {
                     .ID = ++id,
                     .label = atom.Atom
