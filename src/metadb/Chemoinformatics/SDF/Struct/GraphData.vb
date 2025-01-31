4ef49851cd2b9251440149ec53321415965f2456
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-31 18:36:37 +0800

    create graph edge data

--- src/metadb/Chemoinformatics/SDF/Struct/GraphData.vb
+++ src/metadb/Chemoinformatics/SDF/Struct/GraphData.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.GraphTheory
 Imports Microsoft.VisualBasic.Data.GraphTheory.Network
+Imports Microsoft.VisualBasic.Language
 
 Namespace SDF.Models
 
@@ -10,8 +11,22 @@ Namespace SDF.Models
     Public Module GraphData
 
         <Extension>
-        Public Function AsMolecularGraph(Of Node As {New, Network.Node}, Edge As {New, Network.Edge(Of Node)}, G As NetworkGraph(Of Node, Edge))(mol As [Structure]) As G
+        Public Function AsMolecularGraph(Of Node As {New, Network.Node}, Edge As {New, Network.Edge(Of Node)}, G As {New, NetworkGraph(Of Node, Edge)})(mol As [Structure]) As G
+            Dim g As New G
+            Dim id As i32 = 0
 
+            For Each atom As Atom In mol.Atoms
+                Call g.AddVertex(New Node With {
+                    .ID = ++id,
+                    .label = atom.Atom
+                })
+            Next
+
+            For Each key As Bound In mol.Bounds
+                Call g.CreateEdge(mol.Atoms(key.i).Atom, mol.Atoms(key.j).Atom, key.Type)
+            Next
+
+            Return g
         End Function
     End Module
 End Namespace
