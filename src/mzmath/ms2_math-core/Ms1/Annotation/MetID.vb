c9386bf85a58616288ca7a7dee556f94d1fc416a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-07 19:54:08 +0800

    metabolite annotation id

---
+++ src/mzmath/ms2_math-core/Ms1/Annotation/MetID.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+
+Namespace Ms1.Annotations
+
+    Public Class MetID : Implements IReadOnlyId, IMS1Annotation, ICompoundNameProvider
+
+        Public Property id As String Implements IReadOnlyId.Identity, IKeyedEntity(Of String).Key
+        Public Property precursor_type As String Implements IMS1Annotation.precursor_type
+        Public Property intensity As Double Implements IMs1Scan.intensity
+        Public Property mz As Double Implements IMs1.mz
+        Public Property rt As Double Implements IRetentionTime.rt
+        Public Property name As String Implements ICompoundNameProvider.CommonName
+
+    End Class
+End Namespace
