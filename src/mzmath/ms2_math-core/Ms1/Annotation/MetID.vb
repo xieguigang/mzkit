d2f4462430ef6044e7397c1b28df21c4f405fe85
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-07 20:19:18 +0800

    add xml comment docs

--- src/mzmath/ms2_math-core/Ms1/Annotation/MetID.vb
+++ src/mzmath/ms2_math-core/Ms1/Annotation/MetID.vb
@@ -4,6 +4,9 @@ Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 
 Namespace Ms1.Annotations
 
+    ''' <summary>
+    ''' the ion annotation model
+    ''' </summary>
     Public Class MetID : Implements IReadOnlyId, IMS1Annotation, ICompoundNameProvider
 
         ''' <summary>
@@ -12,6 +15,10 @@ Namespace Ms1.Annotations
         ''' <returns></returns>
         <XmlAttribute>
         Public Property unique_id As String Implements IReadOnlyId.Identity, IKeyedEntity(Of String).Key
+        ''' <summary>
+        ''' the ion adducts of this ion precursor
+        ''' </summary>
+        ''' <returns></returns>
         <XmlAttribute> Public Property precursor_type As String Implements IMS1Annotation.precursor_type
         <XmlAttribute> Public Property intensity As Double Implements IMs1Scan.intensity
 
@@ -21,6 +28,10 @@ Namespace Ms1.Annotations
         ''' <returns></returns>
         <XmlAttribute> Public Property mz As Double Implements IMs1.mz
         <XmlAttribute> Public Property rt As Double Implements IRetentionTime.rt
+        ''' <summary>
+        ''' the metabolite name of current ion that annotated.
+        ''' </summary>
+        ''' <returns></returns>
         <XmlText> Public Property name As String Implements ICompoundNameProvider.CommonName
 
         Public Overrides Function ToString() As String
