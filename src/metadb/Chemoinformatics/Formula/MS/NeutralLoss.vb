f8a1af93aa0c85fdbf047ffa314fc67616e13392
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-28 16:43:26 +0800

    make improvements of the product annotation model

--- src/metadb/Chemoinformatics/Formula/MS/NeutralLoss.vb
+++ src/metadb/Chemoinformatics/Formula/MS/NeutralLoss.vb
@@ -68,67 +68,11 @@
 
 #End Region
 
-Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
-Imports std = System.Math
 Imports AdductParser = BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType.Parser
+Imports std = System.Math
 
 Namespace Formula.MS
-
-    ''' <summary>
-    ''' This class is the storage of product ion assignment used in MS-FINDER program.
-    ''' </summary>
-    Public Class ProductIon
-
-        Public Property Mass As Double
-
-        Public Property Intensity As Double
-
-        Public Property Formula As Formula
-
-        Public Property IonMode As IonModes
-
-        Public Property Smiles As String
-
-        Public Property MassDiff As Double
-
-        Public Property IsotopeDiff As Double
-
-        Public Property Comment As String
-
-        Public Property Name As String
-
-        Public Property ShortName As String
-
-        Public Property CandidateInChIKeys As List(Of String) = New List(Of String)()
-
-        Public Property Frequency As Double
-
-        Public Property CandidateOntologies As List(Of String) = New List(Of String)()
-
-        Sub New()
-        End Sub
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Sub New(formula As String, name As String, comment As String)
-            Call Me.New(FormulaScanner.ScanFormula(formula).CountsByElement, name, comment)
-        End Sub
-
-        Sub New(formula As IDictionary(Of String, Integer), name As String, comment As String)
-            _Formula = New Formula(formula)
-            _Mass = _Formula.ExactMass
-            _Intensity = 1
-            _IonMode = IonModes.Unknown
-            _Comment = comment
-            _Name = name
-            _ShortName = name
-            _Frequency = 1
-        End Sub
-
-        Public Overrides Function ToString() As String
-            Return $"{Name} ({Formula.EmpiricalFormula}); m/z: {Mass}"
-        End Function
-    End Class
 
     ''' <summary>
     ''' This class is the storage of each neutral loss information used in MS-FINDER program.
