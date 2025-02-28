fd1cbde7131a1409efcdaa0fc499f26ff8011904
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-28 15:45:53 +0800

    config for run annotation test

--- src/mzmath/ms2_simulator/test/annotation_test.vb
+++ src/mzmath/ms2_simulator/test/annotation_test.vb
@@ -1,11 +1,25 @@
-﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Insilicon
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Insilicon
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
 Imports BioNovoGene.BioDeep.Chemoinformatics.SMILES
 
 Module annotation_test
 
     Sub Main()
+        Call annotation_test()
+    End Sub
+
+    Sub annotation_test()
+        Dim formuyla As Formula = FormulaScanner.ScanFormula("C7H11N3O2")
+        Dim adducts As MzCalculator = Provider.ParseAdductModel("[M+H]+")
+        Dim result = PeakAnnotation.DoPeakAnnotation(New LibraryMatrix({170.16, 124.2}, {100, 20}), adducts, formuyla, da:=0.3)
+
+        Pause()
+    End Sub
+
+    Sub smiles_test()
         Dim formuyla As Formula = FormulaScanner.ScanFormula("C7H11N3O2")
         Dim smiles As ChemicalFormula = ChemicalFormula.ParseGraph("CN1C=NC(C[C@H](N)C(O)=O)=C1")
         Dim anno As New SMILESAnnotator(smiles, formuyla)
