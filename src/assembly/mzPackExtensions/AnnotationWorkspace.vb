3cc661877a2770af0a396c38e685868deedb230e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-11 08:41:16 +0800

    add warning message

--- src/assembly/mzPackExtensions/AnnotationWorkspace.vb
+++ src/assembly/mzPackExtensions/AnnotationWorkspace.vb
@@ -81,6 +81,9 @@ Imports Microsoft.VisualBasic.Serialization.JSON
 Public Class AnnotationWorkspace : Implements IDisposable, IWorkspaceReader
 
     ReadOnly pack As StreamPack
+    ''' <summary>
+    ''' summary of the library result count
+    ''' </summary>
     ReadOnly libraries As New Dictionary(Of String, Integer)
     ReadOnly samplefiles As New List(Of String)
     ReadOnly source As String
@@ -287,6 +290,11 @@ Public Class AnnotationWorkspace : Implements IDisposable, IWorkspaceReader
 
     Public Sub CreateLibraryResult(library$, result As IEnumerable(Of AlignmentHit))
         Dim i As Integer = 0
+        Dim tags As New List(Of String)
+
+        If library.StringEmpty Then
+            library = "missing!"
+        End If
 
         For Each peak_result As AlignmentHit In result.SafeQuery
             i += 1
@@ -300,12 +308,21 @@ Public Class AnnotationWorkspace : Implements IDisposable, IWorkspaceReader
                 Dim bin As New BinaryDataWriter(file)
                 Dim buf As MemoryStream = peak_result.PackAlignment
 
+                Call tags.Add($"{peak_result.xcms_id}/{peak_result.libname}")
                 Call bin.Write(buf.ToArray)
                 Call bin.Flush()
             End Using
         Next
 
-        Call libraries.Add(library, i)
+        If library = "missing!" Then
+            Call $"missing library reference name for {tags.Take(10).JoinBy(", ")}...".Warning
+        End If
+
+        If libraries.ContainsKey(library) Then
+            Call libraries.Add(library, i + libraries(library))
+        Else
+            Call libraries.Add(library, i)
+        End If
     End Sub
 
     ''' <summary>
