e70eea283c091fe21029ba9eea672d27d8824ab2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-17 14:14:20 +0800

    split source file into multiple parts

---
+++ src/metadb/Massbank/MetaLib/Models/BiosampleSource.vb
@@ -0,0 +1,41 @@
+﻿Imports BioNovoGene.BioDeep.Chemoinformatics
+Imports Microsoft.VisualBasic.Data.IO.MessagePack.Serialization
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace MetaLib.Models
+
+    Public Class BiosampleSource
+
+        <MessagePackMember(0)> Public Property biosample As String
+        <MessagePackMember(1)> Public Property source As String
+
+        ''' <summary>
+        ''' the reference source
+        ''' </summary>
+        ''' <returns></returns>
+        <MessagePackMember(2)> Public Property reference As String
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+
+    End Class
+
+    ''' <summary>
+    ''' the data model of the compound class information
+    ''' </summary>
+    ''' <remarks>
+    ''' this class information model is mainly address on the HMDB
+    ''' metabolite ontology class levels.
+    ''' </remarks>
+    Public Class CompoundClass : Implements ICompoundClass
+
+        <MessagePackMember(0)> Public Property kingdom As String Implements ICompoundClass.kingdom
+        <MessagePackMember(1)> Public Property super_class As String Implements ICompoundClass.super_class
+        <MessagePackMember(2)> Public Property [class] As String Implements ICompoundClass.class
+        <MessagePackMember(3)> Public Property sub_class As String Implements ICompoundClass.sub_class
+        <MessagePackMember(4)> Public Property molecular_framework As String Implements ICompoundClass.molecular_framework
+
+    End Class
+
+End Namespace
--- src/metadb/Massbank/MetaLib/Models/MetaLib.vb
+++ src/metadb/Massbank/MetaLib/Models/MetaLib.vb
@@ -72,43 +72,8 @@ Imports System.Runtime.CompilerServices
 Imports BioNovoGene.BioDeep.Chemistry.MetaLib.CrossReference
 Imports BioNovoGene.BioDeep.Chemoinformatics
 Imports Microsoft.VisualBasic.Data.IO.MessagePack.Serialization
-Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace MetaLib.Models
-
-    Public Class BiosampleSource
-
-        <MessagePackMember(0)> Public Property biosample As String
-        <MessagePackMember(1)> Public Property source As String
-
-        ''' <summary>
-        ''' the reference source
-        ''' </summary>
-        ''' <returns></returns>
-        <MessagePackMember(2)> Public Property reference As String
-
-        Public Overrides Function ToString() As String
-            Return Me.GetJson
-        End Function
-
-    End Class
-
-    ''' <summary>
-    ''' the data model of the compound class information
-    ''' </summary>
-    ''' <remarks>
-    ''' this class information model is mainly address on the HMDB
-    ''' metabolite ontology class levels.
-    ''' </remarks>
-    Public Class CompoundClass : Implements ICompoundClass
-
-        <MessagePackMember(0)> Public Property kingdom As String Implements ICompoundClass.kingdom
-        <MessagePackMember(1)> Public Property super_class As String Implements ICompoundClass.super_class
-        <MessagePackMember(2)> Public Property [class] As String Implements ICompoundClass.class
-        <MessagePackMember(3)> Public Property sub_class As String Implements ICompoundClass.sub_class
-        <MessagePackMember(4)> Public Property molecular_framework As String Implements ICompoundClass.molecular_framework
-
-    End Class
 
     ''' <summary>
     ''' 对``chebi/kegg/pubchem/HMDB/metlin``的物质注释信息整合库，这个数据库只要为了生成编号，名称之类的注释信息而构建的
