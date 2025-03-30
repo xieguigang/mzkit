f0beb6b9334e3babf223583d52e6d9b4fc329673
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-31 00:55:32 +0800

    fix of the pixel based heatmap render

--- src/visualize/MsImaging/Blender/Renderer/PixelRender.vb
+++ src/visualize/MsImaging/Blender/Renderer/PixelRender.vb
@@ -61,6 +61,7 @@ Imports System.Drawing.Imaging
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.BitmapImage
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors.Scaler
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
 Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Linq
@@ -227,40 +228,30 @@ Namespace Blender
         ''' <paramref name="dimension"/> parameter.</returns>
         Public Overrides Function RenderPixels(pixels As PixelData(), dimension As Size, heatmap As HeatMapBrushes) As GraphicsData
             Dim color As Color
-            Dim colors As Color() = colorSet.Select(Function(br) br.Color).ToArray
+            Dim colors As ValueScaleColorProfile = heatmap.GetMapping(pixels.Select(Function(c) c.intensity))
             Dim index As Integer
             Dim level As Double
-            Dim indexrange As DoubleRange = New Double() {0, colors.Length - 1}
-            Dim levelRange As DoubleRange = New Double() {0, 1}
             ' create a blank canvas image
             Dim raw As Bitmap = DrawBackground(dimension, Color.Transparent)
-            Dim defaultColor As Color = defaultFill.TranslateColor
-            Dim isTransparentFill As Boolean = defaultFill.TextEquals("Transparent")
+            Dim defaultColor As Color = heatmap.defaultFill.TranslateColor
+            Dim isTransparentFill As Boolean = heatmap.defaultFill.TextEquals("Transparent")
             Dim skipTransparent As Boolean = (Not overlaps Is Nothing) OrElse isTransparentFill
+            Dim intensityRange As Double() = colors.ValueMinMax
+
+            colors = colors.ReScaleToValueRange(0, 1)
 
             Using buffer As BitmapBuffer = BitmapBuffer.FromBitmap(raw)
-                For Each point As PixelData In PixelData.ScalePixels(pixels)
+                For Each point As PixelData In PixelData.ScalePixels(pixels, setRange:=intensityRange)
                     level = point.level
+                    color = colors.GetColor(level, index)
 
-                    If level <= 0.000005 OrElse level.IsNaNImaginary Then
+                    If level <= 0.000005 OrElse level <= 0 OrElse level.IsNaNImaginary Then
                         color = defaultColor
 
                         ' skip draw on this pixel for missing pixel
                         If skipTransparent OrElse isTransparentFill Then
                             Continue For
                         End If
-                    Else
-                        index = levelRange.ScaleMapping(level, indexrange)
-
-                        If index <= 0 Then
-                            index = 0
-
-                            If skipTransparent OrElse isTransparentFill Then
-                                Continue For
-                            End If
-                        End If
-
-                        color = colors(index)
                     End If
 
                     ' imzXML里面的坐标是从1开始的
