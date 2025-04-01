87ef38c4e2debd12e911a819eeeb4e48eed2e35e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-01 16:23:52 +0800

    Update ms3_cache_reader.R

--- Rscript/Library/mzkit_app/test/lcms/ms3_cache_reader.R
+++ Rscript/Library/mzkit_app/test/lcms/ms3_cache_reader.R
@@ -2,9 +2,16 @@ require(mzkit);
 
 let rawdata = "\\192.168.1.254\backup3\项目以外内容\2025\三级MSn测试\rawdata\QC1.mzML";
 let cachefile = "\\192.168.1.254\backup3\项目以外内容\2025\三级MSn测试\test1\tmp\.cache\raw\d\QC1.mzPack";
-let loaddata = open.mzpack(rawdata);
+let mgf_ascii = "\\192.168.1.254\backup3\项目以外内容\2025\三级MSn测试\test1\samples\QC1\ms2_ascii_cache.mgf";
+let cache = "\\192.168.1.254\backup3\项目以外内容\2025\三级MSn测试\test1\samples\QC1\ionPeaks.dat";
+
+# let loaddata = open.mzpack(rawdata);
        
-loaddata |> write.mzPack(file = cachefile, version = 1);
+# loaddata |> write.mzPack(file = cachefile, version = 1);
 
 let data = open.mzpack(cachefile);
+let ions = ms2_peaks(data);
 
+assembly::write.mgf(ions, file = mgf_ascii);
+mzweb::write.cache(ions, file = cache, 
+    tag_filesource = FALSE);
