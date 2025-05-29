69cd90172a6b9eaa7c6f5d30d649b6edbc610cf9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-29 13:49:18 +0800

    configs of the formula test

---
+++ src/metadb/FormulaSearch.Extensions/test/formulaTest.vb
@@ -0,0 +1,16 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
+
+Module formulaTest
+
+    Sub Main()
+        Dim formulas = New String() {""}
+
+        For Each str As String In formulas
+            Call Console.WriteLine(AnthocyaninValidator.CalculateProbability(str))
+        Next
+
+        Pause()
+    End Sub
+
+
+End Module
