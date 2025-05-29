08aec00fef65113acb119e1ab5c7f0b52a6e42c1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-29 14:33:30 +0800

    configs for the negative test

--- src/metadb/FormulaSearch.Extensions/AnthocyaninValidator.vb
+++ src/metadb/FormulaSearch.Extensions/AnthocyaninValidator.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Text.RegularExpressions
 Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports std = System.Math
 
 Public Module AnthocyaninValidator
@@ -9,6 +10,14 @@ Public Module AnthocyaninValidator
     ReadOnly H_Weight As Double = Formula.AllAtomElements!H.isotopic
     ReadOnly O_Weight As Double = Formula.AllAtomElements!O.isotopic
 
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="formula"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' cutoff is 30%
+    ''' </remarks>
     Public Function CalculateProbability(formula As String) As Double
         Dim score As Double = 0
         ' 解析化学式
@@ -41,5 +50,68 @@ Public Module AnthocyaninValidator
         score += If(h_oRatio > 1.2 AndAlso h_oRatio < 2.5, 0.5, 0)
 
         Return score / 4.5 * 100 ' 总分4.5分转换为百分比
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="formula"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' cutoff is 50%
+    ''' </remarks>
+    Public Function IsLikelyAnthocyanin(formula As String) As Integer
+        Dim elements = FormulaScanner.ScanFormula(formula).CountsByElement
+        Dim score As Integer = 0
+
+        ' 必须元素检查
+        If Not elements.ContainsKey("C") OrElse
+           Not elements.ContainsKey("H") OrElse
+           Not elements.ContainsKey("O") Then
+            Return 0
+        End If
+        score += 3
+
+        Dim c As Integer = elements("C")
+        Dim h As Integer = elements("H")
+        Dim o As Integer = elements("O")
+        Dim n As Integer = If(elements.ContainsKey("N"), elements("N"), 0)
+
+        ' 氧含量检查（关键特征）
+        If o < 5 Then Return 0
+        score += 2
+
+        ' 碳骨架范围（核心结构C15-C22）
+        If c >= 15 AndAlso c <= 22 Then
+            score += 2
+        Else
+            score -= 2
+        End If
+
+        ' 不饱和度验证
+        Dim maxH As Integer = 2 * c + n - 18
+        If h <= maxH Then
+            score += 2
+        Else
+            score -= 1
+        End If
+
+        ' 氮含量检查
+        If n = 0 OrElse n = 1 Then
+            score += 1
+        Else
+            score -= 1
+        End If
+
+        Static elementSet As Index(Of String) = {"C", "H", "O", "N", "CL"}
+
+        ' 排除其他元素
+        For Each element In elements.Keys
+            If Not element.ToUpperInvariant() Like elementSet Then
+                score -= 1
+            End If
+        Next
+
+        Return std.Max(0, score)
     End Function
 End Module
