0ffa4d3d145efaa7e006414122ea7a9797b3cc34
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-15 22:06:11 +0800

    fix of the symbol name reference error

--- Rscript/Library/mzkit_app/src/mzkit/annotations/PubChem.vb
+++ Rscript/Library/mzkit_app/src/mzkit/annotations/PubChem.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::e7162ae02cdaa6f9a304be703fa65fc7, Rscript\Library\mzkit_app\src\mzkit\annotations\PubChem.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 605
-    '    Code Lines: 357 (59.01%)
-    ' Comment Lines: 193 (31.90%)
-    '    - Xml Docs: 95.85%
-    ' 
-    '   Blank Lines: 55 (9.09%)
-    '     File Size: 26.74 KB
+' Summaries:
 
 
-    ' Module PubChemToolKit
-    ' 
-    '     Function: CID, GetMetaInfo, ImageFlyGetImages, level1Terms, makeTabular
-    '               MeshBackground, ParseMeshTree, pubchemUrl, pugView, queryExternalMetadata
-    '               QueryKnowledgeGraph, queryPubChem, readPubmed, readPugViewRepository, readPugViewXml
-    '               ReadSIDMap, readWebQuerySummary, SIDMapTable
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 605
+'    Code Lines: 357 (59.01%)
+' Comment Lines: 193 (31.90%)
+'    - Xml Docs: 95.85%
+' 
+'   Blank Lines: 55 (9.09%)
+'     File Size: 26.74 KB
+
+
+' Module PubChemToolKit
+' 
+'     Function: CID, GetMetaInfo, ImageFlyGetImages, level1Terms, makeTabular
+'               MeshBackground, ParseMeshTree, pubchemUrl, pugView, queryExternalMetadata
+'               QueryKnowledgeGraph, queryPubChem, readPubmed, readPugViewRepository, readPugViewXml
+'               ReadSIDMap, readWebQuerySummary, SIDMapTable
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -85,6 +85,7 @@ Imports Microsoft.VisualBasic.Text.Xml.Models
 Imports SMRUCC.genomics.Analysis.HTS.GSEA
 Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI
 Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI.MeSH.Tree
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI.PubMed
 Imports SMRUCC.Rsharp
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
@@ -151,7 +152,7 @@ Imports FontStyle = Microsoft.VisualBasic.Imaging.FontStyle
 ''' And 271 million bioactivity data points from 1.2 million biological assays experiments.
 ''' </remarks>
 <Package("pubchem_kit")>
-<RTypeExport("pubmed", GetType(PubMed))>
+<RTypeExport("pubmed", GetType(PubMedTextTable))>
 Module PubChemToolKit
 
     Sub Main()
@@ -188,15 +189,15 @@ Module PubChemToolKit
     ''' <param name="env"></param>
     ''' <returns></returns>
     <ExportAPI("read.pubmed")>
-    <RApiReturn(GetType(PubMed))>
+    <RApiReturn(GetType(PubMedTextTable))>
     Public Function readPubmed(file As String(), Optional lazy As Boolean = True, Optional env As Environment = Nothing) As Object
         If lazy Then
             Return file.Select(Function(path) DataStream.OpenHandle(path, trim:=True) _
-                .AsLinq(Of PubMed)(silent:=True)).IteratesALL _
+                .AsLinq(Of PubMedTextTable)(silent:=True)).IteratesALL _
                 .DoCall(AddressOf pipeline.CreateFromPopulator)
         Else
             Return file _
-                .Select(Function(path) path.LoadCsv(Of PubMed)(mute:=True)) _
+                .Select(Function(path) path.LoadCsv(Of PubMedTextTable)(mute:=True)) _
                 .IteratesALL _
                 .ToArray
         End If
