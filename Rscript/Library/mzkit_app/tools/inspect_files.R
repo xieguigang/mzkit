7929867b114bcd8494953ac664f8915f56901bec
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-16 16:18:12 +0800

    extract metadata

--- Rscript/Library/mzkit_app/tools/inspect_files.R
+++ Rscript/Library/mzkit_app/tools/inspect_files.R
@@ -1,6 +1,6 @@
 require(mzkit);
 
-[@info "the csv table file should contains at least two data field: name and formula"]
+[@info "the csv table file should contains at least two data field: name and formula, adducts field is optional"]
 let metabolites = ?"--targets" || stop("A csv table data file that contains the target name and target formula is required!");
 let rawdir = ?"--rawdir" || stop("A directory that contains the raawdata file is required!");
 [@info "the rawdata polarity mode, should be value 1 for positive and -1 for nagative"]
@@ -17,8 +17,31 @@ let rawdata = list.files(rawdir, pattern = ["*.mzXML", "*.mzML"])
     names = path -> basename(path)
 )
 ;
+let adducts = {
+    if (as.integer(ion_mode) > 0) {                    
+        ["[M+H]+" "[M-H2O+H]+" "[M-2H2O+H]+" "[M+NH4]+" "[M+Na]+" "[2M+H]+" "[M]+"]             
+    } else {                
+        ["[M-H]-" "[M+COOH]-"];
+    }
+};
 
 for(let meta in as.list(metabolites, byrow = TRUE)) {
     let [name, formula] = meta$formula;
     let exact_mass = formula::eval(formula);
+    let adduct_types = {
+        if (is.null(meta$adducts)) {
+            adducts;
+        } else {
+            meta$adducts;
+        }
+    }
+    let mz = math::mz(exact_mass, mode = adduct_types);
+
+    print(mz);
+    stop();
+
+    let xic = lapply(rawdata, function(file) {
+        
+    });
+
 }
