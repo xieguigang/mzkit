6caceec55a3230eeabc13b4aa4d3c3c2ef0997bd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-22 01:16:05 +0800

    add description information about the csv data fields

--- src/mzmath/TargetedMetabolomics/MRM/IonPair.vb
+++ src/mzmath/TargetedMetabolomics/MRM/IonPair.vb
@@ -1,63 +1,64 @@
 ﻿#Region "Microsoft.VisualBasic::6752693422b268d31864701dbbc7ba67, mzmath\TargetedMetabolomics\MRM\IonPair.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 125
-    '    Code Lines: 77 (61.60%)
-    ' Comment Lines: 33 (26.40%)
-    '    - Xml Docs: 96.97%
-    ' 
-    '   Blank Lines: 15 (12.00%)
-    '     File Size: 4.82 KB
+' Summaries:
 
 
-    '     Class IonPair
-    ' 
-    '         Properties: accession, name, precursor, product, rt
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: (+2 Overloads) Assert, EqualsTo, GetIsomerism, populateGroupElement, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 125
+'    Code Lines: 77 (61.60%)
+' Comment Lines: 33 (26.40%)
+'    - Xml Docs: 96.97%
+' 
+'   Blank Lines: 15 (12.00%)
+'     File Size: 4.82 KB
+
+
+'     Class IonPair
+' 
+'         Properties: accession, name, precursor, product, rt
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: (+2 Overloads) Assert, EqualsTo, GetIsomerism, populateGroupElement, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.ComponentModel
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MarkupData.mzML
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
@@ -79,22 +80,35 @@ Namespace MRM.Models
         ''' </summary>
         ''' <returns></returns>
         <Column(Name:="ID")>
+        <Description("The unique reference id of current metabolite ion, example as hmdb id or cas number.")>
         Public Property accession As String
         ''' <summary>
         ''' The display title name
         ''' </summary>
         ''' <returns></returns>
+        ''' 
+        <Description("The metabolite ion display name")>
         Public Property name As String Implements IKeyedEntity(Of String).Key
         ''' <summary>
         ''' Q1 precursor ion m/z
         ''' </summary>
         ''' <returns></returns>
+        ''' 
+        <Description("The Q1 precursor ion m/z")>
         Public Property precursor As Double
         ''' <summary>
         ''' Q3 product ion m/z
         ''' </summary>
         ''' <returns></returns>
+        ''' 
+        <Description("The Q3 product ion m/z")>
         Public Property product As Double
+        ''' <summary>
+        ''' The retention time of the ion pair
+        ''' </summary>
+        ''' <returns></returns>
+        '''
+        <Description("The retention time of the ion pair, should be in time data unit Seconds.")>
         Public Property rt As Double?
 
         Sub New()
