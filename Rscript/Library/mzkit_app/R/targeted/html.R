e0d10a0d74cbba963e6b3f13dc5ee6e58af4e20b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-23 13:52:58 +0800

    evaluate the pca data

--- Rscript/Library/mzkit_app/R/targeted/html.R
+++ Rscript/Library/mzkit_app/R/targeted/html.R
@@ -4,10 +4,13 @@ const MRM_dataReport = function(xic, tpa) {
 
     let local_res = system.file("data/MRM/peaksdata.html", package = "mzkit");
     let template = htmlReport::htmlTemplate(local_res);
+    let peaktable = as.data.frame(unlist(tpa), peaktable = TRUE);
+    let pca = prcomp(peaktable, pc = 2);
 
     template = template + list(
         xic = base64(JSON::json_encode(xic)),
-        area = base64(JSON::json_encode(tpa))
+        area = base64(JSON::json_encode(tpa)),
+        pca = base64(JSON::json_encode(pca$score))
     );
 
     .Internal::html(template);
