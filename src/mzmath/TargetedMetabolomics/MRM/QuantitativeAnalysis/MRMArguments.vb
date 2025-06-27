3abc297737f0c1867d430a6578ca9ac29938a646
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-27 14:47:47 +0800

    parse from json

--- src/mzmath/TargetedMetabolomics/MRM/QuantitativeAnalysis/MRMArguments.vb
+++ src/mzmath/TargetedMetabolomics/MRM/QuantitativeAnalysis/MRMArguments.vb
@@ -1,62 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::39f9c3bf2e03852713a3beac853f104e, mzmath\TargetedMetabolomics\MRM\QuantitativeAnalysis\MRMArguments.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 81
-    '    Code Lines: 62 (76.54%)
-    ' Comment Lines: 8 (9.88%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 11 (13.58%)
-    '     File Size: 3.19 KB
+' Summaries:
 
 
-    '     Class MRMArguments
-    ' 
-    '         Properties: angleThreshold, baselineQuantile, bspline_degree, bspline_density, integratorTicks
-    '                     peakAreaMethod, peakwidth, sn_threshold, strict, timeWindowSize
-    '                     tolerance, TPAFactors
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetDefaultArguments, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 81
+'    Code Lines: 62 (76.54%)
+' Comment Lines: 8 (9.88%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 11 (13.58%)
+'     File Size: 3.19 KB
+
+
+'     Class MRMArguments
+' 
+'         Properties: angleThreshold, baselineQuantile, bspline_degree, bspline_density, integratorTicks
+'                     peakAreaMethod, peakwidth, sn_threshold, strict, timeWindowSize
+'                     tolerance, TPAFactors
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetDefaultArguments, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -166,7 +166,18 @@ Namespace MRM
             Dim json As Dictionary(Of String, String) = json_str.LoadJSON(Of Dictionary(Of String, String))
             Dim args As New MRMArguments
 
-
+            args.tolerance = Tolerance.ParseScript(json!tolerance)
+            args.timeWindowSize = json!timeWindowSize
+            args.angleThreshold = json!angleThreshold
+            args.baselineQuantile = json!baselineQuantile
+            args.integratorTicks = json!integratorTicks
+            args.peakAreaMethod = [Enum].Parse(GetType(PeakAreaMethods), json!peakAreaMethod)
+            args.peakwidth = New DoubleRange(json!peakmin, json!peakmax)
+            args.sn_threshold = json!sn_threshold
+            args.bspline_degree = json!bspline_degree
+            args.bspline_density = json!bspline_density
+            args.joint_peaks = json!joint_peaks.ParseBoolean
+            args.strict = json!strict.ParseBoolean
 
             Return args
         End Function
