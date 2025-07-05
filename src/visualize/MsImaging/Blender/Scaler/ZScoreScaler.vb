e72f13aaef4de5d9eb1c55799bb149171eba37e7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-05 23:26:39 +0800

    how to do intensity data z-score scaling?

---
+++ src/visualize/MsImaging/Blender/Scaler/ZScoreScaler.vb
@@ -0,0 +1,9 @@
+﻿Namespace Blender.Scaler
+
+    Public Class ZScoreScaler : Inherits Scaler
+
+        Public Overrides Function ToScript() As String
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
