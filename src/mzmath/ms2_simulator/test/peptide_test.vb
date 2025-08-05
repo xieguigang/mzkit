5fa4040cac1a4fa5ac0dfc03223e47cb8be8490c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-05 20:00:52 +0800

    test seeds

---
+++ src/mzmath/ms2_simulator/test/peptide_test.vb
@@ -0,0 +1,10 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Insilicon
+
+Module peptide_test
+
+    Sub Main()
+        Dim peptides = PeptideMass.CreateLibrary(4).ToArray
+
+        Pause()
+    End Sub
+End Module
