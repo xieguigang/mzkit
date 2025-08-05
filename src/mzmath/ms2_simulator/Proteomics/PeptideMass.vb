8dedbb0e485d0497065eaf110762c47284632f7b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-05 19:57:39 +0800

    create peptide metabolite

---
+++ src/mzmath/ms2_simulator/Proteomics/PeptideMass.vb
@@ -0,0 +1,18 @@
+﻿Imports BioNovoGene.BioDeep.Chemoinformatics
+Imports SMRUCC.genomics.SequenceModel
+Imports SMRUCC.genomics.SequenceModel.Polypeptides
+
+Public Module PeptideMass
+
+    Public Iterator Function CreateLibrary(len As Integer) As IEnumerable(Of MetaboliteAnnotation)
+        For Each prot As String In Polypeptide.Generate(len)
+            Yield New MetaboliteAnnotation With {
+                .Id = prot,
+                .CommonName = prot,
+                .Formula = MolecularWeightCalculator.PolypeptideFormula(prot).ToString,
+                .ExactMass = MolecularWeightCalculator.CalcMW_Polypeptide(prot)
+            }
+        Next
+    End Function
+
+End Module
