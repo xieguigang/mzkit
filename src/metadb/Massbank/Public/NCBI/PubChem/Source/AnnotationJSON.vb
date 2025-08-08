ca74f9132a795de4e982c74e73abe57540123f41
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-08 21:28:40 +0800

    read the annotation data source

--- src/metadb/Massbank/Public/NCBI/PubChem/Source/AnnotationJSON.vb
+++ src/metadb/Massbank/Public/NCBI/PubChem/Source/AnnotationJSON.vb
@@ -1,10 +1,21 @@
 ﻿Imports System.IO
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.application.json
 Imports Microsoft.VisualBasic.MIME.application.json.Javascript
 
 Namespace NCBI.PubChem.DataSources
 
     Public Class AnnotationJSON
+
+        Public Shared Function GetAnnotations(files As IEnumerable(Of String)) As IEnumerable(Of Annotation)
+            Return files.SafeQuery _
+                .Select(Function(path)
+                            Using s As Stream = path.Open(FileMode.Open, doClear:=False, [readOnly]:=True)
+                                Return GetAnnotations(s).ToArray
+                            End Using
+                        End Function) _
+                .IteratesALL
+        End Function
 
         Public Shared Iterator Function GetAnnotations(s As Stream) As IEnumerable(Of Annotation)
             Dim json As JsonObject = New JsonParser(New StreamReader(s), False).OpenJSON
