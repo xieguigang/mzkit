59a2e052783f1aa69786c8f3ea305df916b2f6b2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-08-26 15:07:14 +0800

    auto config label

--- src/visualize/MsImaging/Blender/Ruler.vb
+++ src/visualize/MsImaging/Blender/Ruler.vb
@@ -1,63 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::bf6e2f5bd469d952f0177d3d235ccd00, visualize\MsImaging\Blender\Ruler.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 87
-    '    Code Lines: 67 (77.01%)
-    ' Comment Lines: 2 (2.30%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 18 (20.69%)
-    '     File Size: 3.26 KB
+' Summaries:
 
 
-    '     Class Ruler
-    ' 
-    '         Properties: width
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: eval
-    ' 
-    '         Sub: DrawOnCanvas, DrawOnImage, layout
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 87
+'    Code Lines: 67 (77.01%)
+' Comment Lines: 2 (2.30%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 18 (20.69%)
+'     File Size: 3.26 KB
+
+
+'     Class Ruler
+' 
+'         Properties: width
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: eval
+' 
+'         Sub: DrawOnCanvas, DrawOnImage, layout
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -128,9 +128,9 @@ Namespace Blender
             Dim rulerWidth As Double = 0
             Dim css As CSSEnvirnment = g.LoadEnvironment
             Dim pen As Pen = css.GetPen(Stroke.TryParse(theme.lineStroke))
-            Dim font As Font = CSS.GetFont(CSSFont.TryParse(theme.tagCSS))
+            Dim font As Font = css.GetFont(CSSFont.TryParse(theme.tagCSS))
             ' width of the ruler standards for
-            Dim physical As String = eval(rect, dimsize, resolution, rulerWidth).ToString("F2") & " um"
+            Dim physical As String = AutoLengthFormat(eval(rect, dimsize, resolution, rulerWidth))
             Dim fontsize As SizeF = g.MeasureString(physical, font)
             Dim left, right As PointF
             Dim bottom As Double = rect.Bottom - fontsize.Height * 2
@@ -152,7 +152,7 @@ Namespace Blender
         ''' </summary>
         ''' <param name="micrometers">以微米为单位的距离值</param>
         ''' <returns>格式化后的距离字符串（单位：km/m/cm/mm/μm）</returns>
-        Public Function AutoLengthFormat(micrometers As Double) As String
+        Public Shared Function AutoLengthFormat(micrometers As Double) As String
             ' 处理无效值
             If micrometers <= 0 Then Return "0 μm"
             If micrometers.IsNaNImaginary Then Return "n/a μm"
