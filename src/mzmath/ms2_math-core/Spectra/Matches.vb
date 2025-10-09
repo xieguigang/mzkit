35b7691c2b6b33a1ba36f0f79f475bf5300f9499
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-09 17:19:58 +0800

    how to make binary serch

---
+++ src/mzmath/ms2_math-core/Spectra/Matches.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+
+Namespace Spectra
+
+    ''' <summary>
+    ''' the spectrum fragment matches method 
+    ''' </summary>
+    Public Module Matches
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="sortMz">
+        ''' data must be sorted by the <see cref="ms2.mz"/> fragment mass value in asc order.
+        ''' </param>
+        ''' <param name="mz"></param>
+        ''' <param name="tolerance"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function BinarySearch(sortMz As ms2(), mz As Double, tolerance As Tolerance) As ms2
+
+        End Function
+
+    End Module
+End Namespace
