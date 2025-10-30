bfe1622deb275320dd5487e735d30903a29a7167
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-10-30 13:10:54 +0800

    quantify value model for export str report

---
+++ src/mzmath/TargetedMetabolomics/Peaks/QuantifyValue.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic.Language
+
+Public Class QuantifyValue : Inherits Value(Of String)
+
+    Public ReadOnly Property NumberValue As Double
+        Get
+            Select Case Strings.UCase(Value)
+                Case "NA", "N/A" : Return Double.NaN
+                Case "ND", "NF", "N/D", "N/F" : Return 0
+                Case Else
+                    Return Value.ParseDouble
+            End Select
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return Value
+    End Function
+
+    Public Overloads Shared Narrowing Operator CType(val As QuantifyValue) As Double
+        Return val.NumberValue
+    End Operator
+
+End Class
