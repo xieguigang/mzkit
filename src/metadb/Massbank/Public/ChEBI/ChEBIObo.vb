242c5ce6e15d84b1a961d266f10cdd069a12dc6a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-04 20:16:25 +0800

    use the single term

--- src/metadb/Massbank/Public/ChEBI/ChEBIObo.vb
+++ src/metadb/Massbank/Public/ChEBI/ChEBIObo.vb
@@ -1,57 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::27ae7507f30b350dd6ba38153453aa92, metadb\Massbank\Public\ChEBI\ChEBIObo.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 92
-    '    Code Lines: 68 (73.91%)
-    ' Comment Lines: 16 (17.39%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 8 (8.70%)
-    '     File Size: 4.27 KB
+' Summaries:
 
 
-    '     Module ChEBIObo
-    ' 
-    '         Function: ExtractTerm, ImportsMetabolites, SafeGetString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 92
+'    Code Lines: 68 (73.91%)
+' Comment Lines: 16 (17.39%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 8 (8.70%)
+'     File Size: 4.27 KB
+
+
+'     Module ChEBIObo
+' 
+'         Function: ExtractTerm, ImportsMetabolites, SafeGetString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -114,13 +114,13 @@ Namespace ChEBI
                     .InChI = properties.SafeGetString("http://purl.obolibrary.org/obo/chebi/inchi"),
                     .InChIkey = properties.SafeGetString("http://purl.obolibrary.org/obo/chebi/inchikey"),
                     .SMILES = properties.SafeGetString("http://purl.obolibrary.org/obo/chebi/smiles"),
-                    .MetaCyc = xref.TryGetValue("MetaCyc").JoinBy(", "),
-                    .DrugBank = xref.TryGetValue("DrugBank").JoinBy(", "),
-                    .Wikipedia = xref.TryGetValue("Wikipedia").JoinBy(", "),
-                    .HMDB = xref.TryGetValue("HMDB").JoinBy(", "),
-                    .KNApSAcK = xref.TryGetValue("KNApSAcK").JoinBy(", "),
-                    .lipidmaps = xref.TryGetValue("LIPID_MAPS_instance").JoinBy(", "),
-                    .KEGGdrug = kegg_id.Where(Function(cid) cid.StartsWith("D"c)).JoinBy(", ")
+                    .MetaCyc = xref.TryGetValue("MetaCyc").DefaultFirst,
+                    .DrugBank = xref.TryGetValue("DrugBank").DefaultFirst,
+                    .Wikipedia = xref.TryGetValue("Wikipedia").DefaultFirst,
+                    .HMDB = xref.TryGetValue("HMDB").DefaultFirst,
+                    .KNApSAcK = xref.TryGetValue("KNApSAcK").DefaultFirst,
+                    .lipidmaps = xref.TryGetValue("LIPID_MAPS_instance").DefaultFirst,
+                    .KEGGdrug = kegg_id.Where(Function(cid) cid.StartsWith("D"c)).DefaultFirst
                 }
             }
         End Function
