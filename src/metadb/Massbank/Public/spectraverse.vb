72505885541f7203792d339ae4b08a9b3ee1adc4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-07 10:15:03 +0800

    add spectraverse metadata

--- src/metadb/Massbank/Public/spectraverse.vb
+++ src/metadb/Massbank/Public/spectraverse.vb
@@ -1,4 +1,7 @@
-﻿Public Class spectraverse
+﻿Imports BioNovoGene.BioDeep.Chemistry.MetaLib.Models
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
+
+Public Class spectraverse
 
     Public Property TITLE As String
     Public Property CHARGE As Double
@@ -21,5 +24,26 @@
     Public Property NORMALIZED_COLLISION_ENERGY_1 As Double
     Public Property NORMALIZED_COLLISION_ENERGY_2 As Double
     Public Property NORMALIZED_COLLISION_ENERGY_3 As Double
+
+    Public Function CreateMeta() As MetaInfo
+        Dim uniqueId As String = $"{COMPOUND_NAME}-{FORMULA}".ToLower.MD5
+        Dim metadata As New MetaInfo With {
+            .ID = uniqueId,
+            .formula = FORMULA,
+            .exact_mass = FormulaScanner.EvaluateExactMass(FORMULA),
+            .name = COMPOUND_NAME,
+            .xref = New MetaLib.CrossReference.xref With {
+                .InChIkey = INCHIKEY,
+                .InChI = INCHI,
+                .SMILES = SMILES,
+                .extras = New Dictionary(Of String, String()) From {
+                    {"spectraverse", {TITLE}}
+                }
+            },
+            .description = SOURCE
+        }
+
+        Return metadata
+    End Function
 
 End Class
