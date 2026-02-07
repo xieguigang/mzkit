f3b602b2b1b422c64ec6889be611cda1d1c8611a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-07 10:10:06 +0800

    add spectraverse metadata table

---
+++ src/metadb/Massbank/Public/spectraverse.vb
@@ -0,0 +1,25 @@
+﻿Public Class spectraverse
+
+    Public Property TITLE As String
+    Public Property CHARGE As Double
+    Public Property SOURCE As String
+    Public Property SMILES As String
+    Public Property IONMODE As String
+    Public Property ADDUCT As String
+    Public Property COMPOUND_NAME As String
+    Public Property NUM_PEAKS As Integer
+    Public Property PRECURSOR_MZ As Double
+    Public Property PARENT_MASS As Double
+    Public Property MS_LEVEL As String
+    Public Property INCHIKEY As String
+    Public Property INCHI As String
+    Public Property FORMULA As String
+    Public Property INSTRUMENT_TYPE As String
+    Public Property COLLISION_ENERGY_1 As Double
+    Public Property COLLISION_ENERGY_2 As Double
+    Public Property COLLISION_ENERGY_3 As Double
+    Public Property NORMALIZED_COLLISION_ENERGY_1 As Double
+    Public Property NORMALIZED_COLLISION_ENERGY_2 As Double
+    Public Property NORMALIZED_COLLISION_ENERGY_3 As Double
+
+End Class
