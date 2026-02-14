ff8f9197832510078b9d62a5ee2ddbf0e2365bbf
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-14 17:13:40 +0800

    make bugs fixed for vector cast error

--- Rscript/Library/mzkit_app/src/mzkit/pipeline/annotation.vb
+++ Rscript/Library/mzkit_app/src/mzkit/pipeline/annotation.vb
@@ -1,63 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::e5da6824fcb03986080b01804c73c2ee, Rscript\Library\mzkit_app\src\mzkit\pipeline\library.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1191
-    '    Code Lines: 618 (51.89%)
-    ' Comment Lines: 449 (37.70%)
-    '    - Xml Docs: 93.76%
-    ' 
-    '   Blank Lines: 124 (10.41%)
-    '     File Size: 60.11 KB
+' Summaries:
 
 
-    ' Module library
-    ' 
-    '     Function: assertAdducts, checkInSourceFragments, create_reportTable, create_table, create_workspace
-    '               createAnnotation, filter_unique, GetAnnotations, getByKEGG, ionsFromPeaktable
-    '               loadAll, LoadLocalDatabase, loadPeaktable, loadWorkspace, MakeMoNALibrary
-    '               openRepository, OpenResultPack, PopulateIonData, readResultPack, Save
-    '               set_xicCache, tohtmlString, uniqueAnnotations, writeResultPack, writeWorkspace
-    '               xref
-    ' 
-    '     Sub: commit, Main, peak_assign, push_temp, saveAnnotation
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1191
+'    Code Lines: 618 (51.89%)
+' Comment Lines: 449 (37.70%)
+'    - Xml Docs: 93.76%
+' 
+'   Blank Lines: 124 (10.41%)
+'     File Size: 60.11 KB
+
+
+' Module library
+' 
+'     Function: assertAdducts, checkInSourceFragments, create_reportTable, create_table, create_workspace
+'               createAnnotation, filter_unique, GetAnnotations, getByKEGG, ionsFromPeaktable
+'               loadAll, LoadLocalDatabase, loadPeaktable, loadWorkspace, MakeMoNALibrary
+'               openRepository, OpenResultPack, PopulateIonData, readResultPack, Save
+'               set_xicCache, tohtmlString, uniqueAnnotations, writeResultPack, writeWorkspace
+'               xref
+' 
+'     Sub: commit, Main, peak_assign, push_temp, saveAnnotation
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -951,7 +951,7 @@ Module library
     ''' <returns></returns>
     <ExportAPI("config_mapping")>
     Public Function configMapping(libs As LocalRepository, mapping As list) As LocalRepository
-        Call libs.SetIdMapping(mapping.slots.AsCharacter)
+        Call libs.SetIdMapping(mapping.as_character)
         Return libs
     End Function
 
