cef2d1d92ff96d48210170d015169bf659b97254
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-20 18:14:39 +0800

    fix of the object reference errr

--- src/metadb/XrefEngine/ClassyfireInfoTable.vb
+++ src/metadb/XrefEngine/ClassyfireInfoTable.vb
@@ -62,18 +62,13 @@ Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports SMRUCC.genomics.foundation.OBO_Foundry.Tree
 
-Public Class ClassyfireInfoTable : Implements ICompoundClass
+Public Class ClassyfireInfoTable : Inherits CompoundClass
 
     ''' <summary>
     ''' Compound id in given database.
     ''' </summary>
     ''' <returns></returns>
     Public Property CompoundID As String
-    Public Property kingdom As String Implements ICompoundClass.kingdom
-    Public Property super_class As String Implements ICompoundClass.super_class
-    Public Property [class] As String Implements ICompoundClass.class
-    Public Property sub_class As String Implements ICompoundClass.sub_class
-    Public Property molecular_framework As String Implements ICompoundClass.molecular_framework
 
     Public Shared Iterator Function PopulateMolecules(anno As IEnumerable(Of ClassyfireAnnotation), chemOntClassify As ChemOntClassify) As IEnumerable(Of ClassyfireInfoTable)
         Dim lineages As New Dictionary(Of String, NamedCollection(Of GenericTree))
