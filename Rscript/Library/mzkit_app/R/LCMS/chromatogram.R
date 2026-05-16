bfb8dbf260e5b24021d32463c917f3192c12577c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-16 10:36:26 +0800

    Update chromatogram.R

--- Rscript/Library/mzkit_app/R/LCMS/chromatogram.R
+++ Rscript/Library/mzkit_app/R/LCMS/chromatogram.R
@@ -49,16 +49,16 @@ const make_chromatogram_exports = function(rawdir, sampleinfo, group_name,
     }
 
     bitmap(file = file.path(export_dir, `TIC_${basename(dirname(rawdir))}.png`)) {
-        plot(overlaps_data, size = overlaps_size, fill = FALSE, padding = overlaps_layout);
+        plot(overlaps_data, size = overlaps_size, fill = FALSE, padding = overlaps_layout, colors = "paper");
     }
     pdf(file = file.path(export_dir, `TIC_${basename(dirname(rawdir))}.pdf`)) {
-        plot(overlaps_data, size = overlaps_size, fill = FALSE, padding = overlaps_layout);
+        plot(overlaps_data, size = overlaps_size, fill = FALSE, padding = overlaps_layout, colors = "paper");
     }
 
     bitmap(file = file.path(export_dir, `BPC_${basename(dirname(rawdir))}.png`)) {
-        plot(overlaps_data, bpc = TRUE, size = overlaps_size, fill = FALSE, padding = overlaps_layout);
+        plot(overlaps_data, bpc = TRUE, size = overlaps_size, fill = FALSE, padding = overlaps_layout, colors = "paper");
     }
     pdf(file = file.path(export_dir, `BPC_${basename(dirname(rawdir))}.pdf`)) {
-        plot(overlaps_data, bpc = TRUE, size = overlaps_size, fill = FALSE, padding = overlaps_layout);
+        plot(overlaps_data, bpc = TRUE, size = overlaps_size, fill = FALSE, padding = overlaps_layout, colors = "paper");
     }
 }
