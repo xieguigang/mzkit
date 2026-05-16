56952b7b50c64fbb39204e3cd44a81292c9e9ed2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-16 10:26:39 +0800

    Update viz_raw_func.R

--- Rscript/Library/mzkit_app/tools/viz_raw_func.R
+++ Rscript/Library/mzkit_app/tools/viz_raw_func.R
@@ -1,6 +1,6 @@
 require(mzkit);
 
-make_chromatogram_exports = function(rawdir = "\\192.168.3.15\sda\2026\wzc\1\pos", sampleinfo = "\\192.168.3.15\sda\2026\wzc\1\sampleinfo.csv", group_name = "QC", 
+make_chromatogram_exports (rawdir = "\\192.168.3.15\sda\2026\wzc\1\pos", sampleinfo = "\\192.168.3.15\sda\2026\wzc\1\sampleinfo.csv", group_name = "QC", 
                                            export_dir = "Z:/aaaaa/", 
                                            overlaps_size = [2900,1600],
                                            overlaps_layout = "padding:5% 5% 10% 12%;", 
