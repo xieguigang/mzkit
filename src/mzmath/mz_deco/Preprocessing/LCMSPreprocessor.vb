ed50b19049702b53ae1f3314819a966a5b138e80
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-23 22:34:47 +0800

    split souirce files ito multiple parts

--- src/mzmath/mz_deco/Preprocessing/LCMSPreprocessor.vb
+++ src/mzmath/mz_deco/Preprocessing/LCMSPreprocessor.vb
@@ -362,33 +362,4 @@ Namespace LCMS.Preprocessing
 
     End Class
 
-    ''' <summary>
-    ''' 预处理结果类
-    ''' 包含预处理前后的数据和统计信息
-    ''' </summary>
-    Public Class PreprocessingResult
-
-        ''' <summary>原始离子数据</summary>
-        Public Property OriginalIons As xcms2()
-        ''' <summary>预处理后的离子数据</summary>
-        Public Property ProcessedIons As xcms2()
-        ''' <summary>被过滤掉的离子数据</summary>
-        Public Property FilteredIons As xcms2()
-        ''' <summary>样本信息</summary>
-        Public Property Samples As SampleInfo()
-
-        ''' <summary>预处理前的表达矩阵</summary>
-        Public Property MatrixBeforePreprocessing As Double(,)
-        ''' <summary>预处理后的表达矩阵</summary>
-        Public Property MatrixAfterPreprocessing As Double(,)
-
-        ''' <summary>缺失值处理统计信息</summary>
-        Public Property MissingValueStats As MissingValueStatistics
-        ''' <summary>归一化统计信息</summary>
-        Public Property NormalizationStats As NormalizationStatistics
-        ''' <summary>批次矫正统计信息</summary>
-        Public Property BatchCorrectionStats As BatchCorrectionStatistics
-
-    End Class
-
 End Namespace
