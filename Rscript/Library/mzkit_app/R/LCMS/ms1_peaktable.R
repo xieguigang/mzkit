87db70e5ad368bccb0423d65f36528da73945165
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-12 17:19:52 +0800

    make enable of cache

--- Rscript/Library/mzkit_app/R/LCMS/ms1_peaktable.R
+++ Rscript/Library/mzkit_app/R/LCMS/ms1_peaktable.R
@@ -133,56 +133,60 @@ const deconv_xicfile = function(path, mzbins = NULL, args = list(
     message("just run simple peak finding?");
     str(simple);
 
-    for(let mz_xic in xicdata) {
-        peaks = c(peaks, {
-            if (simple) {
-                find_peaks.simple(
-                    x = mz_xic,
-                    peak_width = c(args$min_peak_width, args$max_peak_width),
-                    snr_threshold = 0, # args$snr_threshold,
-                    filename = rawfile,
-                    joint = FALSE,
-                    interpolate = FALSE
-                );
-            } else {
-                find_peaks(
-                    x = mz_xic,
-                    peak_method = args$peak_method,
-                    snr_threshold = args$snr_threshold,
-                    window_half_width = args$window_half_width,
-                    min_peak_width = args$min_peak_width,
-                    max_peak_width = args$max_peak_width,
-                    min_peak_height = args$min_peak_height,
-                    centWave_min_scale = args$centWave_min_scale,
-                    centWave_max_scale = args$centWave_max_scale,
-                    centWave_scale_step = args$centWave_scale_step,
-                    centWave_max_gap = args$centWave_max_gap,
-                    matched_filter_sigma = args$matched_filter_sigma,
-                    matched_filter_truncate_width = args$matched_filter_truncate_width,
-                    derivative_smooth_window = args$derivative_smooth_window,
-                    derivative_threshold_factor = args$derivative_threshold_factor,
-                    noise_segment_count = args$noise_segment_count,
-                    peak_merge_distance = args$peak_merge_distance,
-                    area_method = args$area_method,
-                    baseline_method = args$baseline_method,
-                    baseline_percentile = args$baseline_percentile,
-                    local_minimum_boundary_points = args$local_minimum_boundary_points,
-                    gaussian_max_iterations = args$gaussian_max_iterations,
-                    gaussian_convergence = args$gaussian_convergence,
-                    recalculate_snr = args$recalculate_snr,
-                    as_peaks = TRUE,                               
-                    filename = rawfile
-                );
-            }
-        });
+    if (file.exists(peakfile ) && file.exists(peakdata )) {
+        message(`[${rawfile}] cache existed, skip of the peak finding process`);
+    } else {
+        for(let mz_xic in xicdata) {
+            peaks = c(peaks, {
+                if (simple) {
+                    find_peaks.simple(
+                        x = mz_xic,
+                        peak_width = c(args$min_peak_width, args$max_peak_width),
+                        snr_threshold = 0, # args$snr_threshold,
+                        filename = rawfile,
+                        joint = FALSE,
+                        interpolate = FALSE
+                    );
+                } else {
+                    find_peaks(
+                        x = mz_xic,
+                        peak_method = args$peak_method,
+                        snr_threshold = args$snr_threshold,
+                        window_half_width = args$window_half_width,
+                        min_peak_width = args$min_peak_width,
+                        max_peak_width = args$max_peak_width,
+                        min_peak_height = args$min_peak_height,
+                        centWave_min_scale = args$centWave_min_scale,
+                        centWave_max_scale = args$centWave_max_scale,
+                        centWave_scale_step = args$centWave_scale_step,
+                        centWave_max_gap = args$centWave_max_gap,
+                        matched_filter_sigma = args$matched_filter_sigma,
+                        matched_filter_truncate_width = args$matched_filter_truncate_width,
+                        derivative_smooth_window = args$derivative_smooth_window,
+                        derivative_threshold_factor = args$derivative_threshold_factor,
+                        noise_segment_count = args$noise_segment_count,
+                        peak_merge_distance = args$peak_merge_distance,
+                        area_method = args$area_method,
+                        baseline_method = args$baseline_method,
+                        baseline_percentile = args$baseline_percentile,
+                        local_minimum_boundary_points = args$local_minimum_boundary_points,
+                        gaussian_max_iterations = args$gaussian_max_iterations,
+                        gaussian_convergence = args$gaussian_convergence,
+                        recalculate_snr = args$recalculate_snr,
+                        as_peaks = TRUE,                               
+                        filename = rawfile
+                    );
+                }
+            });
+        }
+
+        # peaks = as.vector(peaks);
+
+        message(`get ${length(peaks)} feature peaks!`);
+
+        write_peaks(peaks, file = peakdata);
+        write.csv(as.data.frame(peaks), file = peakfile);
     }
-
-    # peaks = as.vector(peaks);
-
-    message(`get ${length(peaks)} feature peaks!`);
-
-    write_peaks(peaks, file = peakdata);
-    write.csv(as.data.frame(peaks), file = peakfile);
 }
 
 const mz_bin_features = function(mzbins) {
